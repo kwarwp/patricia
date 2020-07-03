e001ab9fc8c97a946440e4b8a753de9bdea2d306
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-03 11:43:51 -0300

    Saved stacy/main.py @2020-07-03 14:43

--- stacy/main.py
+++ stacy/main.py
@@ -1,4 +1,12 @@
 # patricia.stacy.main.py
 
-x = 10
-print(x)
+import numpy as np
+from scipy import stats
+
+jogadores = [40000, 18000, 12000, 250000, 30000, 140000, 300000, 40000, 800000]
+np.mean(jogadores)
+np.median(jogadores)
+np.quantile(jogadores, [0, 0.25, 0.50, 0.75, 1])
+np.std(jogadores, ddof=1)
+
+stats.describe(jogadores)
