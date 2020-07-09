5b84cfea66f7b2604fdd90a1f05eda04485a0587
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-09 16:17:29 -0300

    Saved roxanne/__log__.py @2020-07-09 19:17

--- roxanne/__log__.py
+++ roxanne/__log__.py
@@ -111,3 +111,23 @@ TypeError: object() takes no parameters
     (self.__nome)
 AttributeError: 'Pessoa' object has no attribute '__nome'
 '''},
+{'date': 'Thu Jul 09 2020 16:17:12.863 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 14
+    isaac.sobre_pessoa()
+  module <module> line 10
+    		print(f'Nome: {self.__nome} | Idade: {self.__idade} | Profissão: {self.__profissao} | Redes Sociais: {self.__nick}')
+  module <module> line 1
+    (self.__nome)
+AttributeError: 'Pessoa' object has no attribute '__nome'
+'''},
