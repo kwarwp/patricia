eae881384001d50809475c7b69f9913698e6ba95
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-09 17:27:00 -0300

    Saved sara/main.py @2020-07-09 20:27

--- sara/main.py
+++ sara/main.py
@@ -1,18 +1,24 @@
 # patricia.sara.main.py
 
-from _spy.vitollino.main import Cena
+from _spy.vitollino.main import Cena, Element
 
 __version__ = "20.07"
 __author__ = "Paulo Assumpção"
 
 CENA_CALCADA = "https://i.imgur.com/zOxshRh.jpg"
-CENA_DIREITA = ""
+BANHISTA = "https://i.imgur.com/CWQ00XG.jpg"
 
 class Calcada:
+
+    def __init__():
+        """ Mostra a cena da praia """
+        self.cena = Cena(CENA_CALCADA)
+        self.banhista = Element(BANHISTA, cena=self.cena)
+        
     """ representa uma cena na calçada da praia """
     def vai(self):
-        """ Mostra a cena da praia """
-        Cena(CENA_CALCADA).vai()
+        self.cena.vai()
+
 
 if __name__ == "__main__":
     Calcada().vai()
