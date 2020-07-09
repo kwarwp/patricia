e69b8c3ef9d536a070e6ff40a88187d3e00d3976
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-09 17:34:53 -0300

    Saved adda/main.py @2020-07-09 20:34

--- adda/main.py
+++ adda/main.py
@@ -11,18 +11,26 @@ Changelog
         * NEW: O jogo original.
 
 """
-from _spy.vitollino.main import Cena
-# from grace.main import Praia
+from _spy.vitollino.main import Cena, Elemento, STYLE
+from grace.main import Praia
 __version__ = "20.07"
 __author__ = "Carlo"
+STYLE["width"] = 500
 CENA_CALCADA = "https://i.imgur.com/zOxshRh.jpg"
+BANHISTA = "https://i.imgur.com/CWQ00XG.png"
 
 
 class Calcada:
     """ Representa uma cena da calçada da praia """
+    def __init__(self):
+        """ Mostra a cena da praia """
+        self.cena = Cena(CENA_CALCADA, direita=Praia())
+        self.banhista = Elemento(BANHISTA, x=100, y=200, cena=self.cena)
+        #Cena(CENA_CALCADA).vai()
     def vai(self):
         """ Mostra a cena da praia """
-        #Cena(CENA_CALCADA, direita=Praia()).vai()
-        Cena(CENA_CALCADA).vai()
+        self.cena.vai()
+        #Cena(CENA_CALCADA).vai()
     
-Calcada()
+if __name__ == "__main__":
+    Calcada().vai()
