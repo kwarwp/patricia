e4a2a6fa1fde7dfd7f1f313d10874ee3f12687e7
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-09 17:01:50 -0300

    Created angie/main.py @2020-07-09 20:01

---
+++ angie/main.py
@@ -0,0 +1,14 @@
+# patricia.angie.main.py
+_author_ = "Monica"
+
+from _spy.vitollino.main import Cena
+
+cena_praia = "https://lirp-cdn.multiscreensite.com/79bbfd96/dms3rep/multi/opt/Sem-t%C3%ADtulo-1-373ce17d-1920w.jpg"
+
+class Calcada:
+
+	""" Representa a cena da calcada """
+	def vai(self):
+    	"""" Mostra a cena da praia """
+        cena(cena_praia).vai()
+        
