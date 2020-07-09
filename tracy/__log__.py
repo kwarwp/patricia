713311bf3dabf98ee75ea6b51c2e265294ab7d73
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-09 15:19:39 -0300

    Saved tracy/__log__.py @2020-07-09 18:19

--- tracy/__log__.py
+++ tracy/__log__.py
@@ -27,3 +27,10 @@ SyntaxError: invalid syntax
                                    ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Thu Jul 09 2020 15:19:36.587 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 5
+  print(\n"Qual o nome do projeto?", nome_do_projeto)
+         ^
+SyntaxError: invalid syntax
+'''},
