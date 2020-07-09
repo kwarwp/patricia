b45c33b5896bae28886b35a0aafb3ee3a7aae90a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-09 17:14:34 -0300

    Saved angie/__log__.py @2020-07-09 20:14

--- angie/__log__.py
+++ angie/__log__.py
@@ -63,3 +63,10 @@ NameError: name 'cena' is not defined
   ^
 IndentationError: expected an indented block
 '''},
+{'date': 'Thu Jul 09 2020 17:14:32.321 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 12
+  """" Mostra a cena da praia """
+  ^
+IndentationError: expected an indented block
+'''},
