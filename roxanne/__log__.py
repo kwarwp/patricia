2cfedd41678e536603dc1a555f002d9e08431de2
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-09 15:32:55 -0300

    Created roxanne/__log__.py @2020-07-09 18:32

---
+++ roxanne/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Thu Jul 09 2020 15:32:37.534 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 2
+  def oi:
+         ^
+SyntaxError: invalid syntax
+'''},
