4a6f5b2942c2ba9e6b44533335df01826855979d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-09 17:07:16 -0300

    Saved stacy/main.py @2020-07-09 20:07

--- stacy/main.py
+++ stacy/main.py
@@ -1,2 +1,13 @@
 # patricia.stacy.main.py
+__version__= "20.07"
 __author_ = "Marília Campos Galvão"
+CENA_PRAIA = ""
+
+class Calcada:
+    """ Reprenta um calçadão"""
+    def vai(self):
+        """ Mostra a cena da Praia """
+        Cena(CENA_PRAIA: direita=Praia()).vai()
+        
+Calcada().vai()
+
