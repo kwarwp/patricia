d4ee3971b33e1224b6dfcaf20bc4c1c2b558c833
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-09 17:03:59 -0300

    Saved sara/__log__.py @2020-07-09 20:03

--- sara/__log__.py
+++ sara/__log__.py
@@ -6,3 +6,10 @@
                                                 ^
 SyntaxError: EOL while scanning string literal
 '''},
+{'date': 'Thu Jul 09 2020 17:03:50.220 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 7
+  CENA_PRAIA = "https://i.imgur.com/zOxshRh.jpg'
+                                                ^
+SyntaxError: EOL while scanning string literal
+'''},
