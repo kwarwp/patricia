96f5534aafc8fae30cac3c64de2b639383374e83
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-09 17:38:11 -0300

    Saved kathryn/__log__.py @2020-07-09 20:38

--- kathryn/__log__.py
+++ kathryn/__log__.py
@@ -175,3 +175,21 @@ IndentationError: expected an indented block
     STYLE["width"] = 500
 NameError: name 'STYLE' is not defined
 '''},
+{'date': 'Thu Jul 09 2020 17:37:33.321 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 36
+    Calcada().vai()
+  module <module> line 33
+    self.cena.vai()
+AttributeError: 'Calcada' object has no attribute 'cena'
+'''},
