03007b7ec745d2188460689e933e3b607fb4090a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-09 15:33:06 -0300

    Saved roxanne/__log__.py @2020-07-09 18:33

--- roxanne/__log__.py
+++ roxanne/__log__.py
@@ -6,3 +6,10 @@
          ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Thu Jul 09 2020 15:32:50.64 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 4
+  print(f'Olá, {nome}, meu nome é doginho!')
+  ^
+IndentationError: unexpected indent
+'''},
