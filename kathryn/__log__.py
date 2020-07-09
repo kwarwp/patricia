ad1300fd7aab3c128b6c05d928ed279efe4c9869
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-09 15:37:38 -0300

    Saved kathryn/__log__.py @2020-07-09 18:37

--- kathryn/__log__.py
+++ kathryn/__log__.py
@@ -31,3 +31,19 @@ TypeError: %d format: a number is required, not str
     print("%s is %ba feet tall than %s" % ("Tom", 6, "Robson"))
 ValueError: unsupported format character 'b' (0x62) at index 7
 '''},
+{'date': 'Thu Jul 09 2020 15:37:33.888 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 6
+    print("%s is %b feet tall than %s" % ("Tom", 6, "Robson"))
+ValueError: unsupported format character 'b' (0x62) at index 7
+'''},
