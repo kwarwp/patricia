ba6728d82408a5bb8e6d372caad87a8561f30401
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-09 18:58:16 -0300

    Saved kathryn/main.py @2020-07-09 21:58

--- kathryn/main.py
+++ kathryn/main.py
@@ -2,3 +2,4 @@
 _author_ = 'Rodrigo'
 o_nome_de_um_numero = 5**23
 print('Olá Programadores! Veja, um número:', o_nome_de_um_numero)
+http://1.bp.blogspot.com/_j2E5RPBo45s/S1Hi7RGkIMI/AAAAAAAAADI/Xn66c31EJNw/s1600-h/cartoes_memoria.JPG
