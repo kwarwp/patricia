c7b1f59526eb4eeefd44acb2551ec6604de0f149
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-09 15:14:43 -0300

    Created libby/__log__.py @2020-07-09 18:14

---
+++ libby/__log__.py
@@ -0,0 +1,17 @@
+
+{'date': 'Thu Jul 09 2020 15:14:55.342 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 3
+    print("Olá programação! Veja um número:", o_nome_de_um_numero)
+NameError: name 'o_nome_de_um_numero' is not defined
+'''},
