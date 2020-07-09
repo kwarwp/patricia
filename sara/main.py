f00262f4019dc23c708023e928325a6ac16c15c9
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-09 17:18:07 -0300

    Saved sara/main.py @2020-07-09 20:18

--- sara/main.py
+++ sara/main.py
@@ -6,11 +6,13 @@ __version__ = "20.07"
 __author__ = "Paulo Assumpção"
 
 CENA_CALCADA = "https://i.imgur.com/zOxshRh.jpg"
+CENA_DIREITA = ""
 
 class Calcada:
     """ representa uma cena na calçada da praia """
     def vai(self):
         """ Mostra a cena da praia """
         Cena(CENA_CALCADA).vai()
-        
-Calcada().vai()
+
+if __name__ == "__main__":
+    Calcada().vai()
