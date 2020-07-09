33fb0256a0d4a28df80ae69ac0329157f4831ecf
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-09 15:19:01 -0300

    Saved naomi/main.py @2020-07-09 18:19

--- naomi/main.py
+++ naomi/main.py
@@ -1,4 +1,6 @@
 # patricia.naomi.main.py
 _author_= "Anni"
 nome="Papai Noel"
+numero_de_presentes=2**45
 print("Olá,",nome,"!")
+print("Quantos presentes você pode distribuir?",numero_de_presentes)
