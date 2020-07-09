171850b6ea1da743a6933b1d5b8ea18f260633f6
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-09 16:48:22 -0300

    Created callie/__log__.py @2020-07-09 19:48

---
+++ callie/__log__.py
@@ -0,0 +1,5 @@
+
+{'date': 'Thu Jul 09 2020 16:48:20.767 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+Exception: <TypeError: Cannot read property '$methods' of undefined>
+'''},
