5ae6ea5f387f0e4d189e2400338cb39394089c08
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-09 17:19:40 -0300

    Saved lisa/main.py @2020-07-09 20:19

--- lisa/main.py
+++ lisa/main.py
@@ -3,3 +3,10 @@ __author__ = "Victória Regina Caruzo"
 __version__ = "09.07.20"
 
 from _spy.vitollino.main import Cena
+from adda.main import CENA_CALCADA
+from grace.main import CENA_PRAIA
+
+def vai(self):
+    Cena(CENA_CALCADA, direita=Praia()).vai()
+    Cena(CENA_CALCADA).vai()
+
