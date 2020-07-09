7db8cc89de6275c2db52c164c69b357e77a9eac4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-09 16:15:59 -0300

    Saved roxanne/__log__.py @2020-07-09 19:15

--- roxanne/__log__.py
+++ roxanne/__log__.py
@@ -43,3 +43,19 @@ TypeError: oi() takes 0 positional argument but more were given
                          ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Thu Jul 09 2020 16:15:42.331 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 13
+    isaac = Pessoa('Isaac', 27, 'Analista', 'idcesares')
+TypeError: object() takes no parameters
+'''},
