2f751692e31370f8894b07eeaf667b6758e8856f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-09 16:36:39 -0300

    Saved roxanne/main.py @2020-07-09 19:36

--- roxanne/main.py
+++ roxanne/main.py
@@ -1,6 +1,21 @@
 # patricia.roxanne.main.py
-def ola(nome):
-	print(f': Olá, {nome}, meu nome é doginho!')
+class Pessoa:
+	def __init__(self, nome, idade, profissao, nick):
+		self.nome = nome
+		self.idade = idade        
+		self.profissao = profissao
+		self.nick = nick
 
-nome = input("Digite o seu nome: ")    
-ola(nome)
+	def sobre_pessoa(self):
+		print(f'Nome: {self.nome} | Idade: {self.idade} | Profissão: {self.profissao} | Redes Sociais: {self.nick}')
+
+isaac = Pessoa('Isaac', 27, 'Analista', 'idcesares')
+isaac.sobre_pessoa()
+
+
+
+#Olá doginho
+#def ola(nome):
+#	print(f': Olá, {nome}, meu nome é doginho!')
+#nome = input("Digite o seu nome: ")    
+#ola(nome)
