95f16123139bcea94435eff2152eeea1922a8cf1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-09 17:14:48 -0300

    Saved angie/main.py @2020-07-09 20:14

--- angie/main.py
+++ angie/main.py
@@ -2,13 +2,14 @@
 _author_ = "Monica"
 
 from _spy.vitollino.main import Cena
-
+from grace.main import Praia
 cena_praia = "https://lirp-cdn.multiscreensite.com/79bbfd96/dms3rep/multi/opt/Sem-t%C3%ADtulo-1-373ce17d-1920w.jpg"
 
 class Calcada:
 
 	""" Representa a cena da calcada """
 	def vai(self):
-    	"""" Mostra a cena da praia """
-        cena(cena_praia).vai()
-        
+	"""" Mostra a cena da praia """
+        Cena(cena_praia, direita=Praia()).vai()
+        #Cena(cena_praia).vai()
+Calcada().vai()
