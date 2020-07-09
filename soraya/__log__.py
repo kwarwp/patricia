fedc8ad633a80268108f922fab8bfee8d294b55a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-09 17:05:23 -0300

    Created soraya/__log__.py @2020-07-09 20:05

---
+++ soraya/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Thu Jul 09 2020 17:05:18.880 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 17
+  __author__ = "Isabel Hortencia Garnica""
+                                          ^
+SyntaxError: EOL while scanning string literal
+'''},
