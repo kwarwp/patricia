aa80c127dffa61cdba4cb9862bb0e3c3e24f1da4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-09 16:48:47 -0300

    Saved callie/main.py @2020-07-09 19:48

--- callie/main.py
+++ callie/main.py
@@ -1,4 +1,10 @@
 # patricia.callie.main.py
 # Rio de Janeiro, 9 de junho de 2020
 __author__= 'Emanuelle Simas'
+# from _spy_vittollino.main import Cena
+#__version__ = "20.07"
+#Teste de classes
 
+class mclass(self):
+    pass
+    
