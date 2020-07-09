371049233d1b92abfd9d6424b818f7ff08b3f2b4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-09 17:08:36 -0300

    Saved sara/__log__.py @2020-07-09 20:08

--- sara/__log__.py
+++ sara/__log__.py
@@ -111,3 +111,21 @@ TypeError: 'module' object is not callable
     Cena(CENA_CALCADA).vai()
 TypeError: 'module' object is not callable
 '''},
+{'date': 'Thu Jul 09 2020 17:08:26.767 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 16
+    Calcada().vai()
+  module <module> line 14
+    Cena(CENA_CALCADA).vai()
+TypeError: 'module' object is not callable
+'''},
