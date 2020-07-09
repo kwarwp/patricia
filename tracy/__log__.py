dded795fe764960dad0e382743464010f4e0f9c3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-09 15:14:39 -0300

    Created tracy/__log__.py @2020-07-09 18:14

---
+++ tracy/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Thu Jul 09 2020 15:14:32.161 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 5
+  print("Qual o nome do projeto?", /d nome_do_projeto)
+                                    ^
+SyntaxError: invalid syntax
+'''},
