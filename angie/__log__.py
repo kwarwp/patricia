3683300c04933040e284407edfe1fcf9979ccd12
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-09 17:01:56 -0300

    Created angie/__log__.py @2020-07-09 20:01

---
+++ angie/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Thu Jul 09 2020 17:01:53.9 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 12
+  """" Mostra a cena da praia """
+  ^
+IndentationError: expected an indented block
+'''},
