b06af3490fe25c4c839d5e4227b8bd000a9b3d63
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-10 13:03:13 -0300

    Created danae/__log__.py @2020-07-10 16:03

---
+++ danae/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Fri Jul 10 2020 13:03:11.824 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 4
+  document["pydiv"] <= H("Olá", class="title")
+                                      ^
+SyntaxError: invalid syntax
+'''},
