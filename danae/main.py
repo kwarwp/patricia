b7d529e6c3e779f4f4c2bb7d9e82fea738c3a8a0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-10 15:50:53 -0300

    Saved danae/main.py @2020-07-10 18:50

--- danae/main.py
+++ danae/main.py
@@ -2,22 +2,27 @@
 from browser import document, html
 from browser.html import H1 as H, DIV as D, SECTION as S, P, STRONG as B, ARTICLE as R
 class App:
+    """ Implementa um painel de controle de admnistrador.
+        Código original em : https://bulmatemplates.github.io/bulma-templates/templates/admin.html
+    """
     def __init__(self):
-        s, c, t, u, h, b, p, w = CONST.CLS + [" "]
-        #H, D, S, P, B = CONST.ELM
-        document.html = CONST.SITE
-        document.body.html = ""
+        s, c, t, u, h, b, p, w = CONST.CLS + [" "]  # alguma constantes para estilo
+        document.html = CONST.SITE  # Instala o cabeçalho do documento
+        document.body.html = ""  
         body = S(D(D(
             H("Hello World", Class=t)+P("My first website with "+B("Bulma"), Class=u),
         Class=c), Class=b), Class=h+w+p)+S(D(
         Class="tile is-ancestor has-text-centered", Id="ancestor-tile"), Class="info-tiles")
+        # ^^^ constrói a estrutura do corpo, onde ancestor-tile é onde adiciona os ladrilhos
 
-        document.body <= body
-        tiles = document["ancestor-tile"]
+        document.body <= body  # instala os elementos estruturados no corpo do documento
+        tiles = document["ancestor-tile"]  # localiza os elemento onde coloca os ladrilhos
         dados = [("435k", "Usuários"), ("53k", "Produtos"), ("735k", "Pedidos"), ("23", "Devoluções")]
+        # ^^^ Pares (valor, legenda) que vão ser apresentados nos ladrilhos
         [tiles <= D(R(
                 P(valor , Class=t)+P(legenda , Class=u),
             Class="tile is-child box"), Class="tile is-parent") for valor, legenda in dados]
+        # ^^^ usa um list comprehension para instalar cada ladrilho no ancestral 'tiles'
         
 class CONST:
     CLS = "section container title subtitle hero hero-body is-primary".split()
