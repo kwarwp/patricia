4842a7e7c14208c1bfd82cbacda1396a088aa4c3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-12 15:08:09 -0300

    Saved alpha/__log__.py @2020-07-12 18:08

--- alpha/__log__.py
+++ alpha/__log__.py
@@ -6,3 +6,10 @@
   ^
 IndentationError: expected an indented block
 '''},
+{'date': 'Sun Jul 12 2020 15:08:05.955 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 17
+  TOPO_ESQUERDA = "AN"
+  ^
+IndentationError: expected an indented block
+'''},
