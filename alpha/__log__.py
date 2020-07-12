7c01924fedc469505e3258f231a94ec441835d9a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-12 15:33:56 -0300

    Saved alpha/__log__.py @2020-07-12 18:33

--- alpha/__log__.py
+++ alpha/__log__.py
@@ -113,3 +113,19 @@ TypeError: 'module' object is not callable
     	f = open("names.txt", "w")
 IOError: File names.txt not found
 '''},
+{'date': 'Sun Jul 12 2020 15:33:56.299 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 36
+    	f = open("names.txt", "w")
+IOError: File names.txt not found
+'''},
