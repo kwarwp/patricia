6ce21ea8f01877f8125c314ae4b661ca71066578
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-12 15:23:18 -0300

    Saved alpha/__log__.py @2020-07-12 18:23

--- alpha/__log__.py
+++ alpha/__log__.py
@@ -79,3 +79,21 @@ IOError: File names.txt not found
     		self.cena = Cena(cena_praia, direita=Praia())
 TypeError: 'module' object is not callable
 '''},
+{'date': 'Sun Jul 12 2020 15:23:17.885 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 35
+    	Calcada().vai()
+  module <module> line 26
+    		self.cena = Cena(cena_praia, direita=Praia())
+TypeError: 'module' object is not callable
+'''},
