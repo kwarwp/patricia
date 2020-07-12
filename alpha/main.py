dd8f261a4022730bd94bc7dd0c564d9c8c939f76
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-11 22:42:05 -0300

    Saved alpha/main.py @2020-07-12 01:42

--- alpha/main.py
+++ alpha/main.py
@@ -1,1 +1,18 @@
 # patricia.alpha.main.py
+# SPDX-License-Identifier: GPL-3.0-or-later
+""" Alpha - Projeto sem descrição, (mude esta linha).
+
+.. codeauthor:: Nome Sobrenome (golf) <mail@local.tipo>
+
+.. codeauthor:: Nome Sobrenome (hotel) <mail@local.tipo>
+
+.. codeauthor:: Nome Sobrenome (india) <mail@local.tipo>
+
+.. codeauthor:: Nome Sobrenome (juliet) <mail@local.tipo>
+
+Changelog
+---------
+.. versionadded::    20.07
+        Descreva o que você adicionou no código.
+
+"""
