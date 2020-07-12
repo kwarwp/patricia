1aab492a20e7790908868949e603582e1dba9651
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-11 22:41:37 -0300

    Created charlie/main.py @2020-07-12 01:41

---
+++ charlie/main.py
@@ -0,0 +1,18 @@
+# patricia.charlie.main.py
+# SPDX-License-Identifier: GPL-3.0-or-later
+""" Charlie - Projeto sem descrição, (mude esta linha).
+
+.. codeauthor:: Nome Sobrenome (golf) <mail@local.tipo>
+
+.. codeauthor:: Nome Sobrenome (hotel) <mail@local.tipo>
+
+.. codeauthor:: Nome Sobrenome (india) <mail@local.tipo>
+
+.. codeauthor:: Nome Sobrenome (juliet) <mail@local.tipo>
+
+Changelog
+---------
+.. versionadded::    20.07
+        Descreva o que você adicionou no código.
+
+"""
