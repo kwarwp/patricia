f3a80247609bec1712205d0a10f64ef88d554637
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-11 22:43:50 -0300

    Created foxtrot/main.py @2020-07-12 01:43

---
+++ foxtrot/main.py
@@ -0,0 +1,18 @@
+# patricia.foxtrot.main.py
+# SPDX-License-Identifier: GPL-3.0-or-later
+""" Foxtrot - Projeto sem descrição, (mude esta linha).
+
+.. codeauthor:: Nome Sobrenome (golf) <mail@local.tipo>
+
+.. codeauthor:: Nome Sobrenome (hotel) <mail@local.tipo>
+
+.. codeauthor:: Nome Sobrenome (india) <mail@local.tipo>
+
+.. codeauthor:: Nome Sobrenome (juliet) <mail@local.tipo>
+
+Changelog
+---------
+.. versionadded::    20.07
+        Descreva o que você adicionou no código.
+
+"""
