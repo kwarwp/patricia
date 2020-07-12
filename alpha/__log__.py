53658c11f314edb9a29ace84168ac5d5d3d79c50
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-12 15:08:07 -0300

    Created alpha/__log__.py @2020-07-12 18:08

---
+++ alpha/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Sun Jul 12 2020 15:07:50.691 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 17
+  TOPO_ESQUERDA = "AN"
+  ^
+IndentationError: expected an indented block
+'''},
