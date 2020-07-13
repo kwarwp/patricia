2260449e13d4ecad55888b7d851b13136ef35077
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-13 19:52:02 -0300

    Saved lisa/__log__.py @2020-07-13 22:52

--- lisa/__log__.py
+++ lisa/__log__.py
@@ -62,3 +62,19 @@ NameError: name 'Calcada' is not defined
     lins = int(input("Insira o num de linhas: 10"))
 ValueError: invalid literal for int() with base 10: ''
 '''},
+{'date': 'Mon Jul 13 2020 19:52:28.689 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 5
+    lins = int(input("10"))
+ValueError: invalid literal for int() with base 10: ''
+'''},
