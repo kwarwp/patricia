b9bb4d933f8207f92f32e8c508e4bb1586236f68
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-13 14:21:46 -0300

    Saved danae/__log__.py @2020-07-13 17:21

--- danae/__log__.py
+++ danae/__log__.py
@@ -139,3 +139,21 @@ SyntaxError: invalid syntax
     raise ValueError("must have exactly one of create/read/write/append mode")
 ValueError: must have exactly one of create/read/write/append mode
 '''},
+{'date': 'Mon Jul 13 2020 14:21:45.876 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 60
+    with Open("x.txt", "r") as no:
+  module <module> line 31
+    raise ValueError("must have exactly one of create/read/write/append mode")
+ValueError: must have exactly one of create/read/write/append mode
+'''},
