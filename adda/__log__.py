9c995e43979592ffb3d79a6fc4d2877a661fcfc8
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-13 19:21:47 -0300

    Created adda/__log__.py @2020-07-13 22:21

---
+++ adda/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Mon Jul 13 2020 19:21:45.251 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 39
+  keys = dict(x=+1, y=-1):
+                          ^
+SyntaxError: invalid syntax
+'''},
