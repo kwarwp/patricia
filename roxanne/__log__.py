ef31796b35222b7afb084bf6e25f53a038961cca
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-13 17:40:11 -0300

    Saved roxanne/__log__.py @2020-07-13 20:40

--- roxanne/__log__.py
+++ roxanne/__log__.py
@@ -387,3 +387,21 @@ TypeError: 'module' object is not callable
     STYLE['width'] = 740
 NameError: name 'STYLE' is not defined
 '''},
+{'date': 'Mon Jul 13 2020 17:39:51.521 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 6
+    class Historia():
+  module <module> line 7
+    	fundoJogodaVelha = Cena(img=imgBoard)
+TypeError: 'module' object is not callable
+'''},
