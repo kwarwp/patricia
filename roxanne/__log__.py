f153a102c3ad7a8ff08caadb6e536b93b4f714c0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-13 17:40:04 -0300

    Saved roxanne/__log__.py @2020-07-13 20:40

--- roxanne/__log__.py
+++ roxanne/__log__.py
@@ -371,3 +371,19 @@ TypeError: 'module' object is not callable
     	cenaJogodaVelha = Cena(img="https://www.pngitem.com/pimgs/m/427-4272622_picture-regarding-free-printable-tic-tac-toe-board.png")
 TypeError: 'module' object is not callable
 '''},
+{'date': 'Mon Jul 13 2020 17:39:44.692 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 3
+    STYLE['width'] = 740
+NameError: name 'STYLE' is not defined
+'''},
