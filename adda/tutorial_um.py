9824ab8fd041abf57fb3585b1cfc27cff3f1e51c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-13 19:09:20 -0300

    Saved adda/tutorial_um.py @2020-07-13 22:09

--- adda/tutorial_um.py
+++ adda/tutorial_um.py
@@ -27,12 +27,14 @@ class Eventos:
         self.banhista = Elemento(self.BANHISTA, , x=100, y=200, cena=self.calcada)
         self.dark_side = Elemento(self.DARK_SIDE, , x=-1100, y=100, cena=self.calcada)
         
-        self.dark_side.elt.bind("hover", self.ve_dark)
+        self.dark_side.elt.bind("mouseover", self.ve_dark)
+        self.dark_side.elt.bind("mouseout", self.ve_dark)
         self.muda = 1000
         
     def vai(self):
         self.calcada.vai()
     def ve_dark(self, ev=None):
+        print(self.muda)
         self.dark_side.x += self.muda
         self.muda = - self.muda
         
