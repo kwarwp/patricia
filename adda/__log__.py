52aac776684e1d2d169b45a5c3e5b2c646959d96
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-13 19:22:01 -0300

    Saved adda/__log__.py @2020-07-13 22:22

--- adda/__log__.py
+++ adda/__log__.py
@@ -6,3 +6,10 @@
                           ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Mon Jul 13 2020 19:22:00.778 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 39
+  keys = dict(x=1, y=-1):
+                         ^
+SyntaxError: invalid syntax
+'''},
