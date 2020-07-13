3caca7fe0dcf0c1156341072a4fe06de5de0d936
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-13 17:33:45 -0300

    Saved roxanne/__log__.py @2020-07-13 20:33

--- roxanne/__log__.py
+++ roxanne/__log__.py
@@ -319,3 +319,19 @@ TypeError: 'module' object is not callable
     historia.vai()
 NameError: name 'historia' is not defined
 '''},
+{'date': 'Mon Jul 13 2020 17:33:26.232 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 11
+    Historia.vai()
+AttributeError: 'function' object has no attribute 'vai'
+'''},
