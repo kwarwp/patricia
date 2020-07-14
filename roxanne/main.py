f43a4568330cab8f0c23326345e11ccfdbcc66c3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-14 12:39:57 -0300

    Saved roxanne/main.py @2020-07-14 15:39

--- roxanne/main.py
+++ roxanne/main.py
@@ -9,8 +9,9 @@ fundo_jogo = 'https://i.pinimg.com/originals/7f/d6/54/7fd654e4ed2675f4606bd72177
 class JogoDaVelha:
 	def __init__(self, fundo_jogo):
 		self.fundo_jogo = Cena(img=fundo_jogo)
+		self.texto_inicial = Texto("Bem-vind@ ao Jogo da Velha!")
 	def inicia(self):
 		self.fundo_jogo.vai()
-        
+		self.texto_inicial.vai()        
 jogo = JogoDaVelha(fundo_jogo)
 jogo.inicia()
