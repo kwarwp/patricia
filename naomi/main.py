273de77e0e6412dac4159baabcf26fb3d92514f1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-14 17:42:30 -0300

    Saved naomi/main.py @2020-07-14 20:42

--- naomi/main.py
+++ naomi/main.py
@@ -7,8 +7,12 @@ numero_de_presentes=2**45
 #print("Quantos presentes você pode distribuir?",numero_de_presentes)
 def _init_ (self):
 	pycharm="http://activufrj.nce.ufrj.br/file/info/ProgOO/Card_pycharm.png"
+	
+	self.pycharm=cena(self.pycharm_)
+	
+	
+def vai(self):
 	s=[1,3,5,7,9]
 	random.shuffle(s,random.random)
 	print(s)
-	self.pycharm=cena(self.pycharm_)
-	
+	self.pycharm.vai()
