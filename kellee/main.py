bf22b38976d991608bbfbf71c76b8a0e087b7163
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-14 14:51:12 -0300

    Saved kellee/main.py @2020-07-14 17:51

--- kellee/main.py
+++ kellee/main.py
@@ -1,7 +1,19 @@
 # patricia.kellee.main.py
-#print("Olá programação!")
-import pygame
 
+"""
+ Iniciando  jogo!
 
+"""
+
+from _spy.vitollino.main import Cena
+_version_="14.07"
+_author_ = "Rosilane"
+
+FUNDO = "https://image.freepik.com/vetores-gratis/plataforma-de-tileset-e-plano-de-fundo-para-criar-jogos-para-celular_22289-375.jpg"
+
+def vai():
+	""" Mostrar o Fundo"""
+	Cena(FUNDO).vai()
+vai()
 
         
