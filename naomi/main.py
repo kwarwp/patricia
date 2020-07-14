ed76b95e950dc5e0174440165d6de82eec35eb66
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-14 17:22:17 -0300

    Saved naomi/main.py @2020-07-14 20:22

--- naomi/main.py
+++ naomi/main.py
@@ -1,6 +1,11 @@
 # patricia.naomi.main.py
 _author_= "Anni"
 nome="Papai Noel"
+import random
 numero_de_presentes=2**45
 print("Olá,",nome,"!")
 print("Quantos presentes você pode distribuir?",numero_de_presentes)
+
+s=[1,3,5,7,9]
+random.shuffle(s,random.random)
+print(s)
