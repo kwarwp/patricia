208af66a1f3b22bef84735f1a2993fc7956a091d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-14 19:39:33 -0300

    Created delta/golf.py @2020-07-14 22:39

---
+++ delta/golf.py
@@ -0,0 +1,12 @@
+# patricia.delta.golf.py
+# SPDX-License-Identifier: GPL-3.0-or-later
+""" Jogo da Memória.
+
+.. codeauthor:: Paulo Assumpção <paulo.assump@gmail.com>
+
+Changelog
+---------
+.. versionadded::    20.07
+        Descreva o que você adicionou no código.
+
+"""
