20485c3b348de984bb69cad1a38570692e905582
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-13 23:46:08 -0300

    Created echo/india.py @2020-07-14 02:46

---
+++ echo/india.py
@@ -0,0 +1,12 @@
+# patricia.echo.india.py
+# SPDX-License-Identifier: GPL-3.0-or-later
+""" Projeto sem descrição, (mude esta linha).
+
+.. codeauthor:: Victória Regina Caruzo <victoriareginalattes@gmail.com>
+
+Changelog
+---------
+.. versionadded::    20.07
+        Descreva o que você adicionou no código.
+
+"""
