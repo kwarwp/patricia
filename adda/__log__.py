902f734c8641e6fd4f9a82d00573a813edf3ca2d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-14 12:24:59 -0300

    Saved adda/__log__.py @2020-07-14 15:24

--- adda/__log__.py
+++ adda/__log__.py
@@ -45,3 +45,25 @@ SyntaxError: keyword can't be an expression
       ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Tue Jul 14 2020 12:24:58.865 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 38
+    Tabuleiro().vai()
+  module <module> line 23
+    self.tabuleiro = Cena(self.MADEIRA)
+  module _spy.vitollino.main line 1003
+    self._cria_divs(width)
+  module _spy.vitollino.main line 1008
+    divesq.style.width = width // 3  # 100
+TypeError: unsupported operand type(s) for //: 'set' and 'int'
+'''},
