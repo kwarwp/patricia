9c33ef0e3a71b16868ced3fce8cbcda2ea350254
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-14 20:35:29 -0300

    Saved naomi/__log__.py @2020-07-14 23:35

--- naomi/__log__.py
+++ naomi/__log__.py
@@ -187,3 +187,21 @@ AttributeError: 'int' object has no attribute '__getitem__'
     self.tabuleiro = Cena(self.CENA_TABULEIRO,,x=400,y=200, cena=self.tabuleiro)
 AttributeError: 'Eventos' object has no attribute 'tabuleiro'
 '''},
+{'date': 'Tue Jul 14 2020 20:35:29.84 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 30
+    Eventos().vai()
+  module <module> line 14
+    self.pycharm = Elemento(self.CARTA_PYCHARM, , x=200, y=200, cena=self.tabuleiro)
+AttributeError: 'Eventos' object has no attribute 'tabuleiro'
+'''},
