28a7df68b525f4ca421e1a8aa4ad056893f381e5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-14 20:31:46 -0300

    Saved naomi/__log__.py @2020-07-14 23:31

--- naomi/__log__.py
+++ naomi/__log__.py
@@ -169,3 +169,21 @@ AttributeError: 'Eventos' object has no attribute 'calcada'
     imagem, kwargs = (imagem, {}) if isinstance(imagem, str) \
 AttributeError: 'int' object has no attribute '__getitem__'
 '''},
+{'date': 'Tue Jul 14 2020 20:31:45.916 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 30
+    Eventos().vai()
+  module <module> line 13
+    self.tabuleiro = Cena(self.CENA_TABULEIRO,,x=400,y=200, cena=self.tabuleiro)
+AttributeError: 'Eventos' object has no attribute 'tabuleiro'
+'''},
