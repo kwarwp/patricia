a639b46e6a49129b308f0b52bce3905e456ff124
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-14 12:25:28 -0300

    Saved roxanne/main.py @2020-07-14 15:25

--- roxanne/main.py
+++ roxanne/main.py
@@ -1,21 +1,12 @@
 # patricia.roxanne.main.py
-class Pessoa:
-	def __init__(self, nome, idade, profissao, nick):
-		self.nome = nome
-		self.idade = idade        
-		self.profissao = profissao
-		self.nick = nick
 
-	def sobre_pessoa(self):
-		print(f'Nome: {self.nome} | Idade: {self.idade} | Profissão: {self.profissao} | Redes Sociais: {self.nick}')
+from _spy.vitollino.main import Cena, Elemento, Texto
 
-isaac = Pessoa('Isaac', 27, 'Analista', 'idcesares')
-isaac.sobre_pessoa()
+STYLE['width'] = 740
+fundo_jogo = 'https://i.pinimg.com/originals/7f/d6/54/7fd654e4ed2675f4606bd72177eb1fb2.jpg'
 
-
-
-#Olá doginho
-#def ola(nome):
-#	print(f': Olá, {nome}, meu nome é doginho!')
-#nome = input("Digite o seu nome: ")    
-#ola(nome)
+class JogoDaVelha:
+	def __init__(self, bgBoard):
+    	self.bgBoard = bgBoard
+	def bg(self):
+    	
