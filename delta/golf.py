7da00fcd1f3238c63d4912796e79d3abace9eb65
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-14 20:47:10 -0300

    Saved delta/golf.py @2020-07-14 23:47

--- delta/golf.py
+++ delta/golf.py
@@ -12,8 +12,15 @@ Changelog
 """
 
 from _spy.vitollino.main import Cena, Elemento, STYLE
+
 __version__ = "20.07"
 __author__ = "Paulo Assumpção"
+
+IMG_CARD_1 = "http://activufrj.nce.ufrj.br/file/ProgOO/Card_pycharm.png?disp=inline"
+IMG_CARD_2 = "http://activufrj.nce.ufrj.br/file/ProgOO/Card_Linux.png?disp=inline"
+IMG_CARD_3 = "http://activufrj.nce.ufrj.br/file/ProgOO/Card_Gitlab.png?disp=inline"
+IMG_CARD_4 = "http://activufrj.nce.ufrj.br/file/ProgOO/Card_github.png?disp=inline"
+IMG_CARD_5 = "http://activufrj.nce.ufrj.br/file/ProgOO/Card_Activ.png?disp=inline"
 
 class Card():
     def __init__(self, image,cena):
@@ -25,20 +32,27 @@ class Card():
         
         
 class Game:
-    def crate_5x5_cards(self):
+    def crate_4x5_cards(self):
         self.cena = Cena()
-        self.card1 = Card("http://activufrj.nce.ufrj.br/file/ProgOO/Card_pycharm.png?disp=inline", self.cena)
-        self.card2 = Card("http://activufrj.nce.ufrj.br/file/ProgOO/Card_Linux.png?disp=inline", self.cena)
-        self.card3 = Card("http://activufrj.nce.ufrj.br/file/ProgOO/Card_Gitlab.png?disp=inline", self.cena)
-        self.card4 = Card("http://activufrj.nce.ufrj.br/file/ProgOO/Card_github.png?disp=inline", self.cena)
-        self.card5 = Card("http://activufrj.nce.ufrj.br/file/ProgOO/Card_Activ.png?disp=inline", self.cena)
+        self.card1 = Card(IMG_CARD_1, self.cena)
+        self.card2 = Card(IMG_CARD_2, self.cena)
+        self.card3 = Card(IMG_CARD_3, self.cena)
+        self.card4 = Card(IMG_CARD_4, self.cena)
+        self.card5 = Card(IMG_CARD_5, self.cena)
         self.cena.vai()
         
     def randomize_cards(self):
         pass
     
+    def create_grid(self, grid_size):
+        for row_num in range(grid_size):
+            new_row = self.create_row(row_num, grid_size)
+            self.grid.append(new_row)
         
+    def rederize_cards(self):
+        pass
 
 
 if __name__ == "__main__":
-    Game().crate_5x5_cards()
+    Game().crate_4x5_cards()
+    print(teste)
