daf8c99f6bdad8cf59522e1ec9b7db505cac490c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-14 15:30:10 -0300

    Saved kellee/__log__.py @2020-07-14 18:30

--- kellee/__log__.py
+++ kellee/__log__.py
@@ -208,3 +208,21 @@ Traceback (most recent call last):
   module <module> line 3
     from _spy.vitollino.main import Fundo
 '''},
+{'date': 'Tue Jul 14 2020 15:30:09.517 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 17
+    vai()
+  module <module> line 16
+    	Cena(FUNDO).vai()
+NameError: name 'FUNDO' is not defined
+'''},
