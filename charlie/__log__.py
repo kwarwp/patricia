23a48b9358f13f90fff13ad319251ed6e45d5996
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-14 20:20:12 -0300

    Created charlie/__log__.py @2020-07-14 23:20

---
+++ charlie/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Tue Jul 14 2020 20:19:50.298 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 64
+  class normal():
+  ^
+IndentationError: unexpected indent
+'''},
