1177a07a6142466c4dccbc74d2f694e730ceec4f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-14 16:24:40 -0300

    Created delta/hotel.py @2020-07-14 19:24

---
+++ delta/hotel.py
@@ -0,0 +1,12 @@
+# patricia.delta.hotel.py
+# SPDX-License-Identifier: GPL-3.0-or-later
+""" Projeto sem descrição, (mude esta linha).
+
+.. codeauthor:: Anni Provietti <anniprovietti@gmail.com>
+
+Changelog
+---------
+.. versionadded::    20.07
+        Descreva o que você adicionou no código.
+
+"""
