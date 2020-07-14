7782dea215c7b283367428b4b599db64f2fee028
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-14 13:35:51 -0300

    Saved kellee/__log__.py @2020-07-14 16:35

--- kellee/__log__.py
+++ kellee/__log__.py
@@ -3,3 +3,10 @@
 'error': '''
 Exception: <SyntaxError: Unexpected token ','>
 '''},
+{'date': 'Tue Jul 14 2020 13:35:51.288 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 17
+  for event in pygame.event.get():
+  ^
+IndentationError: unexpected indent
+'''},
