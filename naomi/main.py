4770358f897112037e6cc5c32e36810964172722
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-14 20:30:35 -0300

    Saved naomi/main.py @2020-07-14 23:30

--- naomi/main.py
+++ naomi/main.py
@@ -11,11 +11,11 @@ class Eventos:
     
     def __init__(self):
         self.tabuleiro = Cena(self.CENA_TABULEIRO)
-        self.pycharm = Elemento(self.CARTA_PYCHARM, , x=100, y=200, cena=self.tabuleiro)
-        self.pycharm=Elemento(self.CARTA_PYCHARM,,X=200,y=300,cena=self.tabuleiro)
-        self.pycharm=Elemento(self.CARTA_PYCHARM,,X=300,y=400,cena=self.tabuleiro)
-        self.pycharm=Elemento(self.CARTA_PYCHARM,,X=200,y=300,cena=self.tabuleiro)
-        self.pycharm=Elemento(self.CARTA_PYCHARM,,X=100,y=200,cena=self.tabuleiro)
+        self.pycharm = Elemento(self.CARTA_PYCHARM, , x=400, y=200, cena=self.tabuleiro)
+        self.pycharm=Elemento(self.CARTA_PYCHARM,,X=100,y=100,cena=self.tabuleiro)
+       # self.pycharm=Elemento(self.CARTA_PYCHARM,,X=300,y=400,cena=self.tabuleiro)
+       # self.pycharm=Elemento(self.CARTA_PYCHARM,,X=200,y=400,cena=self.tabuleiro)
+       # self.pycharm=Elemento(self.CARTA_PYCHARM,,X=100,y=300,cena=self.tabuleiro)
         
         
         
