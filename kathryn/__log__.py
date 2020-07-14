ddf623d80f661d7acbeafe6eb3cf67aa0fa318a9
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-14 11:19:05 -0300

    Saved kathryn/__log__.py @2020-07-14 14:19

--- kathryn/__log__.py
+++ kathryn/__log__.py
@@ -264,3 +264,19 @@ ImportError: No module named _spy.circus.game
     from _spy.circus.game import Circus
 ImportError: cannot import name 'Circus'
 '''},
+{'date': 'Tue Jul 14 2020 11:19:03.658 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 14
+    from _spy.circus.game import Circus
+ImportError: cannot import name 'Circus'
+'''},
