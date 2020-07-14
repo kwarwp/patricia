1c57b1522190bbeff204c9682449d9e3a61d6e57
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-14 17:57:20 -0300

    Saved naomi/main.py @2020-07-14 20:57

--- naomi/main.py
+++ naomi/main.py
@@ -1,18 +1,16 @@
 # patricia.naomi.main.py
-#_author_= "Anni"
-#nome="Papai Noel"
+_author_= "Anni"
+
 import random
 from _spy.vitollino.main import Cena, Elemento
 from browser import document
-#numero_de_presentes=2**45
-#print("Olá,",nome,"!")
-#print("Quantos presentes você pode distribuir?",numero_de_presentes)
+
 class Eventos:
 	py_charm="http://activufrj.nce.ufrj.br/file/info/ProgOO/Card_pycharm.png"
 	tes_te="https://i.imgur.com/CWQ00XG.png"
 	def _init_ (self):
 	
-		self.pycharm=Cena(self.py_charm)
+		#self.pycharm=Cena(self.py_charm)
 		self.teste=Cena(self.tes_te)
 	
 	
