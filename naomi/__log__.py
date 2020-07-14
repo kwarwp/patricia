4944b1453233bf15554ad655e4aaa3782b662df9
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-14 19:24:37 -0300

    Saved naomi/__log__.py @2020-07-14 22:24

--- naomi/__log__.py
+++ naomi/__log__.py
@@ -75,3 +75,21 @@ Traceback (most recent call last):
     self.pycharm=Elemento(self.pycharm_)
 NameError: name 'Elemento' is not defined
 '''},
+{'date': 'Tue Jul 14 2020 19:24:35.643 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 48
+    Eventos().vai()
+  module <module> line 12
+    self.calcada = Cena(self.CENA_CALCADA)
+AttributeError: 'Eventos' object has no attribute 'CENA_CALCADA'
+'''},
