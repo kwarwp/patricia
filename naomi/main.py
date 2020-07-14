f9fc367ff84b2f3a9f4d22f415fa3e36539b5921
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-14 20:12:29 -0300

    Saved naomi/main.py @2020-07-14 23:12

--- naomi/main.py
+++ naomi/main.py
@@ -5,12 +5,13 @@ from browser import document # importa o DOM para atribuir o evento de teclado
 class Eventos:
     """ Associa um evento a uma imagem e captura eventos de teclado. """
     CENA_TABULEIRO = "http://activufrj.nce.ufrj.br/file/ProgOO/Card_verso.png?disp=inline"
+    CENA_CARTA1= "http://activufrj.nce.ufrj.br/file/ProgOO/Card_verso.png?disp=inline"
     CARTA_PYCHARM = "http://activufrj.nce.ufrj.br/file/ProgOO/Card_pycharm.png?disp=inline"
     
     
     def __init__(self):
         self.tabuleiro = Cena(self.CENA_TABULEIRO)
-        self.pycharm = Elemento(self.CARTA_PYCHARM, , x=100, y=200, cena=self.calcada)
+        self.pycharm = Elemento(self.CARTA_PYCHARM, , x=100, y=200, cena=self.tabuleiro)
         
         
         
