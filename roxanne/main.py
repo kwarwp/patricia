e67790682eadc88ef3da8ad3522819ab666d022f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-14 12:27:38 -0300

    Saved roxanne/main.py @2020-07-14 15:27

--- roxanne/main.py
+++ roxanne/main.py
@@ -1,12 +1,14 @@
 # patricia.roxanne.main.py
 
-from _spy.vitollino.main import Cena, Elemento, Texto
+from _spy.vitollino.main import *
 
 STYLE['width'] = 740
+
 fundo_jogo = 'https://i.pinimg.com/originals/7f/d6/54/7fd654e4ed2675f4606bd72177eb1fb2.jpg'
 
 class JogoDaVelha:
-	def __init__(self, bgBoard):
-    	self.bgBoard = bgBoard
-	def bg(self):
-    	
+	def __init__(self, fundo_jogo):
+		self.fundo_jogo = Cena(img=fundo_jogo)
+        
+        
+JogoDaVelha(fundo_jogo)
