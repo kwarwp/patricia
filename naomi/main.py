8d648883a0bdaf092cae8a2d402cb6a314e97181
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-14 17:50:03 -0300

    Saved naomi/main.py @2020-07-14 20:50

--- naomi/main.py
+++ naomi/main.py
@@ -6,15 +6,17 @@ from _spy.vitollino.main import Cena, Elemento
 numero_de_presentes=2**45
 #print("Olá,",nome,"!")
 #print("Quantos presentes você pode distribuir?",numero_de_presentes)
-def _init_ (self):
+class Eventos:
 	py_charm="http://activufrj.nce.ufrj.br/file/info/ProgOO/Card_pycharm.png"
 	tes_te="https://i.imgur.com/CWQ00XG.png"
-	self.pycharm=Cena(self.py_charm)
-	self.teste=Cena(self.tes_te)
+	def _init_ (self):
+	
+		self.pycharm=Cena(self.py_charm)
+		self.teste=Cena(self.tes_te)
 	
 	
-def vai(self):
+	def vai(self):
 	#s=[1,3,5,7,9]
 	#random.shuffle(s,random.random)
 	#print(s)
-	self.teste.vai()
+		self.teste.vai()
