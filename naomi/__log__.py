00dbc04172a11b663fee859d5396f9271d6719d7
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-14 17:37:53 -0300

    Saved naomi/__log__.py @2020-07-14 20:37

--- naomi/__log__.py
+++ naomi/__log__.py
@@ -41,3 +41,20 @@ Traceback (most recent call last):
     self.pycharm=Elemento(self.pycharm)
 NameError: name 'Elemento' is not defined
 '''},
+{'date': 'Tue Jul 14 2020 17:37:51.920 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''[9, 3, 1, 7, 5]
+Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 13
+    self.pycharm=Elemento(self.pycharm_)
+NameError: name 'Elemento' is not defined
+'''},
