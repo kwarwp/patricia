904b9e29aa5bf64f389ab23c2c407cf7a1fe2279
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-14 20:07:01 -0300

    Saved naomi/__log__.py @2020-07-14 23:07

--- naomi/__log__.py
+++ naomi/__log__.py
@@ -111,3 +111,21 @@ AttributeError: 'Eventos' object has no attribute 'CENA_CALCADA'
     self.dark_side = Elemento(self.DARK_SIDE, , x=100, y=100, cena=self.calcada)
 AttributeError: 'Eventos' object has no attribute 'DARK_SIDE'
 '''},
+{'date': 'Tue Jul 14 2020 20:06:58.636 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 48
+    Eventos().vai()
+  module <module> line 14
+    self.dark_side = Elemento(self.DARK_SIDE, , x=100, y=100, cena=self.calcada)
+AttributeError: 'Eventos' object has no attribute 'DARK_SIDE'
+'''},
