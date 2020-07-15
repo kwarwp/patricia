50983d30b292a6f92718fa31216efc4de3a83475
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-15 14:58:25 -0300

    Saved callie/__log__.py @2020-07-15 17:58

--- callie/__log__.py
+++ callie/__log__.py
@@ -309,3 +309,21 @@ AttributeError: 'Elemento' object has no attribute 'carta2'
     self.carta.carta2.carta3.carta4.vai()
 AttributeError: 'Elemento' object has no attribute 'carta2'
 '''},
+{'date': 'Wed Jul 15 2020 14:58:20.956 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 57
+    Jogo().vai()
+  module <module> line 52
+    self.carta.carta2.carta3.carta4.vai()
+AttributeError: 'Jogo' object has no attribute 'carta'
+'''},
