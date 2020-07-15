ca2a7011bf7e05ec335c1abee83daf7d963ff22d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-15 03:12:57 -0300

    Saved callie/main.py @2020-07-15 06:12

--- callie/main.py
+++ callie/main.py
@@ -9,15 +9,17 @@ STYLE["width"] = 600
 TABULEIRO = "https://activufrj.nce.ufrj.br/file/ProgOO/TAB_VAZIO.png"
 CARTA = "https://activufrj.nce.ufrj.br/file/ProgOO/Card_Activ.png"
 CARTA2 = "https://activufrj.nce.ufrj.br/file/ProgOO/Card_github.png"
-#PosicaoX= (width_inicial)*coluna  (2+145)*1=155/ (145+10)*2=310
-#Posicaoy=(heigth_inicial+10)*linha
+CARTA3 = "https://activufrj.nce.ufrj.br/file/ProgOO/Card_Gitlab.png"
+#PosicaoX= (width_inicial+8)*coluna  (8+145)*1=153/ (145+8)*2=306
+#Posicaoy=(heigth_inicial+8)*linha
 class Jogo:
     """ Representa apenas o tabuleiro do jogo """
     def __init__(self):
         """ Mostra o tabuleiro """
         self.tab = Cena(TABULEIRO)
         self.carta = Elemento(CARTA, x=2, y=5,w=145,h=190, cena=self.tab)
-        self.carta2 = Elemento(CARTA, x=155, y=5, w=145,h=190,cena=self.tab)
+        self.carta2 = Elemento(CARTA2, x=153, y=5, w=145,h=190,cena=self.tab)
+        self.carta3 = Elemento(CARTA3, x=2, y=203,w=145,h=190, cena=self.tab)
         ''' Sendo o Width da cena 600, o melhor tamanho 
                             para a carta é w=145 e h-190'''
 
