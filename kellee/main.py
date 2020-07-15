8084e5ebbd9397886f32c965feb0382532dcec44
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-15 16:10:22 -0300

    Saved kellee/main.py @2020-07-15 19:10

--- kellee/main.py
+++ kellee/main.py
@@ -10,7 +10,7 @@ from _spy.vittolino.main import INVENTARIO as inv
 #aqui eu importei os cenarios e os elementos
 praia= Cena (img = "https://image.freepik.com/vetores-gratis/cena-praia-com-rede_1308-31618.jpg")
 jardim= Cena (img = "https://upload.wikimedia.org/wikipedia/commons/8/83/Butchardgardens.jpg")
-casa = cena(img ="https://plantasdecasas.com/storage/2017/07/planta-fachada-casa-115-fr-AG.jpg")
+casa= cena (img = "https://plantasdecasas.com/storage/2017/07/planta-fachada-casa-115-fr-AG.jpg")
 boneca = Elemento (img= "https://i.pinimg.com/originals/05/27/ea/0527ea10d8ef34745e6c808d620c7813.png")
 menino = Elemento (img= "https://cdn.vitaclinica.com.br/wp-content/uploads/2018/06/Wagner1.jpg")
 #aqui eu coloquei o elemento para entrar na cena
