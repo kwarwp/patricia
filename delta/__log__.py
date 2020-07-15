b3faf91c8d071250779a895b7afcd620f5b79f18
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-15 01:58:45 -0300

    Saved delta/__log__.py @2020-07-15 04:58

--- delta/__log__.py
+++ delta/__log__.py
@@ -1144,3 +1144,21 @@ TypeError: posicao() missing 1 positional argument: self
     posicao()
 TypeError: posicao() missing 1 positional argument: self
 '''},
+{'date': 'Wed Jul 15 2020 01:58:44.294 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 31
+    posicao(self)
+  module <module> line 27
+    	self.carta1=Cena(self.carta[0],,x=200,y=400)
+AttributeError: 'GUI' object has no attribute 'carta'
+'''},
