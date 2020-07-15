57aee9650fffe93cd25a094e2e7b47028131d21f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-15 19:55:34 -0300

    Saved bravo/__log__.py @2020-07-15 22:55

--- bravo/__log__.py
+++ bravo/__log__.py
@@ -497,3 +497,23 @@ NameError: name 'true' is not defined
     if txt1.vai == true:
 NameError: name 'true' is not defined
 '''},
+{'date': 'Wed Jul 15 2020 19:55:33.79 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 41
+    teste()    
+  module <module> line 36
+    time.sleep(1)
+  module time line 223
+    raise NotImplementedError("Blocking functions like time.sleep() are not "
+NotImplementedError: Blocking functions like time.sleep() are not supported in the browser. Use functions in module browser.timer instead.
+'''},
