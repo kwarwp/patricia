a97fae1a2c81569beb5d0246e2ce9d8185a2dab0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-15 02:19:07 -0300

    Saved delta/hotel.py @2020-07-15 05:19

--- delta/hotel.py
+++ delta/hotel.py
@@ -18,21 +18,20 @@ from browser import document
 
 """ Imagens do Jogo da Memória: verso, pycharm, linux
 """
-def Embaralha():
 
-	def cartas():
-		carta=[0,0,0]
-		carta[0]=Elemento(img="http://activufrj.nce.ufrj.br/file/ProgOO/Card_pycharm.png?disp=inline")
-		carta[1]=Cena(img="http://activufrj.nce.ufrj.br/file/ProgOO/Card_Linux.png?disp=inline")
-		carta[2]=Cena(img="http://activufrj.nce.ufrj.br/file/ProgOO/Card_verso.png?disp=inline")
-		random.shuffle(carta,random.random)
+def embaralha():
+	carta=[0,0,0]
+	carta[0]=Elemento(img="http://activufrj.nce.ufrj.br/file/ProgOO/Card_pycharm.png?disp=inline")
+	carta[1]=Cena(img="http://activufrj.nce.ufrj.br/file/ProgOO/Card_Linux.png?disp=inline")
+	carta[2]=Cena(img="http://activufrj.nce.ufrj.br/file/ProgOO/Card_verso.png?disp=inline")
+	random.shuffle(carta,random.random)
 	"""def posicao():
 		carta1=Elemento(self.carta[0],,x=200,y=400)
 		self.carta2=Cena(self.carta[1],,x=100,y=100)
       """
-	def vai():
-		
-		carta[2].vai()
+def vai():
+	
+	carta[2].vai()
 
 
-Embaralha()
+embaralha()
