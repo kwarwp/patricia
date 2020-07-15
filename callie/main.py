bd87041eb341ac20fe30a55238ca0730f5ba3e20
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-15 01:51:34 -0300

    Saved callie/main.py @2020-07-15 04:51

--- callie/main.py
+++ callie/main.py
@@ -1,22 +1,26 @@
 # patricia.callie.main.py
 # Rio de Janeiro, 9 de junho de 2020
-from _spy_vittollino.main import Cena, STYLE
+from _spy.vitollino.main import Cena, Elemento, STYLE
 __version__ = "20.07"
 __author__= 'Emanuelle Simas'
 
-STYLE["Width"] = 500
+STYLE["width"] = 500
 
 TABULEIRO = "https://activufrj.nce.ufrj.br/file/ProgOO/TAB_COMPL_5x5.png"
-
+CARTA = "https://activufrj.nce.ufrj.br/file/ProgOO/Card_Activ.png"
+#PosicaoX=
+#Posicaoy=
 class Jogo:
     """ Representa apenas o tabuleiro do jogo """
     def __init__(self):
         """ Mostra o tabuleiro """
         self.tab = Cena(TABULEIRO)
+        self.carta = Elemento(CARTA, x=10, y=10, width=300, heigth=80, cena=self.tab)
 
     def vai(self):
         """ Mostra a cena do tabuleiro """
         self.tab.vai()
+        self.carta.vai()
         #Cena(CENA_CALCADA).vai()
     
 if __name__ == "__main__":
