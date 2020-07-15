dd2dd9595b04cba874fead3b1d528e95c1237fdb
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-15 01:52:00 -0300

    Saved naomi/main.py @2020-07-15 04:51

--- naomi/main.py
+++ naomi/main.py
@@ -1,8 +1,14 @@
+import random
 from _spy.vitollino.main import Cena,Elemento,Texto
 
 def cartas():
-  carta=[1,2,3,4]
-  carta[0]=Cena(img="http://activufrj.nce.ufrj.br/file/ProgOO/Card_pycharm.png?disp=inline")
+	carta=[1,2,3]
+	carta[0]=Cena(img="http://activufrj.nce.ufrj.br/file/ProgOO/Card_pycharm.png?disp=inline")
+	carta[0]=Cena(img="http://activufrj.nce.ufrj.br/file/ProgOO/Card_pycharm.png?disp=inline")
+	carta[1]=Cena(img="http://activufrj.nce.ufrj.br/file/ProgOO/Card_Linux.png?disp=inline")
+	carta[2]=Cena(img="http://activufrj.nce.ufrj.br/file/ProgOO/Card_verso.png?disp=inline")
+	random.shuffle(carta,random.random)
+	carta[0].vai()=carta[0](x=400,y=500)
+	carta[1].vai()  
   
-  carta[0].vai()
 cartas()
