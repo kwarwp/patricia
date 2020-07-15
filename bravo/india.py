ef24edad77f03ba3b0ed92acfa9df51866ecbe19
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-15 20:47:29 -0300

    Saved bravo/india.py @2020-07-15 23:47

--- bravo/india.py
+++ bravo/india.py
@@ -18,26 +18,23 @@ from random import choice, shuffle
 balao = "https://i.pinimg.com/236x/d2/dd/c4/d2ddc45b2599d5fc60f03ec81c53bb6c.jpg"
 balao2 = "https://i.pinimg.com/236x/14/3a/14/143a14c4535873de51301549ba96e051.jpg"
 fundo = "https://i.pinimg.com/originals/ca/a8/25/caa8256ded30c7703fadf79651d7833b.jpg"
-frase1 = "https://i.pinimg.com/236x/4c/f8/6c/4cf86c786beb1c6457f31bf4fcd7ec99.jpg"
-frase2 = "https://i.pinimg.com/236x/7a/d6/c2/7ad6c2fdfd198c6903dee4425825ed9a.jpg"
-pr = "https://romanticopornatureza.files.wordpress.com/2012/03/proximo.gif"
+fr2 = "https://i.pinimg.com/236x/7a/d6/c2/7ad6c2fdfd198c6903dee4425825ed9a.jpg"
+fr1 = "https://i.pinimg.com/236x/4c/f8/6c/4cf86c786beb1c6457f31bf4fcd7ec99.jpg"
 STYLE["width"] , STYLE["height"] = 1200, "600px"
 
 def teste():
     x = 0
     cena = Cena(img = fundo)
-    cenas = Elemento(img = balao, style=dict(left=200, top=250, width=200, height="200px"))
     cena1 = Cena(img = fundo)
+    cenas = Elemento(img = balao, style=dict(left=200, top=250, width=200, height="200px"))
     elementos = Elemento(img = balao2, style=dict(left=800, top=250, width=200, height="200px"))
-    txt = Texto(cena,"Vamos aprender a programar?")#
-    txt1 = Elemento(img = frase1, style=dict(left=500, top=100, width=200, height="200px"))
-    txt2 = Elemento(img = frase2, style=dict(left=500, top=100, width=200, height="200px"))
-    txt1.entra(cena1)
-    #txt1.vai()
-    txt2.entra(cena)
-    txt1.vai=cena.vai
-    #cena2.vai=txt2.vai
+    txt1 = Elemento(img = fr1, style=dict(left=500, top=100, width=200, height="200px")) #Texto(cena,"Vamos aprender a programar?")
+    txt2 = Elemento(img = fr2, style=dict(left=500, top=100, width=200, height="200px"))#Texto(cena,"Escolha entre cenas ou elementos para começar.")
     cena1.vai()
+    txt1.entra(cena1)
+    txt1.vai = cena.vai 
+    txt2.entra(cena)
+    txt2.vai()
     cenas.entra(cena)
     elementos.entra(cena)
     '''if x == 0:
