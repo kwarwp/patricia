65616b0010b55ba65b09c7bcf399261499788726
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-15 04:41:26 -0300

    Saved delta/__log__.py @2020-07-15 07:41

--- delta/__log__.py
+++ delta/__log__.py
@@ -2537,3 +2537,23 @@ TypeError: object() takes no parameters
     		self.card1b = Carta(IMG_PYCHARM, list_cards[1], self.cena)
 TypeError: object() takes no parameters
 '''},
+{'date': 'Wed Jul 15 2020 04:41:24.901 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 56
+    Jogo().vai()
+  module <module> line 41
+    		self.matriz()
+  module <module> line 47
+    		self.card1b = Carta(IMG_PYCHARM, list_cards[1], self.cena)
+TypeError: object() takes no parameters
+'''},
