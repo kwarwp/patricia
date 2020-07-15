00db09c7d3c11194cdd9b789a012d86c87335d81
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-15 19:26:48 -0300

    Saved foxtrot/__log__.py @2020-07-15 22:26

--- foxtrot/__log__.py
+++ foxtrot/__log__.py
@@ -1072,3 +1072,23 @@ NameError: name 'capa' is not defined
     self.cena_sobre.esquerda=capa.vai
 NameError: name 'capa' is not defined
 '''},
+{'date': 'Wed Jul 15 2020 19:26:47.995 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 55
+    class Sobre:
+  module <module> line 57
+    self.cena_sobre.esquerda=Jogo()
+  module <module> line 52
+    self.botao_sobre.vai=sobre()
+TypeError: 'str' object is not callable
+'''},
