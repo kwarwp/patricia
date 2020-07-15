703e61d0d4e551b440e540aef02fd2d66087473c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-15 16:56:05 -0300

    Saved bravo/india.py @2020-07-15 19:56

--- bravo/india.py
+++ bravo/india.py
@@ -17,12 +17,12 @@ from random import choice, shuffle
 balao = "https://4.bp.blogspot.com/-WKsFkjFqfdM/WMATuFHzIqI/AAAAAAAAl1I/Ib-2sPFDfJU0ImaSV8g0fOSBh4_sKAGNwCLcB/s1600/Daniel%2BStudios%2BPack%2BRender%2B%252810%2529.png"
 fundo = "https://i.pinimg.com/originals/ca/a8/25/caa8256ded30c7703fadf79651d7833b.jpg"
 
-STYLE["width"] , STYLE["height"] = 1200, "600px"
+#STYLE["width"] , STYLE["height"] = 1200, "600px"
 
 def teste():
     cena = Cena(img = fundo)
     elmt = Elemento(img = balao, style=dict(left=135, top=400, width=20, height="20px"))
-    cena.vai()
+    #cena.vai()
     elmt.vai()
 teste()    
     
