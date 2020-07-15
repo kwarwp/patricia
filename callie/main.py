a4b371d7c53886a3764633082e7403159ddd0f0f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-15 03:05:58 -0300

    Saved callie/main.py @2020-07-15 06:05

--- callie/main.py
+++ callie/main.py
@@ -9,14 +9,15 @@ STYLE["width"] = 600
 TABULEIRO = "https://activufrj.nce.ufrj.br/file/ProgOO/TAB_VAZIO.png"
 CARTA = "https://activufrj.nce.ufrj.br/file/ProgOO/Card_Activ.png"
 CARTA2 = "https://activufrj.nce.ufrj.br/file/ProgOO/Card_github.png"
-#PosicaoX=
-#Posicaoy=
+#PosicaoX= (width_inicial)*coluna  (2+145)*1=155/ (145+10)*2=310
+#Posicaoy=(heigth_inicial+10)*linha
 class Jogo:
     """ Representa apenas o tabuleiro do jogo """
     def __init__(self):
         """ Mostra o tabuleiro """
         self.tab = Cena(TABULEIRO)
         self.carta = Elemento(CARTA, x=2, y=5,w=145,h=190, cena=self.tab)
+        self.carta2 = Elemento(CARTA, x=155, y=5, w=145,h=190,cena=self.tab)
         ''' Sendo o Width da cena 600, o melhor tamanho 
                             para a carta é w=145 e h-190'''
 
@@ -24,6 +25,7 @@ class Jogo:
         """ Mostra a cena do tabuleiro """
         self.tab.vai()
         self.carta.vai()
+        self.carta2.vai
         #Cena(CENA_CALCADA).vai()
     
 if __name__ == "__main__":
