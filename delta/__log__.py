67d2038f7ee5f1994d8b59e683890940c794855b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-15 02:13:20 -0300

    Saved delta/__log__.py @2020-07-15 05:13

--- delta/__log__.py
+++ delta/__log__.py
@@ -1351,3 +1351,21 @@ AttributeError: 'GUI' object has no attribute 'carta1'
     	self.carta1.vai()
 AttributeError: 'GUI' object has no attribute 'carta1'
 '''},
+{'date': 'Wed Jul 15 2020 02:13:19.910 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 36
+    Embaralha()
+  module <module> line 32
+    	self.carta1.vai()
+AttributeError: 'GUI' object has no attribute 'carta1'
+'''},
