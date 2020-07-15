d56d8f753f3e9d7811c0c4c77540acb486270a11
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-15 18:59:40 -0300

    Created echo/__log__.py @2020-07-15 21:59

---
+++ echo/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Wed Jul 15 2020 19:00:06.222 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 12
+  dado_1 = "https://i.imgur.com/eEqs1z7.png"
+  ^
+IndentationError: unexpected indent
+'''},
