cec0548270bae4c7f26767acc708184fb580c541
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-15 13:39:53 -0300

    Saved kellee/main.py @2020-07-15 16:39

--- kellee/main.py
+++ kellee/main.py
@@ -5,15 +5,15 @@
 
 """
 
-from _spy.vitollino.main import Cena, Elemento, STYLE
-_version_="14.07"
-_author_ = "Rosilane"
+#from _spy.vitollino.main import Cena, Elemento, STYLE
+#_version_="14.07"
+#_author_ = "Rosilane"
 
-FUNDO = "https://image.freepik.com/vetores-gratis/plataforma-de-tileset-e-plano-de-fundo-para-criar-jogos-para-celular_22289-375.jpg"
+#FUNDO = "https://image.freepik.com/vetores-gratis/plataforma-de-tileset-e-plano-de-fundo-para-criar-jogos-para-celular_22289-375.jpg"
 
-def vai():
-	""" Mostrar o Fundo"""
-	Cena(FUNDO).vai()
-vai()
+#def vai():
+#	""" Mostrar o Fundo"""
+#	Cena(FUNDO).vai()
+#vai()
 
         
