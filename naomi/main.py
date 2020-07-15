935b9d9977f5fe0123d3a1baa470a98e61918899
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-15 01:26:50 -0300

    Saved naomi/main.py @2020-07-15 04:26

--- naomi/main.py
+++ naomi/main.py
@@ -1,6 +1,5 @@
 from _sou.vittolinho.main import Cena,Elemento,Texto
 
 def cartas():
-  carta1=Cena(img="http://activufrj.nce.ufrj.br/file/ProgOO/Card_pycharm.png?disp=inline")
-def rolar_cena():
+  carta1=Cena(img="https://www.gratispng.com/png-p73owb/")
   carta1.vai()
