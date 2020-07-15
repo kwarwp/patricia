680af56af322ba449b9f17e435a8f766cef450ba
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-15 15:38:43 -0300

    Created alexa/__log__.py @2020-07-15 18:38

---
+++ alexa/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Wed Jul 15 2020 15:38:41.408 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 8
+  cena1 = Cena( img = 'https://i.imgur.com/POhwRe1.jpg")
+                                                        ^
+SyntaxError: EOL while scanning string literal
+'''},
