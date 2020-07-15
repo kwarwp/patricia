53aba49509f31cc4853a5296e24fb2e39429863f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-15 13:48:11 -0300

    Created natalia/main.py @2020-07-15 16:48

---
+++ natalia/main.py
@@ -0,0 +1,27 @@
+# patricia.natalia.main.py
+
+""" Jogo do Mistério da Praia.
+
+    Agora você desceu para a praia.
+
+Changelog
+---------
+    20.07
+        * NEW: O jogo original.
+
+"""
+from _spy.vitollino.main import Cena
+__version__ = "15.07"
+__author__ = "Rosilane"
+CENA_PRAIA = "https://i.imgur.com/wz5XKcr.jpg"
+
+
+class Praia:
+    """ Representa uma cena da calçada da praia """
+    def vai(self):
+        """ Mostra a cena da praia """
+        Cena(CENA_PRAIA).vai()
+    
+
+if __name__ == "__main__":
+    Praia().vai()
