5e5f4c86f982a327b83e6caf475055661eb15b8b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-15 15:41:45 -0300

    Saved alexa/__log__.py @2020-07-15 18:41

--- alexa/__log__.py
+++ alexa/__log__.py
@@ -20,3 +20,10 @@ SyntaxError: EOL while scanning string literal
                                                        ^
 SyntaxError: EOL while scanning string literal
 '''},
+{'date': 'Wed Jul 15 2020 15:41:45.7 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 9
+  op1 = Elemento (img = "https://images.vexels.com/media/users/3/139740/isolated/preview/bfecbaa063a84b2e9bbd9f8b9b41d410-bot--o-de-reprodu----o-redondo-azul-by-vexels.png")
+  ^
+IndentationError: unexpected indent
+'''},
