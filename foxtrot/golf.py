3cd635cb90439ad779d7a3ad715b51965f18a8bc
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-15 17:50:07 -0300

    Saved foxtrot/golf.py @2020-07-15 20:50

--- foxtrot/golf.py
+++ foxtrot/golf.py
@@ -11,18 +11,18 @@ Changelog
 
 """
 from __spy.vitollino.main import Cena, Elemento, Texto, STYLE
-
 capa = "https://i.imgur.com/SI1BO9E.png"
 botao_jogar = "https://i.imgur.com/F3Q0bDv.png"
 botao_sobre = "https://i.imgur.com/pG9wDIz.png"
+
 class Jogo:
     def __init__(self):
         self.capa=Cena
         self.botao_jogar=Elemento
         self.botao_sobre=Elemento
 
-        self.botao_sobre.entra(self.capa)
-        self.botao_jogar.entra(self.capa)
+        self.botao_sobre.entra()
+        self.botao_jogar.entra()
 
 if __name__ == "__main__":
     Jogo().inicia()
