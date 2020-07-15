edd60620bc5ef5b3eb627a5fc3db1d71ec3fff73
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-15 02:29:08 -0300

    Saved callie/__log__.py @2020-07-15 05:29

--- callie/__log__.py
+++ callie/__log__.py
@@ -189,3 +189,10 @@ SyntaxError: invalid syntax
                                               ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Wed Jul 15 2020 02:29:06.186 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 18
+  self.carta = Elemento(CARTA, x=2, y=5,self.style{width=1000px, height='30px'}, cena=self.tab)
+                                                   ^
+SyntaxError: invalid syntax
+'''},
