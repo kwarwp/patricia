5c48834565cd48d865f86759983b641d648db9c8
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-15 18:59:49 -0300

    Saved echo/__log__.py @2020-07-15 21:59

--- echo/__log__.py
+++ echo/__log__.py
@@ -6,3 +6,10 @@
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Wed Jul 15 2020 19:00:16.574 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 14
+  dado_1.vai()
+  ^
+IndentationError: unexpected indent
+'''},
