309579db75c55c508907391de49744ccafd16436
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-14 21:46:32 -0300

    Saved delta/hotel.py @2020-07-15 00:46

--- delta/hotel.py
+++ delta/hotel.py
@@ -18,28 +18,30 @@ from browser import document # importa o DOM para atribuir o evento de teclado
 class Eventos:
 	""" Associa um evento a uma imagem e captura eventos de teclado. """
 	CENA_TABULEIRO = "http://activufrj.nce.ufrj.br/file/ProgOO/TAB_VAZIO.png?disp=inline"
-	CARTA_VERSO= "http://activufrj.nce.ufrj.br/file/ProgOO/Card_verso.png?disp=inline"
-	CARTA_PYCHARM = "http://activufrj.nce.ufrj.br/file/ProgOO/Card_pycharm.png?disp=inline"
+	CARTA_1= "http://activufrj.nce.ufrj.br/file/ProgOO/Card_verso.png?disp=inline"
+	CARTA_2= "http://activufrj.nce.ufrj.br/file/ProgOO/Card_verso.png?disp=inline"
+	CARTA_3 = "http://activufrj.nce.ufrj.br/file/ProgOO/Card_pycharm.png?disp=inline"
     
-    
+	s=[CARTA_1,CARTA_2,CARTA_3,4]
+
+	random.shuffle(s,random.random)
+      	
+	def linkar (self):
+		CARTA_VERSO=s[0]
+		CARTA_VERSO2=s[1]
+		CARTA_PYCHARM=s[2]
+		#CARTA_VERSO=s[0]
+		     
 	def __init__(self):
 		self.tabuleiro = Cena(self.CENA_TABULEIRO)
-		#self.pycharm = Elemento(self.CARTA_PYCHARM, , , cena=self.tabuleiro)
+		self.pycharm = Elemento(self.CARTA_PYCHARM, , , cena=self.tabuleiro)
 		self.verso=Elemento(self.CARTA_VERSO, , x=100, y=200, cena=self.tabuleiro)
-		self.verso2=Elemento(self.CARTA_VERSO, , x=200, y=300, cena=self.tabuleiro)
+		self.verso2=Elemento(self.CARTA_VERSO2, , x=200, y=300, cena=self.tabuleiro)
         
        # self.pycharm=Elemento(self.CARTA_PYCHARM,,X=100,y=300,cena=self.tabuleiro)
         
         
-	def embaralhar (self):
-
-		s=[self.verso,self.verso2,3,4]
-
-		self.s=random.shuffle(s,random.random)
-      	
-	def linkar (self):
-		self.pycharm=s[0]
-		self.verso=s[1]          
+	         
         
 	def vai(self):
 		""" mostra a cena da calçada. """
