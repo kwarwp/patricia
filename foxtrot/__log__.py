ceb7115356f74e885c11b2cbf875bfe565fb3754
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-15 19:13:27 -0300

    Saved foxtrot/__log__.py @2020-07-15 22:13

--- foxtrot/__log__.py
+++ foxtrot/__log__.py
@@ -907,3 +907,21 @@ IndentationError: unexpected indent
     self.cena_sobre.esquerda=capa.vai
 NameError: name 'capa' is not defined
 '''},
+{'date': 'Wed Jul 15 2020 19:13:26.375 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 62
+    Jogo()
+  module <module> line 52
+    self.cena_sobre.esquerda=capa.vai
+NameError: name 'capa' is not defined
+'''},
