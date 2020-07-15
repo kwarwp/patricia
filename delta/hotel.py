4876094a77c23c4b7fa53b1b5531fc59514fe3b0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-15 04:35:47 -0300

    Saved delta/hotel.py @2020-07-15 07:35

--- delta/hotel.py
+++ delta/hotel.py
@@ -43,7 +43,7 @@ class Jogo:
 	def matriz(self):
 		self.cena = Cena()
 		list_cards=self.embaralha()
-		self.card1a = Carta(IMG_PYCHARM, list_cards[0], self.cena)
+		self.card1a = Carta(IMG_LINUX, list_cards[0], self.cena)
 		self.card1b = Carta(IMG_PYCHARM, list_cards[1], self.cena)
 		self.card2a = Carta(IMG_LINUX, list_cards[2], self.cena)
 		self.card2b = Carta(IMG_LINUX, list_cards[3], self.cena)
