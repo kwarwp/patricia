1a724477f105802d44254eb84f7153ca16201f9f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-15 01:34:40 -0300

    Saved naomi/main.py @2020-07-15 04:34

--- naomi/main.py
+++ naomi/main.py
@@ -3,3 +3,4 @@ from _spy.vitollino.main import Cena,Elemento,Texto
 def cartas():
   carta1=Cena(img="http://activufrj.nce.ufrj.br/file/ProgOO/Card_pycharm.png?disp=inline")
   carta1.vai()
+cartas()
