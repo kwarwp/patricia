2e3f60cd67101a9dedc7b2d782cdc5616f49b258
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-15 17:42:19 -0300

    Saved callie/__log__.py @2020-07-15 20:42

--- callie/__log__.py
+++ callie/__log__.py
@@ -474,3 +474,21 @@ NameError: name 'vira' is not defined
     self.verso.verso1.verso2.verso3.verso4.vai()
 AttributeError: 'Elemento' object has no attribute 'verso1'
 '''},
+{'date': 'Wed Jul 15 2020 17:42:15.706 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 65
+    Jogo().vai()
+  module <module> line 50
+    self.verso.vai= vira
+NameError: name 'vira' is not defined
+'''},
