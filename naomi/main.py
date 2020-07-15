3f6169744b556d29a53111f858736b461c6355bd
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-15 01:17:34 -0300

    Saved naomi/main.py @2020-07-15 04:17

--- naomi/main.py
+++ naomi/main.py
@@ -1,29 +1,4 @@
-# patricia.naomi.main.py
-from _spy.vitollino.main import Cena, Elemento
-from browser import document # importa o DOM para atribuir o evento de teclado
-
-class Eventos:
-    """ Associa um evento a uma imagem e captura eventos de teclado. """
-    CENA_TABULEIRO = "http://activufrj.nce.ufrj.br/file/ProgOO/TAB_VAZIO.png?disp=inline"
-    CARTA_VERSO= "http://activufrj.nce.ufrj.br/file/ProgOO/Card_verso.png?disp=inline"
-    CARTA_PYCHARM = "http://activufrj.nce.ufrj.br/file/ProgOO/Card_pycharm.png?disp=inline"
-    
-    
-    def __init__(self):
-        self.tabuleiro = Cena(self.CENA_TABULEIRO)
-        self.pycharm = Elemento(self.CARTA_PYCHARM, , x=200, y=200, cena=self.tabuleiro)
-        
-       # self.pycharm=Elemento(self.CARTA_PYCHARM,,X=100,y=300,cena=self.tabuleiro)
-        1=self.pycharm
-        2=self.
-        
-        
-        
-    def vai(self):
-        """ mostra a cena da calçada. """
-        self.tabuleiro.vai()
-        
-    
-        
-if __name__ == "__main__":
-    Eventos().vai()
+from _sou.vittolinho.main import Cena,Elemento,Texto
+  def cartas():
+    carta1=Cena(img="http://activufrj.nce.ufrj.br/file/ProgOO/Card_pycharm.png?disp=inline")
+    carta1.vai()
