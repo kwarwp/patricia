cdd9c889ca4ac6cb90e53e6bc364a267739e1ac9
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-15 01:36:36 -0300

    Saved callie/__log__.py @2020-07-15 04:36

--- callie/__log__.py
+++ callie/__log__.py
@@ -106,3 +106,19 @@ AttributeError: 'module' object has no attribute '__setitem__'
     STYLE["width"] = 500
 AttributeError: 'module' object has no attribute '__setitem__'
 '''},
+{'date': 'Wed Jul 15 2020 01:36:30.882 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 7
+    STYLE["width"] = 500
+AttributeError: 'module' object has no attribute '__setitem__'
+'''},
