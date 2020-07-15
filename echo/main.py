dabc9b68a63ddf4c4d51ab74654f6bdc4b89732e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-15 20:40:01 -0300

    Saved echo/main.py @2020-07-15 23:40

--- echo/main.py
+++ echo/main.py
@@ -1,9 +1,10 @@
 # patricia.echo.main.py
 """ Jogo Olimpíadas de Matemática20.07
         * NEW: O jogo original
-        Autor: Grupo Echo
 """
 from _spy.vitollino.main import Cena, Elemento, Labirinto, Texto, Codigo, Sala, STYLE
 from _spy.vittolino.main import INVENTARIO as inv
+STYLE["width"] = 1150
+STYLE["leight"]= "550px"
 tabuleiro = Cena (img ="https://www.ibilce.unesp.br/Home/Departamentos/Matematica/2cejta/avancando-com-o-resto.png")
 tabuleiro.vai()
