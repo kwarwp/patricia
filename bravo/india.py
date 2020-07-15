e1f18066800b515d7e74827570aff14093637ac8
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-15 19:47:27 -0300

    Saved bravo/india.py @2020-07-15 22:47

--- bravo/india.py
+++ bravo/india.py
@@ -14,15 +14,20 @@ from _spy.vitollino.main import Cena, Elemento, STYLE, Texto
 from browser import html, alert, timer
 from random import choice, shuffle
 #from _spy.vitollino.main  import Cena,Elemento,Texto
-balao = "https://4.bp.blogspot.com/-WKsFkjFqfdM/WMATuFHzIqI/AAAAAAAAl1I/Ib-2sPFDfJU0ImaSV8g0fOSBh4_sKAGNwCLcB/s1600/Daniel%2BStudios%2BPack%2BRender%2B%252810%2529.png"
+balao = "https://i.pinimg.com/236x/d2/dd/c4/d2ddc45b2599d5fc60f03ec81c53bb6c.jpg"
+balao2 = "https://i.pinimg.com/236x/14/3a/14/143a14c4535873de51301549ba96e051.jpg"
 fundo = "https://i.pinimg.com/originals/ca/a8/25/caa8256ded30c7703fadf79651d7833b.jpg"
 
-#STYLE["width"] , STYLE["height"] = 1200, "600px"
+STYLE["width"] , STYLE["height"] = 1200, "600px"
 
 def teste():
     cena = Cena(img = fundo)
-    elmt = Elemento(img = balao, style=dict(left=135, top=400, width=20, height="20px"))
-    #cena.vai()
-    elmt.vai()
+    cenas = Elemento(img = balao, style=dict(left=200, top=250, width=200, height="200px"))
+    elementos = Elemento(img = balao2, style=dict(left=800, top=250, width=200, height="200px"))
+    cena.vai()
+    cenas.entra(cena)
+    elementos.entra(cena)
+    cenas.vai()
+    elementos.vai()
 teste()    
     
