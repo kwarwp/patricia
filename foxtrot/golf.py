3223479e545b3bb77e6f7a2da9b709e51940c1a8
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-15 19:07:34 -0300

    Saved foxtrot/golf.py @2020-07-15 22:07

--- foxtrot/golf.py
+++ foxtrot/golf.py
@@ -19,6 +19,8 @@ STYLE["heigth"] = "500px"
 capa_do_jogo = "https://i.imgur.com/0RVnppj.png"
 botao_jogar = "https://i.imgur.com/pG9wDIz.png"
 botao_sobre = "https://i.imgur.com/F3Q0bDv.png"
+sobre = "https://i.imgur.com/mGPgru5.png"
+seta_esquerda = "https://i.imgur.com/wYbmcVb.png"
 #criando classe jogo
 class Jogo:
     def __init__(self):
@@ -43,12 +45,22 @@ class Jogo:
         self.botao_jogar.entra(self.capa)
         
         #textos
-        self.texto_sobre = Texto (self.capa, "Olá! Volte em breve para saber mais!")
         self.texto_jogar = Texto (self.capa, "Olá! Volte em breve para Jogar!")
 
+        #cenasobre
+        self.cena_sobre = Cena (img=sobre)
+        self.cena_sobre = Elemento (img=seta_esquerda, 
+        tit="Voltar", 
+        style= dict(left=0, top=100))
+        self.seta_esquerda.vai=self.capa.vai
+
         #ação caso seja clicado
-        self.botao_sobre.vai=self.texto_sobre.vai
         self.botao_jogar.vai=self.texto_jogar.vai
+        self.botao_sobre.vai=self.cena_sobre.vai
+     
+     
+        
+        
         
 if __name__ == "__main__":
     Jogo()
