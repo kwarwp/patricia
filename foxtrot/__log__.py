dae574325a87be114e6c0ef7187cdeed9c329378
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-15 17:33:32 -0300

    Saved foxtrot/__log__.py @2020-07-15 20:33

--- foxtrot/__log__.py
+++ foxtrot/__log__.py
@@ -97,3 +97,10 @@ IndentationError: expected an indented block
   ^
 IndentationError: expected an indented block
 '''},
+{'date': 'Wed Jul 15 2020 17:33:32.102 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 23
+  
+  ^
+IndentationError: expected an indented block
+'''},
