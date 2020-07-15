012d5e899fc907e8ff633b4f77aa9075547091d9
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-15 04:23:29 -0300

    Saved delta/__log__.py @2020-07-15 07:23

--- delta/__log__.py
+++ delta/__log__.py
@@ -2356,3 +2356,23 @@ NameError: name 'list_cards' is not defined
     		self.card1a = Carta(IMG_PYCHARM, list_cards[0], self.cena)
 TypeError: object() takes no parameters
 '''},
+{'date': 'Wed Jul 15 2020 04:23:28.56 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 52
+    Jogo().vai()
+  module <module> line 37
+    		self.matriz()
+  module <module> line 42
+    		self.card1a = Carta(IMG_PYCHARM, list_cards[0], self.cena)
+TypeError: object() takes no parameters
+'''},
