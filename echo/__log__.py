bb1b159770927f7921d3863df963b5e24ffbba54
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-15 20:32:18 -0300

    Saved echo/__log__.py @2020-07-15 23:32

--- echo/__log__.py
+++ echo/__log__.py
@@ -106,3 +106,19 @@ SyntaxError: invalid syntax
                                                                             ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Wed Jul 15 2020 20:32:17.845 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 7
+    tabuleiro = cena (img = "https://www.ibilce.unesp.br/Home/Departamentos/Matematica/2cejta/avancando-com-o-resto.png")
+NameError: name 'cena' is not defined
+'''},
