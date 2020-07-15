c598c0819991d2284f0c7f98c1fb4f63525248ed
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-15 20:32:57 -0300

    Saved echo/main.py @2020-07-15 23:32

--- echo/main.py
+++ echo/main.py
@@ -2,9 +2,7 @@
 """ Jogo Olimpíadas de Matemática20.07
         * NEW: O jogo original
 """
-from _spy.vitollino.main import Cenafrom _spy.vitollino.main import Cena, Elemento, Labirinto, Texto, Codigo, Sala, STYLE
+from _spy.vitollino.main import Cena, Elemento, Labirinto, Texto, Codigo, Sala, STYLE
 from _spy.vittolino.main import INVENTARIO as inv
-tabuleiro = cena (img = "
-https://www.ibilce.unesp.br/Home/Departamentos/Matematica/2cejta/avancando-com-o-resto.png
-")
+tabuleiro = Cena (img ="https://www.ibilce.unesp.br/Home/Departamentos/Matematica/2cejta/avancando-com-o-resto.png")
 tabuleiro.vai()
