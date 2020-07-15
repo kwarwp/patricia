22695b05fbc7a9205499ede0403db99f1066b746
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-15 20:42:10 -0300

    Saved echo/main.py @2020-07-15 23:42

--- echo/main.py
+++ echo/main.py
@@ -5,5 +5,5 @@
 from _spy.vitollino.main import Cena, Elemento, Labirinto, Texto, Codigo, Sala, STYLE
 from _spy.vittolino.main import INVENTARIO as inv
 """ tabuleiro = Cena (img ="https://www.ibilce.unesp.br/Home/Departamentos/Matematica/2cejta/avancando-com-o-resto.png") """
-tabuleiro = Cena (img ="ftp://infcross.com.br/mestrado/tabuleiro.jpg")
+tabuleiro = Cena (img ="infcross.com.br/mestrado/tabuleiro.jpg")
 tabuleiro.vai()
