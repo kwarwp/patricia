2c21f6fa6dc1fe0ce1f738126f611a014389dc6e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-15 15:36:13 -0300

    Saved bravo/__log__.py @2020-07-15 18:36

--- bravo/__log__.py
+++ bravo/__log__.py
@@ -6,3 +6,10 @@
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Wed Jul 15 2020 15:36:12.131 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 19
+  cena1.vai()
+  ^
+IndentationError: unexpected indent
+'''},
