af77c5c1acb9f6696a561121d7957569482a3392
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-14 21:48:49 -0300

    Saved delta/hotel.py @2020-07-15 00:48

--- delta/hotel.py
+++ delta/hotel.py
@@ -18,25 +18,25 @@ from browser import document # importa o DOM para atribuir o evento de teclado
 class Eventos:
 	""" Associa um evento a uma imagem e captura eventos de teclado. """
 	CENA_TABULEIRO = "http://activufrj.nce.ufrj.br/file/ProgOO/TAB_VAZIO.png?disp=inline"
-	CARTA_1= "http://activufrj.nce.ufrj.br/file/ProgOO/Card_verso.png?disp=inline"
-	CARTA_2= "http://activufrj.nce.ufrj.br/file/ProgOO/Card_verso.png?disp=inline"
-	CARTA_3 = "http://activufrj.nce.ufrj.br/file/ProgOO/Card_pycharm.png?disp=inline"
+	CARTA_VERSO= "http://activufrj.nce.ufrj.br/file/ProgOO/Card_verso.png?disp=inline"
+	#= "http://activufrj.nce.ufrj.br/file/ProgOO/Card_verso.png?disp=inline"
+	CARTA_PYCHARM = "http://activufrj.nce.ufrj.br/file/ProgOO/Card_pycharm.png?disp=inline"
     
-	s=[CARTA_1,CARTA_2,CARTA_3,4]
+	s=[CARTA_VERSO,CARTA_PYCHARM,CARTA_3,4]
 
 	random.shuffle(s,random.random)
       	
 	def linkar (self):
-		CARTA_VERSO=s[0]
-		CARTA_VERSO2=s[1]
-		CARTA_PYCHARM=s[2]
+		CARTA_1=s[0]
+		CARTA_2=s[1]
+		#CARTA_PYCHARM=s[2]
 		#CARTA_VERSO=s[0]
 		     
 	def __init__(self):
 		self.tabuleiro = Cena(self.CENA_TABULEIRO)
-		self.pycharm = Elemento(self.CARTA_PYCHARM, , , cena=self.tabuleiro)
-		self.verso=Elemento(self.CARTA_VERSO, , x=100, y=200, cena=self.tabuleiro)
-		self.verso2=Elemento(self.CARTA_VERSO2, , x=200, y=300, cena=self.tabuleiro)
+		self.carta1 = Elemento(self.CARTA_1, ,x=300,y=400 , cena=self.tabuleiro)
+		self.carta2=Elemento(self.CARTA_2, , x=100, y=200, cena=self.tabuleiro)
+		#self.verso2=Elemento(self.CARTA_VERSO2, , x=200, y=300, cena=self.tabuleiro)
         
        # self.pycharm=Elemento(self.CARTA_PYCHARM,,X=100,y=300,cena=self.tabuleiro)
         
