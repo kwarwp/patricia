57f4c350600306f73e5a152b29bfa199cafff53f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-15 20:33:54 -0300

    Saved echo/__log__.py @2020-07-15 23:33

--- echo/__log__.py
+++ echo/__log__.py
@@ -145,3 +145,10 @@ NameError: name 'cena' is not defined
                                                ^
 SyntaxError: trailing comma not allowed without surrounding parentheses
 '''},
+{'date': 'Wed Jul 15 2020 20:37:22.651 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 5
+  from _spy.vitollino.main import Cenafrom _spy.vitollino.main import Cena, Elemento, Labirinto, Texto, Codigo, Sala, STYLE
+                                               ^
+SyntaxError: trailing comma not allowed without surrounding parentheses
+'''},
