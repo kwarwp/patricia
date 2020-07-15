7dadce04f85dd9d614887176b3a605b8db60beff
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-15 19:31:28 -0300

    Saved foxtrot/__log__.py @2020-07-15 22:31

--- foxtrot/__log__.py
+++ foxtrot/__log__.py
@@ -1110,3 +1110,23 @@ Exception: $locals_$exec_322.Jogo is not a function
   module '$exec_322' line 24
 class Sobre:
 '''},
+{'date': 'Wed Jul 15 2020 19:31:27.353 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 56
+    class Sobre(self):
+  module <module> line 60
+    self.cena_sobre.esquerda=Jogo()
+  module <module> line 54
+    self.botao_sobre.vai=self.Sobre()
+AttributeError: 'Jogo' object has no attribute 'Sobre'
+'''},
