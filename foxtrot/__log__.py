aa85ff3d98e43d833f137ed7722201fa81b1fc9e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-15 17:53:49 -0300

    Saved foxtrot/__log__.py @2020-07-15 20:53

--- foxtrot/__log__.py
+++ foxtrot/__log__.py
@@ -424,3 +424,21 @@ AttributeError: 'module' object has no attribute 'entra'
     self.capa.vai()
 AttributeError: 'module' object has no attribute 'vai'
 '''},
+{'date': 'Wed Jul 15 2020 17:53:49.80 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 28
+    Jogo().inicia()
+  module <module> line 21
+    self.capa.vai()
+AttributeError: 'module' object has no attribute 'vai'
+'''},
