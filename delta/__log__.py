c8ff5fd99f327b23c2e16263882550642c84cd5d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-14 22:13:47 -0300

    Saved delta/__log__.py @2020-07-15 01:13

--- delta/__log__.py
+++ delta/__log__.py
@@ -810,3 +810,19 @@ NameError: name 'CARTA_1' is not defined
     		self.carta1 = Elemento(self.CARTA_1, ,x=300,y=400 , cena=self.tabuleiro)
 AttributeError: 'Eventos' object has no attribute 'CARTA_1'
 '''},
+{'date': 'Tue Jul 14 2020 22:13:36.747 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 80
+    print(teste)
+NameError: name 'teste' is not defined
+'''},
