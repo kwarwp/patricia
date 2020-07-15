309ce5725f8124160672833a8c7ee9e5047d6cc7
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-15 01:29:43 -0300

    Saved callie/__log__.py @2020-07-15 04:29

--- callie/__log__.py
+++ callie/__log__.py
@@ -3,3 +3,10 @@
 'error': '''
 Exception: <TypeError: Cannot read property '$methods' of undefined>
 '''},
+{'date': 'Wed Jul 15 2020 01:29:40.495 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 19
+  self.banhista = Elemento(pass)
+                            ^
+SyntaxError: invalid syntax
+'''},
