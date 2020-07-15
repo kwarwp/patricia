8c778105b271f7d221002bb8f97e3a2b7bdbaf35
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-15 18:08:40 -0300

    Saved foxtrot/__log__.py @2020-07-15 21:08

--- foxtrot/__log__.py
+++ foxtrot/__log__.py
@@ -631,3 +631,10 @@ SyntaxError: invalid syntax
                                                                                                       ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Wed Jul 15 2020 18:08:40.253 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 30
+  self.botao_jogar = Elemento (img= botao_jogar, tit = "jogar",
+                                                                                                                           ^
+SyntaxError: invalid syntax
+'''},
