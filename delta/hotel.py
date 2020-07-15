1e44a175467fb1da7cedf73b7b55a388e1f53cbe
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-15 05:03:21 -0300

    Saved delta/hotel.py @2020-07-15 08:03

--- delta/hotel.py
+++ delta/hotel.py
@@ -2,11 +2,11 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 """ Projeto sem descrição, (mude esta linha)."""
 
-"codeauthor:: Anni Provietti <anniprovietti@gmail.com>
+"""codeauthor:: Anni Provietti <anniprovietti@gmail.com>
 
 """Changelog"""
 
-.. versionadded::    20.07
+.. versionadded::    20.07"""
         
 
 """ modificações de acordo com a sala golf"""
