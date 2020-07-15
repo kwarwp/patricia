5cddc67e2ec653d9530364c6ffb9cc9462eda533
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-14 21:48:55 -0300

    Saved delta/__log__.py @2020-07-15 00:48

--- delta/__log__.py
+++ delta/__log__.py
@@ -717,3 +717,21 @@ AttributeError: 'function' object has no attribute 'shuffle'
     		self.pycharm = Elemento(self.CARTA_PYCHARM, , , cena=self.tabuleiro)
 AttributeError: 'Eventos' object has no attribute 'CARTA_PYCHARM'
 '''},
+{'date': 'Tue Jul 14 2020 21:48:54.741 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 18
+    class Eventos:
+  module <module> line 25
+    	s=[CARTA_VERSO,CARTA_PYCHARM,CARTA_3,4]
+NameError: name 'CARTA_3' is not defined
+'''},
