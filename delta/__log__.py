39a98ec41d47142b5fea791834b9ec7a68827a7c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-15 02:24:27 -0300

    Saved delta/__log__.py @2020-07-15 05:24

--- delta/__log__.py
+++ delta/__log__.py
@@ -1417,3 +1417,21 @@ SyntaxError: invalid syntax
       ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Wed Jul 15 2020 02:24:26.759 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 40
+    embaralha()
+  module <module> line 34
+    		carta[i].vai(x=100,y=100)
+TypeError: vai() got an unexpected keyword argument 'x'
+'''},
