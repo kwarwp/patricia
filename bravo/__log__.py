95c2891ac9cdc21f250868c49e2c9bee2d7f680c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-15 15:35:52 -0300

    Created bravo/__log__.py @2020-07-15 18:35

---
+++ bravo/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Wed Jul 15 2020 15:35:50.493 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 19
+  cena1.vai()
+  ^
+IndentationError: unexpected indent
+'''},
