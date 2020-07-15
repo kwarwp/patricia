5c0232ba041d8c96cbaceb66acca3441cf2fdfbc
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-15 15:49:53 -0300

    Saved kellee/__log__.py @2020-07-15 18:49

--- kellee/__log__.py
+++ kellee/__log__.py
@@ -538,3 +538,23 @@ AttributeError: 'Praia' object has no attribute 'nome'
     self.scorer.update(valor=cena.nome, move=self.xy,
 AttributeError: 'Praia' object has no attribute 'nome'
 '''},
+{'date': 'Wed Jul 15 2020 15:49:53.124 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 55
+    Opcao().vai()
+  module <module> line 51
+    self.banhista.entra(Praia())
+  module _spy.vitollino.main line 472
+    self.scorer.update(valor=cena.nome, move=self.xy,
+AttributeError: 'Praia' object has no attribute 'nome'
+'''},
