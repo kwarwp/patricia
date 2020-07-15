bb8e97a7f6cfd293d7953c050d6c78cdf4787536
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-14 21:26:37 -0300

    Saved delta/__log__.py @2020-07-15 00:26

--- delta/__log__.py
+++ delta/__log__.py
@@ -528,3 +528,21 @@ NameError: name 'faceDown' is not defined
                   ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Tue Jul 14 2020 21:26:26.402 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 83
+    Game().create_2x5_cards()
+  module <module> line 57
+    self.card2a = Card(IMG_CARD_2, [2,0], self.cena, faceDown=False)
+TypeError: __init__() got an unexpected keyword argument 'faceDown'
+'''},
