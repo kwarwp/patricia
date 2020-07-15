85804517faf8f117a0bb00f723bcbbd2693c1b8b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-15 15:38:58 -0300

    Saved alexa/__log__.py @2020-07-15 18:38

--- alexa/__log__.py
+++ alexa/__log__.py
@@ -6,3 +6,10 @@
                                                         ^
 SyntaxError: EOL while scanning string literal
 '''},
+{'date': 'Wed Jul 15 2020 15:38:58.291 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 8
+  cena = Cena( img = 'https://i.imgur.com/POhwRe1.jpg")
+                                                       ^
+SyntaxError: EOL while scanning string literal
+'''},
