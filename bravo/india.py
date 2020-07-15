1d004a4d92d54d3674d79c7249419298c83d08eb
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-15 16:55:22 -0300

    Saved bravo/india.py @2020-07-15 19:55

--- bravo/india.py
+++ bravo/india.py
@@ -10,17 +10,19 @@ Changelog
         Descreva o que você adicionou no código.
 
 """
-#from _spy.vitollino.main  import Cena,Elemento,Texto
-fundo_1 = "https://i.pinimg.com/originals/ca/a8/25/caa8256ded30c7703fadf79651d7833b.jpg" 
-balao = "https://4.bp.blogspot.com/-WKsFkjFqfdM/WMATuFHzIqI/AAAAAAAAl1I/Ib-2sPFDfJU0ImaSV8g0fOSBh4_sKAGNwCLcB/s1600/Daniel%2BStudios%2BPack%2BRender%2B%252810%2529.png"
-fundo = "https://www.10wallpaper.com/wallpaper/1366x768/1504/black_and_blue-High_Quality_HD_Wallpaper_1366x768.jpg"
-# ada.sara.main.py
 from _spy.vitollino.main import Cena, Elemento, STYLE, Texto
 from browser import html, alert, timer
 from random import choice, shuffle
+#from _spy.vitollino.main  import Cena,Elemento,Texto
+balao = "https://4.bp.blogspot.com/-WKsFkjFqfdM/WMATuFHzIqI/AAAAAAAAl1I/Ib-2sPFDfJU0ImaSV8g0fOSBh4_sKAGNwCLcB/s1600/Daniel%2BStudios%2BPack%2BRender%2B%252810%2529.png"
+fundo = "https://i.pinimg.com/originals/ca/a8/25/caa8256ded30c7703fadf79651d7833b.jpg"
+
 STYLE["width"] , STYLE["height"] = 1200, "600px"
+
 def teste():
-	cena = Cena(img = fundo_1)
-	elmt = Elemento(img = balao, style=dict(left=405, top=400, width=20, height="80px"))
-	cena.vai()
+    cena = Cena(img = fundo)
+    elmt = Elemento(img = balao, style=dict(left=135, top=400, width=20, height="20px"))
+    cena.vai()
+    elmt.vai()
 teste()    
+    
