9be3ba466953e514ecdd4a04389f04dcb0f342f5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-15 13:45:58 -0300

    Saved kellee/main.py @2020-07-15 16:45

--- kellee/main.py
+++ kellee/main.py
@@ -16,4 +16,37 @@
 #	Cena(FUNDO).vai()
 #vai()
 
-        
+
+""" Jogo do Mistério da Praia.
+
+    Descubra o mistério desta praia.
+
+Changelog
+---------
+    20.07
+        * NEW: O jogo original.
+
+"""
+from _spy.vitollino.main import Cena, Elemento, STYLE
+from natalia.main import Praia
+__version__ = "15.07"
+__author__ = "Rosilane"
+STYLE["width"] = 500
+CENA_CALCADA = "https://i.imgur.com/zOxshRh.jpg"
+BANHISTA = "https://i.imgur.com/CWQ00XG.png"
+
+
+class Calcada:
+    """ Representa uma cena da calçada da praia """
+    def __init__(self):
+        """ Mostra a cena da praia """
+        self.cena = Cena(CENA_CALCADA, direita=Praia())
+        self.banhista = Elemento(BANHISTA, x=100, y=200, cena=self.cena)
+        #Cena(CENA_CALCADA).vai()
+    def vai(self):
+        """ Mostra a cena da praia """
+        self.cena.vai()
+        #Cena(CENA_CALCADA).vai()
+    
+if __name__ == "__main__":
+    Calcada().vai()
