036c45acf5a32d439fcb942182bd60a195e1a815
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-15 02:59:58 -0300

    Saved alpha/__log__.py @2020-07-15 05:59

--- alpha/__log__.py
+++ alpha/__log__.py
@@ -229,3 +229,21 @@ AttributeError: 'GUI' object has no attribute 'boneco'
     if andaboneco.self.boneco.x > 100 and self.boneco.y > 100:
 NameError: name 'andaboneco' is not defined
 '''},
+{'date': 'Wed Jul 15 2020 02:59:57.145 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 28
+    class Eventos:
+  module <module> line 45
+    document.bind("keydown", self.anda_boneco)  # captura o evento de teclado
+AttributeError: 'GUI' object has no attribute 'anda_boneco'
+'''},
