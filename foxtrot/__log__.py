067c1fe54214a697f2009e94f1e3c8a36f44d4a3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-15 17:20:38 -0300

    Saved foxtrot/__log__.py @2020-07-15 20:20

--- foxtrot/__log__.py
+++ foxtrot/__log__.py
@@ -28,3 +28,19 @@ Exception: <TypeError: $B.imported['_spy.vitollino.main.'] is undefined>
 'error': '''
 Exception: <TypeError: $B.imported['__spy.vitollino.main.'] is undefined>
 '''},
+{'date': 'Wed Jul 15 2020 17:20:37.759 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 14
+    STYLE["width"] = 600
+AttributeError: 'module' object has no attribute '__setitem__'
+'''},
