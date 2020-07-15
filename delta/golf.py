4e41110725ef2d3011ae60b6e2f3956c7c899e9b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-15 06:36:39 -0300

    Saved delta/golf.py @2020-07-15 09:36

--- delta/golf.py
+++ delta/golf.py
@@ -45,7 +45,7 @@ class Card():
         #if self.faceDown:
         #    self.card =Elemento(IMG_CARD_FACE_DOWN, x=pos_x, y=pos_y, width=IMG_WIDTH, height=IMG_HEIGHT, cena=cena)
         #else: 
-        self.card = Elemento(self.image, x=pos_x, y=pos_y, width=IMG_WIDTH, height=IMG_HEIGHT, cena=self.cena)
+        self.card.img(self.image)
         
         # para teste
         self.nomeTexto = Texto(self.card, "texto")
