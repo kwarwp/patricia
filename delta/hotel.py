a75eb49f518bc3e6099101a0d4d7d326051dc6b0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-14 21:03:27 -0300

    Saved delta/hotel.py @2020-07-15 00:03

--- delta/hotel.py
+++ delta/hotel.py
@@ -13,23 +13,48 @@ Changelog
 
 import random
 from _spy.vitollino.main import Cena, Elemento
-from browser import document 
+from browser import document # importa o DOM para atribuir o evento de teclado
 
-class evento:
-	def linkar (self):
+class Eventos:
+    """ Associa um evento a uma imagem e captura eventos de teclado. """
+    CENA_TABULEIRO = "http://activufrj.nce.ufrj.br/file/ProgOO/TAB_VAZIO.png?disp=inline"
+    CARTA_VERSO= "http://activufrj.nce.ufrj.br/file/ProgOO/Card_verso.png?disp=inline"
+    CARTA_PYCHARM = "http://activufrj.nce.ufrj.br/file/ProgOO/Card_pycharm.png?disp=inline"
+    
+    
+    def __init__(self):
+        self.tabuleiro = Cena(self.CENA_TABULEIRO)
+        self.pycharm = Elemento(self.CARTA_PYCHARM, , x=100, y=200, cena=self.tabuleiro)
+        
+       # self.pycharm=Elemento(self.CARTA_PYCHARM,,X=100,y=300,cena=self.tabuleiro)
+        
+        
+        
+        
+    def vai(self):
+        """ mostra a cena da calçada. """
+        self.tabuleiro.vai()
+        
+    def linkar (self):
 		self.Carta1="http://activufrj.nce.ufrj.br/file/ProgOO/Card_verso.png?disp=inline"
 
-	def embaralhar (self):
+    def embaralhar (self):
 
 		s=['Carta1','Carta2','Carta3','Carta4']
 
-		random.shuffle(s,random.random)
-      	self.s=s
+		self.s=random.shuffle(s,random.random)
+      	
    
    
-	def mostrar_carta(self):
+    def mostrar_carta(self):
 		self.s.vai()
-def __name__=="__main__":
-	self.eventos.vai()
+        
+    
+        
+if __name__ == "__main__":
+    Eventos().vai()
+
+
+
     
 
