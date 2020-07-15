1a2576ab6dbcdf3e71ee884738ed9888786dbe15
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-15 08:23:06 -0300

    Saved delta/__log__.py @2020-07-15 11:23

--- delta/__log__.py
+++ delta/__log__.py
@@ -2766,3 +2766,10 @@ NameError: name 'Texto' is not defined
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Wed Jul 15 2020 08:22:56.42 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 105
+  def shuffle_cards(self):   
+  ^
+IndentationError: unexpected indent
+'''},
