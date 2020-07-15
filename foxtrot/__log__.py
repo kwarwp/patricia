eab477a57b2b1b6346e92155adde0b99467e46be
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-15 17:18:27 -0300

    Created foxtrot/__log__.py @2020-07-15 20:18

---
+++ foxtrot/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Wed Jul 15 2020 17:18:24.733 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 20
+  class Jogo
+             ^
+SyntaxError: invalid syntax
+'''},
