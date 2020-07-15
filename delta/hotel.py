6b994159c5b29c8ba6d6d9378bf4c2097d440a45
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-15 01:45:08 -0300

    Saved delta/hotel.py @2020-07-15 04:45

--- delta/hotel.py
+++ delta/hotel.py
@@ -13,57 +13,11 @@ Changelog
 
 import random
 from _spy.vitollino.main import Cena, Elemento
-from browser import document # importa o DOM para atribuir o evento de teclado
-
-class Eventos:
-	""" Associa um evento a uma imagem e captura eventos de teclado. """
-	CENA_TABULEIRO = "http://activufrj.nce.ufrj.br/file/ProgOO/TAB_VAZIO.png?disp=inline"
-	CARTA_VERSO= "http://activufrj.nce.ufrj.br/file/ProgOO/Card_verso.png?disp=inline"
-	#= "http://activufrj.nce.ufrj.br/file/ProgOO/Card_verso.png?disp=inline"
-	CARTA_PYCHARM = "http://activufrj.nce.ufrj.br/file/ProgOO/Card_pycharm.png?disp=inline"
-	
-	carta=[1,2,3,4]
-    
-	
-	
-		
-	
-	    
-	def __init__(self):
-		self.tabuleiro = Cena(self.CENA_TABULEIRO)
-		  
-		carta[0]= Elemento(self.CARTA_PYCHARM, ,x=100,y=300 , cena=self.tabuleiro)
-		self.carta[1]= Elemento(self.CARTA_VERSO, , x=200, y=300, cena=self.tabuleiro)
-		
-           
-        
-       # self.pycharm=Elemento(self.CARTA_PYCHARM,,X=100,y=300,cena=self.tabuleiro)
-	#def embaralhar(self):
-		
-		random.shuffle(self.carta,random.random)
-		   
-	
-        
-	def vai(self):
-		""" mostra a cena da calçada. """
-		self.tabuleiro.vai()
-		
-        
-        	 
-    	 
-           
-
-    
-   
-	#def mostrar_carta(self):
-		#self.s.vai()
-        
-    
-        
-if __name__ == "__main__":
-    Eventos().vai()
 
 
-
-    
-
+""" Imagens do Jogo da Memória: verso, pycharm, linux
+"""
+def cartas():
+	carta[0]=Cena(img="http://activufrj.nce.ufrj.br/file/ProgOO/Card_pycharm.png?disp=inline")
+	carta[1]=Cena(img="http://activufrj.nce.ufrj.br/file/ProgOO/Card_Linux.png?disp=inline")
+	carta[2]=Cena(img="http://activufrj.nce.ufrj.br/file/ProgOO/Card_verso.png?disp=inline")
