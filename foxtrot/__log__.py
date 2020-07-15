5c6a4422b948ed5ad74e593904e7eb8114002405
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-15 19:32:20 -0300

    Saved foxtrot/__log__.py @2020-07-15 22:32

--- foxtrot/__log__.py
+++ foxtrot/__log__.py
@@ -1130,3 +1130,23 @@ class Sobre:
     self.botao_sobre.vai=self.Sobre()
 AttributeError: 'Jogo' object has no attribute 'Sobre'
 '''},
+{'date': 'Wed Jul 15 2020 19:32:19.854 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 56
+    class Sobre(self):
+  module <module> line 60
+    self.cena_sobre.esquerda=Jogo()
+  module <module> line 54
+    self.botao_sobre.vai=self.Sobre
+AttributeError: 'Jogo' object has no attribute 'Sobre'
+'''},
