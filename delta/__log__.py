5092d87af266dd1fbc0d0c3396e0d09a92ddd7b2
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-15 02:57:57 -0300

    Saved delta/__log__.py @2020-07-15 05:57

--- delta/__log__.py
+++ delta/__log__.py
@@ -1800,3 +1800,21 @@ AttributeError: 'GUI' object has no attribute 'muda'
     	document.bind("keydown",self.muda)
 AttributeError: 'GUI' object has no attribute 'muda'
 '''},
+{'date': 'Wed Jul 15 2020 02:57:55.707 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 44
+    embaralha()
+  module <module> line 37
+    			carta[i].vai()
+TypeError: list indices must be integer, not Cena
+'''},
