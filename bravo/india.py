0bd8ef3abbc9053c077833fcfb64fe710c2d1e2d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-15 16:27:21 -0300

    Saved bravo/india.py @2020-07-15 19:27

--- bravo/india.py
+++ bravo/india.py
@@ -18,24 +18,7 @@ from _spy.vitollino.main import Cena, Elemento, STYLE, Texto
 from browser import html, alert, timer
 from random import choice, shuffle
 STYLE["width"] , STYLE["height"] = 1200, "600px"
-
-class Kamui:
-    def __init__(self):
-        self.pulo = -10
-        self.issun_top = 380
-        _kamui = fundo#"https://i.imgur.com/R3pCA3a.jpg"
-        _okami = "https://i.imgur.com/1jSbzEj.png"
-        _issun = "https://i.imgur.com/lAYKq3F.png"
-        self.kamui = Cena(_kamui)
-        self.kamui.vai()
-        def pula_issun(_=0, issun_=self.issun.elt.style):
-            issun_.top = self.issun_top + self.pulo
-            self.pulo = -self.pulo
-        self.pula_issun = timer.set_interval(pula_issun, 300)
-        def para_issun(_=0):
-            Texto(self.kamui, tit="Issun - O artista intinerante", txt="Essa é a história da deusa Amaterasu").vai()
-            timer.clear_interval(self.pula_issun)
-        self.issun.vai = para_issun
-
-if __name__ == "__main__":
-    _ = Kamui()
+def teste():
+	cena = Cena(img = fundo)
+	cena.vai()
+teste()    
