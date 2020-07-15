c9fe19645f318f3698c4d5d19791d82374881eaf
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-14 21:25:17 -0300

    Saved delta/__log__.py @2020-07-15 00:25

--- delta/__log__.py
+++ delta/__log__.py
@@ -471,3 +471,10 @@ IndentationError: unexpected indent
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Tue Jul 14 2020 21:25:07.479 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 57
+  self.card2a = Card(IMG_CARD_2, [2,0], self.cena, self.faceDown=False)
+                                                                ^
+SyntaxError: keyword can't be an expression
+'''},
