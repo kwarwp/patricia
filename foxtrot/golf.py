2cb50c9eff798cd6756dac1bc4efa61b89350a6c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-15 17:22:06 -0300

    Created foxtrot/golf.py @2020-07-15 20:22

---
+++ foxtrot/golf.py
@@ -0,0 +1,28 @@
+# patricia.foxtrot.golf.py
+# SPDX-License-Identifier: GPL-3.0-or-later
+""" Projeto SuperPython World,
+
+.. codeauthor:: Raquel Fernandes <raquelmachado4993@gmail.com>
+
+Changelog
+---------
+.. versionadded::    20.07
+        Página inicial do Jogo.
+
+"""
+from __spy.vitollino.main import Cena,Elemento,Texto
+
+capa_do_jogo = "https://i.imgur.com/0RVnppj.png"
+botao_jogar = "https://i.imgur.com/F3Q0bDv.png"
+botao_sobre = "https://i.imgur.com/pG9wDIz.png"
+
+class Jogo:
+    def ___init___(self):
+        self.capa = Cena (img= capa_do_jogo)
+        self.botao_jogar = Elemento (img = botao_jogar)
+        self.botao_sobre = Elemento (img = botao_sobre)
+
+        self.botao_sobre.entra(self.capa)
+        self.botao_jogar.entra(self.capa)
+
+
