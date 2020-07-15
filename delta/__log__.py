acb5dff7210b4fd3b4258e30351c10db21ddc412
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-15 03:31:19 -0300

    Saved delta/__log__.py @2020-07-15 06:31

--- delta/__log__.py
+++ delta/__log__.py
@@ -1992,3 +1992,21 @@ AttributeError: 'GUI' object has no attribute 'FUNDO'
     	self.fundo=Cena(self.FUN_DO)
 AttributeError: 'GUI' object has no attribute 'FUN_DO'
 '''},
+{'date': 'Wed Jul 15 2020 03:31:18.481 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 33
+    embaralha()
+  module <module> line 30
+    	self.carta[2].vai()
+AttributeError: 'GUI' object has no attribute 'carta'
+'''},
