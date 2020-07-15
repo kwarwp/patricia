4ce8cbfc3bb6bca81c2e84a9b173420e3112cc76
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-15 05:05:03 -0300

    Saved delta/__log__.py @2020-07-15 08:05

--- delta/__log__.py
+++ delta/__log__.py
@@ -2679,3 +2679,10 @@ SyntaxError: invalid syntax
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Wed Jul 15 2020 05:05:01.946 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 46
+  self.verso=False
+  ^
+IndentationError: unexpected indent
+'''},
