b3578595d9afe5f303ddd1cdeac26d1524a8582d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-15 02:33:50 -0300

    Saved delta/__log__.py @2020-07-15 05:33

--- delta/__log__.py
+++ delta/__log__.py
@@ -1588,3 +1588,21 @@ NameError: name 'sleep' is not defined
     raise NotImplementedError("Blocking functions like time.sleep() are not "
 NotImplementedError: Blocking functions like time.sleep() are not supported in the browser. Use functions in module browser.timer instead.
 '''},
+{'date': 'Wed Jul 15 2020 02:33:48.844 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 43
+    embaralha()
+  module <module> line 36
+    		carta[i+1].vai()
+IndexError: list index out of range
+'''},
