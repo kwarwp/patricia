3dd769484a54b05d1a21255d92519c89fd3265cc
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-16 12:19:23 -0300

    Saved sarah/main.py @2020-07-16 15:19

--- sarah/main.py
+++ sarah/main.py
@@ -1,32 +1,39 @@
 # patricia.sarah.main.py
+# SPDX-License-Identifier: GPL-3.0-or-later
 
-""" Jogo Olimpíadas de Matemática20.07
-        * NEW: O jogo original
+""" Jogo do Mistério da Praia.
+
+    Descubra o mistério desta praia.
+
+Changelog
+---------
+    20.07
+        * NEW: O jogo original.
+
 """
-
 from _spy.vitollino.main import Cena, Elemento, STYLE
 from grace.main import Praia
 __version__ = "20.07"
-__author__ = "Renato"
+__author__ = "Carlo"
 STYLE["width"] = 500
-TABULEIRO = "http://www.infcross.com.br/mestrado/tabuleiro.jpg"
-BOTAO = "http://www.infcross.com.br/mestrado/botao.jpg"
+CENA_CALCADA = "https://i.imgur.com/zOxshRh.jpg"
+BANHISTA = "https://i.imgur.com/CWQ00XG.png"
 
 
-class Jogo:
+class Calcada:
     """ Representa uma cena da calçada da praia """
     def __init__(self):
-        """ Mostra o tabuleiro """
-        self.cena = Cena(TABULEIRO, direita=Praia())
-        self.banhista = Elemento(botap, x=100, y=200, cena=self.cena)
+        """ Mostra a cena da praia """
+        self.cena = Cena(CENA_CALCADA, direita=Praia())
+        self.banhista = Elemento(BANHISTA, x=100, y=200, cena=self.cena)
         #Cena(CENA_CALCADA).vai()
     def vai(self):
-        """ Mostra o tabuleiro """
+        """ Mostra a cena da praia """
         self.cena.vai()
         #Cena(CENA_CALCADA).vai()
     
 if __name__ == "__main__":
-    Jogo().vai()
+    Calcada().vai()
     
     
     
