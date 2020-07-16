23a92f01bbfcb7b41d22b2cecf738654ac14bfbc
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-16 14:20:17 -0300

    Saved delta/__log__.py @2020-07-16 17:20

--- delta/__log__.py
+++ delta/__log__.py
@@ -3709,3 +3709,21 @@ SyntaxError: keyword can't be an expression
     Game.vai()
 AttributeError: 'module' object has no attribute 'vai'
 '''},
+{'date': 'Thu Jul 16 2020 14:20:16.6 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 45
+    start().vai()
+  module <module> line 34
+    self.inicio = Cena( Card.vai())
+AttributeError: 'module' object has no attribute 'vai'
+'''},
