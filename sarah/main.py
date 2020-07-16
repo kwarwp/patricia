6a3bb86f3f32db4b2324e34d9c66c8637c473284
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-16 12:04:38 -0300

    Saved sarah/main.py @2020-07-16 15:04

--- sarah/main.py
+++ sarah/main.py
@@ -1,14 +1,28 @@
 # patricia.sarah.main.py
-_autor_ = "Renato Silva"
-
+# patricia.echo.main.py
 """ Jogo Olimpíadas de Matemática20.07
         * NEW: O jogo original
+        Autor: Grupo Echo (Aline, Lorena, Renato e Victoria)
 """
 from _spy.vitollino.main import Cena, Elemento, Labirinto, Texto, Codigo, Sala, STYLE
 from _spy.vittolino.main import INVENTARIO as inv
+
 """ tabuleiro = Cena (img ="https://www.ibilce.unesp.br/Home/Departamentos/Matematica/2cejta/avancando-com-o-resto.png") """
-STYLE["width"] = 1250
+
+STYLE["width"] = 1150
 STYLE["height"] = "550px"
+
+tabuleiro ="http://www.infcross.com.br/mestrado/tabuleiro.jpg"
+botao = "https://imgur.com/oC9lAgW.jpg"
 img ="http://www.infcross.com.br/mestrado/tabuleiro.jpg"
-tabuleiro = Cena (img)
-tabuleiro.vai()
+#tabuleiro = Cena (img)
+#tabuleiro.vai()
+
+
+def cena_principal():
+    inicio = Cena(img=tabuleiro)
+    inicio_e = Elemento(img=botao)
+    inicio.vai()
+    inicio_e.vai()
+    
+cena_principal() 
