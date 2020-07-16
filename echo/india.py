d11d4eb1455a06a8301ffe0a46ddd2f2fa09a25d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-15 23:11:57 -0300

    Saved echo/india.py @2020-07-16 02:11

--- echo/india.py
+++ echo/india.py
@@ -15,6 +15,20 @@ from _spy.vitollino.main import Cena, INVENTARIO, STYLE
 from random import choice
 
 STYLE["width"] = 1150
-STYLE["height"] = 550px
+STYLE["height"] = 550
 
 TABULEIRO = "http://www.infcross.com.br/mestrado/tabuleiro.jpg"
+BOTAO = "https://imgur.com/oC9lAgW"
+
+
+"""def cena_principal():
+    inicio = Cena(img=tabuleiro)
+    inicio_e = Elemento (img = botao, tit="Gravidade", style = dict(left= 70,top=170, width=1150, height=550,bottom=100))
+    lado1 = Cena(img=tabuleiro, direita =inicio)
+
+    
+    inicio_e.entra(inicio)
+
+    inicio.vai()
+    
+cena_principal()"""
