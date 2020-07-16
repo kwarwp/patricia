082071045d683cd8afb9f0ec80639c4eaa9141b7
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-16 11:05:07 -0300

    Saved amanda/main.py @2020-07-16 14:05

--- amanda/main.py
+++ amanda/main.py
@@ -7,6 +7,8 @@ SEGUNDA TELA
 """
 from _spy.vitollino.main import Cena, Elemento, Labirinto, Texto, Codigo, Sala, STYLE
 from _spy.vittolino.main import INVENTARIO as inv
+from kelle.main import cena
+#from alexa.main import oi
 
 STYLE["width"] = 1250
 STYLE["leight"]= "550px"
@@ -17,7 +19,7 @@ class tela:
 	siga2 = Elemento (img="https://i.imgur.com/kRmXNkz.png", style=dict(left=850, top=380, width=150, height="150px"))
 	siga.entra(fundo)
 	siga2.entra(fundo)
-	#siga.vai=fundo.vai
+	siga.vai=cena().vai
 	#siga2.vai=fundo.vai
 	fundo.vai()
 
