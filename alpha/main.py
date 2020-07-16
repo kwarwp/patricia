a65300f9b8dd75d45fcca08fae75eb7439b43261
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-16 12:03:18 -0300

    Saved alpha/main.py @2020-07-16 15:03

--- alpha/main.py
+++ alpha/main.py
@@ -15,7 +15,7 @@ Código alterado de Monica Novellino <monicanovellino@gmail.com>
 .. versionadded::    20.07
         Adicionei 5 imagens iniciais do labirinto e alterei o pacman (podem criar outro peronagem)
         Contador adicionado para gerar as seguintes fases e posiçoes iniciais em função da linha da matriz
-        Tirei a matriz do início não estava lendo a posição
+        Tirei a matriz do início pq não estava lendo a posição
         
 
 """
