290378ab1f97763fb8fb1534513449a56c55d0c9
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-16 11:45:43 -0300

    Saved foxtrot/__log__.py @2020-07-16 14:45

--- foxtrot/__log__.py
+++ foxtrot/__log__.py
@@ -1775,3 +1775,34 @@ AttributeError: 'bool' object has no attribute 'vai'
     self.texto_castelo = Texto (self.castelo, "Parado aí! Identifique-se!")
 AttributeError: 'Jogo' object has no attribute 'castelo'
 '''},
+{'date': 'Thu Jul 16 2020 11:45:42.306 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 14
+    from _spy.vitollino.main import Cena,Elemento,Texto, ST
+ImportError: cannot import name 'ST'
+
+ImportError
+Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 264
+    action()
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 14
+    from _spy.vitollino.main import Cena,Elemento,Texto, ST
+'''},
