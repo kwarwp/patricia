71cc5a00e85714a6b6267efd344cf842cb85c50a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-16 13:45:26 -0300

    Saved amanda/__log__.py @2020-07-16 16:45

--- amanda/__log__.py
+++ amanda/__log__.py
@@ -283,3 +283,21 @@ TypeError: 'module' object is not callable
     	siga.vai= cena().vai
 TypeError: 'module' object is not callable
 '''},
+{'date': 'Thu Jul 16 2020 13:45:26.431 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 16
+    class tela:
+  module <module> line 20
+    	siga.entra(fundo, direita= cena() )
+TypeError: 'module' object is not callable
+'''},
