2fc64ebce0dda8e8105cddef928c9a3cdd1d395d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-16 10:42:08 -0300

    Saved foxtrot/__log__.py @2020-07-16 13:42

--- foxtrot/__log__.py
+++ foxtrot/__log__.py
@@ -1297,3 +1297,21 @@ AttributeError: 'Jogo' object has no attribute 'texto_sobre'
   ^
 IndentationError: expected an indented block
 '''},
+{'date': 'Thu Jul 16 2020 10:42:07.122 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 88
+    Jogo()
+  module <module> line 77
+    self.entradadocastelo.vai = Cena (img=portadocastelo)
+AttributeError: 'Jogo' object has no attribute 'entradadocastelo'
+'''},
