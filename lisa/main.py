e85148d4b6cf4b673fb449db369ced05faa054b7
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-16 13:08:43 -0300

    Saved lisa/main.py @2020-07-16 16:08

--- lisa/main.py
+++ lisa/main.py
@@ -2,11 +2,39 @@
 __author__ = "Victória Regina Caruzo"
 __version__ = "09.07.20"
 
-from _spy.vitollino.main import Cena
+"""from _spy.vitollino.main import Cena
 from adda.main import CENA_CALCADA
 from grace.main import CENA_PRAIA
 
 def vai(self):
     Cena(CENA_CALCADA, direita=Praia()).vai()
-    Cena(CENA_CALCADA).vai()
+    Cena(CENA_CALCADA).vai()"""
+    
+from _spy.vitollino.main import Cena, Elemento, STYLE
+""" from grace.main import Praia """
+
+__version__ = "1.0"
+__author__ = "ECHO"
+STYLE["width"] = 1345
+STYLE["height"] = "620px"
+TABULEIRO = "http://www.infcross.com.br/mestrado/tabuleiro.jpg"
+BOTAO = "http://www.infcross.com.br/mestrado/botao.jpg"
+
+class Tabuleiro:
+    """ Representa uma cena da calçada da praia """
+    def vai(self):
+        """ Mostra a cena do tabuleiro """
+        Cena(TABULEIRO).vai()
+
+class Jogo:
+    """ Representa uma cena de tabuleiro com o botão """
+    def __init__(self):
+        """ Mostra o tabuleiro com o botão start"""
+        self.cena = Cena(TABULEIRO, direita=Tabuleiro())
+        self.start = Elemento(BOTAO, x=50, y=50, cena=self.cena)
+        #Cena(CENA_JOGO).vai()
+    def vai(self):
+        """ Mostra o tabuleiro """
+        self.cena.vai()
+        #Cena(CENA_JOGO).vai()
 
