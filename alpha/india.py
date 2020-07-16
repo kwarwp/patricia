55f5228a37acddb3c516c81c3f1051fc35a13473
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-16 11:26:52 -0300

    Saved alpha/india.py @2020-07-16 14:26

--- alpha/india.py
+++ alpha/india.py
@@ -81,8 +81,8 @@ class Eventos:
 #            global matrizFase
             self.ambiente = Cena(self.matrizFase[cont][0]) #lê a cena que está descrita na primeira coluna da matriz
             STYLE["width"] = 640
-            self.x1 = matrizFase[0][1]
-            self.y1 = matrizFase[0][2]
+            self.x1 = 100#matrizFase[0][1]
+            self.y1 = 100#matrizFase[0][2]
             self.boneco = Elemento(self.BONECO, x=self.x1, y=self.y1, cena=self.ambiente)
             self.boneco.x = self.x1#100#int(matrizFase[cont][1]) #posição x_inicial da fase, descrita na matriz pela segunda coluna
             self.boneco.y = self.y1#100#int(matrizFase[cont][2]) #posição y_inicial da fase descita pela terceira coluna
