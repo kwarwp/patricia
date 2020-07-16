df0d6dfd491f4c01d56c683d53058db0ce5e1de0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-16 11:02:00 -0300

    Saved foxtrot/__log__.py @2020-07-16 14:01

--- foxtrot/__log__.py
+++ foxtrot/__log__.py
@@ -1649,3 +1649,21 @@ AttributeError: 'Jogo' object has no attribute 'texto_castelo'
     self.castelo.vai = self.texto_castelo.vai
 AttributeError: 'Jogo' object has no attribute 'texto_castelo'
 '''},
+{'date': 'Thu Jul 16 2020 11:01:59.182 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 94
+    Jogo()
+  module <module> line 68
+    self.castelo.vai = self.texto_castelo.vai
+AttributeError: 'Jogo' object has no attribute 'texto_castelo'
+'''},
