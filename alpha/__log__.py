0ed4a188a0db31a23ca69401565234beeec2fbfc
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-16 10:11:55 -0300

    Saved alpha/__log__.py @2020-07-16 13:11

--- alpha/__log__.py
+++ alpha/__log__.py
@@ -423,3 +423,19 @@ NameError: name 'matrizFase' is not defined
     matrizFase = [[link2,60,260],     #matrizFase = [[local_imagem_fase, x_inicial, y_inicial]...]
 NameError: name 'link2' is not defined
 '''},
+{'date': 'Thu Jul 16 2020 10:11:53.628 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 30
+    matrizFase = [[link2,60,260],     #matrizFase = [[local_imagem_fase, x_inicial, y_inicial]...]
+NameError: name 'link2' is not defined
+'''},
