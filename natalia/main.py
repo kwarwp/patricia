c22f3ac31778f8bbb044422f534a854850ef8527
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-16 13:12:49 -0300

    Saved natalia/main.py @2020-07-16 16:12

--- natalia/main.py
+++ natalia/main.py
@@ -1,14 +1,34 @@
 # patricia.echo.main.py
 """ Jogo Olimpíadas de Matemática20.07
         * NEW: O jogo original
-        Autor: Aline Moisinho
+        Autor: Grupo Echo (Aline, Lorena, Renato e Victória)
 """
-from _spy.vitollino.main import Cena, Elemento, Labirinto, Texto, Codigo, Sala, STYLE
-from _spy.vittolino.main import INVENTARIO as inv
 
-""" tabuleiro = Cena (img ="https://www.ibilce.unesp.br/Home/Departamentos/Matematica/2cejta/avancando-com-o-resto.png") """
-STYLE["width"] = 1150
-STYLE["height"] = "550px"
-img ="http://www.infcross.com.br/mestrado/tabuleiro.jpg"
-tabuleiro = Cena (img)
-tabuleiro.vai()
+from _spy.vitollino.main import Cena, Elemento, STYLE
+""" from grace.main import Praia """
+
+__version__ = "1.0"
+__author__ = "ECHO"
+STYLE["width"] = 1345
+STYLE["height"] = "620px"
+TABULEIRO = "http://www.infcross.com.br/mestrado/tabuleiro.jpg"
+BOTAO = "http://www.infcross.com.br/mestrado/botao.jpg"
+
+class Tabuleiro:
+    """ Representa uma cena da calçada da praia """
+    def vai(self):
+        """ Mostra a cena do tabuleiro """
+        Cena(TABULEIRO).vai()
+
+class Jogo:
+    """ Representa uma cena de tabuleiro com o botão """
+    def __init__(self):
+        """ Mostra o tabuleiro com o botão start"""
+        self.cena = Cena(TABULEIRO, direita=Tabuleiro())
+        self.start = Elemento(BOTAO, x=50, y=50, cena=self.cena)
+        #Cena(CENA_JOGO).vai()
+    def vai(self):
+        """ Mostra o tabuleiro """
+        self.cena.vai()
+        #Cena(CENA_JOGO).vai()
+    
