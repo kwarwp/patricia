b230338afa9195f218abebe75c061e70408494f3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-16 01:20:18 -0300

    Saved soraya/main.py @2020-07-16 04:20

--- soraya/main.py
+++ soraya/main.py
@@ -1,4 +1,8 @@
 # patricia.soraya.main.py
+from _spy.vitollino.main import Cena, Elemento, INVENTARIO, STYLE, Musica
+from texto.main import Texto
 
+CENAINICIO = "https://i.imgur.com/mbt7XHq.png"
+PLAY = "https://i.imgur.com/QiiOf5O.png"
 
 
