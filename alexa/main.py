9311e518ed3de1b271cf2a9ab02b2251af641f57
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-15 23:02:23 -0300

    Saved alexa/main.py @2020-07-16 02:02

--- alexa/main.py
+++ alexa/main.py
@@ -6,13 +6,13 @@ STYLE["height"] = "550px"
 
 class oi:
     #primeira cena (onde está localizado as duas opções
-    cena = Cena( img = "https://i.imgur.com/POhwRe1.jpg")
+    cena = Cena( img = "https://i.imgur.com/S5tAUe8.png")
     #opção 1 
-    op1 = Elemento ( img = "https://images.vexels.com/media/users/3/139740/isolated/preview/bfecbaa063a84b2e9bbd9f8b9b41d410-bot--o-de-reprodu----o-redondo-azul-by-vexels.png" , x = 20, y=290)
+    op1 = Elemento ( img = "https://images.vexels.com/media/users/3/139740/isolated/preview/bfecbaa063a84b2e9bbd9f8b9b41d410-bot--o-de-reprodu----o-redondo-azul-by-vexels.png" , x = 700, y=390, tit= "Clique")
     #cena que direciona a opção 1
     cena2 = Cena ( img = "https://i.imgur.com/wdwjGCt.jpg")
     #opção 2
-    op2 = Elemento ( img = "https://images.vexels.com/media/users/3/139740/isolated/preview/bfecbaa063a84b2e9bbd9f8b9b41d410-bot--o-de-reprodu----o-redondo-azul-by-vexels.png")
+    op2 = Elemento ( img = "https://images.vexels.com/media/users/3/139740/isolated/preview/bfecbaa063a84b2e9bbd9f8b9b41d410-bot--o-de-reprodu----o-redondo-azul-by-vexels.png", x = 350, y=390, tit= "Clique")
     #cena que direciona a opção2
     cena3 = Cena ( img = "https://i.imgur.com/nsi5Cwh.jpg")
     #elemento que está na cena 3 que foi direcionado pela op2, mas irá direcionar p cena2
