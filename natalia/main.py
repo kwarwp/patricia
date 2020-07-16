e63984927fdfd9538ec4c22d7a727e4cae94d65d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-15 23:06:36 -0300

    Saved natalia/main.py @2020-07-16 02:06

--- natalia/main.py
+++ natalia/main.py
@@ -1,41 +1,21 @@
-# patricia.natalia.main.py
+# patricia.tracy.main.py
+_autor_ = "Aline_Moisinho"
 
-""" Jogo do Mistério da Praia.
-
-    Agora você desceu para a praia.
-
-Changelog
----------
-    20.07
-        * NEW: O jogo original.
-
-"""
-from _spy.vitollino.main import Cena
-__version__ = "15.07"
-__author__ = "Rosilane"
-CENA_PRAIA = "https://i.imgur.com/wz5XKcr.jpg"
-
-
-class Mar:
-    """ Representa uma cena da calçada da praia """
-    def vai(self):
-        """ Mostra a cena da praia """
-        #Cena(CENA_PRAIA).vai()
-    
-
-if __name__ == "__main__":
-    Mar().vai()
-"""
-# patricia.echo.main.py
-""" Jogo Olimpíadas de Matemática20.07
-        * NEW: O jogo original
-"""
-from _spy.vitollino.main import Cena, Elemento, Labirinto, Texto, Codigo, Sala, STYLE
+from _spy.vitollino.main import Cena, Elemento, Texto
 from _spy.vittolino.main import INVENTARIO as inv
-""" tabuleiro = Cena (img ="https://www.ibilce.unesp.br/Home/Departamentos/Matematica/2cejta/avancando-com-o-resto.png") """
-STYLE["width"] = 1150
-STYLE["height"] = "550px"
-img ="http://www.infcross.com.br/mestrado/tabuleiro.jpg"
-tabuleiro = Cena (img)
-tabuleiro.vai()
-"""
+
+tabuleiro ="http://www.infcross.com.br/mestrado/tabuleiro.jpg"
+botao = "https://imgur.com/oC9lAgW"
+
+   
+def cena_principal():
+    inicio = Cena(img=tabuleiro)
+    inicio_e = Elemento (img = botao, tit="Gravidade", style = dict(left= 70,top=170, width=1150, height=550,bottom=100))
+    lado1 = Cena(img=tabuleiro, direita =inicio)
+
+    
+    inicio_e.entra(inicio)
+
+    inicio.vai()
+    
+cena_principal()
