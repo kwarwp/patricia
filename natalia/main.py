c5298638ac9b745d62c938210b1a461bb53d6b98
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-15 23:48:46 -0300

    Saved natalia/main.py @2020-07-16 02:48

--- natalia/main.py
+++ natalia/main.py
@@ -1,17 +1,14 @@
-# patricia.tracy.main.py
-autor = "Lorena Pires Griõn"
-
-from _spy.vitollino.main import Cena, Elemento, Texto
+# patricia.echo.main.py
+""" Jogo Olimpíadas de Matemática20.07
+        * NEW: O jogo original
+        Autor: Grupo Echo (Aline, Lorena, Renato e Victoria)
+"""
+from _spy.vitollino.main import Cena, Elemento, Labirinto, Texto, Codigo, Sala, STYLE
 from _spy.vittolino.main import INVENTARIO as inv
 
-tabuleiro ="http://www.infcross.com.br/mestrado/tabuleiro.jpg"
-botao = "https://imgur.com/oC9lAgW.png"
-
-   
-def cena_principal():
-    inicio = Cena(img=tabuleiro)
-    inicio_e = Elemento(img=botao)
-    inicio_e.entra(inicio)
-    inicio.vai()
-    
-cena_principal()
+""" tabuleiro = Cena (img ="https://www.ibilce.unesp.br/Home/Departamentos/Matematica/2cejta/avancando-com-o-resto.png") """
+STYLE["width"] = 1150
+STYLE["height"] = "550px"
+img ="http://www.infcross.com.br/mestrado/tabuleiro.jpg"
+tabuleiro = Cena (img)
+tabuleiro.vai()
