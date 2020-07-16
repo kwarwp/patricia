9544a8ea30b0d3d5689fb86e177bb78082b72fa4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-16 13:47:05 -0300

    Saved alpha/main.py @2020-07-16 16:47

--- alpha/main.py
+++ alpha/main.py
@@ -81,7 +81,7 @@ class Eventos:
         
         if self.boneco.x > 400 and self.boneco.y > 200:
             global cont #contador estanciado fora do def para gerar a linha a ser lida na lista/MAtriz
-            self.ambiente = Cena(self.listaFase[cont]) #lê a cena que está descrita na primeira coluna da matriz
+            self.ambiente = Cena(self.listaFase[cont]) #lê a cena que está descrita na primeiro ítem da lista
             STYLE["width"] = 640
             self.x2 = int(matrizPosicaoInicial[cont][0]) #posição x_inicial da fase, descrita na matriz pela segunda coluna
             self.y2 = int(matrizPosicaoInicial[cont][1]) #posição y_inicial da fase descita pela terceira coluna
