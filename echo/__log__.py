0159bf4f461b96082f3f1b5619734de883ce5c96
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-15 22:15:52 -0300

    Saved echo/__log__.py @2020-07-16 01:15

--- echo/__log__.py
+++ echo/__log__.py
@@ -500,3 +500,10 @@ SyntaxError: EOL while scanning string literal
                       ^
 SyntaxError: EOL while scanning string literal
 '''},
+{'date': 'Wed Jul 15 2020 22:15:51.421 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 8
+  STYLE ["width"]"1150"
+                  ^
+SyntaxError: invalid syntax
+'''},
