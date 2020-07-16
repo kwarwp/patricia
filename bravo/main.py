237694c7cde9f526865537ff548a4150a277c2b1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-16 10:08:30 -0300

    Saved bravo/main.py @2020-07-16 13:08

--- bravo/main.py
+++ bravo/main.py
@@ -1,9 +1,9 @@
 # patricia.bravo.main.py
-# patricia.soraya.main.py
+
 __autor__ = "Isabel Hortencia Garnica"
 __autor__ = "Gabriela"
 __autor__ = "Pedro"
-__autor__ = "Rosilene"
+__autor__ = "Rosilane Lessa da Fonte"
 __version__ = "16.07.2020"
 
 from _spy.vitollino.main import Cena, Elemento, INVENTARIO, STYLE, Musica
