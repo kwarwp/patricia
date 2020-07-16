a87b12a39a1c0b3adcd2bf4156fe2e6714cfd8bc
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-16 09:56:49 -0300

    Saved alpha/__log__.py @2020-07-16 12:56

--- alpha/__log__.py
+++ alpha/__log__.py
@@ -310,3 +310,21 @@ SyntaxError: invalid syntax
                                                                                     ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Thu Jul 16 2020 09:56:46.844 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 96
+    Eventos().vai()
+  module <module> line 53
+    self.boneco = Elemento(self.BONECO, x=self.x1, y=self.y1, cena=self.ambiente)
+AttributeError: 'Eventos' object has no attribute 'y1'
+'''},
