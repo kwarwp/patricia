1ad61d6ecfecfa92d6100da3c0481fe61f1042b3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-15 22:59:59 -0300

    Saved echo/__log__.py @2020-07-16 01:59

--- echo/__log__.py
+++ echo/__log__.py
@@ -661,3 +661,19 @@ NameError: name 'leight' is not defined
     STYLE [width] = 800 
 NameError: name 'width' is not defined
 '''},
+{'date': 'Wed Jul 15 2020 23:00:26.733 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 15
+    STYLE[width] * 800 
+NameError: name 'width' is not defined
+'''},
