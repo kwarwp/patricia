e5b18929a6103ce153f77b5aca07f52d7497864c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-16 12:40:23 -0300

    Saved sarah/main.py @2020-07-16 15:40

--- sarah/main.py
+++ sarah/main.py
@@ -23,7 +23,7 @@ class Jogo:
     """ Representa uma cena de tabuleiro com o botão """
     def __init__(self):
         """ Mostra o tabuleiro com o botão start"""
-        self.cena = Cena(TABULEIRO)
+        self.cena = Cena(TABULEIRO, direita=Tabuleiro())
         self.banhista = Elemento(BOTAO, x=50, y=50, cena=self.cena)
         #Cena(CENA_CALCADA).vai()
     def vai(self):
