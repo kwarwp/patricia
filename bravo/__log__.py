418a89e128a5f4c375af7d3e3fe866acbc8db86b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-16 13:32:04 -0300

    Saved bravo/__log__.py @2020-07-16 16:32

--- bravo/__log__.py
+++ bravo/__log__.py
@@ -654,3 +654,19 @@ AttributeError: 'gameInicio' object has no attribute 'vai'
     gameInicio().vai()
 AttributeError: 'gameInicio' object has no attribute 'vai'
 '''},
+{'date': 'Thu Jul 16 2020 13:32:04.378 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 15
+    STYLE["width"] = 1150
+NameError: name 'STYLE' is not defined
+'''},
