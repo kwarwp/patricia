02f147f83dd996bc43e474cd84d06a8dbf4c2c5e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-16 11:35:09 -0300

    Saved foxtrot/__log__.py @2020-07-16 14:35

--- foxtrot/__log__.py
+++ foxtrot/__log__.py
@@ -1757,3 +1757,21 @@ AttributeError: 'Jogo' object has no attribute 'texto_castelo'
     porta = Elemento (FOCO, x=150, y=150, w=50, h=50, cena=self.castelo, style={"opacity": 100}, vai=self.texto_castelo.vai)
 AttributeError: 'bool' object has no attribute 'vai'
 '''},
+{'date': 'Thu Jul 16 2020 11:35:08.738 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 89
+    Jogo()
+  module <module> line 66
+    self.texto_castelo = Texto (self.castelo, "Parado aí! Identifique-se!")
+AttributeError: 'Jogo' object has no attribute 'castelo'
+'''},
