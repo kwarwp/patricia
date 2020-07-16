8e734b77a316f4230ed4a9e9edd5b950670c2142
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-16 11:57:24 -0300

    Saved sarah/main.py @2020-07-16 14:57

--- sarah/main.py
+++ sarah/main.py
@@ -4,7 +4,10 @@ _autor_ = "Lorena Pires Griõn"
 from _spy.vitollino.main import Cena, Elemento, Texto
 from _spy.vittolino.main import INVENTARIO as inv
 
+STYLE["width"] = 1150
+STYLE["height"] = "550px"
 tabuleiro ="http://www.infcross.com.br/mestrado/tabuleiro.jpg"
+
 botao = "http://www.infcross.com.br/mestrado/botao.jpg"
 
    
