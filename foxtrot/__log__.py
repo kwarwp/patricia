f11153d0f6e7d85842cdef033b40fe046db024ed
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-16 11:50:24 -0300

    Saved foxtrot/__log__.py @2020-07-16 14:50

--- foxtrot/__log__.py
+++ foxtrot/__log__.py
@@ -1824,3 +1824,21 @@ Traceback (most recent call last):
     self.castelo.direita = self.cenaauthor.vai
 AttributeError: 'Jogo' object has no attribute 'cenaauthor'
 '''},
+{'date': 'Thu Jul 16 2020 11:50:23.687 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 96
+    Jogo()
+  module <module> line 92
+    self.texto_castelo.foi = self.cenauthor.vai
+AttributeError: 'Jogo' object has no attribute 'cenauthor'
+'''},
