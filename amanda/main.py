05cc810a00d5573ff6e6a95d5d0d25d3b7f93acb
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-16 13:45:15 -0300

    Saved amanda/main.py @2020-07-16 16:45

--- amanda/main.py
+++ amanda/main.py
@@ -17,10 +17,35 @@ class tela:
 	fundo= Cena (img = "https://i.imgur.com/DwSOcuv.png")
 	siga = Elemento (img="https://i.imgur.com/kRmXNkz.png", style=dict(left=180, top=380, width=150, height="150px"))
 	siga2 = Elemento (img="https://i.imgur.com/kRmXNkz.png", style=dict(left=850, top=380, width=150, height="150px"))
-	siga.entra(fundo)
+	siga.entra(fundo, direita= cena() )
 	siga2.entra(fundo)
 	fundo.vai()
-	siga.vai= cena().vai
-	siga2.vai= oi().vai
+	#siga.vai= cena().vai
+	#siga2.vai= oi().vai
 
 tela()
+
+
+
+#from grace.main import Praia
+# = "20.07"
+#__author__ = "Carlo"
+#STYLE["width"] = 500
+#CENA_CALCADA = "https://i.imgur.com/zOxshRh.jpg"
+#BANHISTA = "https://i.imgur.com/CWQ00XG.png"
+
+
+#class Calcada:
+#    """ Representa uma cena da calçada da praia """
+ #   def __init__(self):
+#        """ Mostra a cena da praia """
+#         self.cena = Cena(CENA_CALCADA, direita=Praia())
+ #        self.banhista = Elemento(BANHISTA, x=100, y=200, cena=self.cena)
+        #Cena(CENA_CALCADA).vai()
+ #    def vai(self):
+ #        """ Mostra a cena da praia """
+ #        self.cena.vai()
+        #Cena(CENA_CALCADA).vai()
+    
+# if __name__ == "__main__":
+#     Calcada().vai()
