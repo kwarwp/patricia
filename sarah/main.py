d0f848c34a8842d510038987dd9da788065481b3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-16 00:18:33 -0300

    Created sarah/main.py @2020-07-16 03:18

---
+++ sarah/main.py
@@ -0,0 +1,12 @@
+# patricia.sarah.main.py
+""" Jogo Olimpíadas de Matemática20.07
+        * NEW: O jogo original
+"""
+from _spy.vitollino.main import Cena, Elemento, Labirinto, Texto, Codigo, Sala, STYLE
+from _spy.vittolino.main import INVENTARIO as inv
+""" tabuleiro = Cena (img ="https://www.ibilce.unesp.br/Home/Departamentos/Matematica/2cejta/avancando-com-o-resto.png") """
+STYLE["width"] = 1150
+STYLE["height"] = "550px"
+img ="http://www.infcross.com.br/mestrado/tabuleiro.jpg"
+tabuleiro = Cena (img)
+tabuleiro.vai()
