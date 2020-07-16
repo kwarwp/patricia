2e4ce797fbcad2efd607c08883c1b7265a825d04
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-16 12:15:20 -0300

    Saved sarah/main.py @2020-07-16 15:15

--- sarah/main.py
+++ sarah/main.py
@@ -1,48 +1,9 @@
 # patricia.sarah.main.py
-# patricia.echo.main.py
+
 """ Jogo Olimpíadas de Matemática20.07
         * NEW: O jogo original
-        Autor: Grupo Echo (Aline, Lorena, Renato e Victoria)
 """
-from _spy.vitollino.main import Cena, Elemento, Labirinto, Texto, Codigo, Sala, STYLE
-from _spy.vittolino.main import INVENTARIO as inv
 
-""" tabuleiro = Cena (img ="https://www.ibilce.unesp.br/Home/Departamentos/Matematica/2cejta/avancando-com-o-resto.png") """
-
-STYLE["width"] = 1345
-STYLE["height"] = "620px"
-
-tabuleiro ="http://www.infcross.com.br/mestrado/tabuleiro.jpg"
-botao = "http://www.infcross.com.br/mestrado/botao.jpg"
-#tabuleiro = Cena (img)
-#tabuleiro.vai()
-
-
-def cena_principal():
-    inicio = Cena(img=tabuleiro)
-    inicio_e = Elemento(img=botao)
-    inicio.vai()
-    inicio_e.vai()
-cena_principal() 
-
-
-
-
-
-
-# patricia.adda.main.py
-# SPDX-License-Identifier: GPL-3.0-or-later
-
-""" Jogo do Mistério da Praia.
-
-    Descubra o mistério desta praia.
-
-Changelog
----------
-    20.07
-        * NEW: O jogo original.
-
-"""
 from _spy.vitollino.main import Cena, Elemento, STYLE
 from grace.main import Praia
 __version__ = "20.07"
