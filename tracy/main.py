efe3658ffac0a08025ee6a51262bc4736f938bab
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-15 22:58:11 -0300

    Saved tracy/main.py @2020-07-16 01:58

--- tracy/main.py
+++ tracy/main.py
@@ -1,5 +1,15 @@
 # patricia.tracy.main.py
 __autor__ = "Lorena Pires Griõn"
 
-nome_do_projeto = "Active Light"
-print("Qual o nome do projeto?", nome_do_projeto)
+
+from _spy.vitollino.main import Cena, Elemento, Labirinto, Texto, Codigo, Sala, STYLE
+from _spy.vittolino.main import INVENTARIO as inv
+""" tabuleiro = Cena (img ="https://www.ibilce.unesp.br/Home/Departamentos/Matematica/2cejta/avancando-com-o-resto.png") """
+STYLE["width"] = 1150
+STYLE["height"] = "550px"
+img ="http://www.infcross.com.br/mestrado/tabuleiro.jpg"
+botao = "https://imgur.com/oC9lAgW"
+tabuleiro = Cena (img)
+botaovai = Cena(botao)
+tabuleiro.vai()
+botao.vai()
