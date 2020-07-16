7fe1fee7e4d4ec9f89eac121dd2a58575188cc40
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-16 12:23:19 -0300

    Saved sarah/main.py @2020-07-16 15:23

--- sarah/main.py
+++ sarah/main.py
@@ -18,17 +18,17 @@ __version__ = "1.0"
 __author__ = "ECHO"
 STYLE["width"] = 500
 CENA_CALCADA = "http://www.infcross.com.br/mestrado/tabuleiro.jpg"
-BANHISTA = "http://www.infcross.com.br/mestrado/botao.jpg"
+BOTAO = "http://www.infcross.com.br/mestrado/botao.jpg"
 
 class Calcada:
     """ Representa uma cena da calçada da praia """
     def __init__(self):
-        """ Mostra a cena da praia """
+        """ Mostra o tabuleiro com o botão start"""
         self.cena = Cena(CENA_CALCADA, direita=Praia())
-        self.banhista = Elemento(BANHISTA, x=100, y=200, cena=self.cena)
+        self.banhista = Elemento(BOTAO, x=100, y=200, cena=self.cena)
         #Cena(CENA_CALCADA).vai()
     def vai(self):
-        """ Mostra a cena da praia """
+        """ Mostra o tabuleiro """
         self.cena.vai()
         #Cena(CENA_CALCADA).vai()
     
