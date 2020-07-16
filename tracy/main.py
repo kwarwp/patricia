413ad0625383d1388e4d5f79830fa92e56e21571
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-15 23:14:30 -0300

    Saved tracy/main.py @2020-07-16 02:14

--- tracy/main.py
+++ tracy/main.py
@@ -1,15 +1,21 @@
 # patricia.tracy.main.py
-__autor__ = "Lorena Pires Griõn"
+_autor_ = "Lorena Pires Griõn"
 
-
-from _spy.vitollino.main import Cena, Elemento, Labirinto, Texto, Codigo, Sala, STYLE
+from _spy.vitollino.main import Cena, Elemento, Texto
 from _spy.vittolino.main import INVENTARIO as inv
-""" tabuleiro = Cena (img ="https://www.ibilce.unesp.br/Home/Departamentos/Matematica/2cejta/avancando-com-o-resto.png") """
-STYLE["width"] = 1150
-STYLE["height"] = "550px"
-img ="http://www.infcross.com.br/mestrado/tabuleiro.jpg"
+
+tabuleiro ="http://www.infcross.com.br/mestrado/tabuleiro.jpg"
 botao = "https://imgur.com/oC9lAgW"
-tabuleiro = Cena (img)
-botaovai = Cena(botao)
-tabuleiro.vai()
-botao.vai()
+
+   
+def cena_principal():
+    inicio = Cena(img=tabuleiro)
+    inicio_e = Elemento (img = botao, tit="Gravidade", style = dict(left= 70,top=170, width=1150, height=550,bottom=100))
+    lado1 = Cena(img=tabuleiro, direita =inicio)
+
+    
+    inicio_e.entra(inicio)
+
+    inicio.vai()
+    
+cena_principal() 
