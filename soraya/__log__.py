c0c4e80bca72551ba5cf90cf2e4b47f83f5fb5c6
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-16 01:34:28 -0300

    Saved soraya/__log__.py @2020-07-16 04:34

--- soraya/__log__.py
+++ soraya/__log__.py
@@ -55,3 +55,21 @@ Traceback (most recent call last):
     self.play = Elemento(PLAY, x=570, y=470,w=180,h=120, cena=gameInicio, vai = self.mostradia)
 AttributeError: 'gameInicio' object has no attribute 'mostradia'
 '''},
+{'date': 'Thu Jul 16 2020 01:34:21.729 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 34
+    gameInicio()
+  module <module> line 23
+    self.play = Elemento(PLAY, x=570, y=470,w=100,h=100, cena=gameInicio, vai = self.redimensionar)
+AttributeError: 'gameInicio' object has no attribute 'redimensionar'
+'''},
