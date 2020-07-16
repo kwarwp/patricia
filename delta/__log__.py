4704f6733ba04279857be5309f442c75ad3dbd4e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-16 13:26:15 -0300

    Saved delta/__log__.py @2020-07-16 16:26

--- delta/__log__.py
+++ delta/__log__.py
@@ -3276,3 +3276,23 @@ NameError: name 'Game' is not defined
     list_objects = [ Card("PyCharm", IMG_CARD_1, list_cards[0], Game2x5.cena, Game2x5.rule), 
 NameError: name 'Game2x5' is not defined
 '''},
+{'date': 'Thu Jul 16 2020 13:26:11.365 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 161
+    Game2x3().vai()
+  module <module> line 67
+    self.create_2x4_cards()
+  module <module> line 79
+    list_objects = [ Card("PyCharm", IMG_CARD_1, list_cards[0], Game2x3.cena, Game2x3.rule), 
+NameError: name 'Game2x5' is not defined
+'''},
