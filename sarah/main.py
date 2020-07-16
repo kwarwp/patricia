cb44d8696464397f9668287314ade91ca4361be9
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-16 00:19:33 -0300

    Saved sarah/main.py @2020-07-16 03:19

--- sarah/main.py
+++ sarah/main.py
@@ -6,7 +6,7 @@ from _spy.vitollino.main import Cena, Elemento, Labirinto, Texto, Codigo, Sala, 
 from _spy.vittolino.main import INVENTARIO as inv
 """ tabuleiro = Cena (img ="https://www.ibilce.unesp.br/Home/Departamentos/Matematica/2cejta/avancando-com-o-resto.png") """
 STYLE["width"] = 1250
-STYLE["height"] = "580px"
+STYLE["height"] = "590px"
 img ="http://www.infcross.com.br/mestrado/tabuleiro.jpg"
 tabuleiro = Cena (img)
 tabuleiro.vai()
