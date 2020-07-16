9ebfd99acd1bb4590330e5da2b1fc0fe20d0aee3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-16 11:23:58 -0300

    Saved bravo/__log__.py @2020-07-16 14:23

--- bravo/__log__.py
+++ bravo/__log__.py
@@ -622,3 +622,19 @@ AttributeError: 'gameInicio' object has no attribute 'vai'
     gameInicio().vai()
 AttributeError: 'gameInicio' object has no attribute 'vai'
 '''},
+{'date': 'Thu Jul 16 2020 11:23:51.304 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 40
+    gameInicio().vai()
+AttributeError: 'gameInicio' object has no attribute 'vai'
+'''},
