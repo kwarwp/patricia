8cd74cac02fd4cb38407208af4542c0806aa22c1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-15 22:35:52 -0300

    Saved delta/hotel.py @2020-07-16 01:35

--- delta/hotel.py
+++ delta/hotel.py
@@ -1,61 +1,135 @@
 # patricia.delta.hotel.py
 # SPDX-License-Identifier: GPL-3.0-or-later
-""" Projeto sem descrição, (mude esta linha)."""
+#Código do Paulo adaptado para matriz 2x3
+#Algumas tentativas, minhas, anteriores foram frustradas com relação a posicionamento
+#
+from _spy.vitollino.main import Cena, Elemento, Texto, STYLE
+import random
+import time
 
-"""codeauthor:: Anni Provietti <anniprovietti@gmail.com>
+#_version_ = "20.07"
+#_author_ = "Paulo Assumpção"
 
-Changelog
+IMG_CARD_FACE_DOWN = "http://activufrj.nce.ufrj.br/file/ProgOO/Card_verso.png?disp=inline"
+IMG_CARD_1 = "http://activufrj.nce.ufrj.br/file/ProgOO/Card_pycharm.png?disp=inline"
+IMG_CARD_2 = "http://activufrj.nce.ufrj.br/file/ProgOO/Card_Linux.png?disp=inline"
+IMG_CARD_3 = "http://activufrj.nce.ufrj.br/file/ProgOO/Card_Gitlab.png?disp=inline"
+IMG_CARD_4 = "http://activufrj.nce.ufrj.br/file/ProgOO/Card_github.png?disp=inline"
+IMG_CARD_5 = "http://activufrj.nce.ufrj.br/file/ProgOO/Card_Activ.png?disp=inline"
 
-.. versionadded::    20.07"""
+IMG_WIDTH = 150
+IMG_HEIGHT = 150
+
+class Card():
+    def __init__(self, name, image, position, cena, rule):
+        self.rule = rule
+        self.name = name
+        self.cena = cena
+        self.image = image
+        self.faceDown = True
+        self.position = position
+        self.pos_x = 50 + self.position[0] * IMG_WIDTH
+        self.pos_y = 50 + self.position[1] * IMG_HEIGHT
+        self.card = Elemento(IMG_CARD_FACE_DOWN, , x=self.pos_x, y=self.pos_y, width=IMG_WIDTH, height=IMG_HEIGHT, cena=self.cena)
+        self.card.elt.bind("click", self.turnUp)
+        self.card.elt.bind("click", self.turnUp)
+        self.removed = False
+        
+    def turnUp(self, env=None):
+        self.card = Elemento(self.image, tit=self.name, x=self.pos_x, y=self.pos_y, width=IMG_WIDTH, height=IMG_HEIGHT, cena=self.cena)
+        self.faceDown = False 
+        self.card.elt.bind("click", self.turnDown)
+        self.rule(self)
+        
+    def turnDown(self, env=None):
+        self.card = Elemento(IMG_CARD_FACE_DOWN, tit=self.name, x=self.pos_x, y=self.pos_y, width=IMG_WIDTH, height=IMG_HEIGHT, cena=self.cena)
+        self.faceDown = True
+        self.card.elt.bind("click", self.turnUp)
+        
+        
+class Game:
+    # referência para o Elemento
+    previous_selected_card = None
+    
+    cena = Cena()
+    
+    def vai(self): 
+        self.create_2x3_cards()
+    
+    def create_2x3_cards(self):
+        """ 
+            matrix 2x3:
+            1A 1B 2A 2B 
+            3A 3B 4A 4B  
+        """
+        list_cards = self.shuffle_cards()
+        
+        self.card1a = Card("PyCharm", IMG_CARD_1, list_cards[0], Game.cena, Game.rule)
+        self.card1b = Card("PyCharm", IMG_CARD_1, list_cards[1], Game.cena, Game.rule)
+        
+        self.card2a = Card("Linux", IMG_CARD_2, list_cards[2], Game.cena, Game.rule)
+        self.card2b = Card("Linux", IMG_CARD_2, list_cards[3], Game.cena, Game.rule)
+        
+        self.card3a = Card("GitLab", IMG_CARD_3, list_cards[4], Game.cena, Game.rule)
+        self.card3b = Card("GitLab", IMG_CARD_3, list_cards[5], Game.cena, Game.rule)
+        
+        self.card4a = Card("GitHub", IMG_CARD_4, list_cards[6], Game.cena, Game.rule)
+        self.card4b = Card("GitHub", IMG_CARD_4, list_cards[7], Game.cena, Game.rule)
+        
+        #self.card5a = Card("Activ", IMG_CARD_5, list_cards[8], Game.cena, Game.rule)
+        #self.card5b = Card("Activ", IMG_CARD_5, list_cards[9], Game.cena, Game.rule)
+        
+        Game.cena.vai()
+
+    @staticmethod
+    def rule(selected_card):
+    
+        # abortar se o clique ocorrer sobre a mesma carta
+        if Game.previous_selected_card == selected_card:
+            return
+        
+        # tem um par selecionado?
+        if Game.previous_selected_card is None:
+            # primeira carta selecionada
+            Game.previous_selected_card = selected_card
+            # desabilita o clique sobre carta virada
+            Game.previous_selected_card.card.elt.unbind("click")
+            return
+        
+        # Não acertou
+        if Game.previous_selected_card.name != selected_card.name:            
+            
+            
+           
+            
+            # Aqui tem q esperar pelo menos 3 segundos, como fazer? (sleep, não funciona)
+            
+            
+            #Texto(Game.cena, "Opa!", "Errou!!!").vai()
+            selected_card.turnDown()
+            # reabilita a ação o clique e vira a carta 1 para baixo
+            Game.previous_selected_card.card.elt.bind("click", Game.previous_selected_card.turnUp)
+            Game.previous_selected_card.turnDown()
+            
+            # reabilita a ação do clique e vira a carta 2 para baixo
+            selected_card.card.elt.bind("click", selected_card.turnUp)
+            Game.previous_selected_card = None
+            
+        # acertou 
+        else:
+            # desabilita o clique sobre as cartas acertadas
+            Game.previous_selected_card = None
+            selected_card.card.elt.unbind("click")
+            Texto(Game.cena, "Acertou!!!").vai()
         
 
-""" modificações de acordo com a sala golf"""
+    def shuffle_cards(self):   
+        list_cards =  [(0,0), (1,0), (2,0), (3,0), (0,1), (1,1), (2,1), (3,1)]
+        random.shuffle(list_cards)
+        return list_cards
+        
 
-import random
-from _spy.vitollino.main import Cena, Elemento, Texto
-from browser import document
-
-
-""" Imagens do Jogo da Memória: verso, pycharm, linux
-
-"""
-IMG_PYCHARM="http://activufrj.nce.ufrj.br/file/ProgOO/Card_pycharm.png?disp=inline"
-IMG_LINUX="http://activufrj.nce.ufrj.br/file/ProgOO/Card_Linux.png?disp=inline"
-IMG_VERSO="http://activufrj.nce.ufrj.br/file/ProgOO/Card_verso.png?disp=inline"
-IMG_WIDTH=200
-IMG_HEIGHT=200
-class Carta():
-	def __init__(self,imagem,posicao,cena):
-		self.imagem=imagem
-		self.posicao=posicao
-		pos_x = 50 + self.posicao[0]*IMG_WIDTH
-		pos_y = 50 + self.posicao[1]*IMG_HEIGHT
-		Elemento(self.imagem, x=pos_x, y=pos_y, width=IMG_WIDTH, height=IMG_HEIGHT, cena=cena)
-	def cartas():
-		if self.verso:
-			Elemento(self.imagem, x=pos_x, y=pos_y, width=IMG_WIDTH, height=IMG_HEIGHT, cena=cena)
-		else: 
-			Elemento(self.imagem, x=pos_x, y=pos_y, width=IMG_WIDTH, height=IMG_HEIGHT, cena=cena)
-class Jogo:
-	def vai(self):
-		self.matriz()
-	"matriz 2x3"
-	def matriz(self):
-		self.cena = Cena()
-		list_cards=self.embaralha()
-          # self.verso=False
-		self.card1a = Carta(IMG_PYCHARM, list_cards[0], self.cena)
-		self.card1b = Carta(IMG_PYCHARM, list_cards[1], self.cena)
-		self.card2a = Carta(IMG_LINUX, list_cards[2], self.cena)
-		self.card2b = Carta(IMG_LINUX, list_cards[3], self.cena)
-		self.card2a = Carta(IMG_LINUX, list_cards[4], self.cena)
-		self.card2b = Carta(IMG_LINUX, list_cards[5], self.cena)        
-		self.cena.vai()
-	def embaralha(self):   
-		list_cards =  [(0,0), (1,0),(0,1),(1,1),(2,0),(2,1)]
-		random.shuffle(list_cards,random.random)
-		return list_cards
 if __name__ == "__main__":
-    Jogo().vai()
+    Game().vai()
 	
 
