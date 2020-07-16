90edb053ff70c2c791ea05f5d93f942763e5cc69
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-16 12:08:45 -0300

    Saved alpha/india.py @2020-07-16 15:08

--- alpha/india.py
+++ alpha/india.py
@@ -73,7 +73,7 @@ class Eventos:
         #se o elemento atingiu uma porta, muda para a próxima cena
         # FALTA mapear os pontos, criar função para passar parametros ou chamar outra classe
         #ideia de cria uma matriz com os pontos de localização do portal
-        if self.boneco.x > 400 and self.boneco.y > 200:
+        if self.boneco.x > 400 and self.boneco.y > 400:
             global cont #contador estanciado fora do def para gerar a linha a ser lida na matrizFase
             self.ambiente = Cena(self.matrizFase[cont][0]) #lê a cena que está descrita na primeira coluna da matriz
             STYLE["width"] = 640
