1bf4778caf302c072f2a500140d66a80b1a745cb
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-16 10:00:12 -0300

    Saved alpha/__log__.py @2020-07-16 13:00

--- alpha/__log__.py
+++ alpha/__log__.py
@@ -371,3 +371,21 @@ NameError: name 'matrizFase' is not defined
     self.x1 = matrizFase[0][0]
 NameError: name 'matrizFase' is not defined
 '''},
+{'date': 'Thu Jul 16 2020 10:00:10.172 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 97
+    Eventos().vai()
+  module <module> line 51
+    self.x1 = matrizFase[0][1]
+NameError: name 'matrizFase' is not defined
+'''},
