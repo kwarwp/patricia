9a425d694f6692104760dd7b9bcf7fa5870f5de1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-16 00:29:47 -0300

    Saved delta/__log__.py @2020-07-16 03:29

--- delta/__log__.py
+++ delta/__log__.py
@@ -2954,3 +2954,23 @@ IndexError: list index out of range
     self.create_2x4_cards()
 AttributeError: 'Game' object has no attribute 'create_2x4_cards'
 '''},
+{'date': 'Thu Jul 16 2020 00:29:40.832 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 155
+    Game_().vai()
+  module <module> line 85
+    self.create_2x5_cards()
+  module <module> line 95
+    self.card1a = Card("PyCharm", IMG_CARD_1, list_cards[0], Game_.cena, Game.rule)
+NameError: name 'Game' is not defined
+'''},
