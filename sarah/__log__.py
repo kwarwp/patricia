f85a3391cb7c06ff6b60b764c5566f82a8ce8e16
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-16 12:30:14 -0300

    Saved sarah/__log__.py @2020-07-16 15:30

--- sarah/__log__.py
+++ sarah/__log__.py
@@ -74,3 +74,21 @@ NameError: name 'BANHISTA' is not defined
     self.banhista = Elemento(botap, x=100, y=200, cena=self.cena)
 NameError: name 'botap' is not defined
 '''},
+{'date': 'Thu Jul 16 2020 12:30:13.761 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 37
+    Jogo().vai()
+  module <module> line 28
+    self.cena = Cena(TABULEIRO, direita=TABULEIRO())
+TypeError: 'str' object is not callable
+'''},
