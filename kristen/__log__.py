5ebae247e06e9b4b8208e2df26afa98d84cfa5c8
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-16 00:44:15 -0300

    Saved kristen/__log__.py @2020-07-16 03:44

--- kristen/__log__.py
+++ kristen/__log__.py
@@ -13,3 +13,10 @@ SyntaxError: non-keyword arg after keyword arg
                                                                        ^
 SyntaxError: non-keyword arg after keyword arg
 '''},
+{'date': 'Thu Jul 16 2020 00:44:14.480 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 33
+  self.boneco = Elemento(self.BONECO, x=self.pos_x, y=self.pos_y, width, height, cena=self.ambiente)
+                                                                       ^
+SyntaxError: non-keyword arg after keyword arg
+'''},
