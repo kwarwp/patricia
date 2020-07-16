8dc02ae412793d06e6614506abfc51de9b45da35
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-16 12:14:13 -0300

    Saved sarah/main.py @2020-07-16 15:14

--- sarah/main.py
+++ sarah/main.py
@@ -24,3 +24,48 @@ def cena_principal():
     inicio.vai()
     inicio_e.vai()
 cena_principal() 
+
+
+
+
+
+
+# patricia.adda.main.py
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+""" Jogo do Mistério da Praia.
+
+    Descubra o mistério desta praia.
+
+Changelog
+---------
+    20.07
+        * NEW: O jogo original.
+
+"""
+from _spy.vitollino.main import Cena, Elemento, STYLE
+from grace.main import Praia
+__version__ = "20.07"
+__author__ = "Renato"
+STYLE["width"] = 500
+TABULEIRO = "http://www.infcross.com.br/mestrado/tabuleiro.jpg"
+BOTAO = "http://www.infcross.com.br/mestrado/botao.jpg"
+
+
+class Jogo:
+    """ Representa uma cena da calçada da praia """
+    def __init__(self):
+        """ Mostra o tabuleiro """
+        self.cena = Cena(TABULEIRO, direita=Praia())
+        self.banhista = Elemento(BANHISTA, x=100, y=200, cena=self.cena)
+        #Cena(CENA_CALCADA).vai()
+    def vai(self):
+        """ Mostra o tabuleiro """
+        self.cena.vai()
+        #Cena(CENA_CALCADA).vai()
+    
+if __name__ == "__main__":
+    Jogo().vai()
+    
+    
+    
