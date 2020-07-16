fbe9ce1751dfa1f9c2aaff0be9d5c57f75330fec
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-16 11:45:32 -0300

    Saved delta/__log__.py @2020-07-16 14:45

--- delta/__log__.py
+++ delta/__log__.py
@@ -3087,3 +3087,21 @@ Traceback (most recent call last):
     START
 NameError: name 'START' is not defined
 '''},
+{'date': 'Thu Jul 16 2020 11:45:27.844 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 41
+    start().vai()
+  module <module> line 33
+    self.start.vai()
+AttributeError: 'start' object has no attribute 'start'
+'''},
