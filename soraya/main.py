8ca153b851473f843096ff99077bbc4635f6871b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-16 01:13:13 -0300

    Saved soraya/main.py @2020-07-16 04:13

--- soraya/main.py
+++ soraya/main.py
@@ -1,27 +1,4 @@
 # patricia.soraya.main.py
-# SPDX-License-Identifier: GPL-3.0-or-later
-
-""" Jogo do Mistério da Praia.
-
-    Descubra o mistério desta praia.
-
-Changelog
----------
-    20.07
-        * NEW: O jogo original.
-
-"""
-from _spy.vitollino.main import Cena
-from grace.main import praia
-__version__ = "20.07"
-__author__ = "Isabel Hortencia Garnica"
-CENA_PRAIA = "https://i.imgur.com/zOxshRh.jpg"
 
 
-class Calcada:
-    """ Representa uma cena da calçada da praia """
-    def vai(self):
-        """ Mostra a cena da praia """
-        Cena(CENA_PRAIA).vai()
-    
-Calcada().vai()
+
