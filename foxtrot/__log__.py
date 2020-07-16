37025585248fd6d6e4f7204a5ec3ba62b37d910e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-16 11:30:51 -0300

    Saved foxtrot/__log__.py @2020-07-16 14:30

--- foxtrot/__log__.py
+++ foxtrot/__log__.py
@@ -1721,3 +1721,21 @@ NameError: name 'texto_castelo' is not defined
     porta = Elemento (FOCO, x=150, y=150, w=50, h=50, cena=self.castelo, style={"opacity": 100}, vai=self.texto_castelo.vai)
 AttributeError: 'Jogo' object has no attribute 'texto_castelo'
 '''},
+{'date': 'Thu Jul 16 2020 11:30:50.929 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 92
+    Jogo()
+  module <module> line 71
+    porta = Elemento (FOCO, x=150, y=150, w=50, h=50, cena=self.castelo, style={"opacity": 100}, vai=self.texto_castelo.vai)
+AttributeError: 'Jogo' object has no attribute 'texto_castelo'
+'''},
