27010db952e9427b6e9c85350fc35136046cf8f7
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-17 10:51:01 -0300

    Saved adda/tutorial_um.py @2020-07-17 13:51

--- adda/tutorial_um.py
+++ adda/tutorial_um.py
@@ -21,6 +21,7 @@ Changelog
 from _spy.vitollino.main import Cena, Elemento
 from browser import document # importa o DOM para atribuir o evento de teclado
 from adda.praia import cena
+print(cena)
 
 class Eventos:
     """ Associa um evento a uma imagem e captura eventos de teclado. """
