2584bb799e19448bccc6a215e5470963dd00ede1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-17 10:38:17 -0300

    Created adda/praia.py @2020-07-17 13:38

---
+++ adda/praia.py
@@ -0,0 +1,15 @@
+# patricia.adda.praia.py
+# SPDX-License-Identifier: GPL-3.0-or-later
+""" Projeto sem descrição, (mude esta linha).
+
+.. codeauthor:: Nome Sobrenome <mail@local.tipo>
+
+Changelog
+---------
+.. versionadded::    20.07
+        Descreva o que você adicionou no código.
+
+"""
+from _spy.vitollino.main import Cena
+
+cena = Cena("https://i.imgur.com/wz5XKcr.jpg")
