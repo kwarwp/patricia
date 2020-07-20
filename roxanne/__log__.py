bd50c1a0f50da042d89e70655478e54ea1f3bbfb
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-20 17:59:14 -0300

    Saved roxanne/__log__.py @2020-07-20 20:59

--- roxanne/__log__.py
+++ roxanne/__log__.py
@@ -752,3 +752,19 @@ NameError: name 'html' is not defined
     link = html.A()
 NameError: name 'html' is not defined
 '''},
+{'date': 'Mon Jul 20 2020 17:59:10.228 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 4
+    doc["pydiv"].html.clear()
+AttributeError: 'str' object has no attribute 'clear'
+'''},
