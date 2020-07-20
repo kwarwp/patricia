45c2f22810e79ab2e0c6df776e1700ccee849149
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-20 16:07:46 -0300

    Saved roxanne/__log__.py @2020-07-20 19:07

--- roxanne/__log__.py
+++ roxanne/__log__.py
@@ -649,3 +649,19 @@ KeyError: zone
     data = np.random.random(10)
 AttributeError: 'module' object has no attribute 'random'
 '''},
+{'date': 'Mon Jul 20 2020 16:07:42.61 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 3
+    print(data)
+NameError: name 'data' is not defined
+'''},
