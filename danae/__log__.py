6106dc693ccb9c1768551d5de36d72b1d0363b72
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-20 17:43:32 -0300

    Saved danae/__log__.py @2020-07-20 20:43

--- danae/__log__.py
+++ danae/__log__.py
@@ -305,3 +305,27 @@ SyntaxError: invalid syntax
     @section(h+w+p)
 NameError: name 'h' is not defined
 '''},
+{'date': 'Mon Jul 20 2020 17:43:31.170 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 53
+    class App:
+  module <module> line 80
+    @section(h+w+p)
+  module <module> line 44
+    super().init(func, clazz=clazz, oid=oid, tag=S)
+AttributeError: object 'super' has no attribute 'init'
+'''},
