7a5c6c47daf50acfcc4ac0dab26b325e246425a9
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-20 18:08:32 -0300

    Saved danae/__log__.py @2020-07-20 21:08

--- danae/__log__.py
+++ danae/__log__.py
@@ -450,3 +450,23 @@ AttributeError: str has no attribute '__dict__'
     self.func = func
 NameError: name 'func' is not defined
 '''},
+{'date': 'Mon Jul 20 2020 18:08:31.906 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 50
+    class App:
+  module <module> line 78
+    @section(h+w+p)
+  module <module> line 41
+    super().__init__(func, clazz=clazz, oid=oid, tag=S)
+TypeError: __init__() got multiple values for argument 'clazz'
+'''},
