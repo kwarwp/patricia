a06bcf1697e9e9bd9b606e8ab2b20a8b2432f69e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-21 12:29:18 -0300

    Saved charlie/__log__.py @2020-07-21 15:29

--- charlie/__log__.py
+++ charlie/__log__.py
@@ -67,3 +67,21 @@ TypeError: 'module' object is not callable
     self.minipython(cofre, self.mostra_conteudo_minipython)
 AttributeError: 'normal' object has no attribute 'minipython'
 '''},
+{'date': 'Tue Jul 21 2020 12:29:17.499 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 81
+    normal()
+  module <module> line 76
+    minipython(monitor, self.mostra_conteudo_minipython) 
+NameError: name 'monitor' is not defined
+'''},
