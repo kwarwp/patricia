05cea36348d4371b1b31df36bf9ed466e8c4a366
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-22 12:27:50 -0300

    Saved charlie/__log__.py @2020-07-22 15:27

--- charlie/__log__.py
+++ charlie/__log__.py
@@ -128,3 +128,21 @@ NameError: name 'monitor' is not defined
     minipython(self.monitor, self.mostra_conteudo_minipython)
 AttributeError: 'normal' object has no attribute 'mostra_conteudo_minipython'
 '''},
+{'date': 'Wed Jul 22 2020 12:27:50.33 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 84
+    normal().vai()
+  module <module> line 77
+    minipython(self.monitor, self.mostra_conteudo_minipython)
+AttributeError: 'normal' object has no attribute 'mostra_conteudo_minipython'
+'''},
