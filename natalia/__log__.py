8809afab15eb6c75d899e123b5da10fd831ea281
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-22 23:21:20 -0300

    Saved natalia/__log__.py @2020-07-23 02:21

--- natalia/__log__.py
+++ natalia/__log__.py
@@ -186,3 +186,10 @@ IndentationError: expected an indented block
   ^
 IndentationError: expected an indented block
 '''},
+{'date': 'Wed Jul 22 2020 23:24:56.687 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 41
+  janela = TK()
+  ^
+IndentationError: expected an indented block
+'''},
