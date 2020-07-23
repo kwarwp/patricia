eb03826295d8dfcedadef9a7238c3a59924d40f7
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-22 21:36:34 -0300

    Saved lisa/__log__.py @2020-07-23 00:36

--- lisa/__log__.py
+++ lisa/__log__.py
@@ -154,3 +154,10 @@ SyntaxError: invalid syntax
                                                        ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Wed Jul 22 2020 21:37:06.692 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 31
+  """ # class Tabuleiro:
+                                                         ^
+SyntaxError: invalid syntax
+'''},
