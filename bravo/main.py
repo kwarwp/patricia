9d4ff8f9caa77b0091382823f2d5abe72aa2fc59
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-23 20:05:08 -0300

    Saved bravo/main.py @2020-07-23 23:05

--- bravo/main.py
+++ bravo/main.py
@@ -30,15 +30,14 @@ STYLE ["height"] = "600px"
 class gameInicio:
     """ criando o módulo init - inicio do jogo""" 
     def __init__(self):
-        self.gameInicio = Cena(CENAINICIO)
-        self.gameInicio.vai()
+        gameInicio = Cena(CENAINICIO)
+        gameInicio.vai()
         self.dark = Elemento("",style=dict(width="1345px",height="600px"),cena=gameInicio)
-        self.play = Elemento(PLAY, x=570, y=470,w=100,h=100, cena=gameInicio, vai = self.redimensiona)
+        self.play = Elemento(PLAY, x=570, y=470,w=100,h=100, cena=gameInicio, vai = self.redimensiona, direita = teste)
     
     """ criando o módulo para ir para a bravo.india""" 
     def redimensiona(self,ev=0):
-        
-        #redi = Cena()
+        redi = Cena()
         redi.vai = self.q2
         #question = Cena(FUNDO, direita = oi())
         #question.vai()
