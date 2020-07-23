bd09d4a2b1dd695900f8b67931e2e3e9263c4c95
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-22 23:51:03 -0300

    Saved natalia/__log__.py @2020-07-23 02:51

--- natalia/__log__.py
+++ natalia/__log__.py
@@ -409,3 +409,10 @@ IndentationError: unexpected indent
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Wed Jul 22 2020 23:54:39.715 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 51
+  print("dados")
+  ^
+IndentationError: unexpected indent
+'''},
