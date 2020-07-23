cfd813c5d86ba2e71f77a7f14ebd86680b772238
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-23 11:55:39 -0300

    Saved delta/__log__.py @2020-07-23 14:55

--- delta/__log__.py
+++ delta/__log__.py
@@ -3745,3 +3745,34 @@ AttributeError: 'module' object has no attribute 'vai'
     self.inicio = Cena( Card())
 TypeError: 'module' object is not callable
 '''},
+{'date': 'Thu Jul 23 2020 11:55:38.4 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 14
+    from delta.juliete import Game  
+ImportError: cannot import name 'Game'
+
+ImportError
+Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 264
+    action()
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 14
+    from delta.juliete import Game  
+'''},
