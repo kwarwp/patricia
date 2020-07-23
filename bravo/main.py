e919cd96bd08d40000b0f0378bc6049f34d64ba9
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-23 16:38:13 -0300

    Saved bravo/main.py @2020-07-23 19:38

--- bravo/main.py
+++ bravo/main.py
@@ -12,12 +12,13 @@
 Changelog
 ---------
 .. versionadded::    16.07
-       
+       Pagina inicial do Game
 """
 
 from _spy.vitollino.main import Cena, Elemento, INVENTARIO, STYLE, Musica
 from bravo.alexa import oi
 
+""" imagens"""
 CENAINICIO = "https://i.imgur.com/mbt7XHq.png"
 PLAY = "https://i.imgur.com/QiiOf5O.png"
 FUNDO= "https://i.imgur.com/cdMKAka.png"
@@ -25,14 +26,16 @@ PERGUNTA = "https://i.imgur.com/fYmNuBj.png"
 STYLE ["width"] = 1340
 STYLE ["height"] = "600px"
 
+"""criando a classe gameInicio"""
 class gameInicio:
-    # O jogo começa aqui
+    """ criando o módulo init - inicio do jogo""" 
     def __init__(self):
         gameInicio = Cena(CENAINICIO)
         gameInicio.vai()
         dark = Elemento("",style=dict(width="1345px",height="600px"),cena=gameInicio)
         self.play = Elemento(PLAY, x=570, y=470,w=100,h=100, cena=gameInicio, vai = self.redimensiona)
     
+    """ criando o módulo init - inicio do jogo""" 
     def redimensiona(self,ev=0):
         redi = Cena()
         redi.vai = self.q2
