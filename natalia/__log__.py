91b5af8a5449d28db59777591479632cf103fca3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-22 22:35:31 -0300

    Saved natalia/__log__.py @2020-07-23 01:35

--- natalia/__log__.py
+++ natalia/__log__.py
@@ -128,3 +128,19 @@ SyntaxError: invalid syntax
     self.dados = Dados(DADOS, cena=self.cena)
 NameError: name 'Dados' is not defined
 '''},
+{'date': 'Wed Jul 22 2020 22:39:07.845 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 38
+    shuffle(dados)
+NameError: name 'shuffle' is not defined
+'''},
