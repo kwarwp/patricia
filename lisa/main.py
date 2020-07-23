0308123315c26449a7982720432b6c247a2ee70e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-22 21:01:51 -0300

    Saved lisa/main.py @2020-07-23 00:01

--- lisa/main.py
+++ lisa/main.py
@@ -2,26 +2,27 @@
 __author__ = "Victória Regina Caruzo"
 __version__ = "09.07.20"
 
-"""from _spy.vitollino.main import Cena
-from adda.main import CENA_CALCADA
-from grace.main import CENA_PRAIA
-
-def vai(self):
-    Cena(CENA_CALCADA, direita=Praia()).vai()
-    Cena(CENA_CALCADA).vai()"""
-    
 from _spy.vitollino.main import Cena, Elemento, STYLE
+from random import shuffle 
 """ from grace.main import Praia """
 
 __version__ = "1.0"
 __author__ = "ECHO"
-STYLE["width"] = 1345
+STYLE["width"] = 1150
 STYLE["height"] = "620px"
 TABULEIRO = "http://www.infcross.com.br/mestrado/tabuleiro.jpg"
-BOTAO = "http://www.infcross.com.br/mestrado/botao.jpg"
+BOTAO = "http://www.infcross.com.br/mestrado/botao3.jpg"
+DADO1 = "http://www.infcross.com.br/mestrado/dado1.png"
+DADO2 = "http://www.infcross.com.br/mestrado/dado2.png" 
+DADO3 = "http://www.infcross.com.br/mestrado/dado3.png"
+DADO4 = "http://www.infcross.com.br/mestrado/dado4.png"
+DADO5 = "http://www.infcross.com.br/mestrado/dado5.png"
+DADO6 = "http://www.infcross.com.br/mestrado/dado6.png"
+
+dados = [DADO1, DADO2, DADO3, DADO4, DADO5, DADO6]*5
+shuffle(dados)
 
 class Tabuleiro:
-    """ Representa uma cena da calçada da praia """
     def vai(self):
         """ Mostra a cena do tabuleiro """
         Cena(TABULEIRO).vai()
@@ -31,10 +32,17 @@ class Jogo:
     def __init__(self):
         """ Mostra o tabuleiro com o botão start"""
         self.cena = Cena(TABULEIRO, direita=Tabuleiro())
-        self.start = Elemento(BOTAO, x=50, y=50, cena=self.cena)
+        self.start = Elemento(BOTAO, x=5, y=15, cena=self.cena)
         #Cena(CENA_JOGO).vai()
     def vai(self):
         """ Mostra o tabuleiro """
         self.cena.vai()
         #Cena(CENA_JOGO).vai()
 
+"""class Dado:
+    def __init__(self, outra):"""
+    
+    
+    
+if __name__ == "__main__":
+    Jogo().vai()
