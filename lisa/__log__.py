404d5fabd3114d1cbbc5b0bfcbd1ccfd229d2f7a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-22 21:10:23 -0300

    Saved lisa/__log__.py @2020-07-23 00:10

--- lisa/__log__.py
+++ lisa/__log__.py
@@ -108,3 +108,19 @@ SyntaxError: invalid syntax
     shuffle(dados)
 NameError: name 'shuffle' is not defined
 '''},
+{'date': 'Wed Jul 22 2020 21:10:54.971 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 23
+    random.shuffle(dados)
+AttributeError: 'function' object has no attribute 'shuffle'
+'''},
