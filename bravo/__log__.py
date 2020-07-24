238a7ad8f5073a753dda1aeddaea60173bf4518f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-23 21:25:47 -0300

    Saved bravo/__log__.py @2020-07-24 00:25

--- bravo/__log__.py
+++ bravo/__log__.py
@@ -1449,3 +1449,19 @@ AttributeError: 'gameInicio' object has no attribute 'nome'
     self.style.update(**{'position': 'absolute', 'overflow': 'hidden', 'o': 1,
 TypeError: __format__ missing 1 positional argument: 'spec'
 '''},
+{'date': 'Thu Jul 23 2020 21:25:42.724 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 52
+    gameInicio().vai()
+AttributeError: 'gameInicio' object has no attribute 'vai'
+'''},
