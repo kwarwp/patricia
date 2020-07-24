6603817b6f87148ecceb54fbfb1ad3bc4cc631e7
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-24 19:40:50 -0300

    Saved bravo/__log__.py @2020-07-24 22:40

--- bravo/__log__.py
+++ bravo/__log__.py
@@ -1834,3 +1834,21 @@ SyntaxError: invalid syntax
                                                                                                      ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Fri Jul 24 2020 19:40:52.297 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 86
+    teste().vai()    
+  module <module> line 60
+    cenas = Elemento(img = BALAO, style=dict(left=200, top=250, width=200, height="200px", vai= self.um.norte))
+AttributeError: 'teste' object has no attribute 'um'
+'''},
