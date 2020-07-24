de4a2a1837fd34baa357fe21bf6e274c479918ac
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-24 13:00:17 -0300

    Saved bravo/__log__.py @2020-07-24 16:00

--- bravo/__log__.py
+++ bravo/__log__.py
@@ -1520,3 +1520,19 @@ IndentationError: expected an indented block
     gameInicio().vai()
 AttributeError: 'gameInicio' object has no attribute 'vai'
 '''},
+{'date': 'Fri Jul 24 2020 13:00:15.571 GMt-0300 (Brasilia Standard Time) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 52
+    gameInicio().vai()
+AttributeError: 'gameInicio' object has no attribute 'vai'
+'''},
