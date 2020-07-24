0944a9f931503f320c43ee3965e4dd0f63262dfc
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-24 17:38:53 -0300

    Saved alexa/main.py @2020-07-24 20:38

--- alexa/main.py
+++ alexa/main.py
@@ -11,12 +11,13 @@ class oi():
     #primeira cena (onde está localizado as duas opções
     cena = Cena( img = "https://i.imgur.com/S5tAUe8.png")
     #opção 2 certo
-    op2 = Elemento ( img = "https://i.imgur.com/dVn8Gfo.png" , x = 700, y=390, w = 300, tit= "Clique")
+    op2 = Elemento ( img = "https://i.imgur.com/dVn8Gfo.png" , x = 700, y=390, w = 500, tit= "Clique")
     #cena que direciona a opção 2
     cena2 = Cena ( img = "https://i.imgur.com/IYekltO.png")
     #emoji de ok
     ok= Elemento (img = "https://i.imgur.com/jR7lluz.png")
     ok.entra(cena2)
+    ok.vai=cena.vai
     
     ''' SOBRE A OPÇÃO ERRADA:
         - SE O JOGADOR ESCOLHER A OPÇÃO QUE TEM O CÓDIGO ERRADO, ELE IRÁ PARA UMA OUTRA CENA EXPLICANOD O ERRO
@@ -26,13 +27,14 @@ class oi():
               E UM EMOJI DE NEGATIVO'''
     
     #opção 1 errado
-    op1 = Elemento ( img = "https://i.imgur.com/lN2umME.png", x = 250, y=390, w = 300, tit= "Clique")
+    op1 = Elemento ( img = "https://i.imgur.com/lN2umME.png", x = 250, y=390, w = 500, tit= "Clique")
     #cena que direciona a opção1
     cena3 = Cena ( img = "https://i.imgur.com/nsi5Cwh.jpg")
-    errado = Elemento (img = "https://i.imgur.com/elvddXi.png", style=dict(left=40, top=300, width=00, height="200px"))
-    ##elemento que está na cena 3 que foi direcionado pela op2, mas irá direcionar p cena2
-    #certo = Elemento ( img = "https://images.vexels.com/media/users/3/139740/isolated/preview/bfecbaa063a84b2e9bbd9f8b9b41d410-bot--o-de-reprodu----o-redondo-azul-by-vexels.png")
-    #cena do personagem
+    errado = Elemento (img = "https://i.imgur.com/elvddXi.png")
+    errado.entra(cena3)
+    errado.vai= cena.vai
+    '''elemento que está na cena 3 que foi direcionado pela op2, mas irá direcionar p cena2'''
+
     personagem_mulher_maravilha = Elemento(img = "https://i.imgur.com/fK906ve.png",style=dict(left=40, top=300, width=400, height="200px"))   
     personagem_mulher_maravilha.entra(cena2)
     ok= Elemento (img = "https://i.imgur.com/jR7lluz.png")
@@ -43,7 +45,7 @@ class oi():
     op2.entra(cena)
     op1.entra(cena)
     
-    errado.entra(cena3)
+    
     #certo.entra(cena3)
     #certo.vai=cena2.vai
     op2.vai=cena2.vai
