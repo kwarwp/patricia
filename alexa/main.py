41e19c91e421e36b41edbc2f46a623b37b25d54f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-24 17:29:11 -0300

    Saved alexa/main.py @2020-07-24 20:29

--- alexa/main.py
+++ alexa/main.py
@@ -5,7 +5,8 @@ STYLE["width"] = 1150
 STYLE["height"] = "550px"
 
 class oi():
-
+    ''' NESSA PARTE O JOGADOR TERÁ QUE ESCOLHER DUAS OPÇÕES DE COMO SE DECLARA UM ELEMENTO
+    SE O JOGADOR ESCOLHER A OPÇÃO DO CÓDIGO CERTO, ELE IRÁ CONSEGUIR CRIAR UM ELEMENTO'''
 
     #primeira cena (onde está localizado as duas opções
     cena = Cena( img = "https://i.imgur.com/S5tAUe8.png")
@@ -13,14 +14,22 @@ class oi():
     op2 = Elemento ( img = "https://i.imgur.com/Q4KSQKd.png" , x = 700, y=390, w = 300, tit= "Clique")
     #cena que direciona a opção 2
     cena2 = Cena ( img = "https://i.imgur.com/IYekltO.png")
+    #emoji de ok
+    ok= Elemento (img = "https://i.imgur.com/jR7lluz.png")
+    ok.entra(cena2)
     
+    ''' SOBRE A OPÇÃO ERRADA:
+        - SE O JOGADOR ESCOLHER A OPÇÃO QUE TEM O CÓDIGO ERRADO, ELE IRÁ PARA UMA OUTRA CENA EXPLICANOD O ERRO
+        - NESSES CÓDIGOS HÁ:
+              A DECLARAÇÃO DO CÓDIGO ERRADO
+              A CENA QUE IRÁ CORRIGIR O CÓDIGO
+              E UM EMOJI DE NEGATIVO'''
     
-    
-    errado = Elemento (img = "https://i.imgur.com/elvddXi.png", style=dict(left=40, top=300, width=00, height="200px"))
     #opção 1 errado
-    op1 = Elemento ( img = "https://images.vexels.com/media/users/3/139740/isolated/preview/bfecbaa063a84b2e9bbd9f8b9b41d410-bot--o-de-reprodu----o-redondo-azul-by-vexels.png", x = 350, y=390, tit= "Clique")
+    op1 = Elemento ( img = "https://i.imgur.com/lN2umME.png", x = 350, y=390, tit= "Clique")
     #cena que direciona a opção1
     cena3 = Cena ( img = "https://i.imgur.com/nsi5Cwh.jpg")
+    errado = Elemento (img = "https://i.imgur.com/elvddXi.png", style=dict(left=40, top=300, width=00, height="200px"))
     ##elemento que está na cena 3 que foi direcionado pela op2, mas irá direcionar p cena2
     #certo = Elemento ( img = "https://images.vexels.com/media/users/3/139740/isolated/preview/bfecbaa063a84b2e9bbd9f8b9b41d410-bot--o-de-reprodu----o-redondo-azul-by-vexels.png")
     #cena do personagem
@@ -33,7 +42,7 @@ class oi():
     
     op2.entra(cena)
     op1.entra(cena)
-    ok.entra(cena2)
+    
     errado.entra(cena3)
     #certo.entra(cena3)
     #certo.vai=cena2.vai
