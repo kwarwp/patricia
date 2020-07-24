3004ec6153a2d58bdd23d1a562fec0e67626d9d9
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-24 19:30:12 -0300

    Saved bravo/__log__.py @2020-07-24 22:30

--- bravo/__log__.py
+++ bravo/__log__.py
@@ -1766,3 +1766,21 @@ NameError: name 'fundo' is not defined
     cenas = Elemento(img = balao, style=dict(left=200, top=250, width=200, height="200px", vai = self.puxa))
 NameError: name 'balao' is not defined
 '''},
+{'date': 'Fri Jul 24 2020 19:30:11.470 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 89
+    teste().vai()    
+  module <module> line 59
+    cenas.entra(cena)
+NameError: name 'cenas' is not defined
+'''},
