9f237cfc9c6dbf0abc7a3b0f9547e31e5a72ef07
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-25 12:58:42 -0300

    Saved bravo/__log__.py @2020-07-25 15:58

--- bravo/__log__.py
+++ bravo/__log__.py
@@ -2430,3 +2430,21 @@ Exception: <RangeError: Maximum call stack size exceeded>
 'error': '''
 Exception: <RangeError: Maximum call stack size exceeded>
 '''},
+{'date': 'Sat Jul 25 2020 12:58:40.863 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 86
+    teste().vai()    
+  module <module> line 50
+    gabi = Cena(elementof, direita=self.CENA, esquerda = cena2())
+AttributeError: 'teste' object has no attribute 'CENA'
+'''},
