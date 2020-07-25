8db5f036288e461226f2e494cb1d40d4c7d5bcd0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-25 14:35:56 -0300

    Saved bravo/__log__.py @2020-07-25 17:35

--- bravo/__log__.py
+++ bravo/__log__.py
@@ -2578,3 +2578,21 @@ IndentationError: unexpected indent
     self.cena.vai()   
 AttributeError: 'oi' object has no attribute 'cena'
 '''},
+{'date': 'Sat Jul 25 2020 14:35:55.193 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 66
+    oi().vai()
+  module <module> line 62
+    self.cena.vai()   
+AttributeError: 'oi' object has no attribute 'cena'
+'''},
