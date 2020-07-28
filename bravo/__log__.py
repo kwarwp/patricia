28420091b11df0b49f79c9f3479ef21f0d6d6198
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-28 19:29:11 -0300

    Saved bravo/__log__.py @2020-07-28 22:29

--- bravo/__log__.py
+++ bravo/__log__.py
@@ -2766,3 +2766,10 @@ SyntaxError: invalid syntax
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Tue Jul 28 2020 19:29:12.533 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 67
+  def vai(self, *_): 
+  ^
+IndentationError: unexpected indent
+'''},
