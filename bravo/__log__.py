5fe16a0058733195db18c29016a35a8c778f860a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-28 19:20:46 -0300

    Saved bravo/__log__.py @2020-07-28 22:20

--- bravo/__log__.py
+++ bravo/__log__.py
@@ -2745,3 +2745,10 @@ IndentationError: expected an indented block
               ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Tue Jul 28 2020 19:20:47.795 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 19
+  def __init __(self):
+              ^
+SyntaxError: invalid syntax
+'''},
