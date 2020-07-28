b8ec3ef44ec34a07139a60f8c26b5a794e1874e3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-28 19:31:29 -0300

    Saved bravo/juliet.py @2020-07-28 22:31

--- bravo/juliet.py
+++ bravo/juliet.py
@@ -8,6 +8,15 @@ Changelog
 ---------
 .. versionadded::    20.07
         Descreva o que você adicionou no código.
+        ''' NESSA PARTE O JOGADOR TERÁ QUE ESCOLHER DUAS OPÇÕES DE COMO SE DECLARA UM ELEMENTO
+    SE O JOGADOR ESCOLHER A OPÇÃO DO CÓDIGO CERTO, ELE IRÁ CONSEGUIR CRIAR UM ELEMENTO
+       ''' SOBRE A OPÇÃO ERRADA:
+        - SE O JOGADOR ESCOLHER A OPÇÃO QUE TEM O CÓDIGO ERRADO, ELE IRÁ PARA UMA OUTRA CENA EXPLICANOD O ERRO
+        - NESSES CÓDIGOS HÁ:
+              A DECLARAÇÃO DO CÓDIGO ERRADO
+              A CENA QUE IRÁ CORRIGIR O CÓDIGO
+              E UM EMOJI DE NEGATIVO
+               '''elemento que está na cena 3 que foi direcionado pela op2, mas irá direcionar p cena2
 
 """
 from _spy.vitollino.main import Cena, Elemento, Labirinto, Texto, Codigo, Sala, STYLE
@@ -16,53 +25,48 @@ STYLE["width"] = 1150
 STYLE["height"] = "550px"
 
 class oi():
-    ''' NESSA PARTE O JOGADOR TERÁ QUE ESCOLHER DUAS OPÇÕES DE COMO SE DECLARA UM ELEMENTO
-    SE O JOGADOR ESCOLHER A OPÇÃO DO CÓDIGO CERTO, ELE IRÁ CONSEGUIR CRIAR UM ELEMENTO'''
-
+     def __init__(self):
     #primeira cena (onde está localizado as duas opções
-    cena = Cena( img = "https://i.imgur.com/S5tAUe8.png")
+        self.cena = Cena( img = "https://i.imgur.com/S5tAUe8.png")
     #opção 2 certo
-    op2 = Elemento ( img = "https://i.imgur.com/dVn8Gfo.png" , x = 700, y=350, w = 450, h = 150 , tit= "Clique")
+        op2 = Elemento ( img = "https://i.imgur.com/dVn8Gfo.png" , x = 700, y=350, w = 450, h = 150 , tit= "Clique")
     #cena que direciona a opção 2
-    cena2 = Cena ( img = "https://i.imgur.com/IYekltO.png")
+        cena2 = Cena ( img = "https://i.imgur.com/IYekltO.png")
     #emoji de ok
-    ok= Elemento (img = "https://i.imgur.com/jR7lluz.png")
-    ok.entra(cena2)
-    ok.vai=cena.vai
+        ok= Elemento (img = "https://i.imgur.com/jR7lluz.png")
+        ok.entra(cena2)
+        ok.vai=cena.vai
     
-    ''' SOBRE A OPÇÃO ERRADA:
-        - SE O JOGADOR ESCOLHER A OPÇÃO QUE TEM O CÓDIGO ERRADO, ELE IRÁ PARA UMA OUTRA CENA EXPLICANOD O ERRO
-        - NESSES CÓDIGOS HÁ:
-              A DECLARAÇÃO DO CÓDIGO ERRADO
-              A CENA QUE IRÁ CORRIGIR O CÓDIGO
-              E UM EMOJI DE NEGATIVO'''
+
     
     #opção 1 errado
-    op1 = Elemento ( img = "https://i.imgur.com/lN2umME.png", x = 150, y=350, w = 450, h = 150, tit= "Clique")
-    #cena que direciona a opção1
-    cena3 = Cena ( img = "https://i.imgur.com/aN24UEx.png")
-    errado = Elemento (img = "https://i.imgur.com/elvddXi.png")
-    errado.entra(cena3)
-    errado.vai= cena.vai
-    '''elemento que está na cena 3 que foi direcionado pela op2, mas irá direcionar p cena2'''
+        op1 = Elemento ( img = "https://i.imgur.com/lN2umME.png", x = 150, y=350, w = 450, h = 150, tit= "Clique")
+        #cena que direciona a opção1
+        cena3 = Cena ( img = "https://i.imgur.com/aN24UEx.png")
+        errado = Elemento (img = "https://i.imgur.com/elvddXi.png")
+        errado.entra(cena3)
+        errado.vai= cena.vai
 
-    personagem_mulher_maravilha = Elemento(img = "https://i.imgur.com/fK906ve.png",style=dict(left=40, top=300, width=400, height="200px"))   
-    personagem_mulher_maravilha.entra(cena2)
-    ok= Elemento (img = "https://i.imgur.com/jR7lluz.png")
+
+        personagem_mulher_maravilha = Elemento(img = "https://i.imgur.com/fK906ve.png",style=dict(left=40, top=300, width=400, height="200px"))   
+        personagem_mulher_maravilha.entra(cena2)
+        ok= Elemento (img = "https://i.imgur.com/jR7lluz.png")
 
     
     
     
-    op2.entra(cena)
-    op1.entra(cena)
+        op2.entra(cena)
+        op1.entra(cena)
     
     
     #certo.entra(cena3)
     #certo.vai=cena2.vai
-    op2.vai=cena2.vai
-    op1.vai=cena3.vai
+        op2.vai=cena2.vai
+        op1.vai=cena3.vai
     
-    cena.vai()
+    def vai(self, *_): 
+        self.cena.vai()
+
     
 if __name__ == "__main__":
     oi().vai()
