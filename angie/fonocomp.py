ce93d2a6f2bf41589f49f395bfae44196810202d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-29 17:36:15 -0300

    Saved angie/fonocomp.py @2020-07-29 20:36

--- angie/fonocomp.py
+++ angie/fonocomp.py
@@ -12,8 +12,8 @@ Changelog
 """
 from _spy.vitollino.main import Cena, Elemento, Texto
 
-#link_balao = "https://www.google.com/imgres?imgurl=https%3A%2F%2Fstatic.vecteezy.com%2Fsystem%2Fresources%2Fthumbnails%2F000%2F235%2F346%2Foriginal%2Fspeech-bubble-pop-art-comic-book-background.jpg&imgrefurl=https%3A%2F%2Fpt.vecteezy.com%2Farte-vetorial%2F235346-fundo-de-quadrinhos-do-discurso-bolha-pop-art&tbnid=a4iHDHXihmIeBM&vet=12ahUKEwiEpeXhovPqAhWDCLkGHTYCCdYQMygcegUIARDPAQ..i&docid=d0bWFVVCq0leWM&w=3601&h=3601&q=historia%20em%20quadrinhos%20vazia%20desenho&safe=active&ved=2ahUKEwiEpeXhovPqAhWDCLkGHTYCCdYQMygcegUIARDPAQ"
-#link_balao2 = "https://image.freepik.com/vetores-gratis/conjunto-de-historia-em-quadrinhos-com-bolhas-de-bate-papo_1017-13655.jpg"
+link_balao = "https://www.google.com/imgres?imgurl=https%3A%2F%2Fstatic.vecteezy.com%2Fsystem%2Fresources%2Fthumbnails%2F000%2F235%2F346%2Foriginal%2Fspeech-bubble-pop-art-comic-book-background.jpg&imgrefurl=https%3A%2F%2Fpt.vecteezy.com%2Farte-vetorial%2F235346-fundo-de-quadrinhos-do-discurso-bolha-pop-art&tbnid=a4iHDHXihmIeBM&vet=12ahUKEwiEpeXhovPqAhWDCLkGHTYCCdYQMygcegUIARDPAQ..i&docid=d0bWFVVCq0leWM&w=3601&h=3601&q=historia%20em%20quadrinhos%20vazia%20desenho&safe=active&ved=2ahUKEwiEpeXhovPqAhWDCLkGHTYCCdYQMygcegUIARDPAQ"
+link_balao2 = "https://image.freepik.com/vetores-gratis/conjunto-de-historia-em-quadrinhos-com-bolhas-de-bate-papo_1017-13655.jpg"
 
 #def __init__(self):
 #    self.ambiente = Cena(img = self.link_balao)
