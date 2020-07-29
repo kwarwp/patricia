ea041ae43835fee76a98836df5b00462407344f1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-29 17:40:43 -0300

    Saved angie/__log__.py @2020-07-29 20:40

--- angie/__log__.py
+++ angie/__log__.py
@@ -394,3 +394,21 @@ NameError: name 'Eventos' is not defined
     vai()
 TypeError: vai() missing 1 positional argument: self
 '''},
+{'date': 'Wed Jul 29 2020 17:40:42.781 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 28
+    vai(self)
+  module <module> line 24
+    self.ambiente.vai()
+AttributeError: 'GUI' object has no attribute 'ambiente'
+'''},
