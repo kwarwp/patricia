58dd87b671df294e1cfe0b2a53bcb65931bb692a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-29 14:26:53 -0300

    Saved delta/__log__.py @2020-07-29 17:26

--- delta/__log__.py
+++ delta/__log__.py
@@ -3976,3 +3976,21 @@ NameError: name 'chama' is not defined
     self.solucao = Elemento(PEGADINHA, tit="MOUSE", x=380, y=285, w=60, h=60, vai=chama, cena =self.inicio)
 NameError: name 'chama' is not defined
 '''},
+{'date': 'Wed Jul 29 2020 14:26:52.682 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 53
+    start().vai()
+  module <module> line 40
+    self.solucao = Elemento(PEGADINHA, tit="MOUSE", x=380, y=285, w=60, h=60, vai=chama(), cena =self.inicio)
+NameError: name 'chama' is not defined
+'''},
