0208cca6e901f44ecf0f52b881df46d24b0fe25a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-29 13:48:25 -0300

    Saved delta/__log__.py @2020-07-29 16:48

--- delta/__log__.py
+++ delta/__log__.py
@@ -3822,3 +3822,21 @@ SyntaxError: invalid syntax
     self.pegadinha = Elemento(PEGADINHA, tit="PLAY", style=dict(x=100, y=550, width=60), cena = self.inicio())
 NameError: name 'Elemento' is not defined
 '''},
+{'date': 'Wed Jul 29 2020 13:48:23.891 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 49
+    start().vai()
+  module <module> line 35
+    self.pegadinha = Elemento(PEGADINHA, tit="PLAY", style=dict(x=100, y=550, width=60), cena = self.inicio())
+AttributeError: 'start' object has no attribute 'inicio'
+'''},
