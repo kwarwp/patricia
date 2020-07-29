8a427c3b077c842c00aca3395f5edb9969f16378
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-29 17:46:11 -0300

    Saved angie/__log__.py @2020-07-29 20:46

--- angie/__log__.py
+++ angie/__log__.py
@@ -466,3 +466,19 @@ AttributeError: 'GUI' object has no attribute 'link_balao'
     self.ambiente = Cena(self.link_balao2)
 AttributeError: 'GUI' object has no attribute 'link_balao2'
 '''},
+{'date': 'Wed Jul 29 2020 17:46:09.980 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 23
+    vai()
+TypeError: vai() missing 1 positional argument: self
+'''},
