2bd2567f70c937ccfe84010af5d452f21c528c02
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-29 15:27:02 -0300

    Saved delta/__log__.py @2020-07-29 18:27

--- delta/__log__.py
+++ delta/__log__.py
@@ -4010,3 +4010,21 @@ NameError: name 'chama' is not defined
     start().vai()
 TypeError: __init__() missing 1 positional argument: chama
 '''},
+{'date': 'Wed Jul 29 2020 15:27:00.845 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 55
+    start().vai()
+  module <module> line 42
+    self.solucao.bind("<Button-1>", self.chama)
+AttributeError: 'Elemento' object has no attribute 'bind'
+'''},
