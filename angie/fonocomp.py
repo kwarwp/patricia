e1739f2047f9cd157cd347d75d3b0dcb7782086f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-29 16:28:14 -0300

    Created angie/fonocomp.py @2020-07-29 19:28

---
+++ angie/fonocomp.py
@@ -0,0 +1,18 @@
+# patricia.angie.fonocomp.py
+# SPDX-License-Identifier: GPL-3.0-or-later
+""" Projeto Fonocomp
+
+.. codeauthor:: Monica Novellino <monicanovellino@gmail.com>
+
+Changelog
+---------
+.. versionadded::    20.07
+        Página inicial Web.
+
+"""
+
+def __init__(self):
+       print ("teste")
+       
+if __name__ == "__main__":
+    print ("teste")
