6325e97fad69c6dc06f458104b9f2e8373d4ccb4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-29 17:30:05 -0300

    Saved angie/__log__.py @2020-07-29 20:30

--- angie/__log__.py
+++ angie/__log__.py
@@ -339,3 +339,10 @@ SyntaxError: invalid syntax
     cena_historia = cena("https://www.google.com/imgres?imgurl=https%3A%2F%2Fstatic.vecteezy.com%2Fsystem%2Fresources%2Fthumbnails%2F000%2F235%2F346%2Foriginal%2Fspeech-bubble-pop-art-comic-book-background.jpg&imgrefurl=https%3A%2F%2Fpt.vecteezy.com%2Farte-vetorial%2F235346-fundo-de-quadrinhos-do-discurso-bolha-pop-art&tbnid=a4iHDHXihmIeBM&vet=12ahUKEwiEpeXhovPqAhWDCLkGHTYCCdYQMygcegUIARDPAQ..i&docid=d0bWFVVCq0leWM&w=3601&h=3601&q=historia%20em%20quadrinhos%20vazia%20desenho&safe=active&ved=2ahUKEwiEpeXhovPqAhWDCLkGHTYCCdYQMygcegUIARDPAQ")
 NameError: name 'cena' is not defined
 '''},
+{'date': 'Wed Jul 29 2020 17:30:05.42 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 53
+  if self.boneco.x = 500 and self.boneco.y = 500:
+                    ^
+SyntaxError: invalid syntax
+'''},
