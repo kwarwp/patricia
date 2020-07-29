10a84e1e457ff02d79d3142da36477a1f052446a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-29 15:39:18 -0300

    Saved delta/__log__.py @2020-07-29 18:39

--- delta/__log__.py
+++ delta/__log__.py
@@ -4082,3 +4082,21 @@ TypeError: chama() missing 1 positional argument: event
     self.solucao.elt.bind("<Button-1>", chama())
 NameError: name 'chama' is not defined
 '''},
+{'date': 'Wed Jul 29 2020 15:39:17.403 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 56
+    start().vai()
+  module <module> line 43
+    self.solucao.elt.bind("<Button-1>", chama)
+NameError: name 'chama' is not defined
+'''},
