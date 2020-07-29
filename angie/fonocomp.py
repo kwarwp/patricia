3ca806c4b9a2c9419ebddd45a29dcd3acd016658
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-29 17:24:31 -0300

    Saved angie/fonocomp.py @2020-07-29 20:24

--- angie/fonocomp.py
+++ angie/fonocomp.py
@@ -19,4 +19,8 @@ def historia():
     cena_historia = cena(img = "https://www.google.com/imgres?imgurl=https%3A%2F%2Fstatic.vecteezy.com%2Fsystem%2Fresources%2Fthumbnails%2F000%2F235%2F346%2Foriginal%2Fspeech-bubble-pop-art-comic-book-background.jpg&imgrefurl=https%3A%2F%2Fpt.vecteezy.com%2Farte-vetorial%2F235346-fundo-de-quadrinhos-do-discurso-bolha-pop-art&tbnid=a4iHDHXihmIeBM&vet=12ahUKEwiEpeXhovPqAhWDCLkGHTYCCdYQMygcegUIARDPAQ..i&docid=d0bWFVVCq0leWM&w=3601&h=3601&q=historia%20em%20quadrinhos%20vazia%20desenho&safe=active&ved=2ahUKEwiEpeXhovPqAhWDCLkGHTYCCdYQMygcegUIARDPAQ")
     cena_historia.vai()
 
-historia()
+
+def __init__(self):
+    historia()
+if __name__ == "__main__":
+    historia()
