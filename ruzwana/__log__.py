3771702744e418ee6d75e33e49ff1ab8045221f3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-30 16:48:42 -0300

    Saved ruzwana/__log__.py @2020-07-30 19:48

--- ruzwana/__log__.py
+++ ruzwana/__log__.py
@@ -304,3 +304,10 @@ IndentationError: unexpected indent
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Thu Jul 30 2020 16:48:42.424 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 55
+  mapa = mapa.split()
+  ^
+IndentationError: unexpected indent
+'''},
