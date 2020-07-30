7c76d824989f1e5701b6a3876ef32422a299aae4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-30 16:14:05 -0300

    Saved angie/__log__.py @2020-07-30 19:14

--- angie/__log__.py
+++ angie/__log__.py
@@ -852,3 +852,21 @@ Traceback (most recent call last):
     cena = self.v.c(self.SOLO)
 AttributeError: 'Kwarwp' object has no attribute 'v'
 '''},
+{'date': 'Thu Jul 30 2020 16:14:04.602 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 48
+    Kwarwp().cria()
+  module <module> line 31
+    self.v = vitollino()
+TypeError: 'NoneType' object is not callable
+'''},
