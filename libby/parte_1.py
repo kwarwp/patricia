0b39d116a1379ea08d6693ff0977c50ca751a476
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-30 17:02:50 -0300

    Saved libby/parte_1.py @2020-07-30 20:02

--- libby/parte_1.py
+++ libby/parte_1.py
@@ -12,39 +12,49 @@ Changelog
 """
 
 MAPA_INICIO = """
-@....&
-......
-......
-.#.^..
+%%%%%%%
+%..%..&
+%.....%
+%^.%..%
+%%%%%%%
 """
 
 class Kwarwp():
-    """ Jogo para ensino de programação.
-
+    """ Arena onde os desafios ocorrem.
+    
         :param vitollino: Empacota o engenho de jogo Vitollino.
+        :param mapa: Um texto representando o mapa do desafio.
     """
     GLIFOS = {
-    "&": "https://i.imgur.com/dZQ8liT.jpg", # OCA
-    "^": "https://imgur.com/8jMuupz.png", # INDIO
-    "_": "https://i.imgur.com/sGoKfvs.jpg", # SOLO
-    "#": "https://imgur.com/ldI7IbK.png", # TORA
-    "@": "https://imgur.com/tLLVjfN.png", # PICHE
-    "~": "https://i.imgur.com/UAETaiP.gif", # CEU
-    "*": "https://i.imgur.com/PfodQmT.gif", # SOL
-    "%": "https://i.imgur.com/uwYPNlz.png", # CERCA
+    "&": "https://i.imgur.com/dZQ8liT.jpg",  # OCA
+    "^": "https://imgur.com/8jMuupz.png",  # INDIO
+    ".": "https://i.imgur.com/npb9Oej.png",  # VAZIO
+    "_": "https://i.imgur.com/sGoKfvs.jpg",  # SOLO
+    "#": "https://imgur.com/ldI7IbK.png",  # TORA
+    "@": "https://imgur.com/tLLVjfN.png",  # PICHE
+    "~": "https://i.imgur.com/UAETaiP.gif",  # CEU
+    "*": "https://i.imgur.com/PfodQmT.gif",  # SOL
+	"%": "https://i.imgur.com/uwYPNlz.png"  # CERCA
     }
-
+    
     def __init__(self, vitollino=None, mapa=MAPA_INICIO, medidas={}):
         self.v = vitollino()
+        """Cria um matriz com os elementos descritos em cada linha de texto"""
         mapa = mapa.split()
+        """Largura da casa da arena dos desafios, número de colunas no mapa"""
         self.lado, self.col = 100, len(mapa[0]) 
         self.cena = self.cria(mapa=mapa) if vitollino else None
-
+        
     def cria(self, mapa="  "):
+        """ Cria o ambiente de programação Kwarwp.
+            :param mapa: Um texto representando o mapa do desafio.
+        """
+        """Cria um cenário com imagem de terra de chão batido, céu e sol"""
         lado = self.lado
         cena = self.v.c(self.GLIFOS["_"])
         ceu = self.v.a(self.GLIFOS["~"], w=lado*self.col, h=lado, x=0, y=0, cena=cena)
-        sol = self.v.a(self.GLIFOS["*"], w=60, h=60, x=0, y=40, cena=cena)        
+        sol = self.v.a(self.GLIFOS["*"], w=60, h=60, x=0, y=40, cena=cena)
+        """Posiciona os elementos segundo suas posições i, j na matriz mapa"""
         [self.cria_elemento( x=i*lado, y=j*lado+lado, cena=cena)
             for j, linha in enumerate(mapa) for i, imagem in enumerate(linha)]
         cena.vai()
@@ -62,7 +72,7 @@ class Kwarwp():
 def main(vitollino):
     Kwarwp(vitollino)
         
-
+    
 if __name__ == "__main__":
     from _spy.vitollino.main import Jogo, STYLE
     STYLE.update(width=600, height="500px")
