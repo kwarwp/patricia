040645216d4e9da181fa9dcd893f9d23f82a6fe3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-30 15:54:13 -0300

    Created grace/kwarwp.py @2020-07-30 18:54

---
+++ grace/kwarwp.py
@@ -0,0 +1,12 @@
+# patricia.grace.kwarwp.py
+# SPDX-License-Identifier: GPL-3.0-or-later
+""" Meu Kwarwp.
+
+.. codeauthor:: Cibele Ribeiro <cici.rco@gmail.com>
+
+Changelog
+---------
+.. versionadded::    20.07
+        Descreva o que você adicionou no código.
+
+"""
