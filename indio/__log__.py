8a577211b5be569aa886bb135520f5824c7939c9
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-30 16:25:40 -0300

    Saved indio/__log__.py @2020-07-30 19:25

--- indio/__log__.py
+++ indio/__log__.py
@@ -106,3 +106,19 @@ Traceback (most recent call last):
   module <module> line 29
     from _spy.vitollino.main import Cena, Elemento, jogo
 '''},
+{'date': 'Thu Jul 30 2020 16:26:41.309 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 45
+    if __name__ == __main__:
+NameError: name '__main__' is not defined
+'''},
