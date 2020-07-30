136ca181c20d19d5a48514be5c1a903e9d928e43
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-30 16:08:48 -0300

    Created indio/__log__.py @2020-07-30 19:08

---
+++ indio/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Thu Jul 30 2020 16:09:45.807 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 13
+  """ Jogo para ensino de programação.
+  ^
+IndentationError: unexpected indent
+'''},
