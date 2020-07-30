9bf31757037f609dab64a549e1d955d1cc7012e6
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-30 16:08:47 -0300

    Saved stacy/kwarapp.py @2020-07-30 19:08

--- stacy/kwarapp.py
+++ stacy/kwarapp.py
@@ -10,4 +10,18 @@ Changelog
         Descreva o que você adicionou no código.
 
 """
-oi
+from _spy.vitollino.main import Cena,Elemento,Texto, STYLE
+
+STYLE["width"] = 1010
+STYLE["heigth"] = "500px"
+
+GLIFOS = {
+"&": "https://i.imgur.com/dZQ8liT.jpg",  # OCA ⛺
+"^": "https://imgur.com/8jMuupz.png",  # INDIO 
+".": "https://i.imgur.com/npb9Oej.png",  # VAZIO 
+"_": "https://i.imgur.com/sGoKfvs.jpg",  # SOLO 
+"#": "https://imgur.com/ldI7IbK.png",  # TORA 
+"@": "https://imgur.com/tLLVjfN.png",  # PICHE 
+"~": "https://i.imgur.com/UAETaiP.gif",  # CEU 
+"*": "https://i.imgur.com/PfodQmT.gif"  # SOL ☀
+}
