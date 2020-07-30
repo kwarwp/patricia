ca563a367e792c162271740789e7b6d50fbbbd81
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-30 15:54:12 -0300

    Created sucuri/kwarwp.py @2020-07-30 18:54

---
+++ sucuri/kwarwp.py
@@ -0,0 +1,12 @@
+# patricia.sucuri.kwarwp.py
+# SPDX-License-Identifier: GPL-3.0-or-later
+""" Projeto sem descrição, (mude esta linha).
+
+.. codeauthor:: Emanuelle Simas <ellesimas>
+
+Changelog
+---------
+.. versionadded::    20.07
+        Tentativa de ação sobre o kwarwp 
+
+"""
