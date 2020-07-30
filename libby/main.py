b6108dbec3cff193152a8b958f1c1b089b85edab
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-30 15:19:27 -0300

    Saved libby/main.py @2020-07-30 18:19

--- libby/main.py
+++ libby/main.py
@@ -9,13 +9,14 @@ MAPA_INICIO = """
 
 class Kwarwp():
     GLIFOS = {
-    OCA = "https://i.imgur.com/dZQ8liT.jpg"
-    INDIO = "https://imgur.com/8jMuupz.png"
-    SOLO = "https://i.imgur.com/sGoKfvs.jpg"
-    TORA = "https://imgur.com/ldI7IbK.png"
-    PICHE = "https://imgur.com/tLLVjfN.png"
-    CEU = "https://i.imgur.com/UAETaiP.gif"
-    SOL = "https://i.imgur.com/PfodQmT.gif"
+    "&": "https://i.imgur.com/dZQ8liT.jpg",  # OCA
+    "^": "https://imgur.com/8jMuupz.png",  # INDIO
+    ".": "https://i.imgur.com/npb9Oej.png",  # VAZIO
+    "_": "https://i.imgur.com/sGoKfvs.jpg",  # SOLO
+    "#": "https://imgur.com/ldI7IbK.png",  # TORA
+    "@": "https://imgur.com/tLLVjfN.png",  # PICHE
+    "~": "https://i.imgur.com/UAETaiP.gif",  # CEU
+    "*": "https://i.imgur.com/PfodQmT.gif"  # SOL
     }
 
     def __init__(self, vitollino=None, cenario="default"):
