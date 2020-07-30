bbf4ce3c089413f943c9bb87586252ab2f7aeed8
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-30 15:47:46 -0300

    Saved roxanne/main.py @2020-07-30 18:47

--- roxanne/main.py
+++ roxanne/main.py
@@ -1,34 +1,32 @@
+from _spy.vitollino.main import Jogo, STYLE
+STYLE.update(width=600, height="500px")
+
 MAPA_INICIO = """
-%%%%%%%
-%..%..&
-%.....%
-%^.%..%
-%%%%%%%
+@....&
+......
+......
+.#.^..
 """
 
 class Kwarwp():
     GLIFOS = {
-    "&": "https://i.imgur.com/dZQ8liT.jpg",  # OCA ⛺
-    "^": "https://imgur.com/8jMuupz.png",  # INDIO 
-    ".": "https://i.imgur.com/npb9Oej.png",  # VAZIO 
-    "_": "https://i.imgur.com/sGoKfvs.jpg",  # SOLO 
-    "#": "https://imgur.com/ldI7IbK.png",  # TORA 
-    "@": "https://imgur.com/tLLVjfN.png",  # PICHE 
-    "~": "https://i.imgur.com/UAETaiP.gif",  # CEU 
-    "*": "https://i.imgur.com/PfodQmT.gif",  # SOL ☀
-    "%": "https://i.imgur.com/uwYPNlz.png"  # CERCA
+    "&": "https://i.imgur.com/dZQ8liT.jpg",  # OCA
+    "^": "https://imgur.com/8jMuupz.png",  # INDIO
+    ".": "https://i.imgur.com/npb9Oej.png",  # VAZIO
+    "_": "https://i.imgur.com/sGoKfvs.jpg",  # SOLO
+    "#": "https://imgur.com/ldI7IbK.png",  # TORA
+    "@": "https://imgur.com/tLLVjfN.png",  # PICHE
+    "~": "https://i.imgur.com/UAETaiP.gif",  # CEU
+    "*": "https://i.imgur.com/PfodQmT.gif"  # SOL
     }
     
     def __init__(self, vitollino=None, mapa=MAPA_INICIO, medidas={}):
         self.v = vitollino()
-        """Cria um matriz com os elementos descritos em cada linha de texto"""
         mapa = mapa.split()
-        """Largura da casa da arena dos desafios, número de colunas no mapa"""
-        self.lado, self.col = 100, len(mapa[0])
+        self.lado, self.col = 100, len(mapa[0]) 
         self.cena = self.cria(mapa=mapa) if vitollino else None
         
     def cria(self, mapa="  "):
-        """Cria um cenário com imagem de terra de chão batido, céu e sol"""
         lado = self.lado
         cena = self.v.c(self.GLIFOS["_"])
         ceu = self.v.a(self.GLIFOS["~"], w=lado*self.col, h=lado, x=0, y=0, cena=cena)
@@ -41,14 +39,7 @@ class Kwarwp():
     def cria_elemento(self, x, y, cena):
         lado = self.lado
         return self.v.a(self.GLIFOS[imagem], w=lado, h=lado, x=i*lado, y=j*lado+lado, cena=cena)
-        
 
 def main(vitollino):
     Kwarwp(vitollino)
-        
-    
-if __name__ == "__main__":
-    from _spy.vitollino.main import Jogo, STYLE
-    STYLE.update(width=600, height="500px")
-    main(Jogo)
         
