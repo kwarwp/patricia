25db2d41991f7dd7913094ebf05d0181b660d698
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-30 15:08:57 -0300

    Saved roxanne/__log__.py @2020-07-30 18:08

--- roxanne/__log__.py
+++ roxanne/__log__.py
@@ -782,3 +782,19 @@ SyntaxError: invalid syntax : triple string end not found
    ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Thu Jul 30 2020 15:08:44.487 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 43
+    Kwarwp(vitollino)
+NameError: name 'vitollino' is not defined
+'''},
