7b66c2e9817a67420e28d3e1dd7aaca7bb7ea275
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-30 11:10:53 -0300

    Saved bravo/hotel.py @2020-07-30 14:10

--- bravo/hotel.py
+++ bravo/hotel.py
@@ -47,31 +47,39 @@ cena()'''
 
 # patricia.bravo.hotel.py
 # SPDX-License-Identifier: GPL-3.0-or-later
-""" Projeto sem descrição, (mude esta linha).
-
+""" Projeto:: Um jogo de programação no SuperPython.
 .. codeauthor:: Rosilane Lessa <rosilanefonte@gmail.com>
 
 Changelog
 ---------
 .. versionadded::    20.07
-        Descreva o que você adicionou no código.
+        A classe Cena explicará como construir uma CENA.
 
  CENA
 
 """
+""" Importando as bibliotecas;"""
 from _spy.vitollino.main import Cena, Elemento, Labirinto, Texto, Codigo, Sala, STYLE
 from _spy.vitollino.main import INVENTARIO as inv
 
+""" Definindo o tamanho da tela """
 STYLE["width"] = 1250
 STYLE["height"]= "550px"
 
+""" Iniciando a Classe Cena."""
 class cena:
     def __init__(self):
+ 	""" Imagem de explicação"""
         self.expli= expli= Cena(img = "https://i.imgur.com/oHAC3Xi.png")
+        """ Imagem de fundo"""
         fundo= Cena (img = "https://i.imgur.com/hexAWk7.png")
+        """ Imagem de erro"""
         erro= Cena (img = "https://i.imgur.com/idqEr5C.png")
+        """ Imagem de casa"""
         casa= Cena (img = "https://plantasdecasas.com/storage/2017/07/planta-fachada-casa-115-fr-AG.jpg")
+        """ Imagem de retorno sem sucesso"""
         triste = Elemento (img= "https://i.imgur.com/0R5Xo83.png", tit="CLICK", x=1000, y=100)
+        """ Imagem de de retorno com sucesso"""
         feliz = Elemento (img= "https://i.imgur.com/UaihdhW.png", tit="CLICK", x=40, y=80)
         codigo1 = Elemento (img="https://i.imgur.com/QGEgNAt.png", tit="CLICK", style=dict(left=40, top=350, width=500, height="200px"))
         codigo2 = Elemento (img="https://i.imgur.com/ccJqbMb.png", tit="CLICK", style=dict(left=620, top=350, width=500, height="200px"))
