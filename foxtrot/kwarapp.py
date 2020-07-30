b5087f18596f2daff95e65bdd0edcaa6b2113835
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-30 15:52:02 -0300

    Saved foxtrot/kwarapp.py @2020-07-30 18:52

--- foxtrot/kwarapp.py
+++ foxtrot/kwarapp.py
@@ -15,6 +15,16 @@ from _spy.vitollino.main import Cena,Elemento,Texto, STYLE
 STYLE["width"] = 1010
 STYLE["heigth"] = "500px"
 
+"""imagens"""
+"&": "https://i.imgur.com/dZQ8liT.jpg",  # OCA ⛺
+"^": "https://imgur.com/8jMuupz.png",  # INDIO 
+".": "https://i.imgur.com/npb9Oej.png",  # VAZIO 
+"_": "https://i.imgur.com/sGoKfvs.jpg",  # SOLO 
+"#": "https://imgur.com/ldI7IbK.png",  # TORA 
+"@": "https://imgur.com/tLLVjfN.png",  # PICHE 
+"~": "https://i.imgur.com/UAETaiP.gif",  # CEU 
+"*": "https://i.imgur.com/PfodQmT.gif"  # SOL ☀
+
 MAPA_INICIO ="""
 @....&
 ......
