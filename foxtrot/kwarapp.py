1952ed27331752db1563d676df3a198caf85f90b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-30 15:43:21 -0300

    Created foxtrot/kwarapp.py @2020-07-30 18:43

---
+++ foxtrot/kwarapp.py
@@ -0,0 +1,24 @@
+# patricia.foxtrot.kwarapp.py
+# SPDX-License-Identifier: GPL-3.0-or-later
+""" Projeto sem descrição, (mude esta linha).
+
+.. codeauthor:: Marília Campos Galvão <mail@local.tipo>
+
+Changelog
+---------
+.. versionadded::    20.07
+        Descreva o que você adicionou no código.
+
+"""
+from _spy.vitollino.main import Cena,Elemento,Texto, STYLE
+
+STYLE["width"] = 1010
+STYLE["heigth"] = "500px"
+
+MAPA_INICIO ="""
+@....&
+......
+......
+.#.^..
+"""
+class Kwarwp():
