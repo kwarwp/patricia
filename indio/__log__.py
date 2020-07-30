e88ecd6a9227236078c1c11c80ffa4194e26141a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-30 16:10:18 -0300

    Saved indio/__log__.py @2020-07-30 19:10

--- indio/__log__.py
+++ indio/__log__.py
@@ -6,3 +6,10 @@
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Thu Jul 30 2020 16:11:17.487 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 13
+  """ Jogo para ensino de programação.
+  ^
+IndentationError: unexpected indent
+'''},
