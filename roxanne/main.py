83f30b510335ac808add3f26cfaa1ef908a95d2c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-30 15:53:39 -0300

    Saved roxanne/main.py @2020-07-30 18:53

--- roxanne/main.py
+++ roxanne/main.py
@@ -21,7 +21,7 @@ class Kwarwp():
     "@": "https://imgur.com/tLLVjfN.png",  # PICHE
     "~": "https://i.imgur.com/UAETaiP.gif",  # CEU
     "*": "https://i.imgur.com/PfodQmT.gif"  # SOL
-	"%": "https://i.imgur.com/uwYPNlz.png"  # CERCA
+    "%": "https://i.imgur.com/uwYPNlz.png"  # CERCA
     }
     
     def __init__(self, vitollino=None, mapa=MAPA_INICIO, medidas={}):
