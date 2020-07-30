cec986d5394b2a516a5986f7ca567d15eeb6c26d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-30 14:56:06 -0300

    Saved grace/main.py @2020-07-30 17:56

--- grace/main.py
+++ grace/main.py
@@ -1,28 +1,13 @@
 # patricia.grace.main.py
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-""" Jogo do Mistério da Praia.
+""" coloque algo aqui.
 
-    Agora você desceu para a praia.
+    coloque algo aqui.
 
 Changelog
 ---------
     20.07
-        * NEW: O jogo original.
+        * NEW: coloque algo aqui.
 
 """
-from _spy.vitollino.main import Cena
-__version__ = "20.07"
-__author__ = "Carlo"
-CENA_PRAIA = "https://i.imgur.com/wz5XKcr.jpg"
-
-
-class Praia:
-    """ Representa uma cena da calçada da praia """
-    def vai(self):
-        """ Mostra a cena da praia """
-        Cena(CENA_PRAIA).vai()
-    
-
-if __name__ == "__main__":
-    Praia().vai()
