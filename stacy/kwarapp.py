916d844f99a460d98ad106b13e4de4b9d4496c5d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-30 17:01:30 -0300

    Saved stacy/kwarapp.py @2020-07-30 20:01

--- stacy/kwarapp.py
+++ stacy/kwarapp.py
@@ -75,6 +75,11 @@ class Kwarwp():
         cerca = self.v.a(self.CERCA, w=100, h=100, x=500, y=300, cena=cena)
         cerca = self.v.a(self.CERCA, w=100, h=100, x=500, y=200, cena=cena)
         cerca = self.v.a(self.CERCA, w=100, h=100, x=500, y=0, cena=cena)
+        cerca = self.v.a(self.CERCA, w=100, h=100, x=400, y=0, cena=cena)
+        cerca = self.v.a(self.CERCA, w=100, h=100, x=300, y=0, cena=cena)
+        cerca = self.v.a(self.CERCA, w=100, h=100, x=200, y=0, cena=cena)
+        cerca = self.v.a(self.CERCA, w=100, h=100, x=100, y=0, cena=cena)
+        cerca = self.v.a(self.CERCA, w=100, h=100, x=0, y=0, cena=cena)
         cena.vai()
         return cena
         
