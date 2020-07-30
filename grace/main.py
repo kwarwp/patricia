527fccce9bad6100533d40117d2171c90c7f20ba
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-30 15:10:59 -0300

    Saved grace/main.py @2020-07-30 18:10

--- grace/main.py
+++ grace/main.py
@@ -1,5 +1,16 @@
 # patricia.grace.main.py
 # SPDX-License-Identifier: GPL-3.0-or-later
+
+""" coloque algo aqui.
+
+    coloque algo aqui.
+
+Changelog
+---------
+    20.07
+        * NEW: coloque algo aqui.
+
+"""
 class Kwarwp():
     """ Jogo para ensino de programação.
 
