591623ee38c7bfc938cf4b1154dc38d4fdd653e5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-30 16:17:10 -0300

    Saved stacy/kwarapp.py @2020-07-30 19:17

--- stacy/kwarapp.py
+++ stacy/kwarapp.py
@@ -25,3 +25,12 @@ GLIFOS = {
 "~": "https://i.imgur.com/UAETaiP.gif",  # CEU 
 "*": "https://i.imgur.com/PfodQmT.gif"  # SOL ☀
 }
+
+class Kwarwp():
+    def __init__(self, vitollino=None, mapa=MAPA_INICIO, medidas={}):
+    self.v = vitollino()
+    """Cria um matriz com os elementos descritos em cada linha de texto"""
+    mapa = mapa.split()
+    """Largura da casa da arena dos desafios, número de colunas no mapa"""
+    self.lado, self.col = 100, len(mapa[0])
+    self.cena = self.cria(mapa=mapa) if vitollino else None
