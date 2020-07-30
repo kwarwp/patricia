b2a14b958cb124511dca376bb546854bf97adba3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-30 16:59:11 -0300

    Saved libby/__log__.py @2020-07-30 19:59

--- libby/__log__.py
+++ libby/__log__.py
@@ -330,3 +330,27 @@ KeyError: .
     return self.v.a(self.GLIFOS[imagem], w=lado, h=lado, x=i*lado, y=j*lado+lado, cena=cena)
 KeyError: .
 '''},
+{'date': 'Thu Jul 30 2020 16:59:04.369 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 70
+    main(Jogo)
+  module <module> line 65
+    Kwarwp(vitollino)
+  module <module> line 43
+    self.cena = self.cria(mapa=mapa) if vitollino else None
+  module <module> line 50
+    [self.cria_elemento( x=i*lado, y=j*lado+lado, cena=cena)
+  module <module> line 62
+    return self.v.a(self.GLIFOS[imagem], w=lado, h=lado, x=i*lado, y=j*lado+lado, cena=cena)
+KeyError: .
+'''},
