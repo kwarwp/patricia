78585adf0d11e4d0c293e6a1aa3cbe0a4d5c67e2
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-30 16:51:21 -0300

    Saved libby/__log__.py @2020-07-30 19:51

--- libby/__log__.py
+++ libby/__log__.py
@@ -186,3 +186,27 @@ IndentationError: unexpected indent
     return self.v.a(self.GLIFOS[imagem], w=lado, h=lado, x=i*lado, y=j*lado+lado, cena=cena)
 KeyError: .
 '''},
+{'date': 'Thu Jul 30 2020 16:51:13.401 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 66
+    main(Jogo)
+  module <module> line 61
+    Kwarwp(vitollino)
+  module <module> line 43
+    self.cena = self.cria(mapa=mapa) if vitollino else None
+  module <module> line 51
+    [self.cria_elemento( x=i*lado, y=j*lado+lado, cena=cena)
+  module <module> line 58
+    return self.v.a(self.GLIFOS[imagem], w=lado, h=lado, x=i*lado, y=j*lado+lado, cena=cena)
+KeyError: .
+'''},
