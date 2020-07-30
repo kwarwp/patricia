672f007c19c8ed372f93f99a43ae5e6f5030ce36
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-30 15:54:08 -0300

    Created adda/kwarwp.py @2020-07-30 18:54

---
+++ adda/kwarwp.py
@@ -0,0 +1,12 @@
+# patricia.adda.kwarwp.py
+# SPDX-License-Identifier: GPL-3.0-or-later
+""" Meu Kwarwp.
+
+.. codeauthor:: Carlo Oliviera <mail@local.tipo>
+
+Changelog
+---------
+.. versionadded::    20.07
+        Descreva o que você adicionou no código.
+
+"""
