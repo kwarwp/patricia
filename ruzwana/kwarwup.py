ba81c8b5bf094abc50d9cf36401ca320488c1ded
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-30 16:10:28 -0300

    Created ruzwana/kwarwup.py @2020-07-30 19:10

---
+++ ruzwana/kwarwup.py
@@ -0,0 +1,53 @@
+# patricia.ruzwana.kwarwup.py
+# SPDX-License-Identifier: GPL-3.0-or-later
+""" Projeto sem descrição, (mude esta linha).
+
+.. codeauthor:: Nome Sobrenome <mail@local.tipo>
+
+Changelog
+---------
+.. versionadded::    20.07
+        Descreva o que você adicionou no código.
+
+"""
+#from _spy.vitollino.main import * #Cena, Elemento, STYLE, Texto
+
+#import _spy.vitollino.main 
+class Kwarwp():
+    """ Jogo para ensino de programação.
+
+
+
+        :param vitollino: Empacota o engenho de jogo Vitollino.
+    """
+    OCA = "https://i.imgur.com/dZQ8liT.jpg"
+    INDIO = "https://imgur.com/8jMuupz.png"
+    SOLO = "https://i.imgur.com/sGoKfvs.jpg"
+    TORA = "https://imgur.com/ldI7IbK.png"
+    PICHE = "https://imgur.com/tLLVjfN.png"
+    CEU = "https://i.imgur.com/UAETaiP.gif"
+    SOL = "https://i.imgur.com/PfodQmT.gif"
+    toras = "https://i.imgur.com/uwYPNlz.png"
+    cerca = "https://i.imgur.com/aRZeyfo.png"
+
+    def __init__(self, vitollino=None, cenario="default"):
+        self.v = vitollino()
+        self.cena = self.cria(cenario=cenario) if vitollino else None
+
+    def cria(self, cenario="default"):
+        """ Cria o ambiente de programação Kwarwp."""
+        cena = self.v.c(self.SOLO)
+        indio = self.v.a(self.INDIO, w=100, h=100, x=300, y=400, cena=cena)
+        oca = self.v.a(self.OCA, w=100, h=100, x=500, y=100, cena=cena)
+        tora = self.v.a(self.TORA, w=100, h=100, x=100, y=400, cena=cena)
+        piche = self.v.a(self.PICHE, w=100, h=100, x=100, y=100, cena=cena)
+        piche = self.v.a(self.CEU, w=600, h=100, x=0, y=0, cena=cena)
+        sol = self.v.a(self.SOL, w=60, h=60, x=0, y=40, cena=cena)
+        cena.vai()
+        return cena
+        
+     
+if __name__ == "__main__":
+    from _spy.vitollino.main import Jogo, STYLE
+    Kwarwp(Jogo).cria()
+#    Kwarwp()
