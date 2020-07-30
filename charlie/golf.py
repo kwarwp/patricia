333b7e73d4324a6efc3a8d23d0ce51730c1064f0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-30 15:24:03 -0300

    Created charlie/golf.py @2020-07-30 18:24

---
+++ charlie/golf.py
@@ -0,0 +1,19 @@
+# patricia.charlie.golf.py
+# SPDX-License-Identifier: GPL-3.0-or-later
+""" Projeto sem descrição, (mude esta linha).
+
+.. codeauthor:: Nome Sobrenome <mail@local.tipo>
+
+Changelog
+---------
+.. versionadded::    20.07
+        Descreva o que você adicionou no código.
+
+"""
+MAPA_INICIO ="""
+@....&
+......
+......
+.#.^..
+"""
+class Kwarwp():
