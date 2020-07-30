7b00b3f9d3219b261ab2eb281e7ce60dc8b45efc
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-30 16:21:39 -0300

    Saved libby/main.py @2020-07-30 19:21

--- libby/main.py
+++ libby/main.py
@@ -20,20 +20,21 @@ class Kwarwp():
     def cria(self, cenario="default"):
         """ Cria o ambiente de programação Kwarwp."""
         cena = self.v.c(self.SOLO)
-        indio = self.v.a(self.INDIO, w=100, h=100, x=300, y=400, cena=cena)
+        indio = self.v.a(self.INDIO, w=100, h=100, x=100, y=400, cena=cena)
         oca = self.v.a(self.OCA, w=100, h=100, x=500, y=100, cena=cena)
         tora = self.v.a(self.TORA, w=100, h=100, x=100, y=250, cena=cena)
         piche = self.v.a(self.PICHE, w=100, h=100, x=0, y=100, cena=cena)
         ceu = self.v.a(self.CEU, w=600, h=100, x=0, y=0, cena=cena)
         sol = self.v.a(self.SOL, w=60, h=60, x=0, y=40, cena=cena)
+
         cerca = self.v.a(self.CERCA, w=100, h=100, x=300, y=100, cena=cena)
         cerca = self.v.a(self.CERCA, w=100, h=100, x=300, y=200, cena=cena)
-        cerca = self.v.a(self.CERCA, w=100, h=100, x=300, y=300, cena=cena)
         cerca = self.v.a(self.CERCA, w=100, h=100, x=300, y=400, cena=cena)
+        cerca = self.v.a(self.CERCA, w=100, h=100, x=300, y=500, cena=cena)
         
         cerca = self.v.a(self.CERCA, w=100, h=100, x=500, y=500, cena=cena)
         cerca = self.v.a(self.CERCA, w=100, h=100, x=400, y=500, cena=cena)
-        cerca = self.v.a(self.CERCA, w=100, h=100, x=300, y=500, cena=cena)
+        cerca = self.v.a(self.CERCA, w=100, h=100, x=200, y=500, cena=cena)
         cerca = self.v.a(self.CERCA, w=100, h=100, x=200, y=500, cena=cena)
         cerca = self.v.a(self.CERCA, w=100, h=100, x=100, y=500, cena=cena)
         
@@ -46,8 +47,7 @@ class Kwarwp():
         cerca = self.v.a(self.CERCA, w=100, h=100, x=100, y=100, cena=cena)
         cerca = self.v.a(self.CERCA, w=100, h=100, x=200, y=100, cena=cena)
         cerca = self.v.a(self.CERCA, w=100, h=100, x=400, y=100, cena=cena)
-        cerca = self.v.a(self.CERCA, w=100, h=100, x=500, y=100, cena=cena)
-        
+             
         cerca = self.v.a(self.CERCA, w=100, h=100, x=500, y=200, cena=cena)
         cerca = self.v.a(self.CERCA, w=100, h=100, x=500, y=300, cena=cena)
         cerca = self.v.a(self.CERCA, w=100, h=100, x=500, y=400, cena=cena)
