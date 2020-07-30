84c037f26de22bbe92cd962cdc97e8623ab617d7
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-30 16:15:33 -0300

    Saved sucuri/__log__.py @2020-07-30 19:15

--- sucuri/__log__.py
+++ sucuri/__log__.py
@@ -3,3 +3,7 @@
 'error': '''
 Exception: <TypeError: Cannot read property 'charAt' of undefined>
 '''},
+{'date': 'Thu Jul 30 2020 16:15:32.295 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+Exception: <TypeError: Cannot read property 'charAt' of undefined>
+'''},
