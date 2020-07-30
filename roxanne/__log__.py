f4907d32d604eb6a0d796b95b7fd227df7c52f24
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-30 15:55:45 -0300

    Saved roxanne/__log__.py @2020-07-30 18:55

--- roxanne/__log__.py
+++ roxanne/__log__.py
@@ -861,3 +861,10 @@ SyntaxError: invalid syntax
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Thu Jul 30 2020 15:55:31.811 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 16
+  GLIFOS = {
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            ^
+SyntaxError: invalid syntax
+'''},
