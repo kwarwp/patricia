a9feb525b75ce64c432a92b03d48726424ed099d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-30 16:47:41 -0300

    Saved indio/kwarwp.py @2020-07-30 19:47

--- indio/kwarwp.py
+++ indio/kwarwp.py
@@ -24,7 +24,7 @@ class Kwarwp():
     PICHE = "https://imgur.com/tLLVjfN.png"
     CEU = "https://i.imgur.com/UAETaiP.gif"
     SOL = "https://i.imgur.com/PfodQmT.gif"
-
+    CERCA = "https://i.imgur.com/uwYPNlz.png"
     def __init__(self, vitollino=None, cenario="default"):
         self.v = vitollino()
         self.cena = self.cria(cenario=cenario) if vitollino else None
@@ -38,6 +38,7 @@ class Kwarwp():
         piche = self.v.a(self.PICHE, w=100, h=100, x=100, y=100, cena=cena)
         piche = self.v.a(self.CEU, w=600, h=100, x=0, y=0, cena=cena)
         sol = self.v.a(self.SOL, w=60, h=60, x=0, y=40, cena=cena)
+        cerca = self.v.a(self.SOL, w=60, h=60, x=0, y=40, cena=cena)
         cena.vai()
         return cena
         
