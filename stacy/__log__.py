b62c148909759cc21b058de42eff09d8a437b292
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-30 17:19:01 -0300

    Saved stacy/__log__.py @2020-07-30 20:19

--- stacy/__log__.py
+++ stacy/__log__.py
@@ -31,3 +31,19 @@ AttributeError: 'module' object has no attribute 'mean'
     Kwarwp(jogo)
 NameError: name 'jogo' is not defined
 '''},
+{'date': 'Thu Jul 30 2020 17:19:00.310 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 98
+    Kwarwp(Jogo, mapa=MAPA_ROCHA)
+TypeError: __init__() got an unexpected keyword argument 'mapa'
+'''},
