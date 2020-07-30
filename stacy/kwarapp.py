856fb7ff68ddf0f36bfbb0d0685fbe88d199f3d0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-30 16:00:14 -0300

    Created stacy/kwarapp.py @2020-07-30 19:00

---
+++ stacy/kwarapp.py
@@ -0,0 +1,13 @@
+# patricia.stacy.kwarapp.py
+# SPDX-License-Identifier: GPL-3.0-or-later
+""" Projeto sem descrição, (mude esta linha).
+
+.. codeauthor:: Nome Sobrenome <mail@local.tipo>
+
+Changelog
+---------
+.. versionadded::    20.07
+        Descreva o que você adicionou no código.
+
+"""
+oi
