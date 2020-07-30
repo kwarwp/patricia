6e3ac15c1c4d11a77ff0efa8b477e4771dc37b85
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-30 17:06:35 -0300

    Saved soraya/kwarapp.py @2020-07-30 20:06

--- soraya/kwarapp.py
+++ soraya/kwarapp.py
@@ -35,29 +35,39 @@ GLIFOS = {
 }
 
 def __init__(self, vitollino=None, mapa=MAPA_INICIO, medidas={}):
-    self.v = vitollino()
-    """Cria um matriz com os elementos descritos em cada linha de texto"""
-    mapa = mapa.split()
-    """Largura da casa da arena dos desafios, número de colunas no mapa"""
-    self.lado, self.col = 100, len(mapa[0])
-    self.cena = self.cria(mapa=mapa) if vitollino else None
-    
-    def __init__(self, vitollino=None, cenario="default"):
         self.v = vitollino()
-        self.cena = self.cria(cenario=cenario) if vitollino else None
-
-    def cria(self, cenario="default"):
-        """ Cria o ambiente de programação Kwarwp."""
-        cena = self.v.c(self.SOLO)
-        indio = self.v.a(self.INDIO, w=100, h=100, x=300, y=400, cena=cena)
-        oca = self.v.a(self.OCA, w=100, h=100, x=500, y=100, cena=cena)
-        tora = self.v.a(self.TORA, w=100, h=100, x=100, y=400, cena=cena)
-        piche = self.v.a(self.PICHE, w=100, h=100, x=100, y=100, cena=cena)
-        piche = self.v.a(self.CEU, w=600, h=100, x=0, y=0, cena=cena)
-        sol = self.v.a(self.SOL, w=60, h=60, x=0, y=40, cena=cena)
+        """Cria um matriz com os elementos descritos em cada linha de texto"""
+        mapa = mapa.split()
+        """Largura da casa da arena dos desafios, número de colunas no mapa"""
+        self.lado, self.col = 100, len(mapa[0]) 
+        self.cena = self.cria(mapa=mapa) if vitollino else None
+        
+    def cria(self, mapa="  "):
+        """ Cria o ambiente de programação Kwarwp.
+            :param mapa: Um texto representando o mapa do desafio.
+        """
+        """Cria um cenário com imagem de terra de chão batido, céu e sol"""
+        lado = self.lado
+        cena = self.v.c(self.GLIFOS["_"])
+        ceu = self.v.a(self.GLIFOS["~"], w=lado*self.col, h=lado, x=0, y=0, cena=cena)
+        sol = self.v.a(self.GLIFOS["*"], w=60, h=60, x=0, y=40, cena=cena)
+        """Posiciona os elementos segundo suas posições i, j na matriz mapa"""
+        [self.cria_elemento( x=i*lado, y=j*lado+lado, cena=cena)
+            for j, linha in enumerate(mapa) for i, imagem in enumerate(linha)]
         cena.vai()
         return cena
-        
+
+    def cria_elemento(self, x, y, cena):
+        """ Cria um elemento na arena do Kwarwp na posição definida.
+            :param x: coluna em que o elemento será posicionado.
+            :param y: linha em que o elemento será posicionado.
+            :param cena: cena em que o elemento será posicionado.
+        """
+        lado = self.lado
+        return self.v.a(self.GLIFOS[imagem], w=lado, h=lado, x=i*lado, y=j*lado+lado, cena=cena)
+
+def main(vitollino):
+    Kwarwp(vitollino)
         
 if __name__ == "__main__":
     from _spy.vitollino.main import Jogo, STYLE
