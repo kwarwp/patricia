bd99eaddae46aaf108749ab246663f7f4ce980f7
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-30 15:51:02 -0300

    Saved roxanne/main.py @2020-07-30 18:51

--- roxanne/main.py
+++ roxanne/main.py
@@ -1,12 +1,10 @@
-from _spy.vitollino.main import Jogo, STYLE
-STYLE.update(width=600, height="500px")
-
 MAPA_INICIO = """
 @....&
-......
+%.....
 ......
 .#.^..
 """
+
 
 class Kwarwp():
     GLIFOS = {
@@ -18,28 +16,46 @@ class Kwarwp():
     "@": "https://imgur.com/tLLVjfN.png",  # PICHE
     "~": "https://i.imgur.com/UAETaiP.gif",  # CEU
     "*": "https://i.imgur.com/PfodQmT.gif"  # SOL
+    "%": "https://i.imgur.com/uwYPNlz.png"  # CERCA
     }
     
     def __init__(self, vitollino=None, mapa=MAPA_INICIO, medidas={}):
         self.v = vitollino()
+        """Cria um matriz com os elementos descritos em cada linha de texto"""
         mapa = mapa.split()
+        """Largura da casa da arena dos desafios, número de colunas no mapa"""
         self.lado, self.col = 100, len(mapa[0]) 
         self.cena = self.cria(mapa=mapa) if vitollino else None
         
     def cria(self, mapa="  "):
+        """ Cria o ambiente de programação Kwarwp.
+            :param mapa: Um texto representando o mapa do desafio.
+        """
+        """Cria um cenário com imagem de terra de chão batido, céu e sol"""
         lado = self.lado
         cena = self.v.c(self.GLIFOS["_"])
         ceu = self.v.a(self.GLIFOS["~"], w=lado*self.col, h=lado, x=0, y=0, cena=cena)
         sol = self.v.a(self.GLIFOS["*"], w=60, h=60, x=0, y=40, cena=cena)
+        """Posiciona os elementos segundo suas posições i, j na matriz mapa"""
         [self.cria_elemento( x=i*lado, y=j*lado+lado, cena=cena)
             for j, linha in enumerate(mapa) for i, imagem in enumerate(linha)]
         cena.vai()
         return cena
         
     def cria_elemento(self, x, y, cena):
+        """ Cria um elemento na arena do Kwarwp na posição definida.
+            :param x: coluna em que o elemento será posicionado.
+            :param y: linha em que o elemento será posicionado.
+            :param cena: cena em que o elemento será posicionado.
+        """
         lado = self.lado
         return self.v.a(self.GLIFOS[imagem], w=lado, h=lado, x=i*lado, y=j*lado+lado, cena=cena)
 
 def main(vitollino):
     Kwarwp(vitollino)
         
+    
+if __name__ == "__main__":
+    from _spy.vitollino.main import Jogo, STYLE
+    STYLE.update(width=600, height="500px")
+    main(Jogo)
