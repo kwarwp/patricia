5c631518feac7fdcbde78823bbab755a1dc8ddd9
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-30 17:18:07 -0300

    Saved stacy/kwarapp.py @2020-07-30 20:18

--- stacy/kwarapp.py
+++ stacy/kwarapp.py
@@ -46,7 +46,7 @@ class Kwarwp():
     OCA = "https://i.imgur.com/dZQ8liT.jpg"
     INDIO = "https://imgur.com/8jMuupz.png"
     SOLO = "https://i.imgur.com/sGoKfvs.jpg"
-    TORA = "https://imgur.com/ldI7IbK.png"
+    PEDRA = "https://i.imgur.com/Ry3Vmsn.png"
     PICHE = "https://imgur.com/tLLVjfN.png"
     CEU = "https://i.imgur.com/UAETaiP.gif"
     SOL = "https://i.imgur.com/PfodQmT.gif"
@@ -61,7 +61,7 @@ class Kwarwp():
         cena = self.v.c(self.SOLO)
         indio = self.v.a(self.INDIO, w=100, h=100, x=100, y=400, cena=cena)
         oca = self.v.a(self.OCA, w=100, h=100, x=500, y=100, cena=cena)
-        tora = self.v.a(self.TORA, w=100, h=100, x=300, y=300, cena=cena)
+        pedra = self.v.a(self.PEDRA, w=100, h=100, x=300, y=300, cena=cena)
         #piche = self.v.a(self.PICHE, w=100, h=100, x=100, y=100, cena=cena)
         piche = self.v.a(self.CEU, w=600, h=100, x=0, y=0, cena=cena)
         #sol = self.v.a(self.SOL, w=60, h=60, x=0, y=40, cena=cena)
@@ -87,7 +87,7 @@ class Kwarwp():
         cerca = self.v.a(self.CERCA, w=100, h=100, x=0, y=500, cena=cena)
         cerca = self.v.a(self.CERCA, w=100, h=100, x=300, y=200, cena=cena)
         cerca = self.v.a(self.CERCA, w=100, h=100, x=300, y=100, cena=cena)
-        
+        cerca = self.v.a(self.CERCA, w=100, h=100, x=300, y=400, cena=cena)
         cena.vai()
         return cena
         
