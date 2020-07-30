49ccb4e9c1cd3eb30b6a52e8d06971bdb38a4955
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-30 16:55:37 -0300

    Saved soraya/kwarapp.py @2020-07-30 19:55

--- soraya/kwarapp.py
+++ soraya/kwarapp.py
@@ -10,20 +10,31 @@ Changelog
 .. versionadded::    16.07
        Pagina inicial do Game
 """
+MAPA_INICIO = """
+.......
+......&
+...@...
+.^.....
+"""
+
 class Kwarwp():
     """ Jogo para ensino de programação.
 
         :param vitollino: Empacota o engenho de jogo Vitollino.
     """
 
-    OCA = "https://i.imgur.com/dZQ8liT.jpg"
-    INDIO = "https://imgur.com/8jMuupz.png"
-    SOLO = "https://i.imgur.com/sGoKfvs.jpg"
-    TORA = "https://imgur.com/ldI7IbK.png"
-    PICHE = "https://imgur.com/tLLVjfN.png"
-    CEU = "https://i.imgur.com/UAETaiP.gif"
-    SOL = "https://i.imgur.com/PfodQmT.gif"
+GLIFOS = {
+"&": "https://i.imgur.com/dZQ8liT.jpg",  # OCA ⛺
+"^": "https://imgur.com/8jMuupz.png",  # INDIO 
+".": "https://i.imgur.com/npb9Oej.png",  # VAZIO 
+"_": "https://i.imgur.com/sGoKfvs.jpg",  # SOLO 
+"#": "https://imgur.com/ldI7IbK.png",  # TORA 
+"@": "https://imgur.com/tLLVjfN.png",  # PICHE 
+"~": "https://i.imgur.com/UAETaiP.gif",  # CEU 
+"*": "https://i.imgur.com/PfodQmT.gif"  # SOL ☀
+}
 
+    
     def __init__(self, vitollino=None, cenario="default"):
         self.v = vitollino()
         self.cena = self.cria(cenario=cenario) if vitollino else None
