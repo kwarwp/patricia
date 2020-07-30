5a8bc07f06abc26f95e0364f5cd15ac79df4afca
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-30 17:25:22 -0300

    Saved sara/kwarwp3.py @2020-07-30 20:25

--- sara/kwarwp3.py
+++ sara/kwarwp3.py
@@ -65,5 +65,5 @@ class Kwarwp():
     
 if __name__ == "__main__":
     from _spy.vitollino.main import Jogo, STYLE
-    STYLE.update(width=600, height="600px")
+    STYLE.update(width=400, height="400px")
     Kwarwp(Jogo, medidas=STYLE)
