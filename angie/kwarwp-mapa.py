d85a8f11126343b83edf89d78304e394eba9b611
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-30 16:49:00 -0300

    Saved angie/kwarwp-mapa.py @2020-07-30 19:49

--- angie/kwarwp-mapa.py
+++ angie/kwarwp-mapa.py
@@ -13,17 +13,15 @@ Changelog
 MAPA_INICIO = """
 @....&
 ......
-......
+.....#
 .#.^..
 """
     
 MAPA_ROCHA = """
-+++++++++
 @....&
 ......
 ......
 .#.^..
-+++++++++
 """
 
 
@@ -32,28 +30,7 @@ class Kwarwp():
 
         :param vitollino: Empacota o engenho de jogo Vitollino.
     """
-    OCA = "https://i.imgur.com/dZQ8liT.jpg"
-    INDIO = "https://imgur.com/8jMuupz.png"
-    SOLO = "https://i.imgur.com/sGoKfvs.jpg"
-    TORA = "https://imgur.com/ldI7IbK.png"
-    PICHE = "https://imgur.com/tLLVjfN.png"
-    CEU = "https://i.imgur.com/UAETaiP.gif"
-    SOL = "https://i.imgur.com/PfodQmT.gif"
-    CERCA = "https://i.imgur.com/uwYPNlz.png"
     
-    MAPA_INICIO = """
-    @....&
-    ......
-    ......
-    .#.^..
-    """
-    
-    MAPA_ROCHA = """
-    @....&
-    ......
-    ......
-    .#.^..
-    """
     GLIFOS = {
         "&": "https://i.imgur.com/dZQ8liT.jpg",  # OCA ⛺
         "^": "https://imgur.com/8jMuupz.png",  # INDIO 
@@ -62,34 +39,36 @@ class Kwarwp():
         "#": "https://imgur.com/ldI7IbK.png",  # TORA 
         "@": "https://imgur.com/tLLVjfN.png",  # PICHE 
         "~": "https://i.imgur.com/UAETaiP.gif",  # CEU 
-        "*": "https://i.imgur.com/PfodQmT.gif"  # SOL ☀
+        "*": "https://i.imgur.com/PfodQmT.gif",  # SOL ☀
+        "+": "https://i.imgur.com/uwYPNlz.png" # CERCA 
     }
-    def __init__(self, vitollino=None, cenario="default"):
+    
+    def __init__(self, vitollino=None, mapa=MAPA_ROCHA, medidas={}):
         self.v = vitollino()
-        self.cena = self.cria(cenario=cenario) if vitollino else None
-
-    def cria(self, cenario="default"):
+        """Cria um matriz com os elementos descritos em cada linha de texto"""
+        mapa = mapa.split()
+        """Largura da casa da arena dos desafios, número de colunas no mapa"""
+        self.lado, self.col = 100, len(mapa[0])
+        self.cena = self.cria(mapa=mapa) if vitollino else None
+    
+    def cria(self, mapa="  "):
         """ Cria o ambiente de programação Kwarwp."""
-        cena = self.v.c(self.SOLO)
-        indio = self.v.a(self.INDIO, w=100, h=100, x=300, y=400, cena=cena)
-        oca = self.v.a(self.OCA, w=100, h=100, x=500, y=100, cena=cena)
-        tora = self.v.a(self.TORA, w=100, h=100, x=100, y=400, cena=cena)
-        piche = self.v.a(self.PICHE, w=100, h=100, x=100, y=100, cena=cena)
-        piche = self.v.a(self.CEU, w=600, h=100, x=0, y=0, cena=cena)
-        sol = self.v.a(self.SOL, w=60, h=60, x=0, y=40, cena=cena)
-        cerca = self.v.a(self.CERCA, w=100, h=100, x=0, y=500, cena=cena)
-        cerca = self.v.a(self.CERCA, w=100, h=100, x=100, y=500, cena=cena)
-        cerca = self.v.a(self.CERCA, w=100, h=100, x=200, y=500, cena=cena)
-        cerca = self.v.a(self.CERCA, w=100, h=100, x=300, y=500, cena=cena)
-        cerca = self.v.a(self.CERCA, w=100, h=100, x=400, y=500, cena=cena)
-        cerca = self.v.a(self.CERCA, w=100, h=100, x=500, y=500, cena=cena)
+        lado = self.lado
+        cena = self.v.c(self.GLIFOS["_"])
+        ceu = self.v.a(self.GLIFOS["~"], w=lado*self.col, h=lado, x=0, y=0, cena=cena)
+        sol = self.v.a(self.GLIFOS["*"], w=60, h=60, x=0, y=40, cena=cena)
+        
+        [self.cria_elemento( x=i*lado, y=j*lado+lado, cena=cena)
+            for j, linha in enumerate(mapa) for i, imagem in enumerate(linha)]
         cena.vai()
-        return cena
+        return cena    
         
-        
+    def cria_elemento(self, x, y, cena):
+        lado = self.lado
+        return self.v.a(self.GLIFOS[imagem], w=lado, h=lado, x=i*lado, y=j*lado+lado, cena=cena)
+    
 if __name__ == "__main__":
     from _spy.vitollino.main import Jogo, STYLE
     STYLE["width"] = 600
     STYLE["heigth"] = "600px"
-    
     Kwarwp(Jogo)
