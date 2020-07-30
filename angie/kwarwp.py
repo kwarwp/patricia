bf922619dd69ca972188b4a913db3d20148e12f3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-30 16:19:24 -0300

    Saved angie/kwarwp.py @2020-07-30 19:19

--- angie/kwarwp.py
+++ angie/kwarwp.py
@@ -10,7 +10,7 @@ Changelog
         Montando o jogo pela primeira vez
 
 """
-from _spy.vitollino.main import Jogo
+
 
 class Kwarwp():
     """ Jogo para ensino de programação.
@@ -24,7 +24,8 @@ class Kwarwp():
     PICHE = "https://imgur.com/tLLVjfN.png"
     CEU = "https://i.imgur.com/UAETaiP.gif"
     SOL = "https://i.imgur.com/PfodQmT.gif"
-
+    CERCA = "https://i.imgur.com/uwYPNlz.png"
+    
     def __init__(self, vitollino=None, cenario="default"):
         self.v = vitollino()
         self.cena = self.cria(cenario=cenario) if vitollino else None
@@ -38,8 +39,9 @@ class Kwarwp():
         piche = self.v.a(self.PICHE, w=100, h=100, x=100, y=100, cena=cena)
         piche = self.v.a(self.CEU, w=600, h=100, x=0, y=0, cena=cena)
         sol = self.v.a(self.SOL, w=60, h=60, x=0, y=40, cena=cena)
+        cerca = self.v.a(self.CERCA, w=60, h=60, x=0, y=40, cena=cena)
+        
+        
         cena.vai()
         return cena
         
-if __name__ == "__main__":
-    Kwarwp()
