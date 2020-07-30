0e4fbfd0641613cabba8fa100cf458b0ef4e6247
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-30 16:37:26 -0300

    Saved stacy/kwarapp.py @2020-07-30 19:37

--- stacy/kwarapp.py
+++ stacy/kwarapp.py
@@ -10,27 +10,64 @@ Changelog
         Descreva o que você adicionou no código.
 
 """
-from _spy.vitollino.main import Cena,Elemento,Texto, STYLE
+MAPA_INICIO = """
+@....&
+......
+......
+.#.^..
+"""
+MAPA_ROCHA = """
+++++++
+@....&
+......
+......
+.#.^..
+++++++
+"""
 
-STYLE["width"] = 1010
-STYLE["heigth"] = "500px"
-
-GLIFOS = {
-"&": "https://i.imgur.com/dZQ8liT.jpg",  # OCA ⛺
-"^": "https://imgur.com/8jMuupz.png",  # INDIO 
-".": "https://i.imgur.com/npb9Oej.png",  # VAZIO 
-"_": "https://i.imgur.com/sGoKfvs.jpg",  # SOLO 
-"#": "https://imgur.com/ldI7IbK.png",  # TORA 
-"@": "https://imgur.com/tLLVjfN.png",  # PICHE 
-"~": "https://i.imgur.com/UAETaiP.gif",  # CEU 
-"*": "https://i.imgur.com/PfodQmT.gif"  # SOL ☀
-}
 
 class Kwarwp():
-    def __init__(self, vitollino=None, mapa=MAPA_INICIO, medidas={}):
-    self.v = vitollino()
-    """Cria um matriz com os elementos descritos em cada linha de texto"""
-    mapa = mapa.split()
-    """Largura da casa da arena dos desafios, número de colunas no mapa"""
-    self.lado, self.col = 100, len(mapa[0])
-    self.cena = self.cria(mapa=mapa) if vitollino else None
+
+    """ Jogo para ensino de programação.
+
+        :param vitollino: Empacota o engenho de jogo Vitollino.
+    """
+    GLIFOS = {
+    "&": "https://i.imgur.com/dZQ8liT.jpg",  # OCA ⛺
+    "^": "https://imgur.com/8jMuupz.png",  # INDIO 
+    ".": "https://i.imgur.com/npb9Oej.png",  # VAZIO 
+    "_": "https://i.imgur.com/sGoKfvs.jpg",  # SOLO 
+    "#": "https://imgur.com/ldI7IbK.png",  # TORA 
+    "@": "https://imgur.com/tLLVjfN.png",  # PICHE 
+    "~": "https://i.imgur.com/UAETaiP.gif",  # CEU 
+    "*": "https://i.imgur.com/PfodQmT.gif"  # SOL ☀
+    }
+
+    OCA = "https://i.imgur.com/dZQ8liT.jpg"
+    INDIO = "https://imgur.com/8jMuupz.png"
+    SOLO = "https://i.imgur.com/sGoKfvs.jpg"
+    TORA = "https://imgur.com/ldI7IbK.png"
+    PICHE = "https://imgur.com/tLLVjfN.png"
+    CEU = "https://i.imgur.com/UAETaiP.gif"
+    SOL = "https://i.imgur.com/PfodQmT.gif"
+    CERCA = "https://i.imgur.com/uwYPNlz.png"
+
+    def __init__(self, vitollino=None, cenario="default"):
+        self.v = vitollino()
+        self.cena = self.cria(cenario=cenario) if vitollino else None
+
+    def cria(self, cenario="default"):
+        """ Cria o ambiente de programação Kwarwp."""
+        cena = self.v.c(self.SOLO)
+        indio = self.v.a(self.INDIO, w=100, h=100, x=300, y=400, cena=cena)
+        oca = self.v.a(self.OCA, w=100, h=100, x=500, y=100, cena=cena)
+        tora = self.v.a(self.TORA, w=100, h=100, x=100, y=400, cena=cena)
+        piche = self.v.a(self.PICHE, w=100, h=100, x=100, y=100, cena=cena)
+        piche = self.v.a(self.CEU, w=600, h=100, x=0, y=0, cena=cena)
+        sol = self.v.a(self.SOL, w=60, h=60, x=0, y=40, cena=cena)
+        cerca = 
+        cena.vai()
+        return cena
+        
+if __name__== "__main__":
+        
