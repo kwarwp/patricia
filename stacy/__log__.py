1a98eb5f02b97b586ecf2fd5b7c9bc65d1bf738e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-30 17:26:29 -0300

    Saved stacy/__log__.py @2020-07-30 20:26

--- stacy/__log__.py
+++ stacy/__log__.py
@@ -101,3 +101,21 @@ TypeError: cria() got an unexpected keyword argument 'mapa'
     self.cena = self.cria(mapa=mapa) if vitollino else None
 TypeError: cria() got an unexpected keyword argument 'mapa'
 '''},
+{'date': 'Thu Jul 30 2020 17:26:28.8 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 73
+    Kwarwp(Jogo)
+  module <module> line 50
+    self.cena = self.cria(mapa=mapa) if vitollino else None
+TypeError: cria() got an unexpected keyword argument 'mapa'
+'''},
