bcd936210e15a97c5294b6024f48fc1560cd6d42
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-30 16:07:59 -0300

    Saved naomi/__log__.py @2020-07-30 19:07

--- naomi/__log__.py
+++ naomi/__log__.py
@@ -459,3 +459,19 @@ NameError: name 'Nome' is not defined
     if _name_ == "_main_":
 NameError: name '_name_' is not defined
 '''},
+{'date': 'Thu Jul 30 2020 16:07:58.74 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 42
+    if _name_ == "_main_":
+NameError: name '_name_' is not defined
+'''},
