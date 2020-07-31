21c82fe8cb19bb2d26480a685b79ec0015e2ef47
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-31 18:45:51 -0300

    Saved indio/kwarwp.py @2020-07-31 21:45

--- indio/kwarwp.py
+++ indio/kwarwp.py
@@ -1,7 +1,6 @@
 # patricia.indio.kwarwp.py
 # SPDX-License-Identifier: GPL-3.0-or-later
-"""
-Projeto sem descrição, (mude esta linha).
+""" Projeto sem descrição, (mude esta linha).
 
 .. codeauthor:: Rodrigo Esquinelato <resquinelato@gmail.com>
 
@@ -17,7 +16,6 @@ class Kwarwp():
 
         :param vitollino: Empacota o engenho de jogo Vitollino.
     """
-    
     OCA = "https://i.imgur.com/dZQ8liT.jpg"
     INDIO = "https://imgur.com/8jMuupz.png"
     SOLO = "https://i.imgur.com/sGoKfvs.jpg"
@@ -26,54 +24,25 @@ class Kwarwp():
     CEU = "https://i.imgur.com/UAETaiP.gif"
     SOL = "https://i.imgur.com/PfodQmT.gif"
     CERCA = "https://i.imgur.com/uwYPNlz.png"
-    
     def __init__(self, vitollino=None, cenario="default"):
         self.v = vitollino()
         self.cena = self.cria(cenario=cenario) if vitollino else None
 
     def cria(self, cenario="default"):
-        """
-        Cria o ambiente de programação Kwarwp.
-        """
+        """ Cria o ambiente de programação Kwarwp."""
         cena = self.v.c(self.SOLO)
         indio = self.v.a(self.INDIO, w=100, h=100, x=300, y=400, cena=cena)
         oca = self.v.a(self.OCA, w=100, h=100, x=500, y=100, cena=cena)
         tora = self.v.a(self.TORA, w=100, h=100, x=100, y=400, cena=cena)
         piche = self.v.a(self.PICHE, w=100, h=100, x=100, y=100, cena=cena)
-        #ceu = self.v.a(self.CEU, w=600, h=100, x=0, y=0, cena=cena)
-        #sol = self.v.a(self.SOL, w=60, h=60, x=0, y=40, cena=cena)
+        piche = self.v.a(self.CEU, w=600, h=100, x=0, y=0, cena=cena)
+        sol = self.v.a(self.SOL, w=60, h=60, x=0, y=40, cena=cena)
         cerca = self.v.a(self.CERCA, w=60, h=60, x=0, y=500, cena=cena)
         cena.vai()
         return cena
         
 if __name__ == "__main__":
     from _spy.vitollino.main import Jogo, STYLE
-    """
-    class Jogo:
-    def __init__(self):
-        self.c = Cena
-        self.d = self.codigo = Codigo
-        self.q = Sala
-        self.salao = self.s = Salao
-        self.a = Elemento
-        self.texto = self.t = Popup
-        self.n = Texto
-        self.labirinto = self.l = Labirinto
-        self.inventario = self.i = INVENTARIO
-        self.portal = self.p = Portal
-        self.dropper = self.d = Dropper
-        self.droppable = self.r = Droppable
-        self.musica = self.m = Musica
-        self.codigo = Codigo
-        self.document = document
-        self.html = html
-        self.window = win
-        self.timer = timer
-        pass
-        
-        STYLE["width"/"height"] -> Altera a resolução do cenário do jogo
-        STYLE["height"] = "500px"
-    """
     STYLE["width"] = 600
     STYLE["height"] = "100px"
     Kwarwp(Jogo)
