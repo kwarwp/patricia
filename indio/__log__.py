433a46f56938ddbed68bdbd21db53461260d2b7b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-31 18:36:57 -0300

    Saved indio/__log__.py @2020-07-31 21:36

--- indio/__log__.py
+++ indio/__log__.py
@@ -122,3 +122,27 @@ Traceback (most recent call last):
     if __name__ == __main__:
 NameError: name '__main__' is not defined
 '''},
+{'date': 'Fri Jul 31 2020 18:36:55.738 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 79
+    Kwarwp(Jogo)
+  module <module> line 32
+    self.cena = self.cria(cenario=cenario) if vitollino else None
+  module <module> line 38
+    cena = self.v.c(self.SOLO)
+  module _spy.vitollino.main line 1003
+    self._cria_divs(width)
+  module _spy.vitollino.main line 1008
+    divesq.style.width = width // 3  # 100
+TypeError: unsupported operand type(s) for //: 'str' and 'int'
+'''},
