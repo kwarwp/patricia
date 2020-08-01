74d6b0e1df5c2e5baf0b89b6e2df6bbc3f029836
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-01 08:40:24 -0300

    Saved indio/kwarwp.py @2020-08-01 11:40

--- indio/kwarwp.py
+++ indio/kwarwp.py
@@ -61,6 +61,8 @@ class Kwarwp():
         piche = self.v.a(self.CEU, w=600, h=100, x=0, y=0, cena=cena)
         sol = self.v.a(self.SOL, w=60, h=60, x=0, y=40, cena=cena)
         cerca = self.v.a(self.CERCA, w=100, h=100, x=0, y=400, cena=cena)
+        for _ in range (3):
+            cerca = self.v.a(self.CERCA, w=100, h=100, x=0, y= (_*100), cena=cena)
         cena.vai()
         return cena
         
