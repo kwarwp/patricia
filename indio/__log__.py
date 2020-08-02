b3780f03ef2eeb53efcc1865ca5c0dcdc6cc05f7
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-02 00:59:41 -0300

    Saved indio/__log__.py @2020-08-02 03:59

--- indio/__log__.py
+++ indio/__log__.py
@@ -218,3 +218,10 @@ IndentationError: unexpected indent
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Sun Aug 02 2020 00:59:39.549 GMt-0300 (Brasilia Standard Time) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 87
+  for j, linha in enumerate(mapa) for i, imagem in enumerate(linha)
+  ^
+IndentationError: unexpected indent
+'''},
