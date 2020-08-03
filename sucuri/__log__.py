f4cc45881551672169d878ce1638701376fa5bde
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-02 23:43:28 -0300

    Saved sucuri/__log__.py @2020-08-03 02:43

--- sucuri/__log__.py
+++ sucuri/__log__.py
@@ -57,3 +57,23 @@ Exception: <TypeError: Cannot read property 'charAt' of undefined>
   ^
 IndentationError: expected an indented block
 '''},
+{'date': 'Sun Aug 02 2020 23:43:24.597 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 65
+    Kwarwp(Jogo)
+  module <module> line 39
+    self.cena = self.cria(cenario=cenario) if vitollino else None
+  module <module> line 59
+    cerda = cercado()
+NameError: name 'cercado' is not defined
+'''},
