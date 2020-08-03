62e8c0504dacdd2b2a83fcde9ea6b3dc6c51b1b5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-02 23:42:10 -0300

    Saved sucuri/kwarwp.py @2020-08-03 02:42

--- sucuri/kwarwp.py
+++ sucuri/kwarwp.py
@@ -39,19 +39,23 @@ class Kwarwp():
         self.cena = self.cria(cenario=cenario) if vitollino else None
         
     def cercado():
-        pass
+        x = 0
+        for i in range(5):
+            i = x +100
+            modelo = self.v.a(self.CERCA, w=100, h=100, x=i, y=400, cena=cena)
+            return(modelo)
     
 
     def cria(self, cenario="default"):
         """ Cria o ambiente de programação Kwarwp."""
         cena = self.v.c(self.SOLO)
-        indio = self.v.a(self.INDIO, w=100, h=100, x=300, y=400, cena=cena)
+        indio = self.v.a(self.INDIO, w=100, h=100, x=300, y=300, cena=cena)
         oca = self.v.a(self.OCA, w=100, h=100, x=500, y=100, cena=cena)
         #tora = self.v.a(self.TORA, w=100, h=100, x=100, y=400, cena=cena)
         #piche = self.v.a(self.PICHE, w=100, h=100, x=100, y=100, cena=cena)
         ceu = self.v.a(self.CEU, w=600, h=100, x=0, y=0, cena=cena)
         sol = self.v.a(self.SOL, w=60, h=60, x=0, y=40, cena=cena)
-        cerca = self.v.a(self.CERCA, w=100, h=100, x=0, y=400, cena=cena)
+        #cerca = self.v.a(self.CERCA, w=100, h=100, x=0, y=400, cena=cena)
         cena.vai()
         return cena
         
