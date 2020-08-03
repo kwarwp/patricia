53a3167acc27f386f921bfb9b14501acf58accb0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-02 23:26:18 -0300

    Saved sucuri/kwarwp.py @2020-08-03 02:26

--- sucuri/kwarwp.py
+++ sucuri/kwarwp.py
@@ -29,6 +29,7 @@ class Kwarwp():
     PICHE = "https://imgur.com/tLLVjfN.png"
     CEU = "https://i.imgur.com/UAETaiP.gif"
     SOL = "https://i.imgur.com/PfodQmT.gif"
+    CERCA = "https://i.imgur.com/uwYPNlz.png"
 
     def __init__(self, vitollino=None, cenario="default"):
         self.v = vitollino()
@@ -39,10 +40,11 @@ class Kwarwp():
         cena = self.v.c(self.SOLO)
         indio = self.v.a(self.INDIO, w=100, h=100, x=300, y=400, cena=cena)
         oca = self.v.a(self.OCA, w=100, h=100, x=500, y=100, cena=cena)
-        tora = self.v.a(self.TORA, w=100, h=100, x=100, y=400, cena=cena)
+        #tora = self.v.a(self.TORA, w=100, h=100, x=100, y=400, cena=cena)
         piche = self.v.a(self.PICHE, w=100, h=100, x=100, y=100, cena=cena)
         ceu = self.v.a(self.CEU, w=600, h=100, x=0, y=0, cena=cena)
         sol = self.v.a(self.SOL, w=60, h=60, x=0, y=40, cena=cena)
+        cerca = self.v.a(self.CERCA, w=50, h=50, x=100, y=400, cena=cena)
         cena.vai()
         return cena
         
