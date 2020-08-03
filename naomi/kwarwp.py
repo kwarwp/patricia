27d608ed6d7d6c2d31a3b3a7b0919e192829ef93
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-02 23:42:18 -0300

    Saved naomi/kwarwp.py @2020-08-03 02:42

--- naomi/kwarwp.py
+++ naomi/kwarwp.py
@@ -13,8 +13,8 @@ Changelog
 
 MAPA_INICIO = """
 ######
-#@...&.
-#.#..#.
+#@...&
+#.#..#
 #.#.^#
 ######
 """
@@ -33,14 +33,14 @@ class Kwarwp():
     SOL = "https://i.imgur.com/PfodQmT.gif"
     """
     GLIFOS = {
-    "&": "https://i.imgur.com/dZQ8liT.jpg",  # OCA âÂÂÂÂÂÂº
+    "&": "https://i.imgur.com/dZQ8liT.jpg",  # OCA âÂÂÂÂÂÂÂÂº
     "^": "https://imgur.com/8jMuupz.png",  # INDIO 
     ".": "https://i.imgur.com/npb9Oej.png",  # VAZIO 
     "_": "https://i.imgur.com/sGoKfvs.jpg",  # SOLO 
     "#": "https://imgur.com/ldI7IbK.png",  # TORA 
     "@": "https://imgur.com/tLLVjfN.png",  # PICHE 
     "~": "https://i.imgur.com/UAETaiP.gif",  # CEU 
-    "*": "https://i.imgur.com/PfodQmT.gif"  # SOL âÂÂÂÂÂÂ
+    "*": "https://i.imgur.com/PfodQmT.gif"  # SOL âÂÂÂÂÂÂÂÂ
     }
     
     def __init__(self, vitollino=None,mapa=MAPA_INICIO, medida={}):
@@ -81,5 +81,5 @@ class Kwarwp():
     
 if __name__ == "__main__":
     from _spy.vitollino.main import Jogo, STYLE
-    STYLE.update(width=800, height="600px")
+    STYLE.update(width=800, height="500px")
     Kwarwp(Jogo).cria()
