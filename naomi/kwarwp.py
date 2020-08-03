c55849f0d92fa330f2c84d7017d6cc200adce31d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-02 23:32:06 -0300

    Saved naomi/kwarwp.py @2020-08-03 02:32

--- naomi/kwarwp.py
+++ naomi/kwarwp.py
@@ -42,11 +42,19 @@ class Kwarwp():
     "*": "https://i.imgur.com/PfodQmT.gif"  # SOL ☀
     }
     
-    def __init__(self, vitollino=None, cenario="default"):
+    def __init__(self, vitollino=None,mapa=MAPA_INICIO, medida={}):
         self.V = vitollino()
+        """ código antigo
         self.cena = self.cria(cenario=cenario) if vitollino else None
-    def cria(self, cenario="default"):
-        """ criação do ambiente de programação Kwarwp"""
+        """
+        """ cria uma matriz para cada elemento descrito na linha texto"""
+        mapa=mapa.split()
+        """Largura da casa da arena dos desafios, número de colunas no mapa"""
+        self.lado, self.col = 100, len(mapa[0])
+        self.cena = self.cria(mapa=mapa) if vitollino else None
+        
+    def cria(self, mapa=" "):
+        """ criação do ambiente de programação Kwarwp
         cena = self.V.c(self.SOLO)
         indio = self.V.a (self.INDIO,w=100, h=100, x=300,y=400, cena=cena)
         oca= self.V.a(self.OCA, w= 100, h=100, x=500, y=100, cena=cena)
@@ -56,7 +64,20 @@ class Kwarwp():
         sol = self.V.a(self.SOL, w=60, h=60, x=0, y=40, cena=cena)
         cena.vai()
         return cena
-        
+        """
+        """Cria um cenário com imagem de terra de chão batido, céu e sol"""
+        lado = self.lado
+        cena = self.v.c(self.GLIFOS["_"]) 
+        ceu = self.v.a(self.GLIFOS["~"], w=lado*self.col, h=lado, x=0, y=0, cena=cena)
+        sol = self.v.a(self.GLIFOS["*"], w=60, h=60, x=0, y=40, cena=cena)
+        [self.cria_elemento( x=i*lado, y=j*lado+lado, cena=cena)
+        for j, linha in enumerate(mapa) for i, imagem in enumerate(linha)]
+        cena.vai()
+        return cena
+    def cria_elemento(self, x, y, cena):
+    lado = self.lado
+    return self.v.a(self.GLIFOS[imagem], w=lado, h=lado, x=i*lado, y=j*lado+lado, cena=cena)
+    
 if __name__ == "__main__":
     from _spy.vitollino.main import Jogo, STYLE
     STYLE.update(width=600, height="500px")
