b0f938696ed26892ced873b5edc3d4f5d01b8b21
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-02 23:49:36 -0300

    Saved sucuri/kwarwp.py @2020-08-03 02:49

--- sucuri/kwarwp.py
+++ sucuri/kwarwp.py
@@ -13,11 +13,8 @@ Changelog
 from _spy.vitollino.main import Jogo
 from _spy.vitollino.main import STYLE
 
-#comprimento
-STYLE["width"] = 600
-#largura
-STYLE["height"] = "500px"
-
+STYLE["widht"] = 600
+STYLE["height"] = "300px"
 
 
 class Kwarwp():
@@ -32,31 +29,20 @@ class Kwarwp():
     PICHE = "https://imgur.com/tLLVjfN.png"
     CEU = "https://i.imgur.com/UAETaiP.gif"
     SOL = "https://i.imgur.com/PfodQmT.gif"
-    CERCA = "https://i.imgur.com/uwYPNlz.png"
 
     def __init__(self, vitollino=None, cenario="default"):
         self.v = vitollino()
         self.cena = self.cria(cenario=cenario) if vitollino else None
-        
-    def cercado(self):
-        x = 0
-        for i in range(5):
-            i = x +100
-            modelo = self.v.a(self.CERCA,w=100, h=100, i , y=400, cena=cena)
-            return(modelo)
-    
 
     def cria(self, cenario="default"):
         """ Cria o ambiente de programação Kwarwp."""
         cena = self.v.c(self.SOLO)
-        indio = self.v.a(self.INDIO, w=100, h=100, x=300, y=300, cena=cena)
+        indio = self.v.a(self.INDIO, w=100, h=100, x=300, y=400, cena=cena)
         oca = self.v.a(self.OCA, w=100, h=100, x=500, y=100, cena=cena)
-        #tora = self.v.a(self.TORA, w=100, h=100, x=100, y=400, cena=cena)
-        #piche = self.v.a(self.PICHE, w=100, h=100, x=100, y=100, cena=cena)
+        tora = self.v.a(self.TORA, w=100, h=100, x=100, y=400, cena=cena)
+        piche = self.v.a(self.PICHE, w=100, h=100, x=100, y=100, cena=cena)
         ceu = self.v.a(self.CEU, w=600, h=100, x=0, y=0, cena=cena)
         sol = self.v.a(self.SOL, w=60, h=60, x=0, y=40, cena=cena)
-        #cerca = self.v.a(self.CERCA, w=100, h=100, x=0, y=400, cena=cena)
-        cerda = self.cercado()
         cena.vai()
         return cena
         
