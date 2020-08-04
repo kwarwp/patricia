c634b133b7847990d2a36320e3083b426dc258f0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-04 14:37:38 -0300

    Saved indio/__log__.py @2020-08-04 17:37

--- indio/__log__.py
+++ indio/__log__.py
@@ -502,3 +502,10 @@ SyntaxError: EOL while scanning string literal
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Tue Aug 04 2020 14:37:37.198 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 64
+  def __init__(self, vitollino=None, mapa=MAPA_INICIO, medidas={}):
+  ^
+IndentationError: unexpected indent
+'''},
