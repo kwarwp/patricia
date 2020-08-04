bbd2b76cbddd1e2715c9baf8aa7a7f429ed4763a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-04 12:52:46 -0300

    Saved indio/kwarwp.py @2020-08-04 15:52

--- indio/kwarwp.py
+++ indio/kwarwp.py
@@ -22,14 +22,14 @@ class Kwarwp():
         :param vitollino: Empacota o engenho de jogo Vitollino.
     """
     GLIFOS = {
-    "&": "https://i.imgur.com/dZQ8liT.jpg",  # OCA âÂÂÂÂÂÂÂÂÂÂº
+    "&": "https://i.imgur.com/dZQ8liT.jpg",  # OCA âÂÂÂÂÂÂÂÂÂÂÂÂº
     "^": "https://imgur.com/8jMuupz.png",  # INDIO 
     ".": "https://i.imgur.com/npb9Oej.png",  # VAZIO 
     "_": "https://i.imgur.com/sGoKfvs.jpg",  # SOLO 
     "#": "https://imgur.com/ldI7IbK.png",  # TORA 
     "@": "https://imgur.com/tLLVjfN.png",  # PICHE 
     "~": "https://i.imgur.com/UAETaiP.gif",  # CEU 
-    "*": "https://i.imgur.com/PfodQmT.gif",  # SOL âÂÂÂÂÂÂÂ
+    "*": "https://i.imgur.com/PfodQmT.gif",  # SOL âÂÂÂÂÂÂÂÂ
     "+": "https://imgur.com/uwYPNlz.png"   # CERCA
     }
     
