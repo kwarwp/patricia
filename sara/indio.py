cd99aade2328ea03ab15d90f8bb0302ab1538a9b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-04 20:23:54 -0300

    Saved sara/indio.py @2020-08-04 23:23

--- sara/indio.py
+++ sara/indio.py
@@ -37,3 +37,45 @@ class Kwarwp():
         medidas.update(width=w, height=f"{h}px")
         self.cena = self.cria(mapa=self.mapa) if vitollino else None
         
+    def cria(self, mapa=""):
+        from collections import namedtuple as nt
+        Fab = nt("Fab", "objeto imagem")
+        fabrica = {
+            "&": Fab(self.coisa, f"{IMGUR}dZQ8liT.jpg"), # OCA
+            "^": Fab(self.indio, f"{IMGUR}8jMuupz.png"), # INDIO
+            ".": Fab(self.vazio, f"{IMGUR}npb9Oej.png"), # VAZIO
+            "_": Fab(self.coisa, f"{IMGUR}sGoKfvs.jpg"), # SOLO
+            "#": Fab(self.coisa, f"{IMGUR}ldI7IbK.png"), # TORA
+            "@": Fab(self.coisa, f"{IMGUR}tLLVjfN.png"), # PICHE
+            "~": Fab(self.coisa, f"{IMGUR}UAETaiP.gif"), # CEU
+            "*": Fab(self.coisa, f"{IMGUR}PfodQmT.gif"), # SOL
+            "|": Fab(self.coisa, f"{IMGUR}uwYPNlz.png")  # CERCA
+        }
+        
+        mapa = mapa if mapa != "" else self.mapa
+
+        mapa = self.mapa
+        lado = self.lado
+        cena = self.v.c(fabrica["_"].imagem)
+        ceu = self.v.a(fabrica["~"].imagem, w=lado*self.col, h=lado, x=0, y=0, cena=cena)
+        sol = self.v.a(fabrica["*"].imagem, w=60, h=60, x=0, y=40, cena=cena)
+        self.taba = {(i, j): fabrica[imagem].objeto(
+        fabrica[imagem].imagem, x=i*lado, y=j*lado+lado, cena=cena)
+        for j, linha in enumerate(mapa) for i, imagem in enumerate(linha)}
+
+        cena.vai()
+        return cena
+        
+        
+    def coisa(self, imagem, x, y, cena):
+        lado = self.lado
+        return self.v.a(imagem, w=lado, h=lado, x=x, y=y, cena=cena)
+        
+    def indio(self, imagem, x, y, cena):
+        lado = self.lado
+        return Indio(imagem, x=x, y=y, cena=cena)
+        
+if __name__ == "__main__":
+    from _spy.vitollino.main import Jogo, STYLE
+    STYLE.update(width=500, height="400px")
+    Kwarwp(Jogo)
