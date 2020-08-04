6b93f2d9073d63e28d48848babe11ff3439791ae
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-04 15:19:02 -0300

    Saved indio/__log__.py @2020-08-04 18:19

--- indio/__log__.py
+++ indio/__log__.py
@@ -593,3 +593,23 @@ AttributeError: 'GUI' object has no attribute 'lado'
     STYLE["width"] = self.lado*self.col
 AttributeError: 'GUI' object has no attribute 'lado'
 '''},
+{'date': 'Tue Aug 04 2020 15:18:56.751 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 171
+    Kwarwp(Jogo)
+  module <module> line 69
+    self.cena = self.cria(mapa=mapa) if vitollino else None
+  module <module> line 109
+    cena = self.v.c(GLIFOS["_"])
+NameError: name 'GLIFOS' is not defined
+'''},
