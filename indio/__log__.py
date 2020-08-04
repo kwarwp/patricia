07fcb7f07c4e596cb5d1cf477d2bd6ceee2853aa
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-04 09:09:36 -0300

    Saved indio/__log__.py @2020-08-04 12:09

--- indio/__log__.py
+++ indio/__log__.py
@@ -417,3 +417,19 @@ AttributeError: 'GUI' object has no attribute 'lado'
     STYLE["width"] = self.lado*self.col
 AttributeError: 'GUI' object has no attribute 'lado'
 '''},
+{'date': 'Tue Aug 04 2020 09:09:33.300 GMt-0300 (Brasilia Standard Time) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 168
+    STYLE["width"] = self.lado*self.col
+AttributeError: 'GUI' object has no attribute 'lado'
+'''},
