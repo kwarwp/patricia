16684b5e69e46a4dfa406be7e6ab1c7f4ab73a99
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-04 14:38:28 -0300

    Saved indio/__log__.py @2020-08-04 17:38

--- indio/__log__.py
+++ indio/__log__.py
@@ -509,3 +509,23 @@ IndentationError: unexpected indent
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Tue Aug 04 2020 14:38:23.741 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 168
+    Kwarwp(Jogo)
+  module <module> line 66
+    self.cena = self.cria(mapa=mapa) if vitollino else None
+  module <module> line 106
+    cena = self.v.c(self.GLIFOS["_"])
+AttributeError: 'Kwarwp' object has no attribute 'GLIFOS'
+'''},
