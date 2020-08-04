00bafabf5098bdf5ed66d14c4cdf81b6f8e317ff
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-04 20:31:51 -0300

    Saved sara/__log__.py @2020-08-04 23:31

--- sara/__log__.py
+++ sara/__log__.py
@@ -456,3 +456,25 @@ AttributeError: 'Kwarwp' object has no attribute 'vazio'
     raise ValueError('Type names and field names can only contain alphanumeric characters and underscores: %r'%name)
 ValueError: Type names and field names can only contain alphanumeric characters and underscores: 'Fab. objeto'
 '''},
+{'date': 'Tue Aug 04 2020 20:31:48.581 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 92
+    Kwarwp(Jogo)
+  module <module> line 47
+    self.cena = self.cria(mapa=self.mapa) if vitollino else None
+  module <module> line 52
+    Fab = nt("Fab. objeto", "imagem")
+  module _collections line 495
+    raise ValueError('Type names and field names can only contain alphanumeric characters and underscores: %r'%name)
+ValueError: Type names and field names can only contain alphanumeric characters and underscores: 'Fab. objeto'
+'''},
