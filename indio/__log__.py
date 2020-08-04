98f9b348a5ef77ac9371b1a08d6b7eac00c3250a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-04 14:51:21 -0300

    Saved indio/__log__.py @2020-08-04 17:51

--- indio/__log__.py
+++ indio/__log__.py
@@ -545,3 +545,19 @@ AttributeError: 'Kwarwp' object has no attribute 'GLIFOS'
     STYLE["width"] = self.lado*self.col
 AttributeError: 'GUI' object has no attribute 'lado'
 '''},
+{'date': 'Tue Aug 04 2020 14:51:16.496 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 169
+    STYLE["width"] = self.lado*self.col
+AttributeError: 'GUI' object has no attribute 'lado'
+'''},
