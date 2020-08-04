ec78700f7ada590e306dc2610309568a9e88f387
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-04 20:14:43 -0300

    Created sara/indio.py @2020-08-04 23:14

---
+++ sara/indio.py
@@ -0,0 +1,39 @@
+# patricia.sara.indio.py
+# SPDX-License-Identifier: GPL-3.0-or-later
+""" Projeto Indio.
+
+.. codeauthor:: Paulo Assumpcao <paulo.assump@gmail.com>
+
+Changelog
+---------
+.. versionadded::    20.07
+        Criando o indio para o jogo
+
+"""
+
+
+class Indio():
+
+    def __init__(self, imagem, x, y, cena):
+        self.lado = lado = Kwarwp.LADO
+        self.indio = Kwarwp.VITOLLINO.a(imagem, w=lado, h=lado, x=x, y=y, cena=cena)
+
+class Kwarwp():
+    VITOLLINO = None
+    """Referência estática para obter o engenho de jogo."""
+    LADO = None
+    """Referência estática para definir o lado do piso da casa."""
+
+    def __init__(self, vitollino=None, mapa=MAPA_INICIO, medidas={}):
+        Kwarwp.VITOLLINO = self.v = vitollino()
+        """Cria um matriz com os elementos descritos em cada linha de texto"""
+        self.mapa = mapa.split()
+        """Largura da casa da arena dos desafios, número de colunas no mapa"""
+        self.lado, self.col, self.lin = 100, len(self.mapa[0]), len(self.mapa)+1
+        Kwarwp.LADO = self.lado
+        w, h = self.col *self.lado, self.lin *self.lado
+        self.taba = {}
+        """Dicionário que a partir de coordenada (i,J) localiza um piso da taba"""
+        medidas.update(width=w, height=f"{h}px")
+        self.cena = self.cria(mapa=self.mapa) if vitollino else None
+        
