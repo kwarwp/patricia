6f6591fdbcc0b0b249bc747efec24ca506e5f069
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-03 22:07:00 -0300

    Saved indio/kwarwp.py @2020-08-04 01:07

--- indio/kwarwp.py
+++ indio/kwarwp.py
@@ -29,7 +29,7 @@ class Kwarwp():
     "#": "https://imgur.com/ldI7IbK.png",  # TORA 
     "@": "https://imgur.com/tLLVjfN.png",  # PICHE 
     "~": "https://i.imgur.com/UAETaiP.gif",  # CEU 
-    "*": "https://i.imgur.com/PfodQmT.gif"  # SOL âÂÂÂÂ
+    "*": "https://i.imgur.com/PfodQmT.gif",  # SOL âÂÂÂÂ
     "+": "https://imgur.com/uwYPNlz.png"   # CERCA
     }
     
