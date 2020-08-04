0b3207b71d761f192c3fb35dbbfe43f913d9aadc
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-03 22:26:09 -0300

    Saved indio/__log__.py @2020-08-04 01:26

--- indio/__log__.py
+++ indio/__log__.py
@@ -289,3 +289,19 @@ SyntaxError: invalid syntax
     STYLE["width"] = self.col*self.lado
 AttributeError: 'GUI' object has no attribute 'col'
 '''},
+{'date': 'Mon Aug 03 2020 22:26:05.805 GMt-0300 (Brasilia Standard Time) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 165
+    STYLE["width"] = self.col*self.lado
+AttributeError: 'GUI' object has no attribute 'col'
+'''},
