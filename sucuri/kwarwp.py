4efb6f13e8052221d17e1e209a0ab3715f17d071
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-05 20:50:15 -0300

    Saved sucuri/kwarwp.py @2020-08-05 23:50

--- sucuri/kwarwp.py
+++ sucuri/kwarwp.py
@@ -73,10 +73,13 @@ class Kwarwp():
         """
         ceu = self.v.a(self.GLIFOS["~"], w=lado*col, h=lado, x=0,y=0, cena=cena)
         """Adiciona sol na posição e tamanho abaixo"""
-        self.v.a(self.GLIFOS["*"], w=60, h=60, x=0, y=40,cena=cena)
+        sol = self.v.a(self.GLIFOS["*"], w=60, h=60, x=0, y=40,cena=cena)
         """ Enumerate() é uma função que retorna uma tupla (w,r) onde w é o índice e r é o elemento.
-            O primeiro for retorna tupla(índice,linha). mapa = [(0, @....&),(1,......),(2,......),(3,.#.^..)]
-            O segundo for retorna tupla(índice,glifo). glifo = [[(0,@),(1,.),(2,.),(3,.),(4,.),(5,&)],[...]]
+           
+           .. note::
+               Sintaxe List Comprehensions: List Comprehensions 
+               O primeiro for retorna tupla(índice,linha). mapa = [(0, @....&),(1,......),(2,......),(3,.#.^..)]
+               O segundo for retorna tupla(índice,glifo). glifo = [[(0,@),(1,.),(2,.),(3,.),(4,.),(5,&)],[...]]
         """
         objetos = [self.cria_elemento( x=i*lado, y=j*lado+lado, cena=cena)
             for j, linha in enumerate(mapa) for i, imagem in enumerate(linha)]
