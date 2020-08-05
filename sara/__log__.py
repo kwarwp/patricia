68b4f958bb0f9fa35f0d6951386e757cb249501b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-05 17:43:18 -0300

    Saved sara/__log__.py @2020-08-05 20:43

--- sara/__log__.py
+++ sara/__log__.py
@@ -1749,3 +1749,23 @@ KeyError: +
     divesq.style.width = width // 3  # 100
 TypeError: unsupported operand type(s) for //: 'str' and 'int'
 '''},
+{'date': 'Wed Aug 05 2020 17:43:15.693 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 146
+    Kwarwp(Jogo)
+  module <module> line 62
+    self.cena = self.cria(mapa=self.mapa) if vitollino else None
+  module <module> line 89
+    ceu = self.v.a(fabrica["~"].imagem, w=lado*self.col, h=lado, x=0, y=0, cena=cena, vai= self.executa)
+AttributeError: 'Kwarwp' object has no attribute 'executa'
+'''},
