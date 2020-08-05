ffc3136dbbfd3baa82df1098d2307bc14ba0d7a2
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-05 08:45:15 -0300

    Saved adda/__log__.py @2020-08-05 11:45

--- adda/__log__.py
+++ adda/__log__.py
@@ -670,3 +670,19 @@ TypeError: cria() got an unexpected keyword argument 'mapa'
     j.z()
 TypeError: z() missing 1 positional argument: self
 '''},
+{'date': 'Wed Aug 05 2020 08:45:14.497 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 73
+    print([int(join(i for i in c if i.isdigit())) for c in e.banhista.elt.style.backgroundPosition.split()])
+NameError: name 'join' is not defined
+'''},
