6cb3a2225a6532b952b0d32ead9401a884a932f1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-05 16:29:55 -0300

    Saved angie/__log__.py @2020-08-05 19:29

--- angie/__log__.py
+++ angie/__log__.py
@@ -1184,3 +1184,10 @@ SyntaxError: can't assign to function call
                                               ^
 SyntaxError: non-keyword arg after keyword arg
 '''},
+{'date': 'Wed Aug 05 2020 16:29:54.932 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 109
+  Kwarwp(Jogo, mapa=MAPA_ROCHA, {"w":700, "h":600})
+                                                  ^
+SyntaxError: non-keyword arg after keyword arg
+'''},
