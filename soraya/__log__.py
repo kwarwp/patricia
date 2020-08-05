9259e5be6e374b732e58d91b1762808a8b9ca608
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-05 14:13:49 -0300

    Saved soraya/__log__.py @2020-08-05 17:13

--- soraya/__log__.py
+++ soraya/__log__.py
@@ -480,3 +480,23 @@ SyntaxError: invalid syntax
   ^
 IndentationError: expected an indented block
 '''},
+{'date': 'Wed Aug 05 2020 14:13:47.983 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 118
+    Kwarwp(vitollino=Jogo, mapa=MAPA_ROCHA, medidas=STYLE)
+  module <module> line 61
+    self.cena = self.cria(mapa=self.mapa) if vitollino else None
+  module <module> line 91
+    self.taba = {(i, j): fabrica[imagem].objeto(
+KeyError: +
+'''},
