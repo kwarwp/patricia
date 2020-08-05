6a4d71dffcc670b6c8cbb2b866eeb2ae1447d412
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-05 17:41:35 -0300

    Saved callie/main.py @2020-08-05 20:41

--- callie/main.py
+++ callie/main.py
@@ -1,74 +1,46 @@
 # patricia.callie.main.py
-# Rio de Janeiro, 9 de junho de 2020
-    
+# Rio de Janeiro, 5 de agosto de 2020
+"""   Implementação do mapa no Kwarwp
 
-from _spy.vitollino.main import Cena, Elemento, STYLE, ESTYLE
+.. codeauthor:: Emanuelle Simas <ellesimas@gmail.com>
 
-__version__ = "20.07"
-__author__= 'Emanuelle Simas'
+.. version:: 20.01.1
 
-''' Tentativa de implementação do vira e desvira carta
-            para o aplicativo proposto no curso ProgOO.
-            
-                     Grupo Delta
-'''
+"""
+from _spy.vitollino.main import Jogo, STYLE
+
 STYLE["width"] = 600
+STYLE["height"] = "500px"
 
-TABULEIRO = "https://activufrj.nce.ufrj.br/file/ProgOO/TAB_VAZIO.png"
-CARTA = "https://activufrj.nce.ufrj.br/file/ProgOO/Card_Activ.png"
-CARTA2 = "https://activufrj.nce.ufrj.br/file/ProgOO/Card_github.png"
-VERSO = "https://activufrj.nce.ufrj.br/file/ProgOO/Card_verso.png"
-#PosicaoX= (width_inicial+8)*coluna  (8+145)*1=153/ (145+8)*2=306
-#Posicaoy=(heigth_inicial+8)*linha
 
-class Jogo:
-    """ Representa apenas o tabuleiro do jogo """
-    def __init__(self):
-        """ Mostra o tabuleiro
-        Tentativa construção de tabuleiro com 2 pares
-        """
-        self.tab = Cena(TABULEIRO)
-        
-        self.verso = Elemento(VERSO, x=2, y=5,w=145,h=190, cena=self.tab)
-        self.verso2 = Elemento(VERSO, x=153, y=5, w=145,h=190,cena=self.tab)
-        self.verso3 = Elemento(VERSO, x=2, y=203,w=145,h=190, cena=self.tab)
-        self.verso4 = Elemento(VERSO, x=153, y=203, w=145, h=190, cena=self.tab)
-        
-        ''' Sendo o Width da cena 600px, o melhor tamanho 
-                            para a carta é w=145 e h-190'''
-        
-        def vira_carta(self,_=0):
-            '''Troca a carta quando eu clico no verso
-         '''
-            vira = Elemento(CARTA,x=2, y=5,w=145,h=190, cena=self.tab)
-            vira2 = Elemento(CARTA, x=153, y=5, w=145,h=190,cena=self.tab)
-            vira3 = Elemento(CARTA3, x=2, y=203,w=145,h=190, cena=self.tab)
-            vira4 = Elemento(CARTA3, x=153, y=203, w=145, h=190, cena=self.tab)
-            lista_verso = (self.verso, self.verso2, self.verso3, self.verso4)
-            possib = (vira, vira2, vira3,vira4)
-            
-            for i in lista_verso:
-                
-            
-                
-            
-            #lista_carta=(self.carta, self.carta2,self.carta3, self.carta4)
-            
-        self.verso.vai= vira_carta
-        self.verso2.vai=vira_carta
-        self.verso3.vai=vira_carta
-        self.verso.vai=vira_carta
-            
-    def vai(self):
-        """ Mostra a cena do tabuleiro e versos"""
-        self.tab.vai()
-        self.verso.verso1.verso2.verso3.verso4.vai()
-        self.vira_carta.vai()
-        #self.carta.carta2.carta3.carta4.vai()
-        
-        #Cena(CENA_CALCADA).vai()
+class Kwarwp():
+    """ Jogo para ensino de programação.
+
+        :param vitollino: Empacota o engenho de jogo Vitollino.
+    """
+    OCA = "https://i.imgur.com/dZQ8liT.jpg"
+    INDIO = "https://imgur.com/8jMuupz.png"
+    SOLO = "https://i.imgur.com/sGoKfvs.jpg"
+    TORA = "https://imgur.com/ldI7IbK.png"
+    PICHE = "https://imgur.com/tLLVjfN.png"
+    CEU = "https://i.imgur.com/UAETaiP.gif"
+    SOL = "https://i.imgur.com/PfodQmT.gif"
+
+    def __init__(self, vitollino=None, cenario="default"):
+        self.v = vitollino()
+        self.cena = self.cria(cenario=cenario) if vitollino else None
+
+    def cria(self, cenario="default"):
+        """ Cria o ambiente de programação Kwarwp."""
+        cena = self.v.c(self.SOLO)
+        indio = self.v.a(self.INDIO, w=100, h=100, x=300, y=400, cena=cena)
+        oca = self.v.a(self.OCA, w=100, h=100, x=500, y=100, cena=cena)
+        tora = self.v.a(self.TORA, w=100, h=100, x=100, y=400, cena=cena)
+        piche = self.v.a(self.PICHE, w=100, h=100, x=100, y=100, cena=cena)
+        piche = self.v.a(self.CEU, w=600, h=100, x=0, y=0, cena=cena)
+        sol = self.v.a(self.SOL, w=60, h=60, x=0, y=40, cena=cena)
+        cena.vai()
+        return cena
     
 if __name__ == "__main__":
-    Jogo().vai()
-
-    
+    Kwarwp(Jogo) 
