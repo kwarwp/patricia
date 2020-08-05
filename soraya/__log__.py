c5c6eb477c27d3faed560d58ace4bfc8214f8d2d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-05 13:37:36 -0300

    Saved soraya/__log__.py @2020-08-05 16:37

--- soraya/__log__.py
+++ soraya/__log__.py
@@ -320,3 +320,25 @@ NameError: name 'IMGUR' is not defined
     (IMGUR)
 NameError: name 'IMGUR' is not defined
 '''},
+{'date': 'Wed Aug 05 2020 13:37:34.938 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 122
+    Kwarwp(Jogo, mapa=MAPA_ROCHA, medidas=STYLE)
+  module <module> line 63
+    self.cena = self.cria(mapa=self.mapa) if vitollino else None
+  module <module> line 73
+    fabrica = {
+  module <module> line 1
+    (IMGUR)
+NameError: name 'IMGUR' is not defined
+'''},
