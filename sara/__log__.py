7524b653057cf831de0cf8d9109ebfdd3ba243d2
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-05 14:53:05 -0300

    Saved sara/__log__.py @2020-08-05 17:53

--- sara/__log__.py
+++ sara/__log__.py
@@ -1725,3 +1725,27 @@ KeyError: +
     self.taba = { (i, j): fabrica[imagem].objeto(
 KeyError: +
 '''},
+{'date': 'Wed Aug 05 2020 14:53:02.704 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 128
+    Kwarwp(Jogo)
+  module <module> line 48
+    self.cena = self.cria(mapa=self.mapa) if vitollino else None
+  module <module> line 72
+    cena = self.v.c(fabrica["_"].imagem)
+  module _spy.vitollino.main line 1059
+    self._cria_divs(width)
+  module _spy.vitollino.main line 1064
+    divesq.style.width = width // 3  # 100
+TypeError: unsupported operand type(s) for //: 'str' and 'int'
+'''},
