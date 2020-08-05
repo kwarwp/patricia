76ee37df8f91f607649308dac1508d8aa6fa3a51
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-05 19:44:41 -0300

    Saved kathryn/__log__.py @2020-08-05 22:44

--- kathryn/__log__.py
+++ kathryn/__log__.py
@@ -491,3 +491,23 @@ KeyError: +
     self.taba = {(i, j): fabrica[imagem].objeto(
 KeyError: +
 '''},
+{'date': 'Wed Aug 05 2020 19:44:35.719 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 206
+    Kwarwp(Jogo)
+  module <module> line 71
+    self.cena = self.cria(mapa=self.mapa) if vitollino else None
+  module <module> line 150
+    fabrica[imagem].objeto(fabrica[imagem].url_imagem,
+TypeError: coisa() got an unexpected keyword argument 'w'
+'''},
