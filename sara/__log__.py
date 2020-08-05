4acc5fee20c21785cb1362dfced2fbb5ca4db7e7
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-05 17:44:42 -0300

    Saved sara/__log__.py @2020-08-05 20:44

--- sara/__log__.py
+++ sara/__log__.py
@@ -1769,3 +1769,23 @@ TypeError: unsupported operand type(s) for //: 'str' and 'int'
     ceu = self.v.a(fabrica["~"].imagem, w=lado*self.col, h=lado, x=0, y=0, cena=cena, vai= self.executa)
 AttributeError: 'Kwarwp' object has no attribute 'executa'
 '''},
+{'date': 'Wed Aug 05 2020 17:44:39.643 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 145
+    Kwarwp(Jogo)
+  module <module> line 61
+    self.cena = self.cria(mapa=self.mapa) if vitollino else None
+  module <module> line 88
+    ceu = self.v.a(fabrica["~"].imagem, w=lado*self.col, h=lado, x=0, y=0, cena=cena, vai= self.executa)
+AttributeError: 'Kwarwp' object has no attribute 'executa'
+'''},
