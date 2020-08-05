eae234bd638ecaa2733b6080e5fbf3d581f0a987
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-04 23:04:54 -0300

    Saved sara/__log__.py @2020-08-05 02:04

--- sara/__log__.py
+++ sara/__log__.py
@@ -1505,3 +1505,23 @@ KeyError: 0
     self.taba = { (i, j): fabrica[imagem].objeto(
 KeyError: +
 '''},
+{'date': 'Tue Aug 04 2020 23:04:52.82 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 128
+    Kwarwp(Jogo)
+  module <module> line 48
+    self.cena = self.cria(mapa=self.mapa) if vitollino else None
+  module <module> line 76
+    self.taba = { (i, j): fabrica["&"].objeto(
+KeyError: +
+'''},
