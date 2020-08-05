874a5ecb5f682f2b2630cf70901abe622deb66e6
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-04 22:49:04 -0300

    Saved sara/indio.py @2020-08-05 01:49

--- sara/indio.py
+++ sara/indio.py
@@ -72,7 +72,7 @@ class Kwarwp():
         ceu = self.v.a(fabrica["~"].imagem, w=lado*self.col, h=lado, x=0, y=0, cena=cena)
         sol = self.v.a(fabrica["*"].imagem, w=60, h=60, x=0, y=40, cena=cena)
         
-        self.taba = { (i, j): fabrica["^"].objeto(fabrica["_"].imagem, x=i*lado, y=j*lado+lado, cena=cena) for j, linha in enumerate(mapa) for i, imagem in enumerate(linha) }
+        self.taba = { (i, j): fabrica["^"].objeto(fabrica["^"].imagem, x=i*lado, y=j*lado+lado, cena=cena) for j, linha in enumerate(mapa) for i, imagem in enumerate(linha) }
 
         cena.vai()
         return cena
