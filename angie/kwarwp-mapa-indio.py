d4d088eb64bab8d604b6463eddd1ba918417bddb
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-05 13:59:33 -0300

    Saved angie/kwarwp-mapa-indio.py @2020-08-05 16:59

--- angie/kwarwp-mapa-indio.py
+++ angie/kwarwp-mapa-indio.py
@@ -37,46 +37,71 @@ class Indio():
     def __init__(self, imagem, x, y, cena):
         self.lado = lado = Kwarwp.LADO
         self.indio = Kwarwp.VITOLLINO.a(imagem, w=lado, h=lado, x=x, y=y, cena=cena)
-      
+
 class Kwarwp():
     """ Jogo para ensino de programação.
 
         :param vitollino: Empacota o engenho de jogo Vitollino.
     """
-    
-    GLIFOS = {
-        "&": "https://i.imgur.com/dZQ8liT.jpg",  # OCA âº
-        "^": "https://imgur.com/8jMuupz.png",  # INDIO 
-        ".": "https://i.imgur.com/npb9Oej.png",  # VAZIO 
-        "_": "https://i.imgur.com/sGoKfvs.jpg",  # SOLO 
-        "#": "https://imgur.com/ldI7IbK.png",  # TORA 
-        "@": "https://imgur.com/tLLVjfN.png",  # PICHE 
-        "~": "https://i.imgur.com/UAETaiP.gif",  # CEU 
-        "*": "https://i.imgur.com/PfodQmT.gif",  # SOL â
-        "+": "https://i.imgur.com/uwYPNlz.png", # CERCA 
-        "%": "https://i.imgur.com/Ry3Vmsn.png" #ROCHA
-    }
-    
+    VITOLLINO = None
+    """Referência estática para obter o engenho de jogo."""
+    LADO = None
+    """Referência estática para definir o lado do piso da casa."""
+
     def __init__(self, vitollino=None, mapa=MAPA_INICIO, medidas={}):
-        self.v = vitollino()
+        Kwarwp.VITOLLINO = self.v = vitollino()
         """Cria um matriz com os elementos descritos em cada linha de texto"""
-        mapa = mapa.split()
+        self.mapa = mapa.split()
         """Largura da casa da arena dos desafios, número de colunas no mapa"""
-        self.lado, self.col = 100, len(mapa[0])
-        self.cena = self.cria(mapa=mapa) if vitollino else None
-    
+        self.lado, self.col, self.lin = 100, len(self.mapa[0]), len(self.mapa)+1
+        Kwarwp.LADO = self.lado
+        w, h = self.col *self.lado, self.lin *self.lado
+        self.taba = {}
+        """Dicionário que a partir de coordenada (i,J) localiza um piso da taba"""
+        medidas.update(width=w, height=f"{h}px")
+        self.cena = self.cria(mapa=self.mapa) if vitollino else None
+        
+    """método define uma fábrica de componentes."""
     def cria(self, mapa="  "):
-        """ Cria o ambiente de programação Kwarwp."""
+        
+        from collections import namedtuple as nt
+        Fab = nt("Fab", "objeto imagem")
+
+        fabrica = {
+        "&": Fab(self.coisa, f"{IMGUR}dZQ8liT.jpg"), # OCA
+        "^": Fab(self.indio, f"{IMGUR}8jMuupz.png"), # INDIO
+        ".": Fab(self.vazio, f"{IMGUR}npb9Oej.png"), # VAZIO
+        "_": Fab(self.coisa, f"{IMGUR}sGoKfvs.jpg"), # SOLO
+        "#": Fab(self.coisa, f"{IMGUR}ldI7IbK.png"), # TORA
+        "@": Fab(self.coisa, f"{IMGUR}tLLVjfN.png"), # PICHE
+        "~": Fab(self.coisa, f"{IMGUR}UAETaiP.gif"), # CEU
+        "*": Fab(self.coisa, f"{IMGUR}PfodQmT.gif"), # SOL
+        "|": Fab(self.coisa, f"{IMGUR}uwYPNlz.png")  # CERCA
+        }
+
+        mapa = mapa if mapa != "" else self.mapa
+
+        mapa = self.mapa
         lado = self.lado
-        cena = self.v.c(self.GLIFOS["_"])
-        ceu = self.v.a(self.GLIFOS["~"], w=lado*self.col, h=lado, x=0, y=0, cena=cena)
-        sol = self.v.a(self.GLIFOS["*"], w=60, h=60, x=0, y=40, cena=cena)
-        
-        [self.cria_elemento( x=i*lado, y=j*lado+lado, cena=cena)
-            for j, linha in enumerate(mapa) for i, imagem in enumerate(linha)]
+        cena = self.v.c(fabrica["_"].imagem)
+        ceu = self.v.a(fabrica["~"].imagem, w=lado*self.col, h=lado, x=0, y=0, cena=cena)
+        sol = self.v.a(fabrica["*"].imagem, w=60, h=60, x=0, y=40, cena=cena)
+
+        self.taba = {(i, j): fabrica[imagem].objeto(
+            fabrica[imagem].imagem, x=i*lado, y=j*lado+lado, cena=cena)
+            for j, linha in enumerate(mapa) for i, imagem in enumerate(linha)}
         cena.vai()
-        return cena    
+        return cena
+    
+    def coisa(self, imagem, x, y, cena):
+        lado = self.lado
+        return self.v.a(imagem, w=lado, h=lado, x=x, y=y, cena=cena)        
         
+    def indio(self, imagem, x, y, cena):
+        lado = self.lado
+        return Indio(imagem, x=x, y=y, cena=cena)
+        
+    #ver se precisa remover para baixo
     def cria_elemento(self, x, y, cena):
         lado = self.lado
         return self.v.a(self.GLIFOS[imagem], w=lado, h=lado, x=i*lado, y=j*lado+lado, cena=cena)
