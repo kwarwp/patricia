f5cbd7c15dd706c378196a1106932696f2e7062a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-05 09:51:55 -0300

    Saved adda/__log__.py @2020-08-05 12:51

--- adda/__log__.py
+++ adda/__log__.py
@@ -775,3 +775,21 @@ Exception: <SyntaxError: unexpected token: '.'>
 'error': '''
 Exception: <SyntaxError: unexpected token: '.'>
 '''},
+{'date': 'Wed Aug 05 2020 09:51:54.863 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 27
+    class Elemento(Elt):
+  module <module> line 68
+    @im.setter
+NameError: name 'im' is not defined
+'''},
