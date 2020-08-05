435065e415243d07a4410897ad64c49513af5236
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-05 19:34:05 -0300

    Saved sucuri/__log__.py @2020-08-05 22:34

--- sucuri/__log__.py
+++ sucuri/__log__.py
@@ -91,3 +91,23 @@ SyntaxError: non-keyword arg after keyword arg
                                                                                                                                                                                                                                              ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Wed Aug 05 2020 19:34:04.453 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 83
+    Kwarwp(Jogo) 
+  module <module> line 52
+    self.cena = self.cria(mapa=mapa) if vitollino else None
+  module <module> line 76
+    [self.cria_elemento( x=i*lado, y=j*lado+lado, cena=cena)
+AttributeError: 'Kwarwp' object has no attribute 'cria_elemento'
+'''},
