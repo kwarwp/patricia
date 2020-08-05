b3dd53bdb0ead789d41079c850954ee05ff71bd6
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-05 14:05:38 -0300

    Saved soraya/__log__.py @2020-08-05 17:05

--- soraya/__log__.py
+++ soraya/__log__.py
@@ -426,3 +426,23 @@ NameError: name 'IMGUR' is not defined
     fabrica = {
 AttributeError: 'Kwarwp' object has no attribute 'indio'
 '''},
+{'date': 'Wed Aug 05 2020 14:05:36.622 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 117
+    Kwarwp(vitollino=Jogo, mapa=MAPA_ROCHA, medidas=STYLE)
+  module <module> line 61
+    self.cena = self.cria(mapa=self.mapa) if vitollino else None
+  module <module> line 71
+    fabrica = {
+AttributeError: 'Kwarwp' object has no attribute 'indio'
+'''},
