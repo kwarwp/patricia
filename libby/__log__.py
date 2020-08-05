ca44b57fc628bd84c5a9cec01d5ee18b1e364efa
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-05 17:19:49 -0300

    Saved libby/__log__.py @2020-08-05 20:19

--- libby/__log__.py
+++ libby/__log__.py
@@ -672,3 +672,21 @@ AttributeError: 'GUI' object has no attribute 'coisa'
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Wed Aug 05 2020 17:19:36.820 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 37
+    class Kwarwp():
+  module <module> line 63
+    self.taba = {(i, j): fabrica[imagem].objeto(
+NameError: name 'mapa' is not defined
+'''},
