ae0ca6e2deea4a185794a9d93653a721d33de2aa
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-06 11:13:52 -0300

    Saved callie/__log__.py @2020-08-06 14:13

--- callie/__log__.py
+++ callie/__log__.py
@@ -539,3 +539,23 @@ NameError: name 'lado' is not defined
     self.lado = lado
 NameError: name 'lado' is not defined
 '''},
+{'date': 'Thu Aug 06 2020 11:13:50.346 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 86
+    Kwarwp(Jogo) 
+  module <module> line 61
+    self.cena = self.cria(mapa=mapa) if vitollino else None
+  module <module> line 72
+    self.v.c = cenario #alterar depois
+NameError: name 'cenario' is not defined
+'''},
