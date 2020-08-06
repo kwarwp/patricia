db7037e084df5d1acfab229727911fadce304f87
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-06 17:02:28 -0300

    Saved libby/__log__.py @2020-08-06 20:02

--- libby/__log__.py
+++ libby/__log__.py
@@ -1374,3 +1374,27 @@ NameError: name 'IMGUR' is not defined
     fabrica = {
 AttributeError: 'Kwarwp' object has no attribute 'vazio'
 '''},
+{'date': 'Thu Aug 06 2020 17:02:27.837 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 131
+    main(Jogo)
+  module <module> line 126
+    Kwarwp(vitollino)
+  module <module> line 52
+    self.cena = self.cria(mapa=self.mapa) if vitollino else None
+  module <module> line 90
+    self.taba = {(i, j): fabrica[imagem].objeto(
+  module <module> line 122
+    return Indio(imagem, x=x, y=y, cena=cena)
+TypeError: __init__ missing 1 positional argument: 'taba'
+'''},
