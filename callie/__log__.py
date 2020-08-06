a69ffe812bd21732976060d853daca90d1ccc11d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-06 11:15:20 -0300

    Saved callie/__log__.py @2020-08-06 14:15

--- callie/__log__.py
+++ callie/__log__.py
@@ -579,3 +579,23 @@ NameError: name 'cenario' is not defined
     ceu = self.v.a(self.TABULEIRO["~"], w =lado*col, h=lado, x=0, y=0, cena=cena)
 NameError: name 'col' is not defined
 '''},
+{'date': 'Thu Aug 06 2020 11:15:18.399 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 86
+    Kwarwp(Jogo) 
+  module <module> line 61
+    self.cena = self.cria(mapa=mapa) if vitollino else None
+  module <module> line 79
+    tabuleiro = [self.cria_pecas(x=lado*pp, y=lado+lado*p)
+TypeError: cria_pecas missing 1 positional argument: 'cena'
+'''},
