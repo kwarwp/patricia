903dda2f27a3dbc45ab8d4d7a130ea0953153223
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-06 18:17:01 -0300

    Saved indio/__log__.py @2020-08-06 21:17

--- indio/__log__.py
+++ indio/__log__.py
@@ -670,3 +670,23 @@ AttributeError: 'GUI' object has no attribute 'o_indio'
     self.indio.o_indio.executa()
 AttributeError: 'GUI' object has no attribute 'indio'
 '''},
+{'date': 'Thu Aug 06 2020 18:16:56.189 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 190
+    Kwarwp(Jogo, medidas=STYLE)
+  module <module> line 85
+    self.cena = self.cria(mapa=self.mapa) if vitollino else None
+  module <module> line 123
+    self.taba = {(i, j): 
+AttributeError: 'Fab' object has no attribute 'url_imagem'
+'''},
