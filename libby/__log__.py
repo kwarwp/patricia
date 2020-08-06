55308a59fd1d72a18e5dfd28a68f33e2aaffa2da
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-05 21:07:51 -0300

    Saved libby/__log__.py @2020-08-06 00:07

--- libby/__log__.py
+++ libby/__log__.py
@@ -787,3 +787,26 @@ SyntaxError: invalid syntax
     fabrica = {
 AttributeError: 'Kwarwp' object has no attribute 'vazio'
 '''},
+{'date': 'Wed Aug 05 2020 21:07:38.430 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''cria(self, mapa=%%%%%%%,%..%..&,%..#..%,%^.%..%,%%%%%%%, col=7
+Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 157
+    main(Jogo, STYLE)
+  module <module> line 151
+    Kwarwp(vitollino, medidas=medidas)
+  module <module> line 77
+    self.cena = self.cria(mapa=self.mapa) if vitollino else None
+  module <module> line 108
+    self.taba = {(i, j): fabrica[imagem].objeto(fabrica[imagem].imagem, x=i*lado, y=j*lado+lado, cena=cena)
+KeyError: %
+'''},
