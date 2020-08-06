861481004c09dfb5e20c56cc577ce1040403de74
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-06 17:06:28 -0300

    Saved callie/__log__.py @2020-08-06 20:06

--- callie/__log__.py
+++ callie/__log__.py
@@ -1003,3 +1003,21 @@ NameError: name 'w' is not defined
     def __init__(self, vitollino=None, mapa = MAPA_INICIAL, medidas = {w,h}):
 NameError: name 'w' is not defined
 '''},
+{'date': 'Thu Aug 06 2020 17:06:25.800 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 32
+    class Kwarwp():
+  module <module> line 45
+    def __init__(self, vitollino=None, mapa = MAPA_INICIAL, medidas = {w,h}):
+NameError: name 'w' is not defined
+'''},
