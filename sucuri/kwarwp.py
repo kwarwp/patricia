782aeeecb028e5dfbf14c662366a8fefa65545c3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-05 21:01:14 -0300

    Saved sucuri/kwarwp.py @2020-08-06 00:01

--- sucuri/kwarwp.py
+++ sucuri/kwarwp.py
@@ -12,11 +12,11 @@ STYLE["width"] = 700
 STYLE["height"] = "600px"
 
 MAPA_INICIO = """
-              %%%%%%%
-              %.@...&
-              %.....%
-              %.....%
-              %%%%%%%
+              #######
+              #.@...#
+              #.....#
+              #.....#
+              #######
 """
 
 
@@ -29,10 +29,10 @@ class Kwarwp():
     """O GLIFOS corresponde ao dicionário que guarda a imagem dos elementos"""
     GLIFOS = {"&": "https://i.imgur.com/dZQ8liT.jpg",  # OCA âº
              "^": "https://imgur.com/8jMuupz.png",  # INDIO 
-             "%" : "https://i.imgur.com/uwYPNlz.png", #CERCA 
+             "°" : "https://i.imgur.com/uwYPNlz.png", #CERCA 
              ".": "https://i.imgur.com/npb9Oej.png",  # VAZIO 
              "_": "https://i.imgur.com/sGoKfvs.jpg",  # SOLO 
-             "#": "https://imgur.com/ldI7IbK.png",  # TORA 
+             "|": "https://imgur.com/ldI7IbK.png",  # TORA 
              "@": "https://imgur.com/tLLVjfN.png",  # PICHE 
              "~": "https://i.imgur.com/UAETaiP.gif",  # CEU 
              "*": "https://i.imgur.com/PfodQmT.gif"  # SOL â
