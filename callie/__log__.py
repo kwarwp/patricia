baa034652c1cba7fe2d58899c760e14cdc4dd7a9
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-06 17:36:21 -0300

    Saved callie/__log__.py @2020-08-06 20:36

--- callie/__log__.py
+++ callie/__log__.py
@@ -1057,3 +1057,21 @@ NameError: name 'w' is not defined
     medidas.update(width=w, height=f"{h}px")
 NameError: name 'w' is not defined
 '''},
+{'date': 'Thu Aug 06 2020 17:36:19.560 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 105
+    Kwarwp(Jogo) 
+  module <module> line 53
+    w,h = self.coluna*self.lado, self.lin*self.lado
+AttributeError: 'Kwarwp' object has no attribute 'lin'
+'''},
