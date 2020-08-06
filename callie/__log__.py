cf56dba943368a3e096ac3856921781b95ccb471
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-06 16:48:24 -0300

    Saved callie/__log__.py @2020-08-06 19:48

--- callie/__log__.py
+++ callie/__log__.py
@@ -922,3 +922,23 @@ AttributeError: 'Kwarwp' object has no attribute 'vazio'
     ceu = self.v.a(fabrica["~"].imagem, w=lado*self.col, h=lado, x=0, y=0, cena=cena)
 AttributeError: 'Kwarwp' object has no attribute 'col'
 '''},
+{'date': 'Thu Aug 06 2020 16:48:22.479 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 111
+    Kwarwp(Jogo) 
+  module <module> line 64
+    self.cena = self.cria(mapa=self.mapa) if vitollino else None
+  module <module> line 89
+    self.taba = {(i, j): fabrica[imagem].objeto(
+TypeError: vazio() takes 1 positional arguments but more were given
+'''},
