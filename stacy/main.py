925e7fcdb4b655ebce019b33df4ea5d800d89041
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-06 15:50:17 -0300

    Saved stacy/main.py @2020-08-06 18:50

--- stacy/main.py
+++ stacy/main.py
@@ -4,3 +4,21 @@ __author_ = "Marília Campos Galvão"
 
 
 from collection import 
+
+MAPA_INICIO = """
+@....&
+......
+......
+.#.^..
+"""
+
+GLIFOS = {
+"&": "https://i.imgur.com/dZQ8liT.jpg",  # OCA ⛺
+"^": "https://imgur.com/8jMuupz.png",  # INDIO 
+".": "https://i.imgur.com/npb9Oej.png",  # VAZIO 
+"_": "https://i.imgur.com/sGoKfvs.jpg",  # SOLO 
+"#": "https://imgur.com/ldI7IbK.png",  # TORA 
+"@": "https://imgur.com/tLLVjfN.png",  # PICHE 
+"~": "https://i.imgur.com/UAETaiP.gif",  # CEU 
+"*": "https://i.imgur.com/PfodQmT.gif"  # SOL ☀
+}
