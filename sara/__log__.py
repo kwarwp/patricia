3e696e05734849ed2b92779788515df0769c57c0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-06 14:56:59 -0300

    Saved sara/__log__.py @2020-08-06 17:56

--- sara/__log__.py
+++ sara/__log__.py
@@ -1789,3 +1789,25 @@ AttributeError: 'Kwarwp' object has no attribute 'executa'
     ceu = self.v.a(fabrica["~"].imagem, w=lado*self.col, h=lado, x=0, y=0, cena=cena, vai= self.executa)
 AttributeError: 'Kwarwp' object has no attribute 'executa'
 '''},
+{'date': 'Thu Aug 06 2020 14:56:55.533 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 293
+    Kwarwp(Jogo)
+  module <module> line 174
+    self.cena = self.cria(mapa=self.mapa) if vitollino else None
+  module <module> line 206
+    self.taba = { (i, j): fabrica[imagem].objeto(
+  module <module> line 280
+    self.o_indio = Indio(imagem, x=0, y=0, cena=cena, taba=self)
+TypeError: __init__() got an unexpected keyword argument 'taba'
+'''},
