a571e65cfe84a5ce4a7e9d5dfb688a427c08c7be
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-06 16:43:56 -0300

    Saved sara/__log__.py @2020-08-06 19:43

--- sara/__log__.py
+++ sara/__log__.py
@@ -2751,3 +2751,23 @@ Traceback (most recent call last):
     self.vazio.ocupa(ocupante)
 AttributeError: 'Elemento' object has no attribute 'ocupa'
 '''},
+{'date': 'Thu Aug 06 2020 16:43:52.872 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 297
+    Kwarwp(Jogo, medidas=STYLE)
+  module <module> line 201
+    self.cena = self.cria(mapa=self.mapa) if vitollino else None
+  module <module> line 211
+    fabrica = {
+AttributeError: 'Kwarwp' object has no attribute 'oca'
+'''},
