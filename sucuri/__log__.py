89952e7c88698d2c052ba92da7a0283e9fca963e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-05 21:01:17 -0300

    Saved sucuri/__log__.py @2020-08-06 00:01

--- sucuri/__log__.py
+++ sucuri/__log__.py
@@ -132,3 +132,25 @@ SyntaxError: invalid syntax
                                                                              ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Wed Aug 05 2020 21:01:16.226 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 95
+    Kwarwp(Jogo) 
+  module <module> line 54
+    self.cena = self.cria(mapa=mapa) if vitollino else None
+  module <module> line 85
+    objetos = [self.cria_elemento( x=i*lado, y=j*lado+lado, cena=cena)
+  module <module> line 92
+    return self.v.a(self.GLIFOS[imagem], w=lado, h=lado, x=i*lado, y=j*lado+lado, cena=cena)
+KeyError: #
+'''},
