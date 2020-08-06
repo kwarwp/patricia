875ccf64d67267071b9e6b283d200df923826a99
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-06 11:05:29 -0300

    Saved callie/main.py @2020-08-06 14:05

--- callie/main.py
+++ callie/main.py
@@ -21,6 +21,13 @@ MAPA_INICIAL= """
              #######
 """
 
+
+class indio():
+    """Gera o índio com permissões especiais"""
+    def __init__(self):
+        pass
+    
+    
 class Kwarwp():
     """ Jogo para ensino de programação.
 
@@ -40,21 +47,40 @@ class Kwarwp():
 
     def __init__(self, vitollino=None, mapa = MAPA_INICIAL):
         self.v = vitollino()
-        self.cena = self.cria(cenario=cenario) if vitollino else None
+        """Transforma a string em uma lista onde os 'enter' são considerados vírgula."""
+        mapa = mapa.split()
+        """Largura da casa da arena dos desafios, número de colunas no mapa
         
-    def cria_pecas(self,x,y):
-        ''' Gera 
-        '''
+           .. note::
+              Tente aplicar o seguinte script: > MAPA_INICIO = X 
+              > y = x.split() > z = len(y[0])  > print(x,y,z) 
+              LEN(MAPA[0]) acessa o primeiro item indexado em zero, retornando sua 'quantidade'
+              
+        """
+        self.lado, self.coluna = 100, len(mapa[0])
+        self.cena = self.cria(mapa=mapa) if vitollino else None
+        
+    def cria_pecas(self,x,y,cena):
+        self.lado = lado #trocar posição caso dê problema
+        return self.v.a(self.TABULEIRO[imagem], w=lado, h=lado, x=lado*pp ,y=lado+lado*p, cena=cena)
+        
           
     def cria(self, mapa=" "):
         """ Cria o ambiente de programação Kwarwp."""
+        self.lado = lado
+        self.coluna = col
+        self.v.c = cenario #alterar depois
+        self.v.a = elemento #alterar depois
         cena = self.v.c(self.TABULEIRO["_"])
-        ceu = self.v.a(self.TABULEIRO["~"], w =, h =, x=, y=, cena=cena)
+        ceu = self.v.a(self.TABULEIRO["~"], w =lado*col, h=lado, x=0, y=0, cena=cena)
         sol = self.v.a(self.TABULEIRO["*"], w =60, h =60, x=0, y=40, cena=cena)
-        indio = 
-        tabuleiro = 
+        indio =
+        """"""
+        tabuleiro = [self.v.a(self.cria_pecas, x=lado*pp, y=lado+lado*p)
+            for p,linha in enumerate(mapa) for pp, imagem in enumerate(linha)]
         cena.vai()
-        #return cena
-    
+        return cena
+
+
 if __name__ == "__main__":
     Kwarwp(Jogo) 
