13c57eb844f3a575f5079f0e5dde16a32c3e1fca
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-06 15:49:35 -0300

    Saved angie/__log__.py @2020-08-06 18:49

--- angie/__log__.py
+++ angie/__log__.py
@@ -1516,3 +1516,25 @@ AttributeError: 'Elemento' object has no attribute 'ocupa'
     self.vazio.ocupa(ocupante)
 AttributeError: 'Elemento' object has no attribute 'ocupa'
 '''},
+{'date': 'Thu Aug 06 2020 15:49:35.87 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 294
+    Kwarwp(Jogo, mapa=MAPA_INICIO)
+  module <module> line 202
+    self.cena = self.cria(mapa=self.mapa) if vitollino else None
+  module <module> line 233
+    self.taba = {(i, j): fabrica[imagem].objeto(
+  module <module> line 277
+    self.o_indio = Indio(imagem, x=0, y=0, cena=cena, taba=self)
+TypeError: __init__() got an unexpected keyword argument 'taba'
+'''},
