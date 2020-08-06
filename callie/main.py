7870196a631351c914063b849f05ef3b7aca3992
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-05 22:09:52 -0300

    Saved callie/main.py @2020-08-06 01:09

--- callie/main.py
+++ callie/main.py
@@ -9,65 +9,50 @@
 """
 from _spy.vitollino.main import Jogo, STYLE
 
-#largura e comprimento, respectivamente
-STYLE["width"] = 600
-STYLE["height"] = "500px"
+#largura e altura, respectivamente
+STYLE["width"] = 700
+STYLE["height"] = "600px"
 
+MAPA_INICIAL= """
+             #######
+             #..#..#
+             #..@..&
+             #..#..#
+             #######
+"""
 
 class Kwarwp():
     """ Jogo para ensino de programação.
 
         :param vitollino: Empacota o engenho de jogo Vitollino.
     """
-    OCA = "https://i.imgur.com/dZQ8liT.jpg"
-    INDIO = "https://imgur.com/8jMuupz.png"
-    SOLO = "https://i.imgur.com/sGoKfvs.jpg"
-    TORA = "https://imgur.com/ldI7IbK.png"
-    PICHE = "https://imgur.com/tLLVjfN.png"
-    CEU = "https://i.imgur.com/UAETaiP.gif"
-    SOL = "https://i.imgur.com/PfodQmT.gif"
-    CERCA = "https://i.imgur.com/uwYPNlz.png"
+    
+    TABULEIRO ={"#":"https://i.imgur.com/uwYPNlz.png", # CERCA
+            "^":"https://imgur.com/8jMuupz.png", # INDIO
+            ".":"https://i.imgur.com/npb9Oej.png", #VAZIO
+            "_":"https://i.imgur.com/sGoKfvs.jpg", #SOLO
+            "&":"https://i.imgur.com/dZQ8liT.jpg", #OCA
+            "@":"https://imgur.com/tLLVjfN.png", #PICHE
+            "*":"https://i.imgur.com/PfodQmT.gif", #SOL
+            "~":"https://i.imgur.com/UAETaiP.gif", #CEU
+            "|":"https://imgur.com/ldI7IbK.png",  # TORA 
+            }
 
-    def __init__(self, vitollino=None, cenario="default"):
+    def __init__(self, vitollino=None, mapa = MAPA_INICIAL):
         self.v = vitollino()
         self.cena = self.cria(cenario=cenario) if vitollino else None
         
-    def cercado(self):
-        ''' Gera a cerca da cena
+    def cria_pecas(self,x,y):
+        ''' Gera 
         '''
-        cerca_baixo = (self.v.a(self.CERCA, w=100, h=100, x=0, y=400, cena=cena), 
-                      self.v.a(self.CERCA, w=100, h=100, x=100, y=400, cena=cena),
-                      self.v.a(self.CERCA, w=100, h=100, x=200, y=400, cena=cena),
-                      self.v.a(self.CERCA, w=100, h=100, x=300, y=400, cena=cena),
-                      self.v.a(self.CERCA, w=100, h=100, x=400, y=400, cena=cena),
-                      self.v.a(self.CERCA, w=100, h=100, x=500, y=400, cena=cena))
-                 
-        cerca_esquerda = (self.v.a(self.CERCA, w=100, h=100, x=0, y=100, cena=cena), 
-                         self.v.a(self.CERCA, w=100, h=100, x=0, y=200, cena=cena),
-                         self.v.a(self.CERCA, w=100, h=100, x=0, y=300, cena=cena),
-                         self.v.a(self.CERCA, w=100, h=100, x=0, y=400, cena=cena)) 
-                          
-        cerca_cima = (self.v.a(self.CERCA, w=100, h=100, x=0, y=100, cena=cena), 
-                     self.v.a(self.CERCA, w=100, h=100, x=100, y=100, cena=cena),
-                     self.v.a(self.CERCA, w=100, h=100, x=200, y=100, cena=cena),
-                     self.v.a(self.CERCA, w=100, h=100, x=300, y=100, cena=cena),
-                     self.v.a(self.CERCA, w=100, h=100, x=400, y=100, cena=cena),
-                     self.v.a(self.CERCA, w=100, h=100, x=500, y=100, cena=cena))
-                          
-        cerca_direita = (self.v.a(self.CERCA, w=100, h=100, x=500, y=100, cena=cena), 
-                        self.v.a(self.CERCA, w=100, h=100, x=500, y=300, cena=cena),
-                        self.v.a(self.CERCA, w=100, h=100, x=500, y=400, cena=cena))
-                 
-    def cria(self, cenario="default"):
+          
+    def cria(self, mapa=" "):
         """ Cria o ambiente de programação Kwarwp."""
-        cena = self.v.c(self.SOLO)
-        indio = self.v.a(self.INDIO, w=100, h=100, x=200, y=300, cena=cena)
-        oca = self.v.a(self.OCA, w=100, h=100, x=500, y=200, cena=cena)
-        #tora = self.v.a(self.TORA, w=100, h=100, x=100, y=400, cena=cena)
-        piche = self.v.a(self.PICHE, w=100, h=100, x=100, y=200, cena=cena)
-        ceu = self.v.a(self.CEU, w=600, h=100, x=0, y=0, cena=cena)
-        sol = self.v.a(self.SOL, w=60, h=60, x=0, y=40, cena=cena)
-        cerca = self.cercado()
+        cena = self.v.c(self.TABULEIRO["_"])
+        ceu = self.v.a(self.TABULEIRO["~"], w =, h =, x=, y=, cena=cena)
+        sol = self.v.a(self.TABULEIRO["*"], w =60, h =60, x=0, y=40, cena=cena)
+        indio = 
+        tabuleiro = 
         cena.vai()
         #return cena
     
