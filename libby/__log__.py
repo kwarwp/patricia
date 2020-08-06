23f1a7f194ae9a2603776124d661477e845328b7
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-05 21:00:31 -0300

    Saved libby/__log__.py @2020-08-06 00:00

--- libby/__log__.py
+++ libby/__log__.py
@@ -765,3 +765,25 @@ SyntaxError: invalid syntax
                                                                ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Wed Aug 05 2020 21:00:18.503 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 147
+    main(Jogo, STYLE)
+  module <module> line 141
+    Kwarwp(vitollino, medidas=medidas)
+  module <module> line 77
+    self.cena = self.cria(mapa=self.mapa) if vitollino else None
+  module <module> line 87
+    fabrica = {
+AttributeError: 'Kwarwp' object has no attribute 'vazio'
+'''},
