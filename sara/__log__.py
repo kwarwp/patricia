8c1a4129679e7f994e860003a9fbcce752771f84
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-06 14:59:26 -0300

    Saved sara/__log__.py @2020-08-06 17:59

--- sara/__log__.py
+++ sara/__log__.py
@@ -1867,3 +1867,27 @@ AttributeError: 'Indio' object has no attribute 'vaga'
     self.vaga.sai()
 AttributeError: 'Indio' object has no attribute 'vaga'
 '''},
+{'date': 'Thu Aug 06 2020 14:59:23.7 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 294
+    Kwarwp(Jogo)
+  module <module> line 175
+    self.cena = self.cria(mapa=self.mapa) if vitollino else None
+  module <module> line 207
+    self.taba = { (i, j): fabrica[imagem].objeto(
+  module <module> line 281
+    self.o_indio = Indio(imagem, x=0, y=0, cena=cena, taba=self)
+  module <module> line 29
+    self.vaga
+AttributeError: 'Indio' object has no attribute 'vaga'
+'''},
