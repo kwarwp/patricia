bc9c299ebff4b056abe674a540651b7cd2d0fc87
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-06 14:55:29 -0300

    Saved angie/__log__.py @2020-08-06 17:55

--- angie/__log__.py
+++ angie/__log__.py
@@ -1234,3 +1234,25 @@ SyntaxError: decorator expects function
     coisa = Indio(imagem, x=0, y=0, cena=cena, taba=self)
 TypeError: __init__() got an unexpected keyword argument 'taba'
 '''},
+{'date': 'Thu Aug 06 2020 14:55:28.464 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 282
+    Kwarwp(Jogo, mapa=MAPA_INICIO)
+  module <module> line 190
+    self.cena = self.cria(mapa=self.mapa) if vitollino else None
+  module <module> line 221
+    self.taba = {(i, j): fabrica[imagem].objeto(
+  module <module> line 240
+    coisa = Indio(imagem, x=0, y=0, cena=cena, taba=self)
+TypeError: __init__() got an unexpected keyword argument 'taba'
+'''},
