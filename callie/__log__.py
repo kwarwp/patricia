df213ab51be58a69f520f3dcd403ffa7087be1dd
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-06 16:58:18 -0300

    Saved callie/__log__.py @2020-08-06 19:58

--- callie/__log__.py
+++ callie/__log__.py
@@ -949,3 +949,21 @@ TypeError: vazio() takes 1 positional arguments but more were given
                                                                   ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Thu Aug 06 2020 16:58:16.244 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 32
+    class Kwarwp():
+  module <module> line 45
+    def __init__(self, vitollino=None, mapa = MAPA_INICIAL, medidas = {w,h}):
+NameError: name 'w' is not defined
+'''},
