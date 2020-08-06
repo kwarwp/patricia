847d94cdd6eab92d8b68e7f4e9aa955f7e635773
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-06 16:41:43 -0300

    Saved callie/__log__.py @2020-08-06 19:41

--- callie/__log__.py
+++ callie/__log__.py
@@ -902,3 +902,23 @@ AttributeError: 'GUI' object has no attribute 'coisa'
     fabrica ={"#": Fab(self.coisa, f"{IMGUR}uwYPNlz.png"), # CERCA
 AttributeError: 'Kwarwp' object has no attribute 'vazio'
 '''},
+{'date': 'Thu Aug 06 2020 16:41:41.218 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 110
+    Kwarwp(Jogo) 
+  module <module> line 64
+    self.cena = self.cria(mapa=self.mapa) if vitollino else None
+  module <module> line 86
+    ceu = self.v.a(fabrica["~"].imagem, w=lado*self.col, h=lado, x=0, y=0, cena=cena)
+AttributeError: 'Kwarwp' object has no attribute 'col'
+'''},
