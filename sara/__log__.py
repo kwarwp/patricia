f922378d4c8892f68b9b400e58fdc0ffa783342d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-06 16:21:24 -0300

    Saved sara/__log__.py @2020-08-06 19:21

--- sara/__log__.py
+++ sara/__log__.py
@@ -2688,3 +2688,33 @@ AttributeError: 'Elemento' object has no attribute 'ocupa'
     self.vazio.ocupa(ocupante)
 AttributeError: 'Elemento' object has no attribute 'ocupa'
 '''},
+{'date': 'Thu Aug 06 2020 16:21:20.741 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 323
+    Kwarwp(Jogo, medidas=STYLE)
+  module <module> line 194
+    self.cena = self.cria(mapa=self.mapa) if vitollino else None
+  module <module> line 225
+    self.taba = { (i, j): fabrica[imagem].objeto(
+  module <module> line 311
+    vaga = Vazio("", x=x, y=y, cena=cena, ocupante=self.o_indio)
+  module <module> line 113
+    self.acessa(ocupante)
+  module <module> line 133
+    ocupante.ocupa(self)  
+  module <module> line 82
+    vaga.ocupou(self)
+  module <module> line 147
+    vazio.ocupa(ocupante)
+NameError: name 'vazio' is not defined
+'''},
