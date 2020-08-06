e19fef4fa9ec89f38c348e0542aa8199f19c06ae
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-06 16:52:49 -0300

    Saved libby/__log__.py @2020-08-06 19:52

--- libby/__log__.py
+++ libby/__log__.py
@@ -1277,3 +1277,36 @@ Traceback (most recent call last):
     coisa = Indio(imagem, x=0, y=0, cena=cena, taba=self)
 TypeError: __init__ missing 1 positional argument: 'vaga'
 '''},
+{'date': 'Thu Aug 06 2020 16:52:34.817 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''cria(self, mapa=|||||||,|..|..&,|..#..|,|^.|..|,|||||||, col=7
+Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 301
+    main(Jogo, STYLE)
+  module <module> line 295
+    Kwarwp(vitollino, medidas=medidas)
+  module <module> line 207
+    self.cena = self.cria(mapa=self.mapa) if vitollino else None
+  module <module> line 238
+    self.taba = {(i, j): fabrica[imagem].objeto(fabrica[imagem].imagem, x=i*lado, y=j*lado+lado, cena=cena)
+  module <module> line 263
+    vaga = Vazio("", x=x, y=y, cena=cena, ocupante=coisa)
+  module <module> line 65
+    self.acessa(ocupante)
+  module <module> line 81
+    ocupante.ocupa(self)
+  module <module> line 170
+    vaga.ocupou(self)
+  module <module> line 93
+    self.vazio.ocupa(ocupante)
+AttributeError: 'Elemento' object has no attribute 'ocupa'
+'''},
