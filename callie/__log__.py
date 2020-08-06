b3b455246a9f7c14d8b0b0cb5c00562754090049
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-06 16:13:51 -0300

    Saved callie/__log__.py @2020-08-06 19:13

--- callie/__log__.py
+++ callie/__log__.py
@@ -662,3 +662,19 @@ IndentationError: unexpected indent
 'error': '''
 Exception: <SyntaxError: Illegal return statement>
 '''},
+{'date': 'Thu Aug 06 2020 16:13:48.983 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 25
+    fabrica ={"#": Fab(self.coisa, f"{IMGUR}uwYPNlz.png"), # CERCA
+NameError: name 'Fab' is not defined
+'''},
