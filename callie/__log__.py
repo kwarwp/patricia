88351f60d593cddfee483a89719f76af1a5bd5ef
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-09 23:14:28 -0300

    Saved callie/__log__.py @2020-08-10 02:14

--- callie/__log__.py
+++ callie/__log__.py
@@ -1111,3 +1111,21 @@ AttributeError: 'Kwarwp' object has no attribute 'medidas'
     Kwarwp.VITOLLINO = self.v = vitollino()
 TypeError: 'NoneType' object is not callable
 '''},
+{'date': 'Sun Aug 09 2020 23:14:24.249 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 108
+    Kwarwp(medidas = STYLE) 
+  module <module> line 47
+    Kwarwp.VITOLLINO = self.v = vitollino()
+TypeError: 'NoneType' object is not callable
+'''},
