587cb89fbdc6a877e5a45043e8bfd0cadc29d304
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-12 15:39:17 -0300

    Saved kathryn/__log__.py @2020-08-12 18:39

--- kathryn/__log__.py
+++ kathryn/__log__.py
@@ -518,3 +518,25 @@ TypeError: coisa() got an unexpected keyword argument 'w'
        ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Wed Aug 12 2020 15:39:16.38 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 257
+    Kwarwp(Jogo, medidas=STYLE)
+  module <module> line 47
+    self.cena = self.cria(mapa=self.mapa) if vitollino else None
+  module <module> line 73
+    self.taba = {(i, j): 
+  module <module> line 89
+    coisa = Indio(imagem, x=0, y=0, cena=cena, taba=self)
+TypeError: __init__() got an unexpected keyword argument 'taba'
+'''},
