aff749773c1d0a485d9c4110c1bd4c8ff4eea1d5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-12 15:44:04 -0300

    Saved kathryn/__log__.py @2020-08-12 18:44

--- kathryn/__log__.py
+++ kathryn/__log__.py
@@ -540,3 +540,31 @@ SyntaxError: invalid syntax
     coisa = Indio(imagem, x=0, y=0, cena=cena, taba=self)
 TypeError: __init__() got an unexpected keyword argument 'taba'
 '''},
+{'date': 'Wed Aug 12 2020 15:44:02.834 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 258
+    Kwarwp(Jogo, medidas=STYLE)
+  module <module> line 47
+    self.cena = self.cria(mapa=self.mapa) if vitollino else None
+  module <module> line 73
+    self.taba = {(i, j): 
+  module <module> line 91
+    vaga = Vazio("", x=x, y=y, cena=cena, ocupante=coisa)
+  module <module> line 197
+    self.acessa(ocupante)
+  module <module> line 215
+    ocupante.ocupa(self)
+  module <module> line 166
+    self.vaga.sai()
+AttributeError: 'Indio' object has no attribute 'vaga'
+'''},
