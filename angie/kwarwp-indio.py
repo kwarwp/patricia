fa9454c15c56b1c93881481657f28c0594245591
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-12 15:12:04 -0300

    Created angie/kwarwp-indio.py @2020-08-12 18:12

---
+++ angie/kwarwp-indio.py
@@ -0,0 +1,12 @@
+# patricia.angie.kwarwp-indio.py
+# SPDX-License-Identifier: GPL-3.0-or-later
+""" Projeto sem descrição, (mude esta linha).
+
+.. codeauthor:: Monica Novellino <monicanovellino@gmail.com>
+
+Changelog
+---------
+.. versionadded::    20.08
+        Melhorando o índio
+
+"""
