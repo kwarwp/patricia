106ff1b4c9281eedde0925d90e8740fb504c531d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-12 20:36:53 -0300

    Saved anaconda/__log__.py @2020-08-12 23:36

--- anaconda/__log__.py
+++ anaconda/__log__.py
@@ -174,3 +174,25 @@ IndentationError: unexpected indent
     self.indio = Kwarwp.VIOLLINO.a(imagem, w=lado, h=lado, x=x, y=y, cena=cena)
 AttributeError: 'Kwarwp' object has no attribute 'VIOLLINO'
 '''},
+{'date': 'Wed Aug 12 2020 20:36:52.834 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 272
+    Kwarwp(Jogo, medidas = STYLE) 
+  module <module> line 179
+    self.cena = self.cria(mapa=self.mapa) if vitollino else None
+  module <module> line 212
+    self.taba = {(i, j): fabrica[imagem].objeto(
+  module <module> line 230
+    vaga = Vazio("", x=x, y=y, cena=cena, ocupante=coisa)
+NameError: name 'Vazio' is not defined
+'''},
