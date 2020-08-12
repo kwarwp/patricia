b98df89de6bdb4c6e617a491c3441ce23fd913fc
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-11 23:19:36 -0300

    Created anaconda/__log__.py @2020-08-12 02:19

---
+++ anaconda/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Tue Aug 11 2020 23:19:32.773 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 60
+  """Instância do personagem principal, o índio, vai ser atribuído pela fábrica do índio"""
+  ^
+IndentationError: unexpected indent
+'''},
