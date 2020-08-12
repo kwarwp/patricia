91478a832c2ad72ae2258cbb003da21b3a00b578
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-12 20:37:07 -0300

    Saved anaconda/__log__.py @2020-08-12 23:37

--- anaconda/__log__.py
+++ anaconda/__log__.py
@@ -196,3 +196,25 @@ AttributeError: 'Kwarwp' object has no attribute 'VIOLLINO'
     vaga = Vazio("", x=x, y=y, cena=cena, ocupante=coisa)
 NameError: name 'Vazio' is not defined
 '''},
+{'date': 'Wed Aug 12 2020 20:37:06.815 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 272
+    Kwarwp(Jogo, medidas = STYLE) 
+  module <module> line 179
+    self.cena = self.cria(mapa=self.mapa) if vitollino else None
+  module <module> line 212
+    self.taba = {(i, j): fabrica[imagem].objeto(
+  module <module> line 230
+    vaga = vazio("", x=x, y=y, cena=cena, ocupante=coisa)
+TypeError: __init__() got multiple values for argument 'x'
+'''},
