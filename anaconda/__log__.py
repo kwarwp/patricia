76282659547e9f5ae24af67ee5471d5be2c222cb
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-12 20:36:43 -0300

    Saved anaconda/__log__.py @2020-08-12 23:36

--- anaconda/__log__.py
+++ anaconda/__log__.py
@@ -150,3 +150,27 @@ IndentationError: expected an indented block
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Wed Aug 12 2020 20:36:43.107 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 272
+    Kwarwp(Jogo, medidas = STYLE) 
+  module <module> line 179
+    self.cena = self.cria(mapa=self.mapa) if vitollino else None
+  module <module> line 212
+    self.taba = {(i, j): fabrica[imagem].objeto(
+  module <module> line 228
+    coisa = Indio(imagem, x=0, y=0, cena=cena, taba=self)
+  module <module> line 32
+    self.indio = Kwarwp.VIOLLINO.a(imagem, w=lado, h=lado, x=x, y=y, cena=cena)
+AttributeError: 'Kwarwp' object has no attribute 'VIOLLINO'
+'''},
