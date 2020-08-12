bbf73bd09d20c02926e72880d968aa7ede6af323
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-12 19:35:52 -0300

    Saved anaconda/__log__.py @2020-08-12 22:35

--- anaconda/__log__.py
+++ anaconda/__log__.py
@@ -48,3 +48,25 @@ IndentationError: expected an indented block
   ^
 IndentationError: expected an indented block
 '''},
+{'date': 'Wed Aug 12 2020 19:35:52.38 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 271
+    Kwarwp(Jogo, medidas = STYLE) 
+  module <module> line 178
+    self.cena = self.cria(mapa=self.mapa) if vitollino else None
+  module <module> line 211
+    self.taba = {(i, j): fabrica[imagem].objeto(
+  module <module> line 227
+    coisa = Indio(imagem, x=0, y=0, cena=cena, taba=self)
+TypeError: __init__() got an unexpected keyword argument 'taba'
+'''},
