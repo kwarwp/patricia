3179bb7c3555a507f827caa5a8f86533d19d44d6
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-13 09:12:33 -0300

    Saved sara/__log__.py @2020-08-13 12:12

--- sara/__log__.py
+++ sara/__log__.py
@@ -2997,3 +2997,27 @@ AttributeError: 'Indio' object has no attribute 'AZIMUTE'
     self.azimute = Kwarwp.AZIMUTE.n
 AttributeError: 'Kwarwp' object has no attribute 'AZIMUTE'
 '''},
+{'date': 'Thu Aug 13 2020 09:12:28.939 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 504
+    Kwarwp(Jogo, medidas=STYLE)
+  module <module> line 265
+    self.cena = self.cria(mapa=self.mapa) if vitollino else None
+  module <module> line 300
+    self.taba = {(i, j): fabrica[imagem].objeto(fabrica[imagem].imagem, x=i*lado, y=j*lado+lado, cena=cena)
+  module <module> line 326
+    vaga = Vazio("", x=x, y=y, cena=cena, ocupante=coisa)
+  module <module> line 414
+    self.acessa = self._acessa
+AttributeError: 'Vazio' object has no attribute '_acessa'
+'''},
