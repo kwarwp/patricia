0667da8bd52ec46ed737a617c2736065f3a967e4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-13 16:53:09 -0300

    Saved sara/__log__.py @2020-08-13 19:53

--- sara/__log__.py
+++ sara/__log__.py
@@ -3368,3 +3368,25 @@ TypeError: barra() got an unexpected keyword argument 'vai'
     self.o_indio = Indio(imagem, x=1, y=0, cena=cena, taba=self)
 TypeError: __init__ missing 1 positional argument: 'vai'
 '''},
+{'date': 'Thu Aug 13 2020 16:53:04.461 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 597
+    Kwarwp(Jogo, medidas=STYLE)
+  module <module> line 284
+    self.cena = self.cria(mapa=self.mapa) if vitollino else None
+  module <module> line 320
+    self.taba = {(i, j): fabrica[imagem].objeto(fabrica[imagem].imagem, x=i*lado, y=j*lado+lado, cena=cena, vai=fabrica[imagem].evento)
+  module <module> line 358
+    self.o_indio = Indio(imagem, x=1, y=0, cena=cena, taba=self)
+TypeError: __init__ missing 1 positional argument: 'vai'
+'''},
