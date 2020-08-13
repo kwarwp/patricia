a7c6241ab1b7f30be6b621d9ad4317306a1b47b2
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-13 16:07:53 -0300

    Saved sara/__log__.py @2020-08-13 19:07

--- sara/__log__.py
+++ sara/__log__.py
@@ -3222,3 +3222,23 @@ IndentationError: unexpected indent
     self.azimute = AZIMUTE.n
 NameError: name 'AZIMUTE' is not defined
 '''},
+{'date': 'Thu Aug 13 2020 16:07:47.569 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 591
+    Kwarwp(Jogo, medidas=STYLE)
+  module <module> line 283
+    self.cena = self.cria(mapa=self.mapa) if vitollino else None
+  module <module> line 319
+    self.taba = {(i, j): fabrica[imagem].objeto(fabrica[imagem].imagem, x=i*lado, y=j*lado+lado, cena=cena, vai=self.carrega)
+AttributeError: 'Kwarwp' object has no attribute 'carrega'
+'''},
