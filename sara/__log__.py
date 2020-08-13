cd47143b818a52a0abc9557335922bb48e71a3eb
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-13 08:51:12 -0300

    Saved sara/__log__.py @2020-08-13 11:51

--- sara/__log__.py
+++ sara/__log__.py
@@ -2905,3 +2905,25 @@ AttributeError: 'Indio' object has no attribute 'AZIMUTE'
     self.azimute = self.AZIMUTE.n
 AttributeError: 'Indio' object has no attribute 'AZIMUTE'
 '''},
+{'date': 'Thu Aug 13 2020 08:51:08.277 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 334
+    Kwarwp(Jogo, medidas=STYLE)
+  module <module> line 255
+    self.cena = self.cria(mapa=self.mapa) if vitollino else None
+  module <module> line 279
+    self.taba = {(i, j): fabrica[imagem].objeto(fabrica[imagem].imagem, x=i*lado, y=j*lado+lado, cena=cena)
+  module <module> line 304
+    coisa = Indio(imagem, x=0, y=0, cena=cena, taba=self)
+TypeError: __init__ missing 1 positional argument: 'azimute'
+'''},
