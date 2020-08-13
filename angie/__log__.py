91de55fa438741287b78cf9194d36d93e7ae54b5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-13 16:46:00 -0300

    Saved angie/__log__.py @2020-08-13 19:46

--- angie/__log__.py
+++ angie/__log__.py
@@ -1989,3 +1989,27 @@ TypeError: can't add 'Tora' object to DOMNode instance
     self.elt <= ocupante
 TypeError: can't add 'Tora' object to DOMNode instance
 '''},
+{'date': 'Thu Aug 13 2020 16:45:59.60 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 497
+    Kwarwp(Jogo, mapa=MAPA_INICIO)
+  module <module> line 349
+    self.cena = self.cria(mapa=self.mapa) if vitollino else None
+  module <module> line 383
+    self.taba = {(i, j): fabrica[imagem].objeto(fabrica[imagem].imagem, x=i*lado, y=j*lado+lado, cena=cena)
+  module <module> line 479
+    coisa = Tora(imagem, x=0, y=0, cena=cena, taba=self)
+  module <module> line 315
+    self.mostra()
+AttributeError: 'Tora' object has no attribute 'mostra'
+'''},
