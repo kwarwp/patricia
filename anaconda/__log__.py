07c3e91c20c25abd38a5898cf0d36be26543824c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-12 23:05:31 -0300

    Saved anaconda/__log__.py @2020-08-13 02:05

--- anaconda/__log__.py
+++ anaconda/__log__.py
@@ -258,3 +258,21 @@ TypeError: __init__() got multiple values for argument 'x'
     w,h = self.coluna*self.lado, self.lin*self.lado
 AttributeError: 'Kwarwp' object has no attribute 'lin'
 '''},
+{'date': 'Wed Aug 12 2020 23:05:29.568 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 162
+    Kwarwp(Jogo, medidas = STYLE) 
+  module <module> line 79
+    w,h = self.coluna*self.lado, self.lin*self.lado
+AttributeError: 'Kwarwp' object has no attribute 'lin'
+'''},
