b8297e069ee0399050561ab81bd9f173899b8c9c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-13 19:03:00 -0300

    Saved sara/indio.py @2020-08-13 22:03

--- sara/indio.py
+++ sara/indio.py
@@ -171,7 +171,7 @@ class Indio():
         """
         pass
     
-    # Hummm melhor criar um objeto tora e controlar isso dentro dele, farei isso amanhã
+    # Hummm melhor criar um objeto Tora e controlar isso dentro dele, farei isso amanhã
     # O professor falou que seria algo semelhante ao método andar
     # vamos ver se é necessário separar essas duas ações (método pega e método larga)
     def pega(self):
