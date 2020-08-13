0cf44a4c143789ddf158b2c31ae693800ac1d21a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-13 16:40:37 -0300

    Saved sara/__log__.py @2020-08-13 19:40

--- sara/__log__.py
+++ sara/__log__.py
@@ -3284,3 +3284,25 @@ TypeError: coisa() got an unexpected keyword argument 'vai'
     vaga = Vazio("", x=x, y=y, cena=cena, ocupante=coisa, vai=vai)
 TypeError: __init__() got an unexpected keyword argument 'vai'
 '''},
+{'date': 'Thu Aug 13 2020 16:40:25.214 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 597
+    Kwarwp(Jogo, medidas=STYLE)
+  module <module> line 284
+    self.cena = self.cria(mapa=self.mapa) if vitollino else None
+  module <module> line 320
+    self.taba = {(i, j): fabrica[imagem].objeto(fabrica[imagem].imagem, x=i*lado, y=j*lado+lado, cena=cena, vai=fabrica[imagem].evento)
+  module <module> line 347
+    vaga = Vazio("", x=x, y=y, cena=cena, ocupante=coisa, vai=vai)
+TypeError: __init__() got an unexpected keyword argument 'vai'
+'''},
