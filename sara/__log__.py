fcb2e067c8336745fbb985a5bfc00a7026015a33
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-13 09:32:46 -0300

    Saved sara/__log__.py @2020-08-13 12:32

--- sara/__log__.py
+++ sara/__log__.py
@@ -3198,3 +3198,27 @@ TypeError: can't add 'Oca' object to DOMNode instance
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Thu Aug 13 2020 09:32:42.515 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 561
+    Kwarwp(Jogo, medidas=STYLE)
+  module <module> line 266
+    self.cena = self.cria(mapa=self.mapa) if vitollino else None
+  module <module> line 301
+    self.taba = {(i, j): fabrica[imagem].objeto(fabrica[imagem].imagem, x=i*lado, y=j*lado+lado, cena=cena)
+  module <module> line 326
+    coisa = Indio(imagem, x=0, y=0, cena=cena, taba=self)
+  module <module> line 56
+    self.azimute = AZIMUTE.n
+NameError: name 'AZIMUTE' is not defined
+'''},
