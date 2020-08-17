8b69b3f2302ebc1c5b17c6b7861690a365d9a06a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-17 18:30:38 -0300

    Saved grace/__log__.py @2020-08-17 21:30

--- grace/__log__.py
+++ grace/__log__.py
@@ -153,3 +153,21 @@ Traceback (most recent call last):
   module adda.main line 15
     from grace.main import Praia
 '''},
+{'date': 'Mon Aug 17 2020 18:30:37.51 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 36
+    Introduz().chama()
+  module <module> line 28
+    self.fundo = Cena(FUNDO, direita = Calcada())
+TypeError: 'module' object is not callable
+'''},
