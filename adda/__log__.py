1520707277a20bf0a902f52481463728dff38451
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-17 18:26:06 -0300

    Saved adda/__log__.py @2020-08-17 21:26

--- adda/__log__.py
+++ adda/__log__.py
@@ -958,3 +958,21 @@ Traceback (most recent call last):
   module <module> line 15
     from grace.main import Praia
 '''},
+{'date': 'Mon Aug 17 2020 18:26:05.72 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 37
+    Calcada().vai()
+  module <module> line 28
+    self.cena = Cena(CENA_CALCADA, direita=Praia())
+NameError: name 'Praia' is not defined
+'''},
