64526b39516cf591d8e593e6abac31172f053d7b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-18 22:16:10 -0300

    Saved sucuri/__log__.py @2020-08-19 01:16

--- sucuri/__log__.py
+++ sucuri/__log__.py
@@ -154,3 +154,23 @@ SyntaxError: invalid syntax
     return self.v.a(self.GLIFOS[imagem], w=lado, h=lado, x=i*lado, y=j*lado+lado, cena=cena)
 KeyError: #
 '''},
+{'date': 'Tue Aug 18 2020 22:16:07.133 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 157
+    Kwarwp(Jogo, medidas = STYLE)
+  module <module> line 94
+    self.cena = self.cria(mapa=self.mapa) if vitollino else None
+  module <module> line 107
+    fabrica ={"#": Fab(self.coisa, f"{IMGUR}uwYPNlz.png"), # CERCA
+AttributeError: 'Kwarwp' object has no attribute 'vazio'
+'''},
