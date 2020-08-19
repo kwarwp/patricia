2294846d4afa0421a876ffc196aaa998315161cc
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-19 16:45:32 -0300

    Saved angie/__log__.py @2020-08-19 19:45

--- angie/__log__.py
+++ angie/__log__.py
@@ -2139,3 +2139,33 @@ AttributeError: 'Vazio' object has no attribute 'taba'
     self.taba.fala("ocupa do kwarwp")
 AttributeError: 'dict' object has no attribute 'fala'
 '''},
+{'date': 'Wed Aug 19 2020 16:45:31.591 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 503
+    Kwarwp(Jogo, mapa=MAPA_INICIO)
+  module <module> line 366
+    self.cena = self.cria(mapa=self.mapa) if vitollino else None
+  module <module> line 400
+    self.taba = {(i, j): fabrica[imagem].objeto(fabrica[imagem].imagem, x=i*lado, y=j*lado+lado, cena=cena)
+  module <module> line 468
+    vaga = Vazio("", x=x, y=y, cena=cena, ocupante=coisa)
+  module <module> line 63
+    self.acessa(ocupante)
+  module <module> line 95
+    ocupante.ocupa(self)
+  module <module> line 166
+    self.vaga.sai()
+  module <module> line 490
+    self.taba.fala("sai do kwarwp")
+AttributeError: 'dict' object has no attribute 'fala'
+'''},
