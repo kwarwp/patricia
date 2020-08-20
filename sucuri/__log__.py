570c50571073581cb28030526acd2a03ebb68e82
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-19 23:40:20 -0300

    Saved sucuri/__log__.py @2020-08-20 02:40

--- sucuri/__log__.py
+++ sucuri/__log__.py
@@ -306,3 +306,23 @@ NameError: name 'Piche' is not defined
     class Piche(Vazio):
 NameError: name 'Vazio' is not defined
 '''},
+{'date': 'Wed Aug 19 2020 23:40:19.410 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 441
+    Kwarwp(Jogo, medidas = STYLE) 
+  module <module> line 322
+    self.cena = self.cria(mapa=self.mapa) if vitollino else None
+  module <module> line 352
+    self.ceu = self.v.a(fabrica["~"].imagem, w=lado*self.col, h=lado-10, x=0, y=0, cena=cena, vai=self.executa,
+AttributeError: 'Fab' object has no attribute 'imagem'
+'''},
