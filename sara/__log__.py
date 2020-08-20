5bd1f54d3342dc10021ab1daa9b00027af913380
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-20 17:40:00 -0300

    Saved sara/__log__.py @2020-08-20 20:40

--- sara/__log__.py
+++ sara/__log__.py
@@ -5121,3 +5121,23 @@ AttributeError: 'NoneType' object has no attribute 'a'
                                                                                                                                           ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Thu Aug 20 2020 17:39:59.520 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 415
+    Kwarwp(Jogo, medidas=STYLE)
+  module <module> line 249
+    self.cena = self.cria(mapa=self.mapa) if vitollino else None
+  module <module> line 284
+    sol = self.v.a(fabrica["*"].imagem, w=60, h=60, x=0, y=40, cena=cena, vai=self.esquerda)
+AttributeError: 'Kwarwp' object has no attribute 'esquerda'
+'''},
