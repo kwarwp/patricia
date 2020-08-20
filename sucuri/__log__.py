e2262a3f6f10f4c1571d027b124909a03be65352
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-20 00:06:14 -0300

    Saved sucuri/__log__.py @2020-08-20 03:06

--- sucuri/__log__.py
+++ sucuri/__log__.py
@@ -370,3 +370,29 @@ AttributeError: 'Kwarwp' object has no attribute 'col'
     self.sair = self._sair
 AttributeError: 'Vazio' object has no attribute '_sair'
 '''},
+{'date': 'Thu Aug 20 2020 00:06:13.9 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 441
+    Kwarwp(Jogo, medidas = STYLE) 
+  module <module> line 322
+    self.cena = self.cria(mapa=self.mapa) if vitollino else None
+  module <module> line 361
+    self.taba = {(i, j): fabrica[caracter].objeto(fabrica[caracter].url, x=i*lado, y=j*lado+lado, cena=cena)
+  module <module> line 408
+    vaga = Vazio("", x=x, y=y, cena=cena, ocupante=self.coisa)
+  module <module> line 167
+    self.acessa(ocupante)
+  module <module> line 193
+    ocupante.ocupa(self)
+AttributeError: 'method' object has no attribute 'ocupa'
+'''},
