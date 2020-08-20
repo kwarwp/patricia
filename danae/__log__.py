a21fc58e463fe5ec3cfef9e41a974d90027421ad
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-20 15:22:58 -0300

    Saved danae/__log__.py @2020-08-20 18:22

--- danae/__log__.py
+++ danae/__log__.py
@@ -470,3 +470,35 @@ NameError: name 'func' is not defined
     super().__init__(func, clazz=clazz, oid=oid, tag=S)
 TypeError: __init__() got multiple values for argument 'clazz'
 '''},
+{'date': 'Thu Aug 20 2020 15:22:57.860 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 470
+    Kwarwp(Jogo, medidas = STYLE) 
+  module <module> line 346
+    self.cena = self.cria(mapa=self.mapa) if vitollino else None
+  module <module> line 385
+    self.taba = {(i, j): fabrica[caracter].objeto(fabrica[caracter].url, x=i*lado, y=j*lado+lado, cena=cena)
+  module <module> line 432
+    vaga = Vazio("", x=x, y=y, cena=cena, ocupante=coisa)
+  module <module> line 178
+    self.acessa(ocupante)
+  module <module> line 204
+    ocupante.ocupa(self)
+  module <module> line 257
+    vaga.ocupou(self)
+  module <module> line 214
+    self.vazio.ocupa(ocupante)
+  module _spy.vitollino.main line 586
+    self.elt <= ocupante
+TypeError: can't add 'Piche' object to DOMNode instance
+'''},
