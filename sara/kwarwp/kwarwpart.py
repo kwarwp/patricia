dcd395650abd27daf9bd71461cd047d106e1391b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-20 14:21:54 -0300

    Created sara/kwarwp/kwarwpart.py @2020-08-20 17:21

---
+++ sara/kwarwp/kwarwpart.py
@@ -0,0 +1,14 @@
+# patricia/sara.kwarwp.kwarwpart.py
+# SPDX-License-Identifier: GPL-3.0-or-later
+""" Projeto Indio.
+
+.. codeauthor:: Paulo Assumpção <paulo.assump@gmail.com>
+
+Changelog
+---------
+.. versionadded::    20.08
+        Orgniazação das classes do jogo Kwarwp.
+        As seguintes classes são encontradas:
+        
+
+"""
