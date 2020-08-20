952b564a16f84b4779ff93f32731451e25aa1a3b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-20 16:15:01 -0300

    Saved anaconda/__log__.py @2020-08-20 19:15

--- anaconda/__log__.py
+++ anaconda/__log__.py
@@ -960,3 +960,23 @@ AttributeError: 'Kwarwp' object has no attribute 'vaga'
     teste = Texto("nope", cena=cena)
 TypeError: __init__() got multiple values for argument 'cena'
 '''},
+{'date': 'Thu Aug 20 2020 16:14:59.522 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 317
+    Kwarwp(Jogo, medidas = STYLE) 
+  module <module> line 234
+    self.cena = self.cria(mapa=self.mapa) if vitollino else None
+  module <module> line 264
+    ceu = self.v.a(fabrica["~"].url, w=lado*self.coluna, h=lado, x=0, y=0, cena=cena, vai=self.executa)
+NameError: name 'cena' is not defined
+'''},
