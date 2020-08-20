e882e0775f6c97247cf266fa9338a9c8fb4123b3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-20 08:52:52 -0300

    Saved sara/__log__.py @2020-08-20 11:52

--- sara/__log__.py
+++ sara/__log__.py
@@ -3457,3 +3457,23 @@ AttributeError: 'Fab' object has no attribute 'evento'
     sol = self.v.a(fabrica["*"].imagem, w=60, h=60, x=0, y=40, cena=cena, vai=fabrica["*"].evento)
 AttributeError: 'Fab' object has no attribute 'evento'
 '''},
+{'date': 'Thu Aug 20 2020 08:52:48.511 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 577
+    Kwarwp(Jogo, medidas=STYLE)
+  module <module> line 412
+    self.cena = self.cria(mapa=self.mapa) if vitollino else None
+  module <module> line 449
+    self.taba = {(i, j): fabrica[imagem].objeto(fabrica[imagem].imagem, x=i*lado, y=j*lado+lado, cena=cena, vai=fabrica[imagem].evento)
+AttributeError: 'Fab' object has no attribute 'evento'
+'''},
