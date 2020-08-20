40d70d784c22a28264b91577f7605a57900bb361
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-20 14:24:24 -0300

    Saved sara/indio.py @2020-08-20 17:24

--- sara/indio.py
+++ sara/indio.py
@@ -18,7 +18,7 @@ Changelog
 
 from _spy.vitollino.main import Jogo, STYLE
 from collections import namedtuple as nt
-#from kwarwp.kwarwpart import Vazio, Piche, Oca, Tora, NULO
+from kwarwp.kwarwpart import Vazio, Piche, Oca, Tora, NULO
    
 
 MAPA_INICIO = """
@@ -182,6 +182,7 @@ class Indio():
         pass
     
     def pega(self):
+        print("Pegando objeto")
         """tenta pegar o objeto que está diante dele"""
         destino = (self.posicao[0]+self.azimute.x, self.posicao[1]+self.azimute.y)
         """A posição para onde o índio vai depende do vetor de azimute corrente"""
@@ -216,238 +217,6 @@ class Indio():
         self.indio.ocupa(ocupante)
         self.ocupante = ocupante
         
-# Vamos usar a classe vazio do pacote Kwarwp
-class Vazio():
-    """ Cria um espaço vazio na taba, para alojar os elementos do desafio.
-
-        :param imagem: A figura representando o índio na posição indicada.
-        :param x: Coluna em que o elemento será posicionado.
-        :param y: Cinha em que o elemento será posicionado.
-        :param cena: Cena em que o elemento será posicionado.
-    """
-
-    def __init__(self, imagem, x, y, cena, ocupante=None):
-        self.lado = lado = Kwarwp.LADO
-        self.posicao = (x//lado,y//lado-1)
-        self.vazio = Kwarwp.VITOLLINO.a(imagem, w=lado, h=lado, x=x, y=y, cena=cena)
-        self._nada = Kwarwp.VITOLLINO.a()
-        self.acessa = self._acessa
-        """O **acessa ()** é usado como método dinâmico, variando com o estado da vaga.
-        Inicialmente tem o comportamento de **_acessa ()** que é o estado vago, aceitando ocupantes"""
-        self.ocupante = ocupante or self
-        """O ocupante se não for fornecido é encenado pelo próprio vazio, agindo como nulo"""
-        self.acessa(ocupante)
-        self.sair = self._sair
-        """O **sair ()** é usado como método dinâmico, variando com o estado da vaga.
-        Inicialmente tem o comportamento de **_sair ()** que é o estado leniente, aceitando saidas"""
-
-
-    def _valida_acessa(self, ocupante):
-        self.ocupante.acessa(ocupante)
-        
-
-    def sai(self):
-        """ Rotina de saída falsa, o objeto Indio é usado como uma vaga nula.
-        """
-        pass
-        
-        
-    def _sair(self):
-        """Objeto tenta sair e secebe autorização para seguir"""
-        self.ocupante.siga()
-        
-
-    def _pede_sair(self):
-        """Objeto tenta sair e consulta o ocupante para seguir"""
-        self.ocupante.sair()
-        
-        
-    def _acessa(self, ocupante):
-        """ Atualmente a posição está vaga e pode ser acessada pelo novo ocupante.
-
-        A responsabilidade de ocupar definitivamente a vaga é do candidato a ocupante
-        Caso ele esteja realmente apto a ocupar a vaga e deve cahamar de volta ao vazio
-        com uma chamada ocupou.
-
-            :param ocupante: O canditato a ocupar a posição corrente.
-        """
-        ocupante.ocupa(self)  
-
-
-    def ocupou(self, ocupante):
-        """ O candidato à vaga decidiu ocupá-la e efetivamente entra neste espaço.
-
-        :param ocupante: O canditato a ocupar a posição corrente.
-
-        Este ocupante vai entrar no elemento do Vitollino e definitivamente se tornar
-        o ocupante da vaga. Com isso ele troca o estado do método acessa para primeiro
-        consultar a si mesmo, o ocupante corrente usando o protocolo definido em
-        **_valida_acessa ()**
-
-        """
-        self.vazio.ocupa(ocupante)
-        self.ocupante = ocupante
-        self.acessa = self._valida_acessa
-        self.sair = self._pede_sair
-        
-        
-    @property        
-    def elt(self):
-        """ A propriedade elt faz parte do protocolo do Vitollino para anexar um elemento no outro .
-        No caso do espaço vazio, vai retornar um elemento que não contém nada.
-        """
-        return self.vazio.elt
-        
-        
-    def ocupa(self, vaga):
-        """ Pedido por uma vaga para que ocupe a posição nela.
-        No caso do espaço vazio, não faz nada.
-        """
-        pass
-        
-        
-    def sai(self):
-        """ Pedido por um ocupante para que desocupe a posição nela.
-        """
-        self.ocupante = self
-        self.acessa = self._acessa
-        self.sair = self._sair
-        
-    def pegar(self, requisitante):
-        """ Consulta o ocupante atual se há permissão para pegar e entregar ao requistante.
-            :param requistante: O ator querendo pegar o objeto.
-        """
-        self.ocupante.pegar(requisitante)
-        
-
-
-class Piche(Vazio):
-    """ Poça de Piche que gruda o índio se ele cair nela.
-
-        :param imagem: A figura representando o índio na posição indicada.
-        :param x: Coluna em que o elemento será posicionado.
-        :param y: Cinha em que o elemento será posicionado.
-        :param cena: Cena em que o elemento será posicionado.
-        :param taba: Representa a taba onde o índio faz o desafio.
-    """
-
-
-    def __init__(self, imagem, x, y, cena, taba):
-        self.taba = taba
-        self.vaga = taba
-        self.lado = lado = Kwarwp.LADO
-        self.posicao = (x//lado,y//lado-1)
-        self.vazio = Kwarwp.VITOLLINO.a(imagem, w=lado, h=lado, x=0, y=0, cena=cena)
-        self._nada = Kwarwp.VITOLLINO.a()
-        self.acessa = self._acessa
-        """O **acessa ()** é usado como método dinâmico, variando com o estado da vaga.
-        Inicialmente tem o comportamento de **_acessa ()** que é o estado vago, aceitando ocupantes"""
-        self.sair = self._sair
-        """O **sair ()** é usado como método dinâmico, variando com o estado da vaga.
-        Inicialmente tem o comportamento de **_sair ()** que é o estado vago, aceitando ocupantes"""
-
-
-    def ocupa(self, vaga):
-        """ Pedido por uma vaga para que ocupe a posição nela.
-
-        :param vaga: A vaga que será ocupada pelo componente.
-
-        No caso do piche, requisita que a vaga seja ocupada por ele.
-        """
-        self.vaga.sai()
-        self.posicao = vaga.posicao
-        vaga.ocupou(self)
-        self.vaga = vaga
-
-
-    def _pede_sair(self):
-        """Objeto tenta sair mas não é autorizado"""
-        self.taba.fala("Você ficou preso no piche")
-
-
-class Tora(Piche):
-    """  A Tora é um pedaço de tronco cortado que o índio pode carregar ou empurrar.
-
-        :param imagem: A figura representando o índio na posição indicada.
-        :param x: Coluna em que o elemento será posicionado.
-        :param y: Linha em que o elemento será posicionado.
-        :param cena: Cena em que o elemento será posicionado.
-        :param taba: Representa a taba onde o índio faz o desafio.
-    """
-
-    def pegar(self, requisitante):
-        """ Consulta o ocupante atual se há permissão para pegar e entregar ao requistante.
-
-            :param requistante: O ator querendo pegar o objeto.
-        """
-        vaga = requisitante
-        self.vaga.sai()
-        # self.posicao = vaga.posicao
-        vaga.ocupou(self)
-        self.vaga = vaga
-
-    @property
-    def posicao(self):
-        """ A propriedade posição faz parte do protocolo do double dispatch com o Indio .
-
-        No caso da tora, retorna o a posição do atributo **self.vaga**.
-        """
-        return self.vaga.posicao
-
-    @posicao.setter
-    def posicao(self, _):
-        """ A propriedade posição faz parte do protocolo do double dispatch com o Indio .
-
-        No caso da tora, é uma propriedade de somente leitura, não executa nada.
-        """
-        pass
-
-    @property
-    def elt(self):
-        """ A propriedade elt faz parte do protocolo do Vitollino para anexar um elemento no outro .
-
-        No caso da tora, retorna o elt do elemento do atributo **self.vazio**.
-        """
-        return self.vazio.elt
-
-    def _acessa(self, ocupante):
-        """ Pedido de acesso a essa posição, delegada ao ocupante pela vaga.
-
-        :param ocupante: O componente candidato a ocupar a vaga já ocupada pelo índio.
-
-        No caso da tora, ela age como um obstáculo e não prossegue com o protocolo.
-        """
-        pass
-        
-        
-
-class Oca(Piche):
-    """ A Oca é o destino final do índio, não poderá sair se ele entrar nela.
-
-        :param imagem: A figura representando o índio na posição indicada.
-        :param x: Coluna em que o elemento será posicionado.
-        :param y: Cinha em que o elemento será posicionado.
-        :param cena: Cena em que o elemento será posicionado.
-        :param taba: Representa a taba onde o índio faz o desafio.
-    """
-
-
-    def _pede_sair(self):
-        """Objeto tenta sair mas não é autorizado"""
-        self.taba.fala("Você chegou no seu objetivo")
-
-
-    def _acessa(self, ocupante):
-        """ Atualmente a posição está vaga e pode ser acessada pelo novo ocupante.
-
-        A responsabilidade de ocupar definitivamente a vaga é do candidato a ocupante
-        Caso ele esteja realmente apto a ocupar a vaga e deve cahamar de volta ao vazio
-        com uma chamada ocupou.
-
-            :param ocupante: O canditato a ocupar a posição corrente.
-        """
-        self.taba.fala("Você chegou no seu objetivo")
-        ocupante.ocupa(self)
 
 
 class Kwarwp():
@@ -633,23 +402,6 @@ class Kwarwp():
         """o índio.vai é associado ao seu próprio metodo pega"""
         vaga = Vazio("", x=x, y=y, cena=cena, ocupante=self.o_indio)
         return vaga
-
-
-class Nulo:
-    """Objeto nulo que responde passivamente a todas as requisições."""
-    def __init__(self):
-        self.pegar = self.ocupa = self.nulo
-
-    def nulo(self, *_, **__):
-        """Método nulo, responde passivamente a todas as chamadas.
-
-        :param _: aceita todos os argumentos posicionais.
-        :param __: aceita todos os argumentos nomeados.
-        :return: retorna o próprio objeto nulo.
-        """
-        return self
-
-NULO = Nulo()
 
 
 if __name__ == "__main__":
