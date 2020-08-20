d634a8ee78a05b4a7a187d850eb830d0fea4819b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-20 10:04:58 -0300

    Saved sara/__log__.py @2020-08-20 13:04

--- sara/__log__.py
+++ sara/__log__.py
@@ -3642,3 +3642,25 @@ Exception: <TypeError: Cannot read property '$methods' of undefined>
 'error': '''
 Exception: <TypeError: Cannot read property '$methods' of undefined>
 '''},
+{'date': 'Thu Aug 20 2020 10:04:57.391 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 406
+    Kwarwp(Jogo, medidas=STYLE)
+  module <module> line 241
+    self.cena = self.cria(mapa=self.mapa) if vitollino else None
+  module <module> line 278
+    self.taba = {(i, j): fabrica[imagem].objeto(fabrica[imagem].imagem, x=i*lado, y=j*lado+lado, cena=cena)
+  module <module> line 332
+    coisa = Piche(imagem, x=0, y=0, cena=cena, taba=self)
+TypeError: 'module' object is not callable
+'''},
