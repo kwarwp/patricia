34b4201e8a4d0bc8f262a3a19f12d90320aca89b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-20 09:56:03 -0300

    Saved sara/__log__.py @2020-08-20 12:56

--- sara/__log__.py
+++ sara/__log__.py
@@ -3612,3 +3612,25 @@ IndentationError: unexpected indent
     coisa = Tora(imagem, x=x, y=y, cena=cena, taba=self)
 TypeError: 'module' object is not callable
 '''},
+{'date': 'Thu Aug 20 2020 09:56:02.715 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 576
+    Kwarwp(Jogo, medidas=STYLE)
+  module <module> line 411
+    self.cena = self.cria(mapa=self.mapa) if vitollino else None
+  module <module> line 448
+    self.taba = {(i, j): fabrica[imagem].objeto(fabrica[imagem].imagem, x=i*lado, y=j*lado+lado, cena=cena)
+  module <module> line 550
+    coisa = Tora(imagem, x=x, y=y, cena=cena, taba=self)
+TypeError: 'module' object is not callable
+'''},
