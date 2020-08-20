5afe1f6fcd6a8633eec2f3fbf0ea668c42a07f3c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-20 16:54:33 -0300

    Saved angie/__log__.py @2020-08-20 19:54

--- angie/__log__.py
+++ angie/__log__.py
@@ -2763,3 +2763,27 @@ AttributeError: 'module' object has no attribute 'VITOLLINO'
     Nulo(self)
 TypeError: __init__() takes 1 positional argument but more were given
 '''},
+{'date': 'Thu Aug 20 2020 16:54:33.44 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 371
+    Kwarwp(Jogo, mapa=MAPA_INICIO)
+  module <module> line 203
+    self.cena = self.cria(mapa=self.mapa) if vitollino else None
+  module <module> line 237
+    self.taba = {(i, j): fabrica[imagem].objeto(fabrica[imagem].imagem, x=i*lado, y=j*lado+lado, cena=cena)
+  module <module> line 265
+    vaga = Vazio(imagem, x=x, y=y, cena=cena, ocupante=self)
+  module angie.kwarwpart line 54
+    Kwarwp.VITOLLINO = Kwarwp.VITOLLINO or Jogo()
+AttributeError: 'module' object has no attribute 'VITOLLINO'
+'''},
