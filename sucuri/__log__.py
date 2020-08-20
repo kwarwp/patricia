49ab9544e61aa41f4a56b4abffe0884467300a64
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-20 00:10:30 -0300

    Saved sucuri/__log__.py @2020-08-20 03:10

--- sucuri/__log__.py
+++ sucuri/__log__.py
@@ -396,3 +396,35 @@ AttributeError: 'Vazio' object has no attribute '_sair'
     ocupante.ocupa(self)
 AttributeError: 'method' object has no attribute 'ocupa'
 '''},
+{'date': 'Thu Aug 20 2020 00:10:27.889 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 454
+    Kwarwp(Jogo, medidas = STYLE) 
+  module <module> line 335
+    self.cena = self.cria(mapa=self.mapa) if vitollino else None
+  module <module> line 374
+    self.taba = {(i, j): fabrica[caracter].objeto(fabrica[caracter].url, x=i*lado, y=j*lado+lado, cena=cena)
+  module <module> line 421
+    vaga = Vazio("", x=x, y=y, cena=cena, ocupante=coisa)
+  module <module> line 167
+    self.acessa(ocupante)
+  module <module> line 193
+    ocupante.ocupa(self)
+  module <module> line 246
+    vaga.ocupou(self)
+  module <module> line 203
+    self.vazio.ocupa(ocupante)
+  module _spy.vitollino.main line 586
+    self.elt <= ocupante
+TypeError: can't add 'Piche' object to DOMNode instance
+'''},
