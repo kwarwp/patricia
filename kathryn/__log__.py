38e4c958e29c2a68b0dac43872c08d18b5f4317e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-20 16:32:06 -0300

    Saved kathryn/__log__.py @2020-08-20 19:32

--- kathryn/__log__.py
+++ kathryn/__log__.py
@@ -1004,3 +1004,23 @@ TypeError: can't add 'Piche' object to DOMNode instance
     self.elt <= ocupante
 TypeError: can't add 'Piche' object to DOMNode instance
 '''},
+{'date': 'Thu Aug 20 2020 16:32:03.504 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 542
+    Kwarwp(Jogo, medidas=STYLE)
+  module <module> line 251
+    self.cena = self.cria(mapa=self.mapa) if vitollino else None
+  module <module> line 286
+    self.taba = {(i, j): fabrica[imagem].objeto(fabrica[imagem].imagem, x=i*lado, y=j*lado+lado, cena=cena, vai=self.carrega)
+AttributeError: 'Kwarwp' object has no attribute 'carrega'
+'''},
