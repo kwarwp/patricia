27ccba89698f821c88f35fc7d801ffcfd24100cd
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-20 15:29:57 -0300

    Saved kathryn/__log__.py @2020-08-20 18:29

--- kathryn/__log__.py
+++ kathryn/__log__.py
@@ -844,3 +844,35 @@ TypeError: can't add 'Indio' object to DOMNode instance
     self.elt <= ocupante.elt
 TypeError: can't add 'method' object to DOMNode instance
 '''},
+{'date': 'Thu Aug 20 2020 15:29:54.626 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 344
+    Kwarwp(Jogo, medidas=STYLE)
+  module <module> line 44
+    self.cena = self.cria(mapa=self.mapa) if vitollino else None
+  module <module> line 72
+    self.taba = {(i, j): 
+  module <module> line 127
+    vaga = Vazio("", x=x, y=y, cena=cena, ocupante=coisa)
+  module <module> line 237
+    self.acessa(ocupante)
+  module <module> line 246
+    ocupante.ocupa(self)
+  module <module> line 305
+    vaga.ocupou(self)
+  module <module> line 249
+    self.vazio.ocupa(ocupante)
+  module _spy.vitollino.main line 584
+    self.elt <= ocupante.elt
+TypeError: can't add 'method' object to DOMNode instance
+'''},
