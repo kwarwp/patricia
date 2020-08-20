e71bd373c0f135e102e8e4afe08c04a74b5928a1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-20 16:56:38 -0300

    Saved anaconda/__log__.py @2020-08-20 19:56

--- anaconda/__log__.py
+++ anaconda/__log__.py
@@ -980,3 +980,29 @@ TypeError: __init__() got multiple values for argument 'cena'
     ceu = self.v.a(fabrica["~"].url, w=lado*self.coluna, h=lado, x=0, y=0, cena=cena, vai=self.executa)
 NameError: name 'cena' is not defined
 '''},
+{'date': 'Thu Aug 20 2020 16:56:36.482 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 331
+    Kwarwp(Jogo, medidas = STYLE) 
+  module <module> line 245
+    self.cena = self.cria(mapa=self.mapa) if vitollino else None
+  module <module> line 279
+    self.taba = {(i, j): fabrica[caracter].objeto(fabrica[caracter].url, x=i*lado, y=j*lado+lado, cena=cena)
+  module <module> line 304
+    vaga = Vazio("", x=x, y=y, cena=cena, ocupante=self.a_coisa)
+  module <module> line 145
+    self.acessa(ocupante)
+  module <module> line 165
+    ocupante.ocupa(self)
+AttributeError: 'NoneType' object has no attribute 'ocupa'
+'''},
