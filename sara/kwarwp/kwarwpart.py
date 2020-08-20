37c3bae6ad5a5d078c5a6f09d505da89ec5e1408
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-20 14:24:03 -0300

    Saved sara/kwarwp/kwarwpart.py @2020-08-20 17:24

--- sara/kwarwp/kwarwpart.py
+++ sara/kwarwp/kwarwpart.py
@@ -249,3 +249,18 @@ class Oca(Piche):
         self.taba.fala("Você chegou no seu objetivo")
         ocupante.ocupa(self)
 
+class Nulo:
+    """Objeto nulo que responde passivamente a todas as requisições."""
+    def __init__(self):
+        self.pegar = self.ocupa = self.nulo
+
+    def nulo(self, *_, **__):
+        """Método nulo, responde passivamente a todas as chamadas.
+
+        :param _: aceita todos os argumentos posicionais.
+        :param __: aceita todos os argumentos nomeados.
+        :return: retorna o próprio objeto nulo.
+        """
+        return self
+
+NULO = Nulo()
