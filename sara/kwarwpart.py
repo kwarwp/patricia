ec3be74a4949f7211348c2ad9b1ad2461428a4ea
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-20 15:29:07 -0300

    Saved sara/kwarwpart.py @2020-08-20 18:29

--- sara/kwarwpart.py
+++ sara/kwarwpart.py
@@ -131,6 +131,7 @@ class Piche(Vazio):
         self.taba = taba
         self.vaga = taba
         self.lado = lado = Kwarwp.LADO
+        print(x, lado)
         self.posicao = (x//lado,y//lado-1)
         self.vazio = Kwarwp.VITOLLINO.a(imagem, w=lado, h=lado, x=0, y=0, cena=cena)
         self._nada = Kwarwp.VITOLLINO.a()
