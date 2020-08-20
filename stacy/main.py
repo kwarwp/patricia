41da84beae79d34c693c9407ab34f67a9dec58d4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-20 16:09:31 -0300

    Saved stacy/main.py @2020-08-20 19:09

--- stacy/main.py
+++ stacy/main.py
@@ -34,16 +34,13 @@ MAPA_ROCHA = """
 
 MAPA_INICIO = """
 @....&
-......
-......
+.#..#..
+....#..
 .#.^..
 """
 
 from collections import namedtuple as nt
 from kwarwp.kwarwpart import Vazio, Piche, Oca, Tora, NULO
-
-IMGUR = "https://imgur.com/"
-"""Prefixo do site imgur."""
 
 
 class Vazio():
@@ -226,7 +223,7 @@ class Kwarwp():
         "*": Fab(self.coisa, f"{IMGUR}PfodQmT.gif"), # SOL
         "+": Fab(self.coisa, f"{IMGUR}uwYPNlz.png"),  # CERCA
         "%": Fab(self.coisa, f"{IMGUR}Ry3Vmsn.png") #ROCHA
-           : Fab(self.coisa, F"{IMGUR} = "https://imgur.com/")
+           
 
         
 
