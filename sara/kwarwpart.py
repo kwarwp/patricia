a37fc61996f496020bc22f0a45fd8729837b486f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-20 15:39:17 -0300

    Saved sara/kwarwpart.py @2020-08-20 18:39

--- sara/kwarwpart.py
+++ sara/kwarwpart.py
@@ -126,8 +126,6 @@ class Piche(Vazio):
         from sara.kwarwp import Kwarwp
         """Importando localmente o Kwarwp para evitar referência circular."""
         
-        print("Cheguei aqui!!!")
-        
         self.taba = taba
         self.vaga = taba
         self.lado = lado = Kwarwp.LADO
