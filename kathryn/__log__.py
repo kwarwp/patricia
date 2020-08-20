8486ccc565442e02d6625b4f01d6e4758e884074
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-20 14:41:24 -0300

    Saved kathryn/__log__.py @2020-08-20 17:41

--- kathryn/__log__.py
+++ kathryn/__log__.py
@@ -712,3 +712,25 @@ IndentationError: unexpected indent
     sol = self.v.a(fabrica["*"].imagem, w=60, h=60, x=0, y=40, cena=cena, vai=self.esquerda)
 AttributeError: 'Kwarwp' object has no attribute 'esquerda'
 '''},
+{'date': 'Thu Aug 20 2020 14:41:21.911 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 263
+    Kwarwp(Jogo, medidas=STYLE)
+  module <module> line 37
+    self.cena = self.cria(mapa=self.mapa) if vitollino else None
+  module <module> line 66
+    self.taba = {(i, j): 
+  module <module> line 124
+    coisa = Piche(imagem, x=0, y=0, cena=cena, taba=self)
+NameError: name 'Piche' is not defined
+'''},
