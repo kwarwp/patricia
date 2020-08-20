3e284eaada06d6a319fc2b43fc71196d214e715b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-20 11:17:36 -0300

    Saved sara/__log__.py @2020-08-20 14:17

--- sara/__log__.py
+++ sara/__log__.py
@@ -3680,3 +3680,29 @@ TypeError: 'module' object is not callable
     class Tora(Piche):
 NameError: name 'Piche' is not defined
 '''},
+{'date': 'Thu Aug 20 2020 11:17:35.382 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 623
+    main(Jogo, STYLE)
+  module <module> line 617
+    Kwarwp(vitollino, medidas=medidas)
+  module <module> line 469
+    self.cena = self.cria(mapa=self.mapa) if vitollino else None
+  module <module> line 503
+    self.taba = {(i, j): fabrica[imagem].objeto(fabrica[imagem].imagem, x=i*lado, y=j*lado+lado, cena=cena)
+  module <module> line 568
+    coisa = Piche(imagem, x=0, y=0, cena=cena, taba=self)
+  module <module> line 177
+    Kwarwp.VITOLLINO = Kwarwp.VITOLLINO or Jogo()
+AttributeError: 'module' object has no attribute 'VITOLLINO'
+'''},
