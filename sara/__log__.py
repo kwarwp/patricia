d0d588985d89b8a85760031d6e53ba63ef8a1b14
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-21 11:39:15 -0300

    Saved sara/__log__.py @2020-08-21 14:39

--- sara/__log__.py
+++ sara/__log__.py
@@ -5168,3 +5168,47 @@ AttributeError: 'Kwarwp' object has no attribute 'executa'
                     ^
 SyntaxError: EOL while scanning string literal
 '''},
+{'date': 'Fri Aug 21 2020 11:39:14.106 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Area vazia
+Area vazia
+Area vazia
+Area vazia
+Area vazia
+Area vazia
+Area vazia
+Area vazia
+Area vazia
+Area vazia
+Area vazia
+Area vazia
+Area vazia
+Area vazia
+Area vazia
+Area vazia
+Area vazia
+Area vazia
+Area vazia
+Area vazia
+Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 421
+    Kwarwp(Jogo, medidas=STYLE)
+  module <module> line 259
+    self.cena = self.cria(mapa=self.mapa) if vitollino else None
+  module <module> line 296
+    self.taba = {(i, j): fabrica[imagem].objeto(fabrica[imagem].imagem, x=i*lado, y=j*lado+lado, cena=cena)
+  module <module> line 409
+    self.o_indio = Indio(imagem, x=1, y=2, cena=cena, taba=self)
+  module <module> line 77
+    self.indio = Vazio.VITOLLINO.a(imagem, w=lado, h=lado, x=x, y=y, cena=cena)
+AttributeError: 'Vazio' object has no attribute 'VITOLLINO'
+'''},
