f82471e708298059a5ccb8a9af8fb738c581336f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-21 11:42:25 -0300

    Saved sara/__log__.py @2020-08-21 14:42

--- sara/__log__.py
+++ sara/__log__.py
@@ -5256,3 +5256,25 @@ Traceback (most recent call last):
     self.indio = Vazio.VITOLLINO.a(imagem, w=lado, h=lado, x=x, y=y, cena=cena)
 AttributeError: 'Vazio' object has no attribute 'VITOLLINO'
 '''},
+{'date': 'Fri Aug 21 2020 11:42:24.886 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 413
+    Kwarwp(Jogo, medidas=STYLE)
+  module <module> line 248
+    self.cena = self.cria(mapa=self.mapa) if vitollino else None
+  module <module> line 285
+    self.taba = {(i, j): fabrica[imagem].objeto(fabrica[imagem].imagem, x=i*lado, y=j*lado+lado, cena=cena)
+  module <module> line 339
+    coisa = Piche(imagem, x=0, y=0, cena=cena, taba=self)
+TypeError: 'module' object is not callable
+'''},
