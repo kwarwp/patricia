8d03232ad330290ea4fe6d9cf956d59f8ec1bbbd
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-25 23:58:41 -0300

    Saved sucuri/__log__.py @2020-08-26 02:58

--- sucuri/__log__.py
+++ sucuri/__log__.py
@@ -608,3 +608,35 @@ SyntaxError: invalid syntax
     self.ocupante = NULO
 NameError: name 'NULO' is not defined
 '''},
+{'date': 'Tue Aug 25 2020 23:58:39.535 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 487
+    Kwarwp(Jogo, medidas = STYLE) 
+  module <module> line 363
+    self.cena = self.cria(mapa=self.mapa) if vitollino else None
+  module <module> line 402
+    self.taba = {(i, j): fabrica[caracter].objeto(fabrica[caracter].url, x=i*lado, y=j*lado+lado, cena=cena)
+  module <module> line 449
+    vaga = Vazio("", x=x, y=y, cena=cena, ocupante=coisa)
+  module <module> line 180
+    self.acessa(ocupante)
+  module <module> line 206
+    ocupante.ocupa(self)
+  module <module> line 259
+    vaga.ocupou(self)
+  module <module> line 216
+    self.vazio.ocupa(ocupante)
+  module _spy.vitollino.main line 586
+    self.elt <= ocupante
+TypeError: can't add 'Piche' object to DOMNode instance
+'''},
