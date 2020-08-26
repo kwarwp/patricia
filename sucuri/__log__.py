7ae10dbb940eef9b34ea9b6a5c2a98deed9eeb30
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-25 23:03:59 -0300

    Saved sucuri/__log__.py @2020-08-26 02:03

--- sucuri/__log__.py
+++ sucuri/__log__.py
@@ -584,3 +584,27 @@ IndentationError: unexpected indent
    ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Tue Aug 25 2020 23:03:57.704 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 487
+    Kwarwp(Jogo, medidas = STYLE) 
+  module <module> line 363
+    self.cena = self.cria(mapa=self.mapa) if vitollino else None
+  module <module> line 402
+    self.taba = {(i, j): fabrica[caracter].objeto(fabrica[caracter].url, x=i*lado, y=j*lado+lado, cena=cena)
+  module <module> line 421
+    self.o_indio = Indio(imagem, x=0, y=0, cena=cena, taba=self)
+  module <module> line 53
+    self.ocupante = NULO
+NameError: name 'NULO' is not defined
+'''},
