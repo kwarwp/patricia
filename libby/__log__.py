0465c58505a177ab90a0195bf43a7f1d948536d2
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-27 12:41:54 -0300

    Saved libby/__log__.py @2020-08-27 15:41

--- libby/__log__.py
+++ libby/__log__.py
@@ -1525,3 +1525,25 @@ TypeError: 'module' object is not callable
     coisa = Piche(imagem, x=0, y=0, cena=cena, taba=self)
 TypeError: 'module' object is not callable
 '''},
+{'date': 'Thu Aug 27 2020 12:41:17.211 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 429
+    Kwarwp(Jogo, mapa=MAPA_INICIO)
+  module <module> line 219
+    self.cena = self.cria(mapa=self.mapa) if vitollino else None
+  module <module> line 253
+    self.taba = {(i, j): fabrica[imagem].objeto(fabrica[imagem].imagem, x=i*lado, y=j*lado+lado, cena=cena)
+  module <module> line 397
+    coisa = Piche(imagem, x=0, y=0, cena=cena, taba=self)
+TypeError: 'module' object is not callable
+'''},
