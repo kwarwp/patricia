9159207376f44d476b54319231789c35aa1cded5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-27 19:01:43 -0300

    Saved sucuri/__log__.py @2020-08-27 22:01

--- sucuri/__log__.py
+++ sucuri/__log__.py
@@ -1286,3 +1286,27 @@ NameError: name 'NULO' is not defined
     self.posicao = (x//lado,y//lado)
 TypeError: unsupported operand type(s) for //: 'int' and 'NoneType'
 '''},
+{'date': 'Thu Aug 27 2020 19:01:39.600 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 397
+    Kwarwp(Jogo, medidas = STYLE) 
+  module <module> line 254
+    self.cena = self.cria(mapa=self.mapa) if vitollino else None #tentar entender
+  module <module> line 291
+    self.taba = {(i, j): fabrica[caracter].objeto(fabrica[caracter].url, x=i*lado, y=j*lado+lado, cena=cena)
+  module <module> line 353
+    self.o_indio = Indio(imagem, x=1, y=0, cena=cena, taba=self)
+  module <module> line 57
+    self.posicao = (x//lado,y//lado)
+TypeError: unsupported operand type(s) for //: 'int' and 'NoneType'
+'''},
