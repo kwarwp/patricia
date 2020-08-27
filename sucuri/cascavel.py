23feb055360acc7f752369cc8f54921728e48f6e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-27 17:14:22 -0300

    Saved sucuri/cascavel.py @2020-08-27 20:14

--- sucuri/cascavel.py
+++ sucuri/cascavel.py
@@ -51,6 +51,7 @@ class Indio():
         self.lado = lado = Vazio.LADO # antes self.lado = lado = Kwarwp.LADO
         self.azimute = self.AZIMUTE.n
         """índio olhando para o norte"""
+        self.ocupante = NULO 
         self.taba = taba
         self.vaga = self
         self.posicao = (x//lado,y//lado)
