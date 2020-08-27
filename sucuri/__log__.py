4a1ed0a4a5dc999e29b10a213f3457e3f4bb97f2
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-27 12:06:37 -0300

    Saved sucuri/__log__.py @2020-08-27 15:06

--- sucuri/__log__.py
+++ sucuri/__log__.py
@@ -778,3 +778,19 @@ IndentationError: expected an indented block
                             ^
 IndentationError: expected an indented block
 '''},
+{'date': 'Thu Aug 27 2020 12:06:37.100 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 95
+    class Tora(Piche):
+NameError: name 'Piche' is not defined
+'''},
