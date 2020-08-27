6ea4bcaf287dba9555dd7b7f40e69e4c48ca65f6
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-27 16:01:15 -0300

    Saved sara/kwarwpart.py @2020-08-27 19:01

--- sara/kwarwpart.py
+++ sara/kwarwpart.py
@@ -259,7 +259,7 @@ class Oca(Piche):
         com uma chamada ocupou.
             :param ocupante: O canditato a ocupar a posição corrente.
         """
-        self.taba.fala("Você chegou no seu objetivo", type(self.ocupante))    
+        self.taba.fala("Você chegou no seu objetivo", type(ocupante))    
         ocupante.ocupa(self)
         
     def sai(self):
