2c60b5aba43f3721da3da9dfd41f9ff5f625e3bf
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-27 09:45:50 -0300

    Saved sucuri/__log__.py @2020-08-27 12:45

--- sucuri/__log__.py
+++ sucuri/__log__.py
@@ -702,3 +702,19 @@ IndentationError: expected an indented block
     from sucuri.coralpart import Vazio, Oca, Tora, Nulo
 ImportError: cannot import name 'Vazio'
 '''},
+{'date': 'Thu Aug 27 2020 09:45:49.731 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 52
+    class Tora(Piche):
+NameError: name 'Piche' is not defined
+'''},
