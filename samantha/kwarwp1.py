ebfce609cbfee2de07d782791019fb672cb3946a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-29 17:52:35 -0300

    Saved samantha/kwarwp1.py @2020-08-29 20:52

--- samantha/kwarwp1.py
+++ samantha/kwarwp1.py
@@ -12,13 +12,6 @@ Changelog
 """
 
 MAPA_INICIO = """
-@....&
-......
-......
-.#.^..
-"""
-
-MAPA_ROCHA = """
 +++++++
 +..+..&
 +..#..+
@@ -28,9 +21,10 @@ MAPA_ROCHA = """
 
 
 class Kwarwp():
-    """ Jogo para ensino de programação.
-
+    """ Arena onde os desafios ocorrem.
+    
         :param vitollino: Empacota o engenho de jogo Vitollino.
+        :param mapa: Um texto representando o mapa do desafio.
     """
     GLIFOS = {
     "&": "https://i.imgur.com/dZQ8liT.jpg",  # OCA
@@ -40,32 +34,35 @@ class Kwarwp():
     "#": "https://imgur.com/ldI7IbK.png",  # TORA
     "@": "https://imgur.com/tLLVjfN.png",  # PICHE
     "~": "https://i.imgur.com/UAETaiP.gif",  # CEU
-    "*": "https://i.imgur.com/PfodQmT.gif"  # SOL
+    "*": "https://i.imgur.com/PfodQmT.gif",  # SOL
+    "+": "https://i.imgur.com/uwYPNlz.png"  # Cerca
     }
-
-    def __init__(self, vitollino=None, cenario="default"):
+    
+    def __init__(self, vitollino=None, mapa=MAPA_INICIO, medidas={}):
         self.v = vitollino()
-        self.cena = self.cria(cenario=cenario) if vitollino else None
-
-    def cria(self, cenario="default"):
-        """ Cria o ambiente de programação Kwarwp."""
-        cena = self.v.c(self.SOLO)
-        indio = self.v.a(self.INDIO, w=100, h=100, x=300, y=400, cena=cena)
-        oca = self.v.a(self.OCA, w=100, h=100, x=500, y=100, cena=cena)
-        tora = self.v.a(self.TORA, w=100, h=100, x=100, y=400, cena=cena)
-        piche = self.v.a(self.PICHE, w=100, h=100, x=100, y=100, cena=cena)
-        piche = self.v.a(self.CEU, w=600, h=100, x=0, y=0, cena=cena)
-        sol = self.v.a(self.SOL, w=60, h=60, x=0, y=40, cena=cena)
-        cerca = self.v.a(self.CERCA, w=100, h=100, x=0, y=500, cena=cena)
-        cerca = self.v.a(self.CERCA, w=100, h=100, x=100, y=500, cena=cena)
-        cerca = self.v.a(self.CERCA, w=100, h=100, x=200, y=500, cena=cena)
-        cerca = self.v.a(self.CERCA, w=100, h=100, x=300, y=500, cena=cena)
-        cerca = self.v.a(self.CERCA, w=100, h=100, x=400, y=500, cena=cena)
-        cerca = self.v.a(self.CERCA, w=100, h=100, x=500, y=500, cena=cena)
+        """Cria um matriz com os elementos descritos em cada linha de texto"""
+        mapa = mapa.split()
+        """Largura da casa da arena dos desafios, número de colunas no mapa"""
+        self.lado, self.col = 600//8, len(mapa[0]) 
+        self.cena = self.cria(mapa=mapa) if vitollino else None
+        
+    def cria(self, mapa="  "):
+        """Cria um cenário com imagem de terra de chão batido, céu e sol"""
+        lado = self.lado
+        cena = self.v.c(self.GLIFOS["_"])
+        ceu = self.v.a(self.GLIFOS["~"], w=lado*self.col, h=lado, x=0, y=0, cena=cena)
+        sol = self.v.a(self.GLIFOS["*"], w=60, h=60, x=0, y=40, cena=cena)
+        """Posiciona os elementos segundo suas posições i, j na matriz mapa"""
+        [self.cria_elemento( x=i*lado, y=j*lado+lado, cena=cena)
+            for j, linha in enumerate(mapa) for i, imagem in enumerate(linha)]
         cena.vai()
         return cena
         
+    def cria_elemento(self, x, y, cena):
+        lado = self.lado
+        return self.v.a(self.GLIFOS[imagem], w=lado, h=lado, x=i*lado, y=j*lado+lado, cena=cena)   
+    
 if __name__ == "__main__":
     from _spy.vitollino.main import Jogo, STYLE
-    STYLE.update(width=600, height="600px")
-    Kwarwp(Jogo).cria()
+    STYLE.update(width=500, height="400px")
+    Kwarwp(Jogo)
