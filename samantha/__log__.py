88808f1f89038f00c27b26cb3eb18a63186b3183
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-29 17:39:21 -0300

    Saved samantha/__log__.py @2020-08-29 20:39

--- samantha/__log__.py
+++ samantha/__log__.py
@@ -33,3 +33,23 @@ IndentationError: expected an indented block
   ^
 IndentationError: expected an indented block
 '''},
+{'date': 'Sat Aug 29 2020 17:39:20.975 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 74
+    Kwarwp(Jogo).cria()
+  module <module> line 52
+    self.cena = self.cria(mapa=mapa) if vitollino else None
+  module <module> line 62
+    [self.cria_elemento( x=i*lado, y=j*lado+lado, cena=cena)
+AttributeError: 'Kwarwp' object has no attribute 'cria_elemento'
+'''},
