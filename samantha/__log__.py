97d8fc33ae41eb3db04acca6c9a05f4298b6e627
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-29 17:49:10 -0300

    Saved samantha/__log__.py @2020-08-29 20:49

--- samantha/__log__.py
+++ samantha/__log__.py
@@ -178,3 +178,21 @@ KeyError:
     mapa = mapa.split()
 UnboundLocalError: local variable 'mapa' referenced before assignment
 '''},
+{'date': 'Sat Aug 29 2020 17:49:10.159 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 65
+    Kwarwp(Jogo).cria()
+  module <module> line 42
+    self.cena = self.cria(mapa=mapa) if vitollino else None
+TypeError: cria() got an unexpected keyword argument 'mapa'
+'''},
