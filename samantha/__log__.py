23dd018fa99be65dfb198c8cfe26c5ab0c814687
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-29 17:46:33 -0300

    Saved samantha/__log__.py @2020-08-29 20:46

--- samantha/__log__.py
+++ samantha/__log__.py
@@ -120,3 +120,23 @@ KeyError:
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Sat Aug 29 2020 17:46:32.164 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 74
+    Kwarwp(Jogo).cria()
+  module <module> line 61
+    [self.cria_elemento( x=i*lado, y=j*lado+lado, cena=cena)
+  module <module> line 68
+    return self.v.a(self.GLIFOS[imagem], w=lado, h=lado, x=i*lado, y=j*lado+lado, cena=cena)
+KeyError:  
+'''},
