292e570df13b9fb8e4e9b0e69c3420e9cfaafbb3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-29 17:50:50 -0300

    Saved samantha/__log__.py @2020-08-29 20:50

--- samantha/__log__.py
+++ samantha/__log__.py
@@ -221,3 +221,23 @@ TypeError: cria() got an unexpected keyword argument 'mapa'
   ^
 IndentationError: expected an indented block
 '''},
+{'date': 'Sat Aug 29 2020 17:50:50.61 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 64
+    Kwarwp(Jogo).cria()
+  module <module> line 43
+    self.cena = self.cria(mapa=mapa) if vitollino else None
+  module <module> line 52
+    [self.cria_elemento( x=i*lado, y=j*lado+lado, cena=cena)
+AttributeError: 'Kwarwp' object has no attribute 'cria_elemento'
+'''},
