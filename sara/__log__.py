e51f9d6644f6d703b66ae99ed586eea42aaacc21
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-03 15:29:18 -0300

    Saved sara/__log__.py @2020-09-03 18:29

--- sara/__log__.py
+++ sara/__log__.py
@@ -5390,3 +5390,25 @@ AttributeError: 'Tora' object has no attribute 'empurrante'
     if self.empurrante is None:
 AttributeError: 'Tora' object has no attribute 'empurrante'
 '''},
+{'date': 'Thu Sep 03 2020 15:29:06.666 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 444
+    Kwarwp(Jogo, medidas=STYLE)
+  module <module> line 283
+    self.cena = self.cria(mapa=self.mapa) if vitollino else None
+  module <module> line 320
+    self.taba = {(i, j): fabrica[imagem].objeto(fabrica[imagem].imagem, x=i*lado, y=j*lado+lado, cena=cena)
+  module <module> line 418
+    coisa = Tora(imagem, x=x, y=y, cena=cena, taba=self)
+TypeError: __init__() takes 1 positional arguments but more were given
+'''},
