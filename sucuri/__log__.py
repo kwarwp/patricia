4dfd1ac899ae78161e129069bf2be9b53a6a5970
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-03 16:31:22 -0300

    Saved sucuri/__log__.py @2020-09-03 19:31

--- sucuri/__log__.py
+++ sucuri/__log__.py
@@ -1530,3 +1530,19 @@ ImportError: cannot import name 'Vazio'
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Thu Sep 03 2020 16:31:21.117 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 15
+    from sucuri.jararacapart import Vazio, Oca, Tora, Nulo, Piche
+ImportError: cannot import name 'Vazio'
+'''},
