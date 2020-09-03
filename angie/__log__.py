d1706cce7b983e621e26343eda05dd9ce4b4eda9
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-03 16:37:48 -0300

    Saved angie/__log__.py @2020-09-03 19:37

--- angie/__log__.py
+++ angie/__log__.py
@@ -3138,3 +3138,27 @@ NameError: name 'taba' is not defined
     self.taba = taba
 NameError: name 'taba' is not defined
 '''},
+{'date': 'Thu Sep 03 2020 16:37:47.684 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 404
+    Kwarwp(Jogo, mapa=MAPA_INICIO)
+  module <module> line 236
+    self.cena = self.cria(mapa=self.mapa) if vitollino else None
+  module <module> line 270
+    self.taba = {(i, j): fabrica[imagem].objeto(fabrica[imagem].imagem, x=i*lado, y=j*lado+lado, cena=cena)
+  module <module> line 373
+    vaga = Vazio("", x=x, y=y, cena=cena, ocupante=coisa)
+  module angie.kwarwpart line 66
+    self.taba = taba
+NameError: name 'taba' is not defined
+'''},
