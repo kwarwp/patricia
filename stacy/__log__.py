75c2e092ca4cfec496e491fc0e88464479000f76
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-03 17:58:53 -0300

    Saved stacy/__log__.py @2020-09-03 20:58

--- stacy/__log__.py
+++ stacy/__log__.py
@@ -389,3 +389,27 @@ AttributeError: 'NoneType' object has no attribute 'a'
     self.vazio = Vazio.VITOLLINO.a(imagem, w=lado, h=lado, x=x, y=y, cena=cena) # o x e o y são substituiddos pelo mapa
 AttributeError: 'NoneType' object has no attribute 'a'
 '''},
+{'date': 'Thu Sep 03 2020 17:58:50.405 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 535
+    Kwarwp(Jogo, medidas = STYLE) 
+  module <module> line 415
+    self.cena = self.cria(mapa=self.mapa) if vitollino else None
+  module <module> line 454
+    self.taba = {(i, j): fabrica[caracter].objeto(fabrica[caracter].url, x=i*lado, y=j*lado+lado, cena=cena)
+  module <module> line 468
+    vaga = Vazio("", x=x, y=y, cena=cena, ocupante=coisa)
+  module <module> line 309
+    self.vazio = Vazio.VITOLLINO.a(imagem, w=lado, h=lado, x=x, y=y, cena=cena) # o x e o y são substituiddos pelo mapa
+AttributeError: 'NoneType' object has no attribute 'a'
+'''},
