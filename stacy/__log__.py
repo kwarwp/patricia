22e809d56becdc244b79d377955a0c80b638bde6
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-03 17:10:20 -0300

    Saved stacy/__log__.py @2020-09-03 20:10

--- stacy/__log__.py
+++ stacy/__log__.py
@@ -299,3 +299,10 @@ IndentationError: expected an indented block
   ^
 IndentationError: expected an indented block
 '''},
+{'date': 'Thu Sep 03 2020 17:10:17.783 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 305
+  def limpa(self):
+  ^
+IndentationError: expected an indented block
+'''},
