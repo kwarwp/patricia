3c09eae1cf2ac9ee1c692cf46ab0e6ae2b0f83e9
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-03 16:06:41 -0300

    Saved sara/__log__.py @2020-09-03 19:06

--- sara/__log__.py
+++ sara/__log__.py
@@ -5518,3 +5518,25 @@ AttributeError: 'Nulo' object has no attribute 'ocupa'
     vaga = Vazio("", x=x, y=y, cena=cena, ocupante=coisa)
 TypeError: __init__ missing 1 positional argument: 'taba'
 '''},
+{'date': 'Thu Sep 03 2020 16:06:29.157 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 446
+    Kwarwp(Jogo, medidas=STYLE)
+  module <module> line 283
+    self.cena = self.cria(mapa=self.mapa) if vitollino else None
+  module <module> line 320
+    self.taba = {(i, j): fabrica[imagem].objeto(fabrica[imagem].imagem, x=i*lado, y=j*lado+lado, cena=cena)
+  module <module> line 333
+    vaga = Vazio(imagem, x=x, y=y, cena=cena, ocupante=self)
+TypeError: __init__ missing 1 positional argument: 'taba'
+'''},
