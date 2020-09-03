57e72c372911e77d0f82332bd686bbea71d7de87
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-03 15:08:34 -0300

    Saved sara/kwarwp.py @2020-09-03 18:08

--- sara/kwarwp.py
+++ sara/kwarwp.py
@@ -248,6 +248,9 @@ class Indio():
 
         # de resto o código é semelhante ao _anda
         # TODO refatorar o método _anda e empurra, pois tem código duplicado
+        
+        print("Estou tentanto empurra a tora para a posição", self.posicao[0]+self.azimute.x, self.posicao[1]+self.azimute.y)
+        
         destino = (self.posicao[0]+self.azimute.x, self.posicao[1]+self.azimute.y)
         """A posição para onde o índio vai depende do vetor de azimute corrente"""
         taba = self.taba.taba
