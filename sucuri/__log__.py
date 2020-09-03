11cb6d35ab070644b3e9b9acb7d572331daefc0d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-03 17:18:12 -0300

    Saved sucuri/__log__.py @2020-09-03 20:18

--- sucuri/__log__.py
+++ sucuri/__log__.py
@@ -1594,3 +1594,27 @@ NameError: name 'taba' is not defined
     self.taba = taba
 NameError: name 'taba' is not defined
 '''},
+{'date': 'Thu Sep 03 2020 17:18:11.92 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 394
+    Kwarwp(Jogo, medidas = STYLE) 
+  module <module> line 251
+    self.cena = self.cria(mapa=self.mapa) if vitollino else None #tentar entender
+  module <module> line 288
+    self.taba = {(i, j): fabrica[caracter].objeto(fabrica[caracter].url, x=i*lado, y=j*lado+lado, cena=cena)
+  module <module> line 360
+    vaga = Vazio(imagem, x=x, y=y, cena=cena, ocupante=self)
+  module sucuri.jararacapart line 40
+    self.taba = taba
+NameError: name 'taba' is not defined
+'''},
