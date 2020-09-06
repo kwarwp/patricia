fcd57189b018ae0e08d4e9bbd6f833ee1fcb5c9a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-06 11:18:18 -0300

    Saved danae/__log__.py @2020-09-06 14:18

--- danae/__log__.py
+++ danae/__log__.py
@@ -596,3 +596,19 @@ AttributeError: 'module' object has no attribute 'VITOLLINO'
   ^
 IndentationError: expected an indented block
 '''},
+{'date': 'Sun Sep 06 2020 11:18:18.37 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 65
+    print(help(int))
+IOError: File http://supygirls.pythonanywhere.com/js/builtins_docstrings.js not found
+'''},
