71758e4a166163d68b891b7a9ad1d2f7f22abd24
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-10 15:50:45 -0300

    Created samantha/kwarwp4.py @2020-09-10 18:50

---
+++ samantha/kwarwp4.py
@@ -0,0 +1,12 @@
+# patricia.samantha.kwarwp4.py
+# SPDX-License-Identifier: GPL-3.0-or-later
+""" Testando - EMPURRA A TORA
+
+.. codeauthor:: Raquel M. M. Fernandes <raquelmachado4993@gmail.com>
+
+Changelog
+---------
+.. versionadded::    10.09
+        
+
+"""
