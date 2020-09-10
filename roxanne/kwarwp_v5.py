ef3cafed0006b087c20a6e25a31623036b092137
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-10 17:58:27 -0300

    Saved roxanne/kwarwp_v5.py @2020-09-10 20:58

--- roxanne/kwarwp_v5.py
+++ roxanne/kwarwp_v5.py
@@ -35,7 +35,7 @@
         classe Kwarwp.
 """
 from collections import namedtuple as nt
-from kwarwpart import Vazio, Piche, Oca, Tora, NULO
+from roxanne.kwarwpart import Vazio, Piche, Oca, Tora, NULO
 
 IMGUR = "https://imgur.com/"
 """Prefixo do site imgur."""
