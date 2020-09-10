376737459d4038d1d708fd5ad9dca30612e7ec19
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-10 15:51:02 -0300

    Saved samantha/kwarwp4.py @2020-09-10 18:51

--- samantha/kwarwp4.py
+++ samantha/kwarwp4.py
@@ -10,3 +10,18 @@ Changelog
         
 
 """
+class Nulo:
+    """Objeto nulo que responde passivamente a todas as requisições."""
+    def __init__(self):
+        self.pegar = self.ocupa = self.empurrar = self.nulo
+        
+    def nulo(self, *_, **__):
+        """Método nulo, responde passivamente a todas as chamadas.
+        
+        :param _: aceita todos os argumentos posicionais.
+        :param __: aceita todos os argumentos nomeados.
+        :return: retorna o próprio objeto nulo.
+        """
+        return self 
+
+NULO = Nulo()
