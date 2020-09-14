1472524a811f1d4a82e28b8cd7423abc3ce1422d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-14 11:04:33 -0300

    Saved danae/__log__.py @2020-09-14 11:04

--- danae/__log__.py
+++ danae/__log__.py
@@ -644,3 +644,21 @@ IOError: File http://supygirls.pythonanywhere.com/js/builtins_docstrings.js not 
     ald=j.a(self.ALDEIA, x=x*150, y=y*150, w=tile, h=tile, style=dict(transform="rotate(90deg)"))
 NameError: name 'tile' is not defined
 '''},
+{'date': 'Mon Sep 14 2020 11:04:32.524 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 178
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 44
+    Aldeia(Jogo())
+  module <module> line 34
+    b = [spr(a[x*4+y], x, y) for x in range(4) for y in range(3)]
+IndexError: list index out of range
+'''},
