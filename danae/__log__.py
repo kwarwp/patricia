e5b11f1ac818671bb532e6823a4a2458f8723b72
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-14 11:01:38 -0300

    Saved danae/__log__.py @2020-09-14 11:01

--- danae/__log__.py
+++ danae/__log__.py
@@ -624,3 +624,23 @@ IOError: File http://supygirls.pythonanywhere.com/js/builtins_docstrings.js not 
 'error': ''''''},
 {'date': 'Mon Sep 14 2020 10:50:58.173 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
 'error': ''''''},
+{'date': 'Mon Sep 14 2020 11:01:37.224 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 178
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 43
+    Aldeia(Jogo())
+  module <module> line 32
+    a = [elt(x,y) for x in range(4) for y in range(3)]
+  module <module> line 18
+    ald=j.a(self.ALDEIA, x=x*150, y=y*150, w=tile, h=tile, style=dict(transform="rotate(90deg)"))
+NameError: name 'tile' is not defined
+'''},
