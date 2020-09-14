c9c803cbe06ee818db0c6df8775a82e363acaf39
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-14 10:24:27 -0300

    Created danae/circus.py @2020-09-14 13:24

---
+++ danae/circus.py
@@ -0,0 +1,34 @@
+# patricia.danae.circus.py
+# SPDX-License-Identifier: GPL-3.0-or-later
+""" Projeto sem descrição, (mude esta linha).
+
+.. codeauthor:: Nome Sobrenome <mail@local.tipo>
+
+Changelog
+---------
+.. versionadded::    20.09
+        Descreva o que você adicionou no código.
+
+"""
+class Aldeia:
+    ALDEIA = "https://i.imgur.com/9HQfcNn.png"
+    ALDEIA = "https://i.imgur.com/UCWGCKR.png"
+    def __init__(self, j):
+        def elt(x, y):
+            tile = 100
+            ald=j.a(self.ALDEIA, x=x*150, y=y*150, w=tile, h=tile, style=dict(transform="rotate(90deg)"))
+            ald.siz = (tile*4, tile*3)
+            ald.pos = (-x*tile, -y*tile)
+            ald.entra(cena)
+            return
+            e.elt.html = f"siz {e.siz} pos {e.pos}"
+            e.o = 0.5
+        cena = j.c("https://i.imgur.com/sGoKfvs.jpg")
+        [elt(x,y) for x in range(4) for y in range(3)]
+        cena.vai()
+        
+if __name__ == "__main__":
+    from _spy.vitollino.main import Jogo, STYLE
+    STYLE.update(width=900, height="600px")
+    Aldeia(Jogo())
+        
