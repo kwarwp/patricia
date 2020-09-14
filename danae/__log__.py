b8af430a9f8f6642d2334d4c5457ed7c72b40073
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-14 16:11:06 -0300

    Saved danae/__log__.py @2020-09-14 16:11

--- danae/__log__.py
+++ danae/__log__.py
@@ -769,3 +769,21 @@ SyntaxError: invalid syntax
                              ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Mon Sep 14 2020 16:11:05.142 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 178
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 136
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 311
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 283
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 300
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 59
+    Aldeia(Jogo())
+  module <module> line 47
+    a = [Piso(x, y, (-x*tile, -y*tile), (tile*4, tile*3), cena=cena)
+NameError: name 'tile' is not defined
+'''},
