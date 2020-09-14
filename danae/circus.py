0444fb125f05929853df8f1d5229c41b1933ba6e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-14 15:29:15 -0300

    Saved danae/circus.py @2020-09-14 15:29

--- danae/circus.py
+++ danae/circus.py
@@ -13,30 +13,14 @@ Changelog
 class Piso:
     ALDEIA = "https://i.imgur.com/Gqoucvd.png"
     #ALDEIA = "https://i.imgur.com/UCWGCKR.png"
-    def __init__(self, img, x, y):
-        def elt(x, y):
-            tile = 100
-            ald=j.a(self.ALDEIA, x=x*150, y=y*150, w=tile, h=tile)  # , style=dict(transform="rotate(90deg)"))
-            ald.siz = (tile*4, tile*3)
-            ald.pos = (-x*tile, -y*tile)
-            ald.entra(cena)
-            return ald
-        def spr(ald, x, y):
-            tile = 100
-            ald.siz = (tile*4, tile*3)
-            ald.pos = (-x*tile, -y*tile)
-            #ald.entra(cena)
-            return ald
-            e.elt.html = f"siz {e.siz} pos {e.pos}"
-            e.o = 0.5
-        cena = j.c("https://i.imgur.com/sGoKfvs.jpg")
-        a = [elt(x,y) for x in range(4) for y in range(3)]
-        b = [spr(a[x*3+y], x, y) for x in range(4) for y in range(3)]
+    def __init__(self, x, y, pos, siz, cena):
+        self.elt = j.a(self.ALDEIA, x=x*150, y=y*150, w=tile, h=tile, cena=cena)
+        self.elt.pos = pos
+        self.elt.siz = siz
+    def possize(pos, siz=None)
+        self.elt.pos = pos
+        self.elt.siz = siz if siz else self.elt.siz
         
-        # b = [spr(a[x*4+y],x,y) for x in range(4) for y in range(3)]
-        #a[0].siz = (400, 300)
-        #a[0].entra(cena)
-        cena.vai()
 class Aldeia:
     ALDEIA = "https://i.imgur.com/Gqoucvd.png"
     #ALDEIA = "https://i.imgur.com/UCWGCKR.png"
@@ -57,8 +41,9 @@ class Aldeia:
             e.elt.html = f"siz {e.siz} pos {e.pos}"
             e.o = 0.5
         cena = j.c("https://i.imgur.com/sGoKfvs.jpg")
-        a = [elt(x,y) for x in range(4) for y in range(3)]
-        b = [spr(a[x*3+y], x, y) for x in range(4) for y in range(3)]
+        a = [Piso(x, y, (-x*tile, -y*tile), (tile*4, tile*3), cena=cena)
+             for x in range(4) for y in range(3)]
+        # b = [spr(a[x*3+y], x, y) for x in range(4) for y in range(3)]
         
         # b = [spr(a[x*4+y],x,y) for x in range(4) for y in range(3)]
         #a[0].siz = (400, 300)
