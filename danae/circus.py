5ee60a7a909438c8818c0140f8dc8a7354c36f25
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-15 15:44:43 -0300

    Saved danae/circus.py @2020-09-15 18:44

--- danae/circus.py
+++ danae/circus.py
@@ -10,6 +10,14 @@ Changelog
         Descreva o que você adicionou no código.
 
 """
+class Letra:
+    TRANSP= "https://i.imgur.com/npb9Oej.png"
+    def __init__(self, cena, x, y, lt="A"):
+        OFF = 0x24B6 - ord("A")
+        la = j.a(self.TRANSP, x=x+15, y=y ,w=90, h=90, style={'font-size': '52pt'}, cena=cena)
+        la.elt.html = chr(OFF+ord(lt))
+        
+        
 class Piso:
     #ALDEIA = "https://i.imgur.com/Gqoucvd.png"
     #ALDEIA = "https://i.imgur.com/UCWGCKR.png"
@@ -28,12 +36,30 @@ class Piso:
         
 class Aldeia:
     ALDEIA = "https://i.imgur.com/Gqoucvd.png"
+    ESPRIT = "https://i.imgur.com/XFVLtJE.png"
+    MAPING = "https://i.imgur.com/MRmfpAv.png"
+    YARA = "https://i.imgur.com/RfLJEhs.png"
+    TRANSP= "https://i.imgur.com/npb9Oej.png"
     J = None
     #ALDEIA = "https://i.imgur.com/UCWGCKR.png"
     def __init__(self, j):
         Aldeia.J = j
         tile = 100
-        cena = j.c("https://i.imgur.com/sGoKfvs.jpg")
+        self.cena = cena = j.c("https://i.imgur.com/sGoKfvs.jpg")
+        self.guia()
+        cena.vai()
+    def guia(self):
+        cena = self.cena
+        big = "LS JN HN JN HN KO HO AN FN FN BN IL JO DO AO BL DO JL IO AO DS DN CL HL GS JS HS HS JS GL".split()
+        small = "AN DN DS CN IN HN HN AS IO KN KL KO GS DS DN KN".split()
+        a = [Piso(cena, nk%4*150, nk//4*150, ai+"N" ) for nk, ai in enumerate("ABCDEFGHIJKL")]
+        c = [Piso(cena, 600+nk%3*100, nk//3*100, ai ) for nk, ai in enumerate("LS JN LO JO FN JL GS JS GL".split())]
+        d = [Letra(cena, nk%4*150, nk//4*150, lt ) for nk, lt in enumerate("ABCDEFGHIJKL")]
+        e = [Piso(cena, 200+nk%4*150, 450+nk//4*150, "A"+ai ) for nk, ai in enumerate("NLSO")]
+        #Letra(cena, 0, 0, "A")
+        e = [Letra(cena, 200+nk%4*150, 450+nk//4*150, lt ) for nk, lt in enumerate("NLSO")]
+    def todos(self):
+        cena = self.cena
         big = "LS JN HN JN HN KO HO AN FN FN BN IL JO DO AO BL DO JL IO AO DS DN CL HL GS JS HS HS JS GL".split()
         small = "AN DN DS CN IN HN HN AS IO KN KL KO GS DS DN KN".split()
         # a = [Piso(cena, nk%4*150, nk//4*150, ai+"N" ) for nk, ai in enumerate("ABCDEFGHIJKL")]
@@ -43,11 +69,13 @@ class Aldeia:
         D = [Piso(cena, 600+nk%3*100, 300+nk//3*100, ai ) for nk, ai in enumerate("AN JN BN DO EO DO KL HS KN".split())]
         # b = [spr(a[x*3+y], x, y) for x in range(4) for y in range(3)]
         D = [Piso(cena, 900+nk%4*100, nk//4*100, ai ) for nk, ai in enumerate(small)]
+        j.a(self.ESPRIT, x=220, y=220,w=60, h=60, cena=cena)
+        j.a(self.MAPING, x=420, y=220,w=60, h=60, cena=cena)
+        j.a(self.YARA, x=520, y=20,w=60, h=60, cena=cena)
         
         # b = [spr(a[x*4+y],x,y) for x in range(4) for y in range(3)]
         #a[0].siz = (400, 300)
         #a[0].entra(cena)
-        cena.vai()
         
 if __name__ == "__main__":
     from _spy.vitollino.main import Jogo, STYLE
