6aac9da81b3106f9466f8bccb9f2ac27dc6a3a20
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-15 14:52:38 -0300

    Saved danae/__log__.py @2020-09-15 17:52

--- danae/__log__.py
+++ danae/__log__.py
@@ -887,3 +887,21 @@ TypeError: __init__ missing 1 positional argument: 'siz'
     j.a(ESPRIT, x=210, y=210,w=80, h=80, cena=cena)
 NameError: name 'ESPRIT' is not defined
 '''},
+{'date': 'Tue Sep 15 2020 14:52:37.564 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 57
+    Aldeia(Jogo())
+  module <module> line 47
+    j.a(self.ESPRIT, x=210, y=210,w=80, h=80, cena=cena)
+AttributeError: 'Aldeia' object has no attribute 'ESPRIT'
+'''},
