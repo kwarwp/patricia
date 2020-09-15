e0a30214dd1268c1af1949660b95867f13f7360b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-15 18:21:41 -0300

    Saved sara/__log__.py @2020-09-15 21:21

--- sara/__log__.py
+++ sara/__log__.py
@@ -5820,3 +5820,23 @@ Exception: <TypeError: Cannot read property '$methods' of undefined>
     class Test_Kwarwp(TestCase):
 NameError: name 'TestCase' is not defined
 '''},
+{'date': 'Tue Sep 15 2020 18:21:25.914 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 169
+    main()
+  module <module> line 163
+    import kwarwp.htmlrunner as htmlrun
+  module kwarwp.htmlrunner line 113
+    TestResult = unittest.TestResult
+AttributeError: 'module' object has no attribute 'TestResult'
+'''},
