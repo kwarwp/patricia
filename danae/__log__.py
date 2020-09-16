c415e8ccfc68d79700de38179629d5e2bf502714
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-15 23:27:51 -0300

    Saved danae/__log__.py @2020-09-16 02:27

--- danae/__log__.py
+++ danae/__log__.py
@@ -949,3 +949,25 @@ TypeError: unsupported operand type(s) for +: 'int' and 'str'
     az = azz[ai[1]]
 IndexError: string index out of range
 '''},
+{'date': 'Tue Sep 15 2020 23:27:50.945 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 135
+    desafio2()
+  module <module> line 128
+    circus(3, MASMORRA)
+  module <module> line 98
+    Aldeia(Jogo()).circus(desafio, solucao)
+  module <module> line 92
+    self.desafios[desafio](solucao)
+IndexError: list index out of range
+'''},
