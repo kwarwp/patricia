5e93c71f438c2317e5f14cf12e91f3a6775ba25b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-16 14:06:59 -0300

    Saved danae/__log__.py @2020-09-16 17:06

--- danae/__log__.py
+++ danae/__log__.py
@@ -1318,3 +1318,19 @@ NameError: name 'ORDERED_KEYS' is not defined
     akeys = [f"X=X{l}" for l in Aldeia.Keys]
 AttributeError: 'Aldeia' object has no attribute 'Keys'
 '''},
+{'date': 'Wed Sep 16 2020 14:06:57.951 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 14
+    KEYS = [key for line in ORDERED_KEYS for key in line]
+NameError: name 'ORDERED_KEYS' is not defined
+'''},
