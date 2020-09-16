4fe795901ce0eb13d1f0b8a59e6f3743e6f5c413
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-15 23:39:19 -0300

    Saved danae/__log__.py @2020-09-16 02:39

--- danae/__log__.py
+++ danae/__log__.py
@@ -1021,3 +1021,29 @@ IndexError: string index out of range
     c = [Piso(self.cena, i*100, j*100, solucao(ai) ) 
 TypeError: 'dict' object is not callable
 '''},
+{'date': 'Tue Sep 15 2020 23:39:18.294 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 135
+    desafio2()
+  module <module> line 128
+    circus(3, MASMORRA)
+  module <module> line 98
+    Aldeia(Jogo()).circus(desafio, solucao)
+  module <module> line 92
+    self.desafios[desafio](solucao)
+  module <module> line 84
+    c = [Piso(self.cena, i*100, j*100, ai ) for i, linha in enumerate(solucao) for j, ai in enumerate(linha)]
+  module <module> line 29
+    az = azz[ai[1]]
+IndexError: string index out of range
+'''},
