be13ad3f94ec22cb5863d33a21385b66effad9d1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-16 15:02:51 -0300

    Saved danae/__log__.py @2020-09-16 18:02

--- danae/__log__.py
+++ danae/__log__.py
@@ -1334,3 +1334,21 @@ AttributeError: 'Aldeia' object has no attribute 'Keys'
     KEYS = [key for line in ORDERED_KEYS for key in line]
 NameError: name 'ORDERED_KEYS' is not defined
 '''},
+{'date': 'Wed Sep 16 2020 15:02:51.18 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 167
+    Aldeia.STOR[KEYS], Aldeia.STOR[COUNT] = [], 0
+  module browser.local_storage line 37
+    raise TypeError("value must be string")
+TypeError: value must be string
+'''},
