b98a37b1bd92573c2f76529ca0a95af91dedf6cc
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-17 18:18:49 -0300

    Saved sara/kwarwtest.py @2020-09-17 21:18

--- sara/kwarwtest.py
+++ sara/kwarwtest.py
@@ -164,11 +164,11 @@ class Test_Kwarwp(TestCase):
         self.set_fake()
         cena = self.k.cria()
         coisa = self.k.taba[1,3]
-        self.assertIsInstance(coisa.ocupante,  Tora, f"but ocupante was {coisa.ocupante}")
+        self.assertIsInstance(coisa.ocupante,  Piche, f"but ocupante was {coisa.ocupante}")
         self.assertEqual(100, coisa.lado, f"but coisa.lado was {coisa.lado}")
-        tora = self.elts[self.TORA]
-        self.assertEqual(coisa.ocupante.vazio, tora, f"but coisa.ocupante.indio was {coisa.ocupante.vazio}")
-        self.assertEqual((0, 0), tora.pos, f"but tora.pos was {tora.pos}")
+        piche = self.elts[self.Piche]
+        self.assertEqual(coisa.ocupante.vazio, piche, f"but coisa.ocupante.indio was {coisa.ocupante.vazio}")
+        self.assertEqual((0, 0), piche.pos, f"but tora.pos was {tora.pos}")
 
 
     def testa_cria_tora(self):
