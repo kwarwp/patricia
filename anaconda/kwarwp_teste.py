d7582c5a2a037f838987916678a265acb4058d59
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-17 18:02:14 -0300

    Saved anaconda/kwarwp_teste.py @2020-09-17 21:02

--- anaconda/kwarwp_teste.py
+++ anaconda/kwarwp_teste.py
@@ -40,26 +40,27 @@ class Test_Kwarwp(TestCase):
         """Cria objetos doublê que irão espionar o que estaria sendo feito com os originais."""
         elts = self.elts = {}
         """Coleção de imagens que indicam os Elementos Vitollino que são criados"""
-    class FakeCena:
-        """Usado para substituir a Cena original do Vitollino"""
-        def __init__(self, *_, **__):
-            pass
-        def vai(self, *_, **__):
-            pass
+        
+        class FakeCena:
+             """Usado para substituir a Cena original do Vitollino"""
+            def __init__(self, *_, **__):
+                pass
+            def vai(self, *_, **__):
+                pass
 
-    class FakeElemento:
-         """Usado para substituir o Elemento original do Vitollino
+        class FakeElemento:
+            """Usado para substituir o Elemento original do Vitollino
 
-         Captura a imagem recebida e coloca na coleção de imagens **self.elts**.
-         Também coleta os diversos parâmetros recebidos para que possam ser averiguados.
-         """
-        def __init__(self, img=0, x=0, y=0, w=0, h=0, vai=None, elts=elts, **kwargs):
-            elts[img] = self
-            """Insere este FakeElemento no dicionário, no verbete indicado pela imagem"""
-            self.img, self.x, self.y, self.w, self.h, self.vai = img, x, y, w, h, vai
-            self.destino, self._pos, self._siz = [None]*3
-        def ocupa(self, destino):
-            self.destino = destino.elt
+            Captura a imagem recebida e coloca na coleção de imagens **self.elts**.
+            Também coleta os diversos parâmetros recebidos para que possam ser averiguados.
+            """
+            def __init__(self, img=0, x=0, y=0, w=0, h=0, vai=None, elts=elts, **kwargs):
+                elts[img] = self
+                """Insere este FakeElemento no dicionário, no verbete indicado pela imagem"""
+                self.img, self.x, self.y, self.w, self.h, self.vai = img, x, y, w, h, vai
+                self.destino, self._pos, self._siz = [None]*3
+            def ocupa(self, destino):
+                self.destino = destino.elt
             @property
             def elt(self):
                 return self
@@ -75,10 +76,10 @@ class Test_Kwarwp(TestCase):
             @pos.setter
             def pos(self, value):
                 self._pos = value
-    Vazio.VITOLLINO.a = FakeElemento
-    """Troca o Elemento original pelo fake, na "maternidade""""
-    Vazio.VITOLLINO.c = FakeCena
-    """Troca a Cena original pelo fake, na "maternidade""""
+        Vazio.VITOLLINO.a = FakeElemento
+        """Troca o Elemento original pelo fake, na "maternidade""""
+        Vazio.VITOLLINO.c = FakeCena
+        """Troca a Cena original pelo fake, na "maternidade""""
     
     def testa_cria(self):
         """ Cria o ambiente de programação Kwarwp."""
