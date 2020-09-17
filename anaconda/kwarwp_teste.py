d9c490a686f6ad006e06a4608ec983775270d0ba
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-17 16:59:08 -0300

    Created anaconda/kwarwp_teste.py @2020-09-17 19:59

---
+++ anaconda/kwarwp_teste.py
@@ -0,0 +1,16 @@
+# patricia.anaconda.kwarwp_teste.py
+# SPDX-License-Identifier: GPL-3.0-or-later
+""" Projeto sem descrição, (mude esta linha).
+
+.. codeauthor:: Emanuelle Simas <ellesimas@gmail.com>
+
+Changelog
+---------
+.. versionadded::    20.09
+        Descreva o que você adicionou no código.
+
+"""
+from _spy.vitollino.main import Jogo
+from unittest import TestCase
+
+from anaconda.kwarwp import Kwarwp, Indio, Vazio
