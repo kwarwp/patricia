7eae2096ab97f097071b3870b65cd0a4a1215e7a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-17 17:55:38 -0300

    Saved anaconda/__log__.py @2020-09-17 20:55

--- anaconda/__log__.py
+++ anaconda/__log__.py
@@ -1047,3 +1047,5 @@ SyntaxError: invalid syntax
 'error': ''''''},
 {'date': 'Thu Sep 17 2020 17:49:49.495 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
 'error': ''''''},
+{'date': 'Thu Sep 17 2020 17:55:35.826 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': ''''''},
