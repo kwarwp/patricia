9e3f7bda6fd46d2885c570ecb321ceb5fefe2331
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-19 19:50:34 -0300

    Saved danae/__log__.py @2020-09-19 22:50

--- danae/__log__.py
+++ danae/__log__.py
@@ -1434,3 +1434,21 @@ SyntaxError: invalid syntax
                             ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Sat Sep 19 2020 19:50:33.784 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 185
+    Aldeia.shuffle_keys()
+  module <module> line 61
+    Aldeia.RT_AZIM = Aldeia.RT_AZIM[1:] + Aldeia.RT_AZIM[0]
+TypeError: can only concatenate list (not "str") to list
+'''},
