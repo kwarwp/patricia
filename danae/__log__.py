4f6b4421c586f4f6622ff66ad916ecb824d63666
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-19 20:10:50 -0300

    Saved danae/__log__.py @2020-09-19 23:10

--- danae/__log__.py
+++ danae/__log__.py
@@ -1452,3 +1452,21 @@ SyntaxError: invalid syntax
     Aldeia.RT_AZIM = Aldeia.RT_AZIM[1:] + Aldeia.RT_AZIM[0]
 TypeError: can only concatenate list (not "str") to list
 '''},
+{'date': 'Sat Sep 19 2020 20:10:49.593 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 191
+    Aldeia.STOR[RTAZ] = Aldeia.RT_AZIM
+  module browser.local_storage line 37
+    raise TypeError("value must be string")
+TypeError: value must be string
+'''},
