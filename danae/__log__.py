22f523f6f5e15aa30fa6f8f40ea71448edf3c492
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-19 20:45:41 -0300

    Saved danae/__log__.py @2020-09-19 23:45

--- danae/__log__.py
+++ danae/__log__.py
@@ -1495,3 +1495,23 @@ SyntaxError: EOL while scanning string literal
     self.log(f"COUNT: {Aldeia.STOR[COUNT]} XXkeysXX {Aldeia.KEYS} rt {Aldeia.STOR[RTAZ]}")
 AttributeError: 'GUI' object has no attribute 'log'
 '''},
+{'date': 'Sat Sep 19 2020 20:45:40.744 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 195
+    Aldeia.shuffle_keys()
+  module <module> line 60
+    print(f"COUNT: {Aldeia.STOR[COUNT]} XXkeysXX {Aldeia.KEYS} rt {Aldeia.STOR[RTAZ]}")
+  module <module> line 1
+    (Aldeia.KEYS)
+AttributeError: 'Aldeia' object has no attribute 'KEYS'
+'''},
