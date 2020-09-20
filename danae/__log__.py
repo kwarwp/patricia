9551dfbd147527ef1ba4c901f0c3ddb055668713
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-20 13:57:38 -0300

    Saved danae/__log__.py @2020-09-20 16:57

--- danae/__log__.py
+++ danae/__log__.py
@@ -1606,3 +1606,21 @@ NameError: name 'j' is not defined
     circus(1, [[TOPO_ESQUERDA, TOPO_CENTRO, TOPO_DIREITA], [MEIO_ESQUERDA, CENTRO,
 NameError: name 'circus' is not defined
 '''},
+{'date': 'Sun Sep 20 2020 13:57:37.318 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 212
+    desafio1()
+  module <module> line 199
+    circus(2, MASMORRA, 2)
+TypeError: circus() takes 2 positional arguments but more were given
+'''},
