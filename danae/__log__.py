4c40a60519a8440343820377416281675bbefa76
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-20 12:53:01 -0300

    Saved danae/__log__.py @2020-09-20 15:53

--- danae/__log__.py
+++ danae/__log__.py
@@ -1522,3 +1522,21 @@ AttributeError: 'Aldeia' object has no attribute 'KEYS'
         ^
 SyntaxError: Unbalanced bracket [
 '''},
+{'date': 'Sun Sep 20 2020 12:53:00.850 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 19
+    circus(1, [[TOPO_ESQUERDA, TOPO_CENTRO, TOPO_DIREITA], [MEIO_ESQUERDA, CENTRO,
+  module danae.circus line 158
+    Aldeia(Jogo()).circus(desafio, solucao)
+NameError: name 'Jogo' is not defined
+'''},
