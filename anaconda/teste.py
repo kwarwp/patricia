9cc31bca023d4e26ac8476e3aea408bd7bf440f4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-21 15:21:43 -0300

    Created anaconda/teste.py @2020-09-21 18:21

---
+++ anaconda/teste.py
@@ -0,0 +1,28 @@
+# patricia.anaconda.teste.py
+# SPDX-License-Identifier: GPL-3.0-or-later
+""" Projeto sem descrição, (mude esta linha).
+
+.. codeauthor:: Nome Sobrenome <mail@local.tipo>
+
+Changelog
+---------
+.. versionadded::    20.09
+        Descreva o que você adicionou no código.
+
+"""
+from patricia.danae.circus import Letra, Piso, Aldeia, circus
+#from danae.circus import  circus
+
+def desafio0():
+    TOPO_ESQUERDA = "AN"
+    TOPO_DIREITA = "AN"
+    TOPO_CENTRO = "AN"
+    MEIO_ESQUERDA, CENTRO, MEIO_DIREITA = "AN", "AN", "AN"
+    FUNDO_ESQUERDA, FUNDO_CENTRO, FUNDO_DIREITA =  "AN", "AN", "AN"
+
+    # O comando abaixo voce vai entender no próximo desafio
+    circus(1, [[TOPO_ESQUERDA, TOPO_CENTRO, TOPO_DIREITA], [MEIO_ESQUERDA, CENTRO,
+            MEIO_DIREITA], [FUNDO_ESQUERDA, FUNDO_CENTRO, FUNDO_DIREITA]])
+
+if __name__ == "__main__":
+    desafio0()
