f7a78deb13ae370fdfb86e680234accb1f15afb2
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-21 15:35:26 -0300

    Saved anaconda/__log__.py @2020-09-21 18:35

--- anaconda/__log__.py
+++ anaconda/__log__.py
@@ -1216,3 +1216,19 @@ IndexError: string index out of range
     az = azz[ai[1]]
 IndexError: string index out of range
 '''},
+{'date': 'Mon Sep 21 2020 18:35:25.129 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 27
+    desafio2()
+NameError: name 'desafio2' is not defined
+'''},
