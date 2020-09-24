19ffc65ab95d25433a4e541bf716335b13d4c9e3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-24 15:53:59 -0300

    Saved sara/__log__.py @2020-09-24 18:53

--- sara/__log__.py
+++ sara/__log__.py
@@ -6557,3 +6557,21 @@ NameError: name 'mapa' is not defined
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Thu Sep 24 2020 15:53:56.659 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 634
+    main(Jogo, STYLE)
+  module <module> line 628
+    return Kwarwp(vitollino=vitollino_proxy, medidas=medidas, mapa=MAPA_INICIO, indios=indios)
+NameError: name 'indios' is not defined
+'''},
