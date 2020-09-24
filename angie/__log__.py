bda98b182a0a4bb142592b321d59e2642d7859d0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-24 16:17:39 -0300

    Saved angie/__log__.py @2020-09-24 19:17

--- angie/__log__.py
+++ angie/__log__.py
@@ -3285,3 +3285,21 @@ IndentationError: unexpected indent
     self.os_indios[0].ativa()
 AttributeError: 'GUI' object has no attribute 'os_indios'
 '''},
+{'date': 'Thu Sep 24 2020 16:17:39.167 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 225
+    class Kwarwp():
+  module <module> line 420
+    self.os_indios[0].executa()
+AttributeError: 'GUI' object has no attribute 'os_indios'
+'''},
