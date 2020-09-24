4fa73359a2282979049da8adb1ab762ee155ffa3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-24 16:02:54 -0300

    Saved libby/__log__.py @2020-09-24 19:02

--- libby/__log__.py
+++ libby/__log__.py
@@ -1770,3 +1770,27 @@ NameError: name 'mapa' is not defined
     coisa = Piche(imagem, x=0, y=0, cena=cena, taba=self)
 TypeError: 'module' object is not callable
 '''},
+{'date': 'Thu Sep 24 2020 16:02:13.817 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 557
+    main(Jogo, STYLE)
+  module <module> line 551
+    return Kwarwp(vitollino=vitollino_proxy, medidas=medidas, mapa=mapa, indios=indios)
+  module <module> line 381
+    self.cena = self.cria(mapa=self.mapa) if vitollino else None
+  module <module> line 417
+    self.taba = {(i, j): fabrica[imagem].objeto(fabrica[imagem].imagem, x=i*lado, y=j*lado+lado, cena=cena)
+  module <module> line 494
+    coisa = Piche(imagem, x=0, y=0, cena=cena, taba=self)
+TypeError: 'module' object is not callable
+'''},
