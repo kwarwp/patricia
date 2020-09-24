aad4f6ae0fd5e9ba8a8686012e238540d0ab6ad8
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-24 16:18:47 -0300

    Saved angie/__log__.py @2020-09-24 19:18

--- angie/__log__.py
+++ angie/__log__.py
@@ -3303,3 +3303,21 @@ AttributeError: 'GUI' object has no attribute 'os_indios'
     self.os_indios[0].executa()
 AttributeError: 'GUI' object has no attribute 'os_indios'
 '''},
+{'date': 'Thu Sep 24 2020 16:18:47.507 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 477
+    Kwarwp(Jogo, mapa=MAPA_INICIO)
+  module <module> line 260
+    self.o_indio = NULO
+NameError: name 'NULO' is not defined
+'''},
