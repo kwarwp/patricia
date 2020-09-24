21de0fa7c67a19441fb8f124796beaf55bf4da85
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-24 16:27:30 -0300

    Saved angie/__log__.py @2020-09-24 19:27

--- angie/__log__.py
+++ angie/__log__.py
@@ -3403,3 +3403,27 @@ NameError: name 'deque' is not defined
     self.os_indios[0].executa()
 AttributeError: 'Piche' object has no attribute 'os_indios'
 '''},
+{'date': 'Thu Sep 24 2020 16:27:30.895 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 477
+    Kwarwp(Jogo, mapa=MAPA_INICIO)
+  module <module> line 270
+    self.cena = self.cria(mapa=self.mapa) if vitollino else None
+  module <module> line 316
+    self.taba = {(i, j): fabrica[imagem].objeto(fabrica[imagem].imagem, x=i*lado, y=j*lado+lado, cena=cena)
+  module <module> line 445
+    coisa = Piche(imagem, x=0, y=0, cena=cena, taba=self)
+  module angie.kwarwpart line 338
+    from angie.kwarwptora import Kwarwp
+ImportError: cannot import name 'Kwarwp'
+'''},
