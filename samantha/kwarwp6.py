e8cb20461ee0e412f204fa8d8978f340f9cab662
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-24 15:37:42 -0300

    Created samantha/kwarwp6.py @2020-09-24 18:37

---
+++ samantha/kwarwp6.py
@@ -0,0 +1,12 @@
+# patricia.samantha.kwarwp6.py
+# SPDX-License-Identifier: GPL-3.0-or-later
+""" Testando padrões Proxy e Command.
+
+.. codeauthor:: Raquel M. M. Fernandes <raquelmachado4993@gmail.com>
+
+Changelog
+---------
+.. versionadded::    24.09
+        Experimentando
+
+"""
