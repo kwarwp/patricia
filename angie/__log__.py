f32584b26dd6157410b49ea221426706f0970f88
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-24 16:21:51 -0300

    Saved angie/__log__.py @2020-09-24 19:21

--- angie/__log__.py
+++ angie/__log__.py
@@ -3357,3 +3357,21 @@ NameError: name 'NULO' is not defined
     self.indios = deque(indios or [Indio])
 NameError: name 'deque' is not defined
 '''},
+{'date': 'Thu Sep 24 2020 16:21:52.258 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 477
+    Kwarwp(Jogo, mapa=MAPA_INICIO)
+  module <module> line 269
+    self.indios = deque(indios or [Indio])
+NameError: name 'deque' is not defined
+'''},
