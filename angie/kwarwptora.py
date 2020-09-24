58777172ee93eee5d317cc09da8c0f39c6085b9c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-24 16:00:54 -0300

    Saved angie/kwarwptora.py @2020-09-24 19:00

--- angie/kwarwptora.py
+++ angie/kwarwptora.py
@@ -218,23 +218,43 @@ class Kwarwp():
     LADO = None
     """Referência estática para definir o lado do piso da casa."""
 
-    def __init__(self, vitollino=None, mapa=MAPA_INICIO, medidas={}):
-        Kwarwp.VITOLLINO = self.v = vitollino()
-        """Cria um matriz com os elementos descritos em cada linha de texto"""
-        self.mapa = mapa.split()
-        """Largura da casa da arena dos desafios, número de colunas no mapa"""
-        self.lado, self.col, self.lin = 100, len(self.mapa[0]), len(self.mapa)+1
-        Kwarwp.LADO = self.lado
+#    def __init__(self, vitollino=None, mapa=MAPA_INICIO, medidas={}):
+#        Kwarwp.VITOLLINO = self.v = vitollino()
+#        """Cria um matriz com os elementos descritos em cada linha de texto"""
+#        self.mapa = mapa.split()
+#        """Largura da casa da arena dos desafios, número de colunas no mapa"""
+#        self.lado, self.col, self.lin = 100, len(self.mapa[0]), len(self.mapa)+1
+#        Kwarwp.LADO = self.lado
         
-        self.o_indio = None
-        """Instância do personagem principal, o índio, vai ser atribuído pela fábrica do índio"""
+#        self.o_indio = None
+#        """Instância do personagem principal, o índio, vai ser atribuído pela fábrica do índio"""
 
-        w, h = self.col *self.lado, self.lin *self.lado
+#        w, h = self.col *self.lado, self.lin *self.lado
+#        self.taba = {}
+#        """Dicionário que a partir de coordenada (i,J) localiza um piso da taba"""
+#        medidas.update(width=w, height=f"{h}px")
+#        self.cena = self.cria(mapa=self.mapa) if vitollino else None
+
+    def __init__(self, vitollino=None, mapa=None, medidas={}, indios=()):
+        Vazio.VITOLLINO = self.v = vitollino()
+        self.vitollino = vitollino
+        """Referência estática para obter o engenho de jogo."""
+        self.mapa = (mapa or MAPA_INICIO).split()
+        """Cria um matriz com os elementos descritos em cada linha de texto"""
         self.taba = {}
-        """Dicionário que a partir de coordenada (i,J) localiza um piso da taba"""
+        """Cria um dicionário com os elementos traduzidos a partir da interpretação do mapa"""
+        self.o_indio = NULO
+        self.os_indios = []
+        """Instância do personagem principal, o índio, vai ser atribuído pela fábrica do índio"""
+        self.lado, self.col, self.lin = 100, len(self.mapa[0]), len(self.mapa)+1
+        """Largura da casa da arena dos desafios, número de colunas e linhas no mapa"""
+        Vazio.LADO = self.lado
+        """Referência estática para definir o lado do piso da casa."""
+        w, h = self.col *self.lado, self.lin *self.lado
         medidas.update(width=w, height=f"{h}px")
+        self.indios = deque(indios or [Indio])
         self.cena = self.cria(mapa=self.mapa) if vitollino else None
-        
+
     def cria(self, mapa=""):
         """ Fábrica de componentes.
 
@@ -246,6 +266,8 @@ class Kwarwp():
         fabrica = {
         "&": Fab(self.maloc, f"{IMGUR}dZQ8liT.jpg"), # OCA
         "^": Fab(self.indio, f"{IMGUR}UCWGCKR.png"), # INDIO
+        "$": Fab(self.indio, f"{IMGUR}nvrwu0r.png"), # INDIA
+        "p": Fab(self.indio, f"{IMGUR}HeiupbP.png"), # PAJE
         ".": Fab(self.vazio, f"{IMGUR}npb9Oej.png"), # VAZIO
         "_": Fab(self.coisa, f"{IMGUR}sGoKfvs.jpg"), # SOLO
         "#": Fab(self.atora, f"{IMGUR}0jSB27g.png"), # TORA
@@ -260,16 +282,25 @@ class Kwarwp():
         mapa = self.mapa
         lado = self.lado
         cena = self.v.c(fabrica["_"].imagem)
-        self.ceu = self.v.a(fabrica["~"].imagem, w=lado*self.col, h=lado-10, x=0, y=0, cena=cena, vai=self.executa,
+#        self.ceu = self.v.a(fabrica["~"].imagem, w=lado*self.col, h=lado-10, x=0, y=0, cena=cena, vai=self.executa,
                        style={"padding-top": "10px", "text-align": "center"})
         """No argumento *vai*, associamos o clique no céu com o método **executa ()** desta classe.
            O *ceu* agora é um argumento de instância e por isso é referenciado como **self.ceu**.
         """
-        sol = self.v.a(fabrica["*"].imagem, w=60, h=60, x=0, y=40, cena=cena, vai=self.esquerda)
+#        sol = self.v.a(fabrica["*"].imagem, w=60, h=60, x=0, y=40, cena=cena, vai=self.esquerda)
+        """No argumento *vai*, associamos o clique no sol com o método **esquerda ()** desta classe."""
+#        self.taba = {(i, j): fabrica[imagem].objeto(fabrica[imagem].imagem, x=i*lado, y=j*lado+lado, cena=cena)
+#            for j, linha in enumerate(mapa) for i, imagem in enumerate(linha)}
+        """Posiciona os elementos segundo suas posições i, j na matriz mapa"""
+        self.ceu = self.v.a(fabrica["~"].imagem, w=lado*self.col, h=lado-10, x=0, y=0, cena=cena, vai=self.passo,
+                       style={"padding-top": "10px", "text-align": "center"})
+        """No argumento *vai*, associamos o clique no céu com o método **executa ()** desta classe.
+           O *ceu* agora é um argumento de instância e por isso é referenciado como **self.ceu**.
+        """
+        sol = self.v.a(fabrica["*"].imagem, w=60, h=60, x=0, y=40, cena=cena, vai=self.executa)
         """No argumento *vai*, associamos o clique no sol com o método **esquerda ()** desta classe."""
         self.taba = {(i, j): fabrica[imagem].objeto(fabrica[imagem].imagem, x=i*lado, y=j*lado+lado, cena=cena)
             for j, linha in enumerate(mapa) for i, imagem in enumerate(linha)}
-        """Posiciona os elementos segundo suas posições i, j na matriz mapa"""
         cena.vai()
         return cena
         
@@ -321,14 +352,24 @@ class Kwarwp():
         :param y: linha em que o elemento será posicionado.
         :param cena: cena em que o elemento será posicionado.
         """
-        self.o_indio = Indio(imagem, x=1, y=0, cena=cena, taba=self)
+#        self.o_indio = Indio(imagem, x=1, y=0, cena=cena, taba=self)
+        """ O índio tem deslocamento zero, pois é relativo à vaga.
+            O **x=1** serve para distinguir o indio de outros derivados.
+        """
+#        self.o_indio.indio.vai = lambda *_: self.o_indio.pega()
+        #self.o_indio.indio.vai = lambda *_: self.o_indio.empurra()
+        """o índio.vai é associado ao seu próprio metodo pega"""
+#        vaga = Vazio("", x=x, y=y, cena=cena, ocupante=self.o_indio, taba=self)
+ 
+        self.o_indio = self.indios[0](imagem, x=1, y=0, cena=cena, taba=self, vitollino=self.v)
         """ O índio tem deslocamento zero, pois é relativo à vaga.
             O **x=1** serve para distinguir o indio de outros derivados.
         """
         self.o_indio.indio.vai = lambda *_: self.o_indio.pega()
-        #self.o_indio.indio.vai = lambda *_: self.o_indio.empurra()
         """o índio.vai é associado ao seu próprio metodo pega"""
-        vaga = Vazio("", x=x, y=y, cena=cena, ocupante=self.o_indio, taba=self)
+        vaga = Vazio("", x=x, y=y, cena=cena, ocupante=self.o_indio)
+        self.os_indios.append(self.o_indio)
+        self.indios.rotate()
         return vaga
             
     def fala(self, texto=""):
@@ -342,10 +383,27 @@ class Kwarwp():
         """
         self.o_indio.esquerda()
 
+    def passo(self, *_):
+        """ Ordena a execução do roteiro do índio.
+        """
+        # self.o_indio.esquerda()
+        # self.v.executa()
+        # self.o_indio.passo()
+
+        [indio.passo() for indio in self.os_indios]
+    
     def executa(self, *_):
         """ Ordena a execução do roteiro do índio.
         """
-        self.o_indio.executa()
+    #    self.o_indio.executa()
+    # self.v.ativa()
+    # JogoProxy.ATIVA = True
+    # self.o_indio.ativa()
+    # self.o_indio.executa()
+    # [indio.ativa() and indio.executa() for indio in self.os_indios]
+    self.os_indios[0].ativa()
+    self.v.ativa()
+    self.os_indios[0].executa()
     
     def maloc(self, imagem, x, y, cena):
         """ Cria uma maloca na arena do Kwarwp na posição definida.
