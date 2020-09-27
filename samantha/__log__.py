898eb5b7722a356a96f0516b717f3276201c4e6c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-27 16:34:13 -0300

    Saved samantha/__log__.py @2020-09-27 19:34

--- samantha/__log__.py
+++ samantha/__log__.py
@@ -887,3 +887,31 @@ TypeError: object() takes no parameters
     self.vaga.sai()
 AttributeError: 'Piche' object has no attribute 'vaga'
 '''},
+{'date': 'Sun Sep 27 2020 16:34:12.706 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 557
+    Kwarwp(Jogo, medidas = STYLE) 
+  module <module> line 421
+    self.cena = self.cria(mapa=self.mapa) if vitollino else None
+  module <module> line 461
+    self.taba = {(i, j): fabrica[caracter].objeto(fabrica[caracter].url, x=i*lado, y=j*lado+lado, cena=cena)
+  module <module> line 515
+    vaga = Vazio("", x=x, y=y, cena=cena, ocupante=coisa)
+  module <module> line 226
+    self.acessa(ocupante)
+  module <module> line 252
+    ocupante.ocupa(self)
+  module <module> line 294
+    self.vaga.sai()
+AttributeError: 'Piche' object has no attribute 'vaga'
+'''},
