e81c76e0607cb469d637df8b07da411c47c74039
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-27 13:20:06 -0300

    Saved samantha/__log__.py @2020-09-27 16:20

--- samantha/__log__.py
+++ samantha/__log__.py
@@ -574,3 +574,23 @@ IndentationError: expected an indented block
 'error': '''
 Exception: <TypeError: bases[i].$dict is undefined>
 '''},
+{'date': 'Sun Sep 27 2020 13:20:05.74 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 476
+    Kwarwp(Jogo, medidas = STYLE) 
+  module <module> line 348
+    self.cena = self.cria(mapa=self.mapa) if vitollino else None
+  module <module> line 387
+    self.taba = {(i, j): fabrica[caracter].objeto(fabrica[caracter].url, x=i*lado, y=j*lado+lado, cena=cena)
+KeyError: +
+'''},
