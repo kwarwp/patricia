da12a23df5aeaf1c2bdfdce847711208aeb3bc9b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-27 16:22:39 -0300

    Saved samantha/__log__.py @2020-09-27 19:22

--- samantha/__log__.py
+++ samantha/__log__.py
@@ -775,3 +775,27 @@ AttributeError: 'Kwarwp' object has no attribute 'atora'
   ^
 IndentationError: expected an indented block
 '''},
+{'date': 'Sun Sep 27 2020 16:22:38.791 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 571
+    Kwarwp(Jogo, medidas = STYLE) 
+  module <module> line 435
+    self.cena = self.cria(mapa=self.mapa) if vitollino else None
+  module <module> line 475
+    self.taba = {(i, j): fabrica[caracter].objeto(fabrica[caracter].url, x=i*lado, y=j*lado+lado, cena=cena)
+  module <module> line 528
+    coisa = Piche(imagem, x=0, y=0, cena=cena, taba=self)
+  module <module> line 300
+    self.acessa = self._acessa
+AttributeError: 'Piche' object has no attribute '_acessa'
+'''},
