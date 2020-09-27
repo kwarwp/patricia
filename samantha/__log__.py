82a9298da93b2abfed402111b06361d757f5867e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-27 14:38:56 -0300

    Saved samantha/__log__.py @2020-09-27 17:38

--- samantha/__log__.py
+++ samantha/__log__.py
@@ -614,3 +614,23 @@ KeyError: +
     self.taba = {(i, j): fabrica[caracter].objeto(fabrica[caracter].url, x=i*lado, y=j*lado+lado, cena=cena)
 KeyError: +
 '''},
+{'date': 'Sun Sep 27 2020 14:38:55.52 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 494
+    Kwarwp(Jogo, medidas = STYLE) 
+  module <module> line 363
+    self.cena = self.cria(mapa=self.mapa) if vitollino else None
+  module <module> line 376
+    fabrica ={"#": Fab(self.coisa, f"{IMGUR}uwYPNlz.png"), # CERCA
+AttributeError: 'Kwarwp' object has no attribute 'caverna'
+'''},
