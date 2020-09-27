b6a5044b8b5bd32c83635747412471b4b0de4969
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-27 14:40:11 -0300

    Saved samantha/__log__.py @2020-09-27 17:40

--- samantha/__log__.py
+++ samantha/__log__.py
@@ -634,3 +634,25 @@ KeyError: +
     fabrica ={"#": Fab(self.coisa, f"{IMGUR}uwYPNlz.png"), # CERCA
 AttributeError: 'Kwarwp' object has no attribute 'caverna'
 '''},
+{'date': 'Sun Sep 27 2020 14:40:10.910 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 487
+    Kwarwp(Jogo, medidas = STYLE) 
+  module <module> line 363
+    self.cena = self.cria(mapa=self.mapa) if vitollino else None
+  module <module> line 405
+    self.taba = {(i, j): fabrica[caracter].objeto(fabrica[caracter].url, x=i*lado, y=j*lado+lado, cena=cena)
+  module <module> line 431
+    coisa = Caverna(imagem, x=0, y=0, cena=cena, taba=self)
+NameError: name 'Caverna' is not defined
+'''},
