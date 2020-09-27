81443f18dbfb9c2db044fdcb457ce7679c7908ee
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-27 16:38:15 -0300

    Saved samantha/__log__.py @2020-09-27 19:38

--- samantha/__log__.py
+++ samantha/__log__.py
@@ -937,3 +937,23 @@ AttributeError: 'Piche' object has no attribute 'vaga'
     coisa.vazio.vai = lambda *_: self.o_indio.larga()
 AttributeError: 'Tora' object has no attribute 'vazio'
 '''},
+{'date': 'Sun Sep 27 2020 16:38:14.443 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 558
+    Kwarwp(Jogo, medidas = STYLE) 
+  module <module> line 425
+    self.cena = self.cria(mapa=self.mapa) if vitollino else None
+  module <module> line 438
+    fabrica = {
+AttributeError: 'Kwarwp' object has no attribute 'vazio'
+'''},
