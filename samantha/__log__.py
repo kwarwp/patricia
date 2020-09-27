7127c7733cb544ec85224765edc6a1efd5b83920
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-27 12:53:03 -0300

    Saved samantha/__log__.py @2020-09-27 15:53

--- samantha/__log__.py
+++ samantha/__log__.py
@@ -444,3 +444,21 @@ SyntaxError: invalid syntax
     Ponto = nt("Ponto", "x y")
 NameError: name 'nt' is not defined
 '''},
+{'date': 'Sun Sep 27 2020 12:53:02.527 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 368
+    class Kwarwp():
+  module <module> line 382
+    def __init__(self, vitollino=None, mapa = MAPA_INICIAL, medidas = {}):
+NameError: name 'MAPA_INICIAL' is not defined
+'''},
