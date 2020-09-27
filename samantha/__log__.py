442c74d434a32ab6d147cf5263819afac440fffb
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-27 16:00:10 -0300

    Saved samantha/__log__.py @2020-09-27 19:00

--- samantha/__log__.py
+++ samantha/__log__.py
@@ -748,3 +748,23 @@ SyntaxError: invalid syntax
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Sun Sep 27 2020 16:00:10.153 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 542
+    Kwarwp(Jogo, medidas = STYLE) 
+  module <module> line 414
+    self.cena = self.cria(mapa=self.mapa) if vitollino else None
+  module <module> line 427
+    fabrica = {
+AttributeError: 'Kwarwp' object has no attribute 'atora'
+'''},
