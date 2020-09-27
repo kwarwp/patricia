43ac484d9bdfd2f17ec68b53a57f19b5e3d23aaf
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-27 15:49:58 -0300

    Saved samantha/__log__.py @2020-09-27 18:49

--- samantha/__log__.py
+++ samantha/__log__.py
@@ -692,3 +692,10 @@ SyntaxError: invalid syntax
    ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Sun Sep 27 2020 15:49:57.609 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 310
+  vaga=requisitante
+  ^
+IndentationError: expected an indented block
+'''},
