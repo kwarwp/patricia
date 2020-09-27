bae91b72ee48466d74ce9ffcecf44390b62b693d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-27 13:20:33 -0300

    Saved samantha/__log__.py @2020-09-27 16:20

--- samantha/__log__.py
+++ samantha/__log__.py
@@ -594,3 +594,23 @@ Exception: <TypeError: bases[i].$dict is undefined>
     self.taba = {(i, j): fabrica[caracter].objeto(fabrica[caracter].url, x=i*lado, y=j*lado+lado, cena=cena)
 KeyError: +
 '''},
+{'date': 'Sun Sep 27 2020 13:20:32.683 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 484
+    Kwarwp(Jogo, medidas = STYLE) 
+  module <module> line 356
+    self.cena = self.cria(mapa=self.mapa) if vitollino else None
+  module <module> line 395
+    self.taba = {(i, j): fabrica[caracter].objeto(fabrica[caracter].url, x=i*lado, y=j*lado+lado, cena=cena)
+KeyError: +
+'''},
