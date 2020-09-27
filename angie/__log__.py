b7ac26ad7d4b0c8c2ba01668d49f7937c259b379
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-27 16:58:32 -0300

    Saved angie/__log__.py @2020-09-27 19:58

--- angie/__log__.py
+++ angie/__log__.py
@@ -3738,3 +3738,46 @@ AttributeError: 'Kwarwp' object has no attribute 'VITOLLINO'
   ^
 IndentationError: expected an indented block
 '''},
+{'date': 'Sun Sep 27 2020 16:58:15.737 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''<angie.kwarwpart.Piche object>
+<__main__.Kwarwp object>
+<__main__.Kwarwp object>
+<__main__.Kwarwp object>
+<__main__.Kwarwp object>
+<angie.kwarwpart.Oca object>
+<__main__.Kwarwp object>
+<__main__.Kwarwp object>
+<__main__.Kwarwp object>
+<__main__.Kwarwp object>
+<__main__.Kwarwp object>
+<__main__.Kwarwp object>
+<__main__.Kwarwp object>
+<__main__.Kwarwp object>
+<__main__.Kwarwp object>
+<__main__.Kwarwp object>
+<__main__.Kwarwp object>
+<angie.kwarwpart.Tora object>
+<__main__.Kwarwp object>
+<angie.kwarwpart.Tora object>
+<__main__.Kwarwp object>
+Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 487
+    Kwarwp(Jogo, mapa=MAPA_INICIO)
+  module <module> line 271
+    self.cena = self.cria(mapa=self.mapa) if vitollino else None
+  module <module> line 317
+    self.taba = {(i, j): fabrica[imagem].objeto(fabrica[imagem].imagem, x=i*lado, y=j*lado+lado, cena=cena)
+  module <module> line 379
+    self.o_indio = self.indios[0](imagem, x=1, y=0, cena=cena, taba=self, vitollino=self.v)
+AttributeError: 'Kwarwp' object has no attribute 'indios'
+'''},
