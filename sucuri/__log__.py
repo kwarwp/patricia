887929f823c1091bcf1ebbe3de4d86bbcad22278
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-27 16:56:54 -0300

    Saved sucuri/__log__.py @2020-09-27 19:56

--- sucuri/__log__.py
+++ sucuri/__log__.py
@@ -1918,3 +1918,21 @@ AttributeError: 'NoneType' object has no attribute 'indio'
     def __init__(self, vitollino=None, mapa=MAPA_INICIO, medidas={}):
 NameError: name 'MAPA_INICIO' is not defined
 '''},
+{'date': 'Sun Sep 27 2020 16:56:53.414 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 402
+    Kwarwp(Jogo, medidas=STYLE)
+  module <module> line 238
+    self.o_indio = NULO
+NameError: name 'NULO' is not defined
+'''},
