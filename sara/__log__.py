5110238e80ada8bde4b93e6ecd05e523d43fca9f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-30 18:06:59 -0300

    Saved sara/__log__.py @2020-09-30 21:06

--- sara/__log__.py
+++ sara/__log__.py
@@ -7866,3 +7866,29 @@ TypeError: 'JogoProxy' object is not callable
 'error': '''
 Exception: <TypeError: bases[i].$dict is undefined>
 '''},
+{'date': 'Wed Sep 30 2020 18:04:40.160 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''False False <function _command>
+False False <function _command>
+Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 642
+    main(Jogo, STYLE)
+  module <module> line 636
+    Kwarwp(jogo_proxy, medidas=medidas, mapa=MAPA_INICIO)
+  module <module> line 447
+    self.cena = self.cria(mapa=self.mapa) if vitollino else None
+  module <module> line 484
+    self.taba = {(i, j): fabrica[imagem].objeto(fabrica[imagem].imagem, x=i*lado, y=j*lado+lado, cena=cena)
+  module <module> line 533
+    vaga = Vazio("", x=x, y=y, cena=cena, ocupante=coisa)
+TypeError: __init__ missing 1 positional argument: 'taba'
+'''},
