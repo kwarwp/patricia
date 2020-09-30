f20c73f5dbf308b3d50e53973fdcd741b8a28952
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-30 18:03:58 -0300

    Saved sucuri/__log__.py @2020-09-30 21:03

--- sucuri/__log__.py
+++ sucuri/__log__.py
@@ -1994,3 +1994,21 @@ NameError: name 'NULO' is not defined
     (IMGUR)
 NameError: name 'IMGUR' is not defined
 '''},
+{'date': 'Wed Sep 30 2020 18:01:39.461 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 402
+    Kwarwp(Jogo, medidas=STYLE)
+  module <module> line 238
+    self.o_indio = NULO
+NameError: name 'NULO' is not defined
+'''},
