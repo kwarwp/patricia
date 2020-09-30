7d06db2977c3d3cc6ca64fbaff414aee6d76359a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-30 18:28:52 -0300

    Saved sucuri/__log__.py @2020-09-30 21:28

--- sucuri/__log__.py
+++ sucuri/__log__.py
@@ -2070,3 +2070,27 @@ NameError: name 'NULO' is not defined
     (IMGUR)
 NameError: name 'IMGUR' is not defined
 '''},
+{'date': 'Wed Sep 30 2020 18:26:34.235 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 404
+    Kwarwp(Jogo, medidas=STYLE)
+  module <module> line 247
+    self.cena = self.cria(mapa=self.mapa) if vitollino else None
+  module <module> line 285
+    self.taba = {(i, j): fabrica[imagem].objeto(fabrica[imagem].imagem, x=i*lado, y=j*lado+lado, cena=cena)
+  module <module> line 311
+    coisa = Indio(imagem, x=0, y=0, cena=cena, taba=self)
+  module <module> line 54
+    self.ocupante = NULO
+NameError: name 'NULO' is not defined
+'''},
