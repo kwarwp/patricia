eaf1c54fdba821e23bc7db1a92e29f13983fa343
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-30 18:32:26 -0300

    Saved sucuri/__log__.py @2020-09-30 21:32

--- sucuri/__log__.py
+++ sucuri/__log__.py
@@ -2134,3 +2134,23 @@ TypeError: __init__ missing 1 positional argument: 'taba'
     self.o_indio = NULO
 NameError: name 'NULO' is not defined
 '''},
+{'date': 'Wed Sep 30 2020 18:30:07.830 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 16
+    from sucuri.coralpart import Vazio, Oca, Tora, Nulo, Piche
+  module sucuri.coralpart line 14
+    class Nulo:
+  module sucuri.coralpart line 28
+    NULO = Nulo()
+NameError: name 'Nulo' is not defined
+'''},
