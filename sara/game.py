0334389e055db741798f869473950eb86e0807da
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-10-01 17:15:12 -0300

    Created sara/game.py @2020-10-01 20:15

---
+++ sara/game.py
@@ -0,0 +1,49 @@
+# patricia.sara.game.py
+# SPDX-License-Identifier: GPL-3.0-or-later
+""" Projeto sem descrição, (mude esta linha).
+
+.. codeauthor:: Paulo Assumpção <paulo.assump@gmail.com>
+
+Changelog
+---------
+.. versionadded::    20.10
+        Modulo game.
+
+"""
+
+
+from _spy.vitollino.main import Jogo, STYLE
+from sara.kwarwp import main as kwarwp_main, Indio
+
+MAPA_INICIO = """
+@....&
+......
+.....#
+.#.p`.
+"""
+
+class Game(Indio):
+    def executa(self):
+        """ Roteiro do índio. Conjunto de comandos para ele executar.
+        """
+        self.anda()
+        self.direita()
+        self.pega()
+        self.esquerda()
+        self.anda()
+        self.anda()
+        self.anda()
+        self.direita()
+        self.larga()
+
+
+class Vitollino(Jogo):
+    """ Empacota o engenho de jogo Vitollino """
+    pass
+
+def main(vitollino, medidas):
+    return kwarwp_main(vitollino=vitollino, medidas=medidas, mapa=MAPA_INICIO, indios=(Indio, Kaiowa))
+        
+    
+if __name__ == "__main__":
+    main(Jogo, STYLE)
