8a8a6bb223905e9ce8a012427364981e772054e7
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-10-04 11:32:14 -0300

    Saved samantha/__log__.py @2020-10-04 14:32

--- samantha/__log__.py
+++ samantha/__log__.py
@@ -1219,3 +1219,34 @@ NameError: name 'kwarwp_main' is not defined
     return desafio2(vitollino=vitollino, medidas=medidas, mapa=MAPA_INICIO)
 TypeError: 'module' object is not callable
 '''},
+{'date': 'Sun Oct 04 2020 11:32:13.813 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 19
+    from samantha.kwarwp8 import main as desafio2
+ImportError: cannot import name 'main'
+
+ImportError
+Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 268
+    action()
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 19
+    from samantha.kwarwp8 import main as desafio2
+'''},
