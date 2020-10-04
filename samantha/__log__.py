a3167f3e49814d67228c72ad9c768fd911f91440
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-10-04 12:03:04 -0300

    Saved samantha/__log__.py @2020-10-04 15:03

--- samantha/__log__.py
+++ samantha/__log__.py
@@ -1362,3 +1362,25 @@ IndentationError: unexpected indent
     coisa = Indio(imagem, x=0, y=0, cena=cena, taba=self)
 TypeError: object() takes no parameters
 '''},
+{'date': 'Sun Oct 04 2020 12:03:03.216 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 785
+    Kwarwp(Jogo, medidas = STYLE) 
+  module <module> line 650
+    self.cena = self.cria(mapa=self.mapa) if vitollino else None
+  module <module> line 690
+    self.taba = {(i, j): fabrica[caracter].objeto(fabrica[caracter].url, x=i*lado, y=j*lado+lado, cena=cena)
+  module <module> line 703
+    vaga = Vazio("", x=x, y=y, cena=cena, ocupante=coisa)
+NameError: name 'coisa' is not defined
+'''},
