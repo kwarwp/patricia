f04a495f55c63806516dd9e3a37b3fa20daf86a4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-10-04 11:12:31 -0300

    Saved samantha/__log__.py @2020-10-04 14:12

--- samantha/__log__.py
+++ samantha/__log__.py
@@ -1141,3 +1141,23 @@ NameError: name 'tatu' is not defined
     coisa = Tatu(imagem, x=0,y=0,cena=cena,taba=self)
 NameError: name 'Tatu' is not defined
 '''},
+{'date': 'Sun Oct 04 2020 11:12:30.522 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 486
+    Kwarwp(Jogo, medidas = STYLE) 
+  module <module> line 355
+    self.cena = self.cria(mapa=self.mapa) if vitollino else None
+  module <module> line 368
+    fabrica ={"#": Fab(self.coisa, f"{IMGUR}uwYPNlz.png"), # CERCA
+AttributeError: 'Kwarwp' object has no attribute 'tatu'
+'''},
