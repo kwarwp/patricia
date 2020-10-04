3232fad8963e5919caccfb7a65d1f118c3c2309f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-10-04 14:48:18 -0300

    Saved sara/__log__.py @2020-10-04 17:48

--- sara/__log__.py
+++ sara/__log__.py
@@ -8165,3 +8165,21 @@ Traceback (most recent call last):
     vaga.ocupou(self, pos) # if vaga else self.indio.ocupa(None,pos=pos)
 TypeError: ocupou() takes 2 positional arguments but more were given
 '''},
+{'date': 'Sun Oct 04 2020 14:45:58.453 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 63
+    main(Jogo, STYLE)
+  module <module> line 59
+    return kwarwp_main(vitollino=vitollino, medidas=medidas, mapa=MAPA_INICIO, indios=(Indio, Kaiowa))
+NameError: name 'Kaiowa' is not defined
+'''},
