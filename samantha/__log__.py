f4daaba56e07db126334e3237759b0bb8e466e79
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-10-04 11:11:00 -0300

    Saved samantha/__log__.py @2020-10-04 14:11

--- samantha/__log__.py
+++ samantha/__log__.py
@@ -1097,3 +1097,25 @@ SyntaxError: invalid syntax
     coisa = Tatu(imagem, x=0,y=0,cena=cena,taba=self)
 NameError: name 'Tatu' is not defined
 '''},
+{'date': 'Sun Oct 04 2020 11:10:59.895 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 497
+    Kwarwp(Jogo, medidas = STYLE) 
+  module <module> line 357
+    self.cena = self.cria(mapa=self.mapa) if vitollino else None
+  module <module> line 398
+    self.taba = {(i, j): fabrica[caracter].objeto(fabrica[caracter].url, x=i*lado, y=j*lado+lado, cena=cena)
+  module <module> line 422
+    coisa = tatu(imagem, x=0,y=0,cena=cena,taba=self)
+NameError: name 'tatu' is not defined
+'''},
