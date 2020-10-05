b5cbeeff9360c712faf04fe35bfff7f9734d4cb6
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-10-05 16:34:12 -0300

    Saved sara/game.py @2020-10-05 19:34

--- sara/game.py
+++ sara/game.py
@@ -15,7 +15,7 @@ Changelog
 from _spy.vitollino.main import Jogo, STYLE
 from sara.kwarwp import main as kwarwp_main, Indio
 
-MAPA_INICIO = """
+MAPA_FASE_1 = """
 @....&
 ......
 ......
@@ -57,6 +57,26 @@ class Fase1(Indio):
         self.anda()
         self.anda()
 
+class Fase2(Indio):
+    def executa(self):
+        """ Roteiro do índio. Conjunto de comandos para ele executar.
+        """
+        self.esquerda()
+        self.anda()
+        self.pega()
+        self.direita()
+        self.anda()
+        self.anda()
+        self.anda()
+        self.esquerda()
+        self.anda()
+        self.larga()
+        self.esquerda()
+        self.esquerda()
+        self.anda()
+        self.anda()
+        self.anda()
+        self.anda()
 
 class Kaiowa(Indio):
     def executa(self):
@@ -75,9 +95,15 @@ class Kaiowa(Indio):
 class Vitollino(Jogo):
     """ Empacota o engenho de jogo Vitollino """
     pass
+    
+def game():
+    return kwarwp_main(vitollino=vitollino, medidas=medidas, mapa=MAPA_INICIO, indios=(Fase1,))
+    return kwarwp_main(vitollino=vitollino, medidas=medidas, mapa=MAPA_FASE_2, indios=(Fase2,))
 
 def main(vitollino, medidas):
-    return kwarwp_main(vitollino=vitollino, medidas=medidas, mapa=MAPA_INICIO, indios=(Fase1,))
+    a = kwarwp_main(vitollino=vitollino, medidas=medidas, mapa=MAPA_FASE_2, indios=(Fase2,))
+    print(a)
+    
         
     
 if __name__ == "__main__":
