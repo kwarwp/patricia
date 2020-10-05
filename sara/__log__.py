75199ed2ad0c4f17f205e5ff1ba0971d2f4f292a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-10-05 17:58:51 -0300

    Saved sara/__log__.py @2020-10-05 20:58

--- sara/__log__.py
+++ sara/__log__.py
@@ -8637,3 +8637,23 @@ TypeError: __init__() missing 5 positional arguments: name,image,position,cena,r
     return Game().vai()
 TypeError: __init__() missing 5 positional arguments: name,image,position,cena,rule
 '''},
+{'date': 'Mon Oct 05 2020 17:58:42.911 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 148
+    main(Jogo, STYLE)
+  module <module> line 144
+    return Game().vai()
+  module <module> line 126
+    self.rule = rule
+NameError: name 'rule' is not defined
+'''},
