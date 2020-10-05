dd32991adf41b40d546378e5567bebfb3b1ee282
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-10-05 19:40:05 -0300

    Saved sara/__log__.py @2020-10-05 22:40

--- sara/__log__.py
+++ sara/__log__.py
@@ -8933,3 +8933,23 @@ AttributeError: 'Jogo' object has no attribute 'nome'
     cena = Cena(IMG_FUNDO.imagem)
 AttributeError: 'str' object has no attribute 'imagem'
 '''},
+{'date': 'Mon Oct 05 2020 19:39:56.684 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 180
+    main(Jogo, STYLE)
+  module <module> line 176
+    Game(vitollino, medidas).vai()
+  module <module> line 152
+    cena = Cena(IMG_FUNDO.imagem)
+AttributeError: 'str' object has no attribute 'imagem'
+'''},
