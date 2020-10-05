1fc410c28541190f6b29bfefcd8d1aa2dd2c4dd8
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-10-05 10:29:38 -0300

    Saved samantha/kwarwp7.py @2020-10-05 13:29

--- samantha/kwarwp7.py
+++ samantha/kwarwp7.py
@@ -25,9 +25,9 @@ MAPA_INICIO = """
 MAPA_INICIAL= """
 |||||||||
 |@..@...|
-|..v|.@.|
+|..&|.@.|
 |..@|...|
-|...@.r.|
+|...@...|
 |@....^.|
 """
 
@@ -82,8 +82,8 @@ class Vitollino(Jogo):
     pass
 
 def main(vitollino, medidas):
-    #return kwarwp_main(vitollino=vitollino, medidas=medidas, mapa=MAPA_INICIAL, indios=(Kaiowa,))
-    return kwarwp_main(vitollino=vitollino, medidas=medidas, mapa=MAPA_INICIAL2, indios=(Kaiowa,))
+    return kwarwp_main(vitollino=vitollino, medidas=medidas, mapa=MAPA_INICIAL, indios=(Kaiowa,))
+    #return kwarwp_main(vitollino=vitollino, medidas=medidas, mapa=MAPA_INICIAL2, indios=(Kaiowa,))
     #return kwarwp_main(vitollino=vitollino, medidas=medidas, mapa=MAPA_INICIAL3, indios=(Kaiowa,))
         
     
