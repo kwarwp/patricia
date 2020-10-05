c2ab1aa8f2eacc243ea9808ef918a77fc3880382
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-10-05 18:00:57 -0300

    Saved sara/game.py @2020-10-05 21:00

--- sara/game.py
+++ sara/game.py
@@ -121,7 +121,7 @@ class Game:
     def vai(self):
         from _spy.vitollino.main import Cena, Elemento, STYLE
         
-        self.fase_1 = Elemento(IMG_FASE_1, tit="Fase 1", x=0, y=0, width=IMG_WIDTH, height=IMG_HEIGHT, cena=self.cena)
+        self.fase_1 = Elemento(IMG_FASE_1, tit="Fase 1", x=0, y=0, width=IMG_WIDTH, height=IMG_HEIGHT, cena=Cena)
         self.cena.vai()
 
     
