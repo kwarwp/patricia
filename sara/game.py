be4a0f98543753a0bfe094f31a8c9abfbf569167
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-10-05 18:58:47 -0300

    Saved sara/game.py @2020-10-05 21:58

--- sara/game.py
+++ sara/game.py
@@ -127,13 +127,18 @@ class Game:
         
         fase_1 = Elemento(IMG, tit="Fase 1", x=50, y=0, width=IMG_WIDTH, height=IMG_HEIGHT, cena=cena)
         fase_1.elt.bind("click", self.execute)
+        
         fase_2 = Elemento(IMG, tit="Fase 2", x=200, y=0, width=IMG_WIDTH, height=IMG_HEIGHT, cena=cena)
+        fase_2.elt.bind("click", self.execute)
+        
         fase_3 = Elemento(IMG, tit="Fase 3", x=350, y=0, width=IMG_WIDTH, height=IMG_HEIGHT, cena=cena)
+        fase_3.elt.bind("click", self.execute)
+        
         cena.vai()
         
     def execute(self, env=None):
-        
-        fase1 = kwarwp_main(vitollino=self.vitollino, medidas=self.medidas, mapa=MAPA_FASE_1, indios=(Fase1,))
+        print(env)
+        kwarwp_main(vitollino=self.vitollino, medidas=self.medidas, mapa=MAPA_FASE_1, indios=(Fase1,))
 
     
     
