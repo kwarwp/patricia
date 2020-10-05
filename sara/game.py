eb32fdee6c9c37fd9aaf0dd398620c642b6bae6a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-10-05 18:47:13 -0300

    Saved sara/game.py @2020-10-05 21:47

--- sara/game.py
+++ sara/game.py
@@ -122,8 +122,9 @@ class Game:
         cena = Cena()
         
         fase_1 = Elemento(IMG, tit="Fase 1", x=50, y=0, width=IMG_WIDTH, height=IMG_HEIGHT, cena=cena)
+        fase_1.elt.bind("click", kwarwp_main(vitollino=vitollino, medidas=medidas, mapa=MAPA_FASE_1, indios=(Fase1,)))
         fase_2 = Elemento(IMG, tit="Fase 2", x=200, y=0, width=IMG_WIDTH, height=IMG_HEIGHT, cena=cena)
-        fase_3 = Elemento(IMG, tit="Fase 3", x=500, y=0, width=IMG_WIDTH, height=IMG_HEIGHT, cena=cena)
+        fase_3 = Elemento(IMG, tit="Fase 3", x=350, y=0, width=IMG_WIDTH, height=IMG_HEIGHT, cena=cena)
         cena.vai()
 
     
