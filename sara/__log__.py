d8d29eb6ea87d1d8579f48feb2d2e468c0331bcc
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-10-05 17:46:45 -0300

    Saved sara/__log__.py @2020-10-05 20:46

--- sara/__log__.py
+++ sara/__log__.py
@@ -8583,3 +8583,21 @@ IndentationError: expected an indented block
     return kwarwp_main(vitollino=vitollino, medidas=medidas, mapa=MAPA_INICIO, indios=(Fase1,))
 NameError: name 'MAPA_INICIO' is not defined
 '''},
+{'date': 'Mon Oct 05 2020 17:46:36.537 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 135
+    main(Jogo, STYLE)
+  module <module> line 130
+    return kwarwp_main(vitollino=vitollino, medidas=medidas, mapa=MAPA_INICIO, indios=(Fase1,))
+NameError: name 'MAPA_INICIO' is not defined
+'''},
