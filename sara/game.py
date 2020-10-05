4b652d8828f3e64fd1fc3a3e6afa47634e09fd3f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-10-05 20:03:23 -0300

    Saved sara/game.py @2020-10-05 23:03

--- sara/game.py
+++ sara/game.py
@@ -36,7 +36,7 @@ MAPA_FASE_3 = """
 .#.p$.
 """
 
-IMG_FASE_1 = f"https://activufrj.nce.ufrj.br/file/ProgOO/fase1.png?disp=inline"
+IMG_FASE_1 = "https://activufrj.nce.ufrj.br/file/ProgOO/fase1.png?disp=inline"
 IMG_FASE_2 = f"https://activufrj.nce.ufrj.br/file/ProgOO/fase2.png?disp=inline"
 IMG_FASE_3 = f"https://activufrj.nce.ufrj.br/file/ProgOO/fase3.png?disp=inline"
 IMG_OCA = f"https://i.imgur.com/dZQ8liT.jpg"
