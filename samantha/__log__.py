28d3ac3a64717fb066816c88b6f8229ff9a8fc66
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-10-05 09:46:56 -0300

    Saved samantha/__log__.py @2020-10-05 12:46

--- samantha/__log__.py
+++ samantha/__log__.py
@@ -1569,3 +1569,19 @@ AttributeError: 'Kwarwp' object has no attribute 'Oca'
     fabrica = {
 AttributeError: 'Kwarwp' object has no attribute 'Oca'
 '''},
+{'date': 'Mon Oct 05 2020 09:46:55.235 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 17
+    from samantha.kwarwp9 import main as kwarwp_main, Indio
+ImportError: cannot import name 'main'
+'''},
