8deeff4cee67987ea01fbec25ab4fa8e0435a17a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-10-04 21:06:22 -0300

    Saved sara/__log__.py @2020-10-05 00:06

--- sara/__log__.py
+++ sara/__log__.py
@@ -8452,3 +8452,25 @@ AttributeError: 'method' object has no attribute 'c'
     cena = self.v.c(fabrica["_"].imagem)
 AttributeError: 'method' object has no attribute 'c'
 '''},
+{'date': 'Sun Oct 04 2020 21:06:14.330 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 658
+    main(Jogo, STYLE)
+  module <module> line 652
+    return Kwarwp(vitollino=vitollino_proxy, medidas=medidas, mapa=mapa, indios=indios)
+  module <module> line 461
+    self.cena = self.cria(mapa=self.mapa) if vitollino else None
+  module <module> line 490
+    cena = self.v.c(fabrica["_"].imagem)
+AttributeError: 'method' object has no attribute 'c'
+'''},
