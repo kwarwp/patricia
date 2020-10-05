f18c089c886545b9665ba51e8f49724a1a2d977e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-10-05 18:00:22 -0300

    Saved sara/__log__.py @2020-10-05 21:00

--- sara/__log__.py
+++ sara/__log__.py
@@ -8733,3 +8733,21 @@ NameError: name 'Elemento' is not defined
     self.fase_1 = Elemento(IMG_FASE_1, tit="Fase 1", x=0, y=0, width=IMG_WIDTH, height=IMG_HEIGHT, cena=self.cena)
 NameError: name 'Elemento' is not defined
 '''},
+{'date': 'Mon Oct 05 2020 18:00:13.488 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 119
+    class Game:
+  module <module> line 122
+    cena = Cena()
+NameError: name 'Cena' is not defined
+'''},
