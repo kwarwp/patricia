d6e995e72b803b7372c4c05d44c7652d224596e4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-11-12 18:26:08 -0300

    Saved delta/__log__.py @2020-11-12 21:26

--- delta/__log__.py
+++ delta/__log__.py
@@ -4271,3 +4271,23 @@ AttributeError: 'Tabuleiro' object has no attribute 'vai'
     _casa = self.tabuleiro[_col][_lin]
 AttributeError: 'Casa' object has no attribute '__getitem__'
 '''},
+{'date': 'Thu Nov 12 2020 18:26:08.394 GMt-0300 (Brasilia Standard Time) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 100
+    Main().vai()
+  module <module> line 93
+    jogo.joga(tipo,(perg_lin,perg_col))
+  module <module> line 63
+    _casa = self.tabuleiro[_col][_lin]
+AttributeError: 'Casa' object has no attribute '__getitem__'
+'''},
