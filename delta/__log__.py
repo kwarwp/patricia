d727045d4cb9363a4884e0a5cbf1e187272914d1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-11-12 18:25:24 -0300

    Saved delta/__log__.py @2020-11-12 21:25

--- delta/__log__.py
+++ delta/__log__.py
@@ -4235,3 +4235,19 @@ SyntaxError: invalid syntax
            ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Thu Nov 12 2020 18:25:24.380 GMt-0300 (Brasilia Standard Time) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 100
+    Tabuleiro().vai()
+AttributeError: 'Tabuleiro' object has no attribute 'vai'
+'''},
