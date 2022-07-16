76a79f31915f7a974a95aa8869fb326558b46a99
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-07-16 18:39:43 -0300

    Saved meredith/__log__.py @2022-07-16 21:39

--- meredith/__log__.py
+++ meredith/__log__.py
@@ -323,3 +323,23 @@ AttributeError: 'Kwarwp' object has no attribute 'vazio'
     fabrica = {
 AttributeError: 'Kwarwp' object has no attribute 'vazio'
 '''},
+{'date': 'Sat Jul 16 2022 18:39:01.837 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 112
+    Kwarwp(Jogo)      
+  module <module> line 49
+    self.cena = self.cria(mapa=self.mapa) if vitollino else None
+  module <module> line 84
+    [self.cria_elemento(imagem, x=i*lado, y=j*lado+lado, cena=cena)
+AttributeError: 'Kwarwp' object has no attribute 'cria_elemento'
+'''},
