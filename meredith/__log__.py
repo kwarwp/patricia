75e2d20ace409a4ea132b78e54fac8b4b93b539f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-07-16 18:05:20 -0300

    Saved meredith/__log__.py @2022-07-16 21:05

--- meredith/__log__.py
+++ meredith/__log__.py
@@ -124,3 +124,21 @@ NameError: name 'kwarwp' is not defined
     kwarwp.VITOLLINO = self.v = vitollino()
 NameError: name 'kwarwp' is not defined
 '''},
+{'date': 'Sat Jul 16 2022 18:04:39.86 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 110
+    Kwarwp(Jogo)      
+  module <module> line 44
+    kwarwp.VITOLLINO = self.v = vitollino()
+NameError: name 'kwarwp' is not defined
+'''},
