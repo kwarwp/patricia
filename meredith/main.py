c466abedb4e0bf824639c71a3732941d08a4a967
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-07-16 02:24:07 -0300

    Saved meredith/main.py @2022-07-16 05:24

--- meredith/main.py
+++ meredith/main.py
@@ -17,7 +17,7 @@ class Kwarwp():
     INDIO = "https://imgur.com/8jMuupz.png"
     SOLO = "https://i.imgur.com/sGoKfvs.jpg"
     TORA = "https://imgur.com/jCWaQHm.png"
-    PICHE = "https://imgur.com/tLLVjfN.png"
+    PICHE = "https://imgur.com/emU1u7J.png"
     CEU = "https://i.imgur.com/UAETaiP.gif"
     SOL = "https://i.imgur.com/PfodQmT.gif"
 
@@ -31,7 +31,7 @@ class Kwarwp():
         indio = self.v.a(self.INDIO, w=100, h=100, x=300, y=400, cena=cena)
         oca = self.v.a(self.OCA, w=200, h=200, x=400, y=50, cena=cena)
         tora = self.v.a(self.TORA, w=150, h=150, x=40, y=470, cena=cena)
-        piche = self.v.a(self.PICHE, w=100, h=100, x=100, y=100, cena=cena)
+        piche = self.v.a(self.PICHE, w=200, h=200, x=50, y=100, cena=cena)
         piche = self.v.a(self.CEU, w=600, h=100, x=0, y=0, cena=cena)
         sol = self.v.a(self.SOL, w=60, h=60, x=0, y=40, cena=cena)
         cena.vai()
