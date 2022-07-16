cbe5209211d48a181daa01e87b14bab9ce1114ce
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-07-16 20:35:05 -0300

    Saved meredith/__log__.py @2022-07-16 23:35

--- meredith/__log__.py
+++ meredith/__log__.py
@@ -496,3 +496,23 @@ SyntaxError: invalid syntax
                                      ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Sat Jul 16 2022 20:34:23.585 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 111
+    Kwarwp(Jogo)      
+  module <module> line 59
+    self.cena = self.cria(mapa=self.mapa) if vitollino else None
+  module <module> line 84
+    ceu = self.v.a(fabrica["~"].imagem, w=lado*self.col, h=lado, x=0, y=0, cena=cena, vai= self.executa)
+AttributeError: 'Kwarwp' object has no attribute 'executa'
+'''},
