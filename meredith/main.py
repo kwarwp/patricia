686c9f2395a5e160f4b8c4738a2e8f46942ce064
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-07-16 02:09:05 -0300

    Saved meredith/main.py @2022-07-16 05:09

--- meredith/main.py
+++ meredith/main.py
@@ -16,7 +16,7 @@ class Kwarwp():
     OCA = "https://imgur.com/9sQjdJ1.png"
     INDIO = "https://imgur.com/8jMuupz.png"
     SOLO = "https://i.imgur.com/sGoKfvs.jpg"
-    TORA = "https://imgur.com/ldI7IbK.png"
+    TORA = "https://imgur.com/jCWaQHm.png"
     PICHE = "https://imgur.com/tLLVjfN.png"
     CEU = "https://i.imgur.com/UAETaiP.gif"
     SOL = "https://i.imgur.com/PfodQmT.gif"
@@ -30,7 +30,7 @@ class Kwarwp():
         cena = self.v.c(self.SOLO)
         indio = self.v.a(self.INDIO, w=100, h=100, x=300, y=400, cena=cena)
         oca = self.v.a(self.OCA, w=200, h=200, x=400, y=50, cena=cena)
-        tora = self.v.a(self.TORA, w=100, h=100, x=100, y=400, cena=cena)
+        tora = self.v.a(self.TORA, w=150, h=150, x=40, y=470, cena=cena)
         piche = self.v.a(self.PICHE, w=100, h=100, x=100, y=100, cena=cena)
         piche = self.v.a(self.CEU, w=600, h=100, x=0, y=0, cena=cena)
         sol = self.v.a(self.SOL, w=60, h=60, x=0, y=40, cena=cena)
