07f1b8fcf22b47b99d58fc9380ced61c25c27f1b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-07-16 18:25:17 -0300

    Saved meredith/__log__.py @2022-07-16 21:25

--- meredith/__log__.py
+++ meredith/__log__.py
@@ -180,3 +180,21 @@ NameError: name 'kwarwp' is not defined
     fabrica = {
 NameError: name 'Fb' is not defined
 '''},
+{'date': 'Sat Jul 16 2022 18:24:37.46 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 113
+    Kwarwp(Jogo)      
+  module <module> line 41
+    kwarwp.VITOLLINO = self.v = vitollino()
+NameError: name 'kwarwp' is not defined
+'''},
