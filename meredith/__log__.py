61dfe676320d4db828a9866afde4953b326a09b1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-07-17 20:41:04 -0300

    Saved meredith/__log__.py @2022-07-17 23:41

--- meredith/__log__.py
+++ meredith/__log__.py
@@ -1080,3 +1080,23 @@ IndentationError: expected an indented block
     self.indios = deque(indios or [Indio])
 NameError: name 'deque' is not defined
 '''},
+{'date': 'Sun Jul 17 2022 20:40:22.88 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 649
+    Kwarwp(Jogo)    
+  module <module> line 509
+    self.cena = self.cria(mapa=self.mapa) if vitollino else None
+  module <module> line 517
+    fabrica = {
+AttributeError: 'Kwarwp' object has no attribute 'atora'
+'''},
