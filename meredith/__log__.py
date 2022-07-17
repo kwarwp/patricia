18fa16eb6377eef442e8434b00fea4230e526906
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-07-17 12:41:43 -0300

    Saved meredith/__log__.py @2022-07-17 15:41

--- meredith/__log__.py
+++ meredith/__log__.py
@@ -636,3 +636,21 @@ TypeError: __init__() got an unexpected keyword argument 'taba'
     cena.vai()
 NameError: name 'cena' is not defined
 '''},
+{'date': 'Sun Jul 17 2022 12:41:02.591 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 34
+    class Kwarwp():
+  module <module> line 86
+    cena.vai()
+NameError: name 'cena' is not defined
+'''},
