a22a3e0278d3b1642f0d3684f70e86f1a6fb230f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-07-16 21:27:02 -0300

    Saved meredith/__log__.py @2022-07-17 00:27

--- meredith/__log__.py
+++ meredith/__log__.py
@@ -574,3 +574,25 @@ AttributeError: 'GUI' object has no attribute 'vaga'
     coisa = Indio(imagem, x=0, y=0, cena=cena, taba=self)
 TypeError: __init__() got an unexpected keyword argument 'taba'
 '''},
+{'date': 'Sat Jul 16 2022 21:26:20.422 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 224
+    Kwarwp(Jogo)      
+  module <module> line 86
+    self.cena = self.cria(mapa=self.mapa) if vitollino else None
+  module <module> line 115
+    self.taba = {(i, j): fabrica[imagem].objeto(
+  module <module> line 132
+    coisa = Indio(imagem, x=0, y=0, cena=cena, taba=self)
+TypeError: __init__() got an unexpected keyword argument 'taba'
+'''},
