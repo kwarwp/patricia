49f71bc2ec6539c26720595f208312d6637d83d1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-07-16 21:22:45 -0300

    Saved meredith/__log__.py @2022-07-17 00:22

--- meredith/__log__.py
+++ meredith/__log__.py
@@ -534,3 +534,21 @@ AttributeError: 'Kwarwp' object has no attribute 'executa'
     self.vaga.sai()
 AttributeError: 'GUI' object has no attribute 'vaga'
 '''},
+{'date': 'Sat Jul 16 2022 21:22:03.10 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 18
+    class Indio():
+  module <module> line 56
+    self.vaga.sai()
+AttributeError: 'GUI' object has no attribute 'vaga'
+'''},
