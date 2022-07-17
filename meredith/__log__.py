8a8035a38bcb9b30e51d11f153c6d5cdfd987256
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-07-17 17:08:25 -0300

    Saved meredith/__log__.py @2022-07-17 20:08

--- meredith/__log__.py
+++ meredith/__log__.py
@@ -767,3 +767,25 @@ TypeError: __init__() got an unexpected keyword argument 'taba'
     coisa = Indio(imagem, x=0, y=0, cena=cena, taba=self)
 TypeError: __init__() got an unexpected keyword argument 'taba'
 '''},
+{'date': 'Sun Jul 17 2022 17:07:44.575 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 222
+    Kwarwp(Jogo)    
+  module <module> line 143
+    self.cena = self.cria(mapa=self.mapa) if vitollino else None
+  module <module> line 172
+    self.taba = {(i, j): fabrica[imagem].objeto(
+  module <module> line 188
+    coisa = Indio(imagem, x=0, y=0, cena=cena, taba=self)
+TypeError: __init__() got an unexpected keyword argument 'taba'
+'''},
