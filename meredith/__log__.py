3b9824df77c18a0f3940d4aaf2017ab7ceeeb6e6
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-07-17 20:36:03 -0300

    Saved meredith/__log__.py @2022-07-17 23:36

--- meredith/__log__.py
+++ meredith/__log__.py
@@ -1062,3 +1062,21 @@ TypeError: __init__() got an unexpected keyword argument 'taba'
   ^
 IndentationError: expected an indented block
 '''},
+{'date': 'Sun Jul 17 2022 20:35:21.409 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 649
+    Kwarwp(Jogo)    
+  module <module> line 508
+    self.indios = deque(indios or [Indio])
+NameError: name 'deque' is not defined
+'''},
