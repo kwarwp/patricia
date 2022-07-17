68f91327265680710094054822a356741aadbdf3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-07-17 18:08:20 -0300

    Saved meredith/__log__.py @2022-07-17 21:08

--- meredith/__log__.py
+++ meredith/__log__.py
@@ -861,3 +861,25 @@ IndentationError: unexpected indent
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Sun Jul 17 2022 18:07:39.433 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 387
+    Kwarwp(Jogo)    
+  module <module> line 264
+    self.cena = self.cria(mapa=self.mapa) if vitollino else None
+  module <module> line 296
+    self.taba = {(i, j): fabrica[imagem].objeto(fabrica[imagem].imagem, x=i*lado, y=j*lado+lado, cena=cena)
+  module <module> line 353
+    coisa = Indio(imagem, x=0, y=0, cena=cena, taba=self)
+TypeError: __init__() got an unexpected keyword argument 'taba'
+'''},
