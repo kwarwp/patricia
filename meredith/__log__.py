ffde3e0f1747c6c348ae73b82f3a1e01d9bff633
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-07-17 21:50:22 -0300

    Saved meredith/__log__.py @2022-07-18 00:50

--- meredith/__log__.py
+++ meredith/__log__.py
@@ -1585,3 +1585,33 @@ TypeError: __init__ missing 1 positional argument: 'taba'
     self.vazio.ocupa(ocupante, pos)
 TypeError: ocupa() takes 2 positional arguments but more were given
 '''},
+{'date': 'Sun Jul 17 2022 21:49:40.453 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 752
+    Kwarwp(Jogo)    
+  module <module> line 589
+    self.cena = self.cria(mapa=self.mapa) if vitollino else None
+  module <module> line 623
+    self.taba = {(i, j): fabrica[imagem].objeto(fabrica[imagem].imagem, x=i*lado, y=j*lado+lado, cena=cena)
+  module <module> line 685
+    vaga = Vazio("", x=x, y=y, cena=cena, ocupante=coisa, taba=self)
+  module <module> line 179
+    self.acessa(ocupante)
+  module <module> line 219
+    ocupante.ocupa(self)
+  module <module> line 305
+    vaga.ocupou(self)
+  module <module> line 247
+    self.vazio.ocupa(ocupante, pos)
+TypeError: ocupa() takes 2 positional arguments but more were given
+'''},
